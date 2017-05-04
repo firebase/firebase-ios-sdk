c38fa5bf1443bc103e0b2cb8b44c49a38efa16ad
ryanwilson <ryanwils@gmail.com>
2017-05-04 14:47:17 -0400

    Shorten SwiftNameSupport header comment lines to 80 characters. (#36)
    Some repos use 80 characters as a style guide for headers, this should
    make linters a bit happier.

--- FirebaseCore/Core/FIRSwiftNameSupport.h
+++ FirebaseCore/Core/FIRSwiftNameSupport.h
@@ -17,10 +17,10 @@
 #ifndef FIREBASECORE_CORE_FIRSWIFTNAMESUPPORT_H_
 #define FIREBASECORE_CORE_FIRSWIFTNAMESUPPORT_H_
 
-// In Xcode 7.0-7.2, NS_SWIFT_NAME can only translate factory methods. In order to keep
-// compatibility we will undefine it when linking against earlier iPhone SDKs and redefine it as an
-// empty string. Xcode 7.3 shipped with the iOS 9.3 SDK, so if __IPHONE_9_3 is defined no action is
-// necessary.
+// In Xcode 7.0-7.2, NS_SWIFT_NAME can only translate factory methods. In order
+// to keep compatibility we will undefine it when linking against earlier
+// iPhone SDKs and redefine it as an empty string. Xcode 7.3 shipped with the
+// iOS 9.3 SDK, so if __IPHONE_9_3 is defined no action is necessary.
 #ifndef __IPHONE_9_3
 #ifdef NS_SWIFT_NAME
 #undef NS_SWIFT_NAME
