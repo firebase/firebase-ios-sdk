3313fd2d0704190537995a23271853509f8c80eb
Xiangtian Dai <xiangtian@google.com>
2017-05-22 23:26:28 -0700

    Fixes ApiTests and separates its credentials from Sample (#77)

--- AuthSamples/Sample/AuthCredentialsTemplate.h
+++ AuthSamples/Sample/AuthCredentialsTemplate.h
@@ -21,33 +21,12 @@ Please follow the following steps to populate the valid AuthCredentials
 and copy it to AuthCredentials.h file
 
 You will need to replace the following values:
-$KGOOGLE_CLIENT_ID
-Get the value of the CLIENT_ID key in the GoogleService-Info.plist file..
 
 $KFACEBOOK_APP_ID
 FACEBOOK_APP_ID is the developer's Facebook app's ID, to be used to test the 
 'Signing in with Facebook' feature of Firebase Auth. Follow the instructions 
 on the Facebook developer site: https://developers.facebook.com/docs/apps/register 
-to obtain such an id
-
-$KAPP_ACCESS_TOKEN
-Once you have an Facebook App Id, click on dashboard from your app you can see
-both your App ID and the App Secret. Once you have both of these generate the 
-access token using the step 13 of https://smashballoon.com/custom-facebook-feed/access-token/
-Follow the same link for comprehensive information on how to get the access token.
-
-$KGOOGLE_TEST_ACCOUNT_REFRESH_TOKEN
-GOOGLE_TEST_ACCOUNT_REFRESH_TOKEN is the Google SignIn refresh token obtained for the Google client ID, 
-saved for continuous tests.
-
-The users that are behind these tokens must have user names as declared in the code, i.e., 
-"John Test" for Google and "MichaelTest" for Facebook, or the FirebearApiTests will fail.
-This can be found in ApiTests/FirebearApiTests.m with variable names kFacebookTestAccountName and
-kGoogleTestAccountName
-
+to obtain such an id.
 */
 
-#define KAPP_ACCESS_TOKEN $KAPP_ACCESS_TOKEN
 #define KFACEBOOK_APP_ID $KFACEBOOK_APP_ID
-#define KGOOGLE_CLIENT_ID $KGOOGLE_CLIENT_ID
-#define KGOOGLE_TEST_ACCOUNT_REFRESH_TOKEN $KGOOGLE_TEST_ACCOUNT_REFRESH_TOKEN
