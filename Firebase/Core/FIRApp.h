65392ae7ab65550d8203992131d6b331045a0249
Paul Beusterien <paulbeusterien@google.com>
2017-06-07 16:10:52 -0700

    Restore UIKit import to FIRApp.h to prevent breaking change

--- Firebase/Core/FIRApp.h
+++ Firebase/Core/FIRApp.h
@@ -16,6 +16,11 @@
 
 #import <Foundation/Foundation.h>
 
+#if TARGET_OS_IOS
+// TODO: Remove UIKit import on next breaking change release
+#import <UIKit/UIKit.h>
+#endif
+
 #import "FIRCoreSwiftNameSupport.h"
 
 @class FIROptions;
