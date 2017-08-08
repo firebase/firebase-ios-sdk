399c26fc8288da8786c7c2d75fb0aece4ee2091a
Paul Beusterien <paulbeusterien@google.com>
2017-08-08 14:06:21 -0700

    Update Core to 4.1.0 and Community to 0.1.0 (#185)

--- Firebase/Core/FIROptions.m
+++ Firebase/Core/FIROptions.m
@@ -41,8 +41,8 @@ NSString *const kFIRIsSignInEnabled = @"IS_SIGNIN_ENABLED";
 // Library version ID.
 NSString *const kFIRLibraryVersionID =
     @"4"     // Major version (one or more digits)
-    @"00"    // Minor version (exactly 2 digits)
-    @"04"    // Build number (exactly 2 digits)
+    @"01"    // Minor version (exactly 2 digits)
+    @"00"    // Build number (exactly 2 digits)
     @"000";  // Fixed "000"
 // Plist file name.
 NSString *const kServiceInfoFileName = @"GoogleService-Info";
