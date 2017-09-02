firebase/firebase-ios-sdk

commit 36c20d92b7a1f4ac5cb950df61d381ee60be9670

Add FirAuthUrlPresenter (#222)
* Adds FIRAuthURLPResenter and FIRAuthUIDelegate

Firebase/Auth/FirebaseAuth.podspec
@@ -31,6 +31,7 @@ Simplify your iOS development, grow your user base, and monetize more effectivel
     'Source/**/FIRAuthAPNSTokenType.[mh]',
     'Source/**/FIRAuthAPNSToken.[mh]',
     'Source/**/FIRPhoneAuthCredential.[mh]',
+    'Source/**/FIRAuthDefaultUIDelegate.[mh]',
     'Source/**/FIRPhoneAuthProvider.[mh]'
   s.public_header_files = 'Source/Public/*.h'
   s.preserve_paths =

AuthSamples/Samples.xcodeproj/project.pbxproj
@@ -26,6 +26,7 @@
 		52C975C71EE10B1304EBBEB2 /* Pods_SwiftSample.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = BC8C39EF1F42A0C750FF5186 /* Pods_SwiftSample.framework */; };
 		569C3F4E18627674CABE02AE /* Pods_EarlGreyTests.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = AEE2E563FADF8C3382956B4F /* Pods_EarlGreyTests.framework */; };
 		67AFFB52FF0FC4668D92F2E4 /* Pods_Sample.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = D5FE06BD9AA795DFBA9EFAAD /* Pods_Sample.framework */; };
+		7E04ACA41F565EA900788114 /* FIRAuthURLPresenterTests.m in Sources */ = {isa = PBXBuildFile; fileRef = 7E04ACA31F565EA900788114 /* FIRAuthURLPresenterTests.m */; };
 		7E9969EE1F4E277900627C2B /* FIRGetProjectConfigRequestTests.m in Sources */ = {isa = PBXBuildFile; fileRef = 7E9969EC1F4E277900627C2B /* FIRGetProjectConfigRequestTests.m */; };
 		7EDFD35B1F0EA29200B29DC5 /* Localizable.strings in Resources */ = {isa = PBXBuildFile; fileRef = 7EDFD35D1F0EA29200B29DC5 /* Localizable.strings */; };
 		7EEEFEE61F4E4F75000FF966 /* FIRGetProjectConfigResponseTests.m in Sources */ = {isa = PBXBuildFile; fileRef = 7EEA8ADD1F4E4E840014A23B /* FIRGetProjectConfigResponseTests.m */; };
@@ -183,6 +184,7 @@
 		4FFAD3F37BC4D7CEF0CAD579 /* Pods_FirebaseAuthUnitTests.framework */ = {isa = PBXFileReference; explicitFileType = wrapper.framework; includeInIndex = 0; path = Pods_FirebaseAuthUnitTests.framework; sourceTree = BUILT_PRODUCTS_DIR; };
 		57150555A6B03949ECB58AD9 /* Pods-TestApp.debug.xcconfig */ = {isa = PBXFileReference; includeInIndex = 1; lastKnownFileType = text.xcconfig; name = "Pods-TestApp.debug.xcconfig"; path = "Pods/Target Support Files/Pods-TestApp/Pods-TestApp.debug.xcconfig"; sourceTree = "<group>"; };
 		6EC09307D636721EAAB89BB2 /* Pods_ApiTests.framework */ = {isa = PBXFileReference; explicitFileType = wrapper.framework; includeInIndex = 0; path = Pods_ApiTests.framework; sourceTree = BUILT_PRODUCTS_DIR; };
+		7E04ACA31F565EA900788114 /* FIRAuthURLPresenterTests.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = FIRAuthURLPresenterTests.m; path = ../../Example/Auth/Tests/FIRAuthURLPresenterTests.m; sourceTree = "<group>"; };
 		7E0BC64A1F199D86008BE4E0 /* fr-FR */ = {isa = PBXFileReference; lastKnownFileType = text.plist.strings; name = "fr-FR"; path = "fr-FR.lproj/Localizable.strings"; sourceTree = "<group>"; };
 		7E0BC64E1F19A77C008BE4E0 /* ru-RU */ = {isa = PBXFileReference; lastKnownFileType = text.plist.strings; name = "ru-RU"; path = "ru-RU.lproj/Localizable.strings"; sourceTree = "<group>"; };
 		7E9969EC1F4E277900627C2B /* FIRGetProjectConfigRequestTests.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = FIRGetProjectConfigRequestTests.m; path = ../../Example/Auth/Tests/FIRGetProjectConfigRequestTests.m; sourceTree = "<group>"; };
@@ -557,6 +559,7 @@
 				DE5371B21EA7E89D000DA57F /* Tests-Info.plist */,
 				7E9969EC1F4E277900627C2B /* FIRGetProjectConfigRequestTests.m */,
 				7EEA8ADD1F4E4E840014A23B /* FIRGetProjectConfigResponseTests.m */,
+				7E04ACA31F565EA900788114 /* FIRAuthURLPresenterTests.m */,
 			);
 			path = UnitTests;
 			sourceTree = "<group>";
@@ -1332,6 +1335,7 @@
 				DECEA56E1EBBED1200273585 /* FIRAuthAppCredentialManagerTests.m in Sources */,
 				DE5371DE1EA7E89D000DA57F /* OCMStubRecorder+FIRAuthUnitTests.m in Sources */,
 				DE5371DC1EA7E89D000DA57F /* FIRVerifyPhoneNumberRequestTests.m in Sources */,
+				7E04ACA41F565EA900788114 /* FIRAuthURLPresenterTests.m in Sources */,
 				DE5371B51EA7E89D000DA57F /* FIRAuthAppCredentialTests.m in Sources */,
 				DE5371CE1EA7E89D000DA57F /* FIRSetAccountInfoRequestTests.m in Sources */,
 				DE5371D41EA7E89D000DA57F /* FIRVerifyAssertionRequestTests.m in Sources */,

Example/Auth/Tests/FIRAuthURLPresenterTests.m
@@ -1,0 +1,183 @@
+/*
+ * Copyright 2017 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <Foundation/Foundation.h>
+#import <SafariServices/SafariServices.h>
+#import <XCTest/XCTest.h>
+
+#import "FIRAuthURLPresenter.h"
+#import "FIRAuthUIDelegate.h"
+#import <OCMock/OCMock.h>
+
+/** @var kExpectationTimeout
+    @brief The maximum time waiting for expectations to fulfill.
+ */
+static NSTimeInterval kExpectationTimeout = 1;
+
+@interface FIRAuthDefaultUIDelegate : NSObject <FIRAuthUIDelegate>
+/** @fn defaultUIDelegate
+    @brief Returns a default FIRAuthUIDelegate object.
+    @return The default FIRAuthUIDelegate object.
+ */
++ (id<FIRAuthUIDelegate>)defaultUIDelegate;
+@end
+
+@interface FIRAuthURLPresenterTests : XCTestCase
+
+@end
+
+@implementation FIRAuthURLPresenterTests
+
+/** @fn testFIRAuthURLPresenterNonNilUIDelegate
+    @brief Tests @c FIRAuthURLPresenter class showing UI with a non-nil UIDelegate.
+ */
+- (void)testFIRAuthURLPresenterNonNilUIDelegate {
+ id mockUIDelegate = OCMProtocolMock(@protocol(FIRAuthUIDelegate));
+  NSURL *presenterURL = [NSURL URLWithString:@"https://presenter.url"];
+  FIRAuthURLPresenter *presenter = [[FIRAuthURLPresenter alloc] init];
+
+  if ([SFSafariViewController class]) {
+    XCTestExpectation *callbackMatcherExpectation =
+        [self expectationWithDescription:@"callbackMatcher callback"];
+    FIRAuthURLCallbackMatcher callbackMatcher = ^BOOL(NSURL *_Nonnull callbackURL) {
+      XCTAssertEqualObjects(callbackURL, presenterURL);
+      [callbackMatcherExpectation fulfill];
+      return YES;
+    };
+
+    XCTestExpectation *completionBlockExpectation =
+        [self expectationWithDescription:@"completion callback"];
+    FIRAuthURLPresentationCompletion completionBlock = ^(NSURL *_Nullable callbackURL,
+                                                         NSError *_Nullable error) {
+      XCTAssertEqualObjects(callbackURL, presenterURL);
+      XCTAssertNil(error);
+      [completionBlockExpectation fulfill];
+    };
+
+    id presenterArg = [OCMArg isKindOfClass:[SFSafariViewController class]];
+    OCMExpect([mockUIDelegate presentViewController:presenterArg
+                                           animated:YES
+                                         completion:nil]).andDo(^(NSInvocation *invocation) {
+      __unsafe_unretained id unretainedArgument;
+      // Indices 0 and 1 indicate the hidden arguments self and _cmd.
+      // `presentViewController` is at index 2.
+      [invocation getArgument:&unretainedArgument atIndex:2];
+
+      SFSafariViewController *viewController = unretainedArgument;
+      XCTAssertEqual(viewController.delegate, presenter);
+      XCTAssertTrue([viewController isKindOfClass:[SFSafariViewController class]]);
+    });
+    [presenter presentURL:presenterURL
+               UIDelegate:mockUIDelegate
+          callbackMatcher:callbackMatcher
+               completion:completionBlock];
+    OCMVerifyAll(mockUIDelegate);
+    OCMExpect([mockUIDelegate dismissViewControllerAnimated:OCMOCK_ANY
+                                                 completion:OCMOCK_ANY]).
+          andDo(^(NSInvocation *invocation) {
+      __unsafe_unretained id unretainedArgument;
+      // Indices 0 and 1 indicate the hidden arguments self and _cmd.
+      // `completion` is at index 3.
+      [invocation getArgument:&unretainedArgument atIndex:3];
+      void (^finishBlock)() = unretainedArgument;
+      finishBlock();
+    });
+    [presenter canHandleURL:presenterURL];
+    [self waitForExpectationsWithTimeout:kExpectationTimeout handler:nil];
+    OCMVerifyAll(mockUIDelegate);
+  }
+}
+
+/** @fn testFIRAuthURLPresenterNilUIDelegate
+    @brief Tests @c FIRAuthURLPresenter class showing UI with a nil UIDelegate.
+ */
+- (void)testFIRAuthURLPresenterNilUIDelegate {
+  NSURL *presenterURL = [NSURL URLWithString:@"https://presenter.url"];
+  FIRAuthURLPresenter *presenter = [[FIRAuthURLPresenter alloc] init];
+
+  if ([SFSafariViewController class]) {
+    XCTestExpectation *callbackMatcherExpectation =
+        [self expectationWithDescription:@"callbackMatcher callback"];
+    FIRAuthURLCallbackMatcher callbackMatcher = ^BOOL(NSURL *_Nonnull callbackURL) {
+      XCTAssertEqualObjects(callbackURL, presenterURL);
+      [callbackMatcherExpectation fulfill];
+      return YES;
+    };
+    XCTestExpectation *completionBlockExpectation =
+        [self expectationWithDescription:@"completion callback"];
+    FIRAuthURLPresentationCompletion completionBlock = ^(NSURL *_Nullable callbackURL,
+                                                         NSError *_Nullable error) {
+      XCTAssertEqualObjects(callbackURL, presenterURL);
+      XCTAssertNil(error);
+      [completionBlockExpectation fulfill];
+    };
+
+    id mockUIDelegate = OCMProtocolMock(@protocol(FIRAuthUIDelegate));
+
+    // Swizzle default UIDelegate
+    Method method = class_getClassMethod([FIRAuthDefaultUIDelegate class],
+                                         @selector(defaultUIDelegate));
+    __block IMP originalImplementation;
+    IMP newImplmentation = imp_implementationWithBlock(^id(id object) {
+      return mockUIDelegate;
+    });
+    originalImplementation = method_setImplementation(method, newImplmentation);
+    if ([SFSafariViewController class]) {
+      id presenterArg = [OCMArg isKindOfClass:[SFSafariViewController class]];
+      OCMExpect([mockUIDelegate presentViewController:presenterArg
+                                       animated:[OCMArg any]
+                                     completion:[OCMArg any]]).
+          andDo(^(NSInvocation *invocation) {
+        __unsafe_unretained id unretainedArgument;
+        // Indices 0 and 1 indicate the hidden arguments self and _cmd.
+        // `presentViewController` is at index 2.
+        [invocation getArgument:&unretainedArgument atIndex:2];
+        SFSafariViewController *viewController = unretainedArgument;
+        XCTAssertEqual(viewController.delegate, presenter);
+        XCTAssertTrue([viewController isKindOfClass:[SFSafariViewController class]]);
+      });
+    }
+    [presenter presentURL:presenterURL
+               UIDelegate:nil
+          callbackMatcher:callbackMatcher
+               completion:completionBlock];
+    OCMVerifyAll(mockUIDelegate);
+
+    OCMExpect([mockUIDelegate dismissViewControllerAnimated:OCMOCK_ANY
+                                                 completion:OCMOCK_ANY]).
+        andDo(^(NSInvocation *invocation) {
+      __unsafe_unretained id unretainedArgument;
+      // Indices 0 and 1 indicate the hidden arguments self and _cmd.
+      // `completion` is at index 3.
+      [invocation getArgument:&unretainedArgument atIndex:3];
+      void (^finishBlock)() = unretainedArgument;
+      finishBlock();
+    });
+    [presenter canHandleURL:presenterURL];
+    [self waitForExpectationsWithTimeout:kExpectationTimeout handler:nil];
+    OCMVerifyAll(mockUIDelegate);
+    // Unswizzle.
+    imp_removeBlock(method_setImplementation(method, originalImplementation));
+  }
+}
+
+#pragma mark - Method Swizzling
+
++ (id)mockDefaultUIDelegate {
+  return OCMProtocolMock(@protocol(FIRAuthUIDelegate));
+}
+
+@end

Example/Firebase.xcodeproj/project.pbxproj
@@ -104,6 +104,7 @@
 		79A15731AA31012CD937CF3A /* Pods_Core_Example_iOS.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = CAD129FFEC477E1129AE6AA1 /* Pods_Core_Example_iOS.framework */; };
 		7A02646DEF386689CCFB9011 /* Pods_Core_Tests_iOS.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = AE6C9DD139E1FD21DC0F1082 /* Pods_Core_Tests_iOS.framework */; };
 		7AE9A7433F2BD9A52022AC71 /* Pods_Messaging_Tests_iOS.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 4F902A29FA956ADD762F6921 /* Pods_Messaging_Tests_iOS.framework */; };
+		7E9485421F578AC4005A3939 /* FIRAuthURLPresenterTests.m in Sources */ = {isa = PBXBuildFile; fileRef = 7E94853F1F578A9D005A3939 /* FIRAuthURLPresenterTests.m */; };
 		960665EC1C5F7A0E843A354F /* Pods_Database_Tests_macOS.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 97768125F45377F35CA86EDC /* Pods_Database_Tests_macOS.framework */; };
 		AFAF36F51EC28C25004BDEE5 /* Shared.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = AFAF36F41EC28C25004BDEE5 /* Shared.xcassets */; };
 		AFAF36F61EC28C25004BDEE5 /* Shared.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = AFAF36F41EC28C25004BDEE5 /* Shared.xcassets */; };
@@ -739,6 +740,7 @@
 		6F7376F39846E902979416D4 /* Pods_Database_Example_iOS.framework */ = {isa = PBXFileReference; explicitFileType = wrapper.framework; includeInIndex = 0; path = Pods_Database_Example_iOS.framework; sourceTree = BUILT_PRODUCTS_DIR; };
 		6FAA689FDCBD3261300292D5 /* Pods-Database_IntegrationTests_macOS.debug.xcconfig */ = {isa = PBXFileReference; includeInIndex = 1; lastKnownFileType = text.xcconfig; name = "Pods-Database_IntegrationTests_macOS.debug.xcconfig"; path = "Pods/Target Support Files/Pods-Database_IntegrationTests_macOS/Pods-Database_IntegrationTests_macOS.debug.xcconfig"; sourceTree = "<group>"; };
 		73B480AA654FC97FA72C6293 /* Pods_Storage_Example_macOS.framework */ = {isa = PBXFileReference; explicitFileType = wrapper.framework; includeInIndex = 0; path = Pods_Storage_Example_macOS.framework; sourceTree = BUILT_PRODUCTS_DIR; };
+		7E94853F1F578A9D005A3939 /* FIRAuthURLPresenterTests.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = FIRAuthURLPresenterTests.m; sourceTree = "<group>"; };
 		81E83B5ABAE219234F213B27 /* Pods_Database_Tests_iOS.framework */ = {isa = PBXFileReference; explicitFileType = wrapper.framework; includeInIndex = 0; path = Pods_Database_Tests_iOS.framework; sourceTree = BUILT_PRODUCTS_DIR; };
 		8496034D8156555C5FCF8F14 /* README.md */ = {isa = PBXFileReference; includeInIndex = 1; lastKnownFileType = net.daringfireball.markdown; name = README.md; path = ../README.md; sourceTree = "<group>"; };
 		862CC98282A6123508E8CA49 /* Pods-Database_Example_macOS.release.xcconfig */ = {isa = PBXFileReference; includeInIndex = 1; lastKnownFileType = text.xcconfig; name = "Pods-Database_Example_macOS.release.xcconfig"; path = "Pods/Target Support Files/Pods-Database_Example_macOS/Pods-Database_Example_macOS.release.xcconfig"; sourceTree = "<group>"; };
@@ -1690,6 +1692,7 @@
 		DE9314F91E86C6FF0083EDBF /* Tests */ = {
 			isa = PBXGroup;
 			children = (
+				7E94853F1F578A9D005A3939 /* FIRAuthURLPresenterTests.m */,
 				DE750DB51EB3DD4000A75E47 /* FIRAuthAPNSTokenManagerTests.m */,
 				DE750DB61EB3DD4000A75E47 /* FIRAuthAPNSTokenTests.m */,
 				DE750DB71EB3DD4000A75E47 /* FIRAuthAppCredentialManagerTests.m */,
@@ -4187,6 +4190,7 @@
 				D9B0D4201F578F7200A567C2 /* FIRGetProjectConfigResponseTests.m in Sources */,
 				DE9315711E86C71C0083EDBF /* FIRSetAccountInfoResponseTests.m in Sources */,
 				DE93155F1E86C71C0083EDBF /* FIRAuthTests.m in Sources */,
+				7E9485421F578AC4005A3939 /* FIRAuthURLPresenterTests.m in Sources */,
 				DE750DBD1EB3DD5B00A75E47 /* FIRAuthAPNSTokenTests.m in Sources */,
 				DE0E5BBE1EA7D93500FAA825 /* FIRAuthAppDelegateProxyTests.m in Sources */,
 				DE0E5BBC1EA7D92E00FAA825 /* FIRVerifyClientResponseTests.m in Sources */,

Firebase/Auth/Source/FIRAuthDefaultUIDelegate.m
@@ -1,0 +1,85 @@
+/*
+ * Copyright 2017 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <Foundation/Foundation.h>
+
+#import "FIRAuthUIDelegate.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FIRAuthDefaultUIDelegate : NSObject <FIRAuthUIDelegate>
+/** @fn defaultUIDelegate
+    @brief Unavailable. Please use initWithViewController:
+ */
+- (instancetype)init NS_UNAVAILABLE;
+
+/** @fn initWithViewController:
+    @brief Initializes the instance with a view controller.
+    @param viewController The view controller as the presenting view controller in @c GOIUIDelegate.
+    @return The initialized instance.
+ */
+- (instancetype)initWithViewController:(UIViewController *)viewController NS_DESIGNATED_INITIALIZER;
+@end
+
+@implementation FIRAuthDefaultUIDelegate {
+  /** @var _viewController
+      @brief The presenting view controller.
+   */
+  UIViewController *_viewController;
+}
+
+- (instancetype)initWithViewController:(UIViewController *)viewController {
+  self = [super init];
+  if (self) {
+    _viewController = viewController;
+  }
+  return self;
+}
+
+- (void)presentViewController:(UIViewController *)viewControllerToPresent
+                     animated:(BOOL)flag
+                   completion:(nullable void (^)(void))completion {
+  [_viewController presentViewController:viewControllerToPresent
+                                animated:flag
+                              completion:completion];
+}
+
+- (void)dismissViewControllerAnimated:(BOOL)flag completion:(nullable void (^)(void))completion {
+  [_viewController dismissViewControllerAnimated:flag completion:completion];
+}
+
++ (id<FIRAuthUIDelegate>)defaultUIDelegate {
+  UIViewController *topViewController =
+      [UIApplication sharedApplication].keyWindow.rootViewController;
+  while (true){
+    if (topViewController.presentedViewController) {
+      topViewController = topViewController.presentedViewController;
+    } else if ([topViewController isKindOfClass:[UINavigationController class]]) {
+      UINavigationController *nav = (UINavigationController *)topViewController;
+      topViewController = nav.topViewController;
+    } else if ([topViewController isKindOfClass:[UITabBarController class]]) {
+      UITabBarController *tab = (UITabBarController *)topViewController;
+      topViewController = tab.selectedViewController;
+    } else {
+      break;
+    }
+  }
+  return [[FIRAuthDefaultUIDelegate alloc] initWithViewController:topViewController];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END

Firebase/Auth/Source/FIRAuthErrorUtils.h
@@ -451,6 +451,20 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (NSError *)captchaCheckFailedErrorWithMessage:(nullable NSString *)message;
 
+/** @fn webContextAlreadyPresentedErrorWithMessage:
+    @brief Constructs an @c NSError with the @c FIRAuthErrorCodeWebContextAlreadyPresented code.
+    @param message Error message from the backend, if any.
+    @return The NSError instance associated with the given FIRAuthError.
+ */
++ (NSError *)webContextAlreadyPresentedErrorWithMessage:(nullable NSString *)message;
+
+/** @fn webContextCancelledErrorWithMessage:
+    @brief Constructs an @c NSError with the @c FIRAuthErrorCodeWebContextCancelledcode.
+    @param message Error message from the backend, if any.
+    @return The NSError instance associated with the given FIRAuthError.
+ */
++ (NSError *)webContextCancelledErrorWithMessage:(nullable NSString *)message;
+
 /** @fn keychainErrorWithFunction:status:
     @brief Constructs an @c NSError with the @c FIRAuthErrorCodeKeychainError code.
     @param keychainFunction The keychain function which was invoked and yielded an unexpected

Firebase/Auth/Source/FIRAuthErrorUtils.m
@@ -345,6 +345,18 @@ static NSString *const kFIRAuthErrorMessageAppNotVerified = @"Firebase could not
 static NSString *const kFIRAuthErrorMessageCaptchaCheckFailed = @"The reCAPTCHA response token "
     "provided is either invalid, expired or already";
 
+/** @var kFIRAuthErrorMessageWebContextAlreadyPresented
+    @brief Message for @c FIRAuthErrorCodeWebContextAlreadyPresented error code.
+ */
+static NSString *const kFIRAuthErrorMessageWebContextAlreadyPresented = @"User interaction is "
+    "still ongoing, another view cannot be presented.";
+
+/** @var kFIRAuthErrorMessageWebContextCancelled
+    @brief Message for @c FIRAuthErrorCodeWebContextCancelled error code.
+ */
+static NSString *const kFIRAuthErrorMessageWebContextCancelled = @"The interaction was cancelled "
+    "by the user.";
+
 /** @var kFIRAuthErrorMessageInternalError
     @brief Message for @c FIRAuthErrorCodeInternalError error code.
  */
@@ -455,6 +467,10 @@ static NSString *FIRAuthErrorDescription(FIRAuthErrorCode code) {
       return kFIRAuthErrorMessageAppNotVerified;
     case FIRAuthErrorCodeCaptchaCheckFailed:
       return kFIRAuthErrorMessageCaptchaCheckFailed;
+    case FIRAuthErrorCodeWebContextAlreadyPresented:
+      return kFIRAuthErrorMessageWebContextAlreadyPresented;
+    case FIRAuthErrorCodeWebContextCancelled:
+      return kFIRAuthErrorMessageWebContextCancelled;
   }
 }
 
@@ -562,6 +578,10 @@ static NSString *const FIRAuthErrorCodeString(FIRAuthErrorCode code) {
       return @"ERROR_APP_NOT_VERIFIED";
     case FIRAuthErrorCodeCaptchaCheckFailed:
       return @"ERROR_CAPTCHA_CHECK_FAILED";
+    case FIRAuthErrorCodeWebContextAlreadyPresented:
+      return @"ERROR_WEB_CONTEXT_ALREADY_PRESENTED";
+    case FIRAuthErrorCodeWebContextCancelled:
+      return @"ERROR_WEB_CONTEXT_CANCELLED";
   }
 }
 
@@ -871,6 +891,14 @@ static NSString *const FIRAuthErrorCodeString(FIRAuthErrorCode code) {
 
 + (NSError *)captchaCheckFailedErrorWithMessage:(nullable NSString *)message {
   return [self errorWithCode:FIRAuthInternalErrorCodeCaptchaCheckFailed message:message];
+}
+
++ (NSError *)webContextAlreadyPresentedErrorWithMessage:(nullable NSString *)message {
+  return [self errorWithCode:FIRAuthInternalErrorCodeWebContextAlreadyPresented message:message];
+}
+
++ (NSError *)webContextCancelledErrorWithMessage:(nullable NSString *)message {
+  return [self errorWithCode:FIRAuthInternalErrorCodeWebContextCancelled message:message];
 }
 
 + (NSError *)keychainErrorWithFunction:(NSString *)keychainFunction status:(OSStatus)status {

Firebase/Auth/Source/FIRAuthInternalErrors.h
@@ -322,6 +322,17 @@ typedef NS_ENUM(NSInteger, FIRAuthInternalErrorCode) {
   FIRAuthInternalErrorCodeQuotaExceeded =
       FIRAuthPublicErrorCodeFlag | FIRAuthErrorCodeQuotaExceeded,
 
+  /** Indicates that an attempt was made to present a new web context while one was already being
+        presented.
+   */
+  FIRAuthInternalErrorCodeWebContextAlreadyPresented =
+      FIRAuthPublicErrorCodeFlag | FIRAuthErrorCodeWebContextAlreadyPresented,
+
+  /** Indicates that the URL presentation was cancelled prematurely by the user.
+   */
+  FIRAuthInternalErrorCodeWebContextCancelled =
+      FIRAuthPublicErrorCodeFlag | FIRAuthErrorCodeWebContextCancelled,
+
   // The enum values between 17046 and 17051 are reserved and should NOT be used for new error
   // codes.
 

commit 9fbb8063c818a710bccab3f272655950155e7f14

Fixes macOS unit test failures introduced in #222 .

Firebase/Auth/FirebaseAuth.podspec
@@ -30,8 +30,9 @@ Simplify your iOS development, grow your user base, and monetize more effectivel
     'Source/**/FIRAuthAPNSTokenManager.[mh]',
     'Source/**/FIRAuthAPNSTokenType.[mh]',
     'Source/**/FIRAuthAPNSToken.[mh]',
-    'Source/**/FIRPhoneAuthCredential.[mh]',
     'Source/**/FIRAuthDefaultUIDelegate.[mh]',
+    'Source/**/FIRAuthURLPresenter.[mh],
+    'Source/**/FIRPhoneAuthCredential.[mh]',
     'Source/**/FIRPhoneAuthProvider.[mh]'
   s.public_header_files = 'Source/Public/*.h'
   s.preserve_paths =

FirebaseCommunity.podspec
@@ -42,6 +42,8 @@ Firebase Development CocoaPod including experimental and community supported fea
       'Firebase/Auth/Source/**/FIRAuthAPNSTokenManager.[mh]',
       'Firebase/Auth/Source/**/FIRAuthAPNSTokenType.[mh]',
       'Firebase/Auth/Source/**/FIRAuthAPNSToken.[mh]',
+      'Firebase/Auth/Source/**/FIRAuthDefaultUIDelegate.[mh]',
+      'Firebase/Auth/Source/**/FIRAuthURLPresenter.[mh]',
       'Firebase/Auth/Source/**/FIRPhoneAuthCredential.[mh]',
       'Firebase/Auth/Source/**/FIRPhoneAuthProvider.[mh]'
     sp.public_header_files = 'Firebase/Auth/Source/Public/*.h'