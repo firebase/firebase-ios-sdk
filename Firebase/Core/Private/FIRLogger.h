6ac69540ce38c746e73235c273e6164acf56d942
Michael Haney <mikehaney24@users.noreply.github.com>
2017-09-05 16:03:53 -0700

    Ensure that FIRLogger functions are compiled as C
    Wrap the FIRLogger functions in an extern "C" when being compiled as C++.

--- Firebase/Core/Private/FIRLogger.h
+++ Firebase/Core/Private/FIRLogger.h
@@ -88,6 +88,10 @@ extern void FIRLogBasic(FIRLoggerLevel level,
 #endif
 );
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 /**
  * The following functions accept the following parameters in order:
  * (required) service name of type FIRLoggerService.
@@ -111,5 +115,9 @@ extern void FIRLogInfo(FIRLoggerService service, NSString *messageCode, NSString
     NS_FORMAT_FUNCTION(3, 4);
 extern void FIRLogDebug(FIRLoggerService service, NSString *messageCode, NSString *message, ...)
     NS_FORMAT_FUNCTION(3, 4);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
 
 NS_ASSUME_NONNULL_END
