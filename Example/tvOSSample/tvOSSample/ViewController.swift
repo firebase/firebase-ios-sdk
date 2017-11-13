136fd042844cf6e6ffde614698016bfa52eca91a
Ryan Wilson <wilsonryan@google.com>
2017-11-13 13:25:20 -0500

    Initial tvOS project sample.

---
+++ Example/tvOSSample/tvOSSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tvOSSample
+//
+//  Created by Ryan Wilson on 11/13/17.
+//  Copyright © 2017 Firebase. All rights reserved.
+//
+
+import UIKit
+import FirebaseCommunity
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
