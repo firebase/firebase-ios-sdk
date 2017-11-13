2a22832d7e8664ca3db29b83c197f1c81b5842b4
Paul Beusterien <paulbeusterien@google.com>
2017-11-13 12:40:32 -0800

    style fixes

--- Example/Core/App/tvOS/ViewController.m
+++ Example/Core/App/tvOS/ViewController.m
@@ -25,11 +25,9 @@
   // Do any additional setup after loading the view, typically from a nib.
 }
 
-
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
 }
-
 
 @end
