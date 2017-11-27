6e8ca840005cb2c3cfe2fb2c22e7e1a3f30801e4
Ryan Wilson <wilsonryan@google.com>
2017-11-27 13:22:10 -0500

    Fixed FIRMessaging header

--- Firebase/Messaging/Public/FIRMessaging.h
+++ Firebase/Messaging/Public/FIRMessaging.h
@@ -16,17 +16,6 @@
 
 #import <Foundation/Foundation.h>
 
-
-// NS_SWIFT_NAME can only translate factory methods before the iOS 9.3 SDK.
-// Wrap it in our own macro if it's a non-compatible SDK.
-#ifndef NS_SWIFT_NAME
-#ifdef __IPHONE_9_3
-#define NS_SWIFT_NAME(X) NS_SWIFT_NAME(X)
-#else
-#define NS_SWIFT_NAME(X)  // Intentionally blank.
-#endif  // #ifdef __IPHONE_9_3
-#endif  // #ifndef NS_SWIFT_NAME
-
 /**
  *  @related FIRMessaging
  *
