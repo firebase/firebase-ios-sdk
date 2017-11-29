123ce4cdea12140a5e6aa75de5627fc1dfc7e787
Paul Beusterien <paulbeusterien@google.com>
2017-11-29 07:20:23 -0800

    Instructions for static library interop with Analytics (#503)

---
+++ patch/FirebaseAnalytics.h
@@ -0,0 +1,9 @@
+// Firebase Analytics umbrella header for interoperating with open source builds
+// More details at https://github.com/firebase/firebase-ios-sdk#source-pod-integration
+
+#import "FIRAnalytics+AppDelegate.h"
+#import "FIRAnalytics.h"
+#import "FIRAnalyticsSwiftNameSupport.h"
+#import "FIREventNames.h"
+#import "FIRParameterNames.h"
+#import "FIRUserPropertyNames.h"
