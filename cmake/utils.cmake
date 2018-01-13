e9b77588b3be166f4eb6aadcec5ae841e3ca672f
Marek Gilbert <mcg@google.com>
2018-01-13 13:45:07 -0800

    Rewrite cc_test to take named arguments
    Cut down on build file verbosity by having cc_test take SOURCES and
    DEPENDS. The separate invocation of target_link_libraries is no longer
    necessary.

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -12,16 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Defines a new test executable and does all the things we want done with
-# tests:
+include(CMakeParseArguments)
+
+# cc_test(
+#   target
+#   SOURCES sources...
+#   DEPENDS libraries...
+# )
 #
-#   * add_executable (with the given arguments)
-#   * add_Test - defines a test with the same name
-#   * declares that the test links against gtest
-#   * adds the executable as a dependency of the `check` target.
+# Defines a new test executable target with the given target name, sources, and
+# dependencies.  Implicitly adds DEPENDS on GTest::GTest and GTest::Main.
 function(cc_test name)
-  add_executable(${name} ${ARGN})
+  set(multi DEPENDS SOURCES)
+  cmake_parse_arguments(cct "" "" "${multi}" ${ARGN})
+
+  list(APPEND cct_DEPENDS GTest::GTest GTest::Main)
+
+  add_executable(${name} ${cct_SOURCES})
   add_test(${name} ${name})
 
-  target_link_libraries(${name} GTest::GTest GTest::Main)
+  target_link_libraries(${name} ${cct_DEPENDS})
 endfunction()
