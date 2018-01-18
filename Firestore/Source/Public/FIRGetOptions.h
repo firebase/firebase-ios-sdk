3abd4499305669ab3f8bc4b8634ee4b81b2b481b
Rich Gowman <rgowman@google.com>
2018-01-17 20:12:50 -0500

    Fix doc comments.

--- Firestore/Source/Public/FIRGetOptions.h
+++ Firestore/Source/Public/FIRGetOptions.h
@@ -31,7 +31,7 @@ NS_SWIFT_NAME(GetOptions)
 /**
  * Returns the default options.
  *
- * Equiavlent to `[[FIRGetOptions alloc] initWithSource:FIRDefault]` in
+ * Equiavlent to `[[FIRGetOptions alloc] initWithSource:FIRSourceDefault]` in
  * objective-c.
  */
 + (FIRGetOptions *)defaultOptions NS_SWIFT_NAME(defaultOptions());
@@ -39,19 +39,19 @@ NS_SWIFT_NAME(GetOptions)
 /**
  * Describes whether we should get from server or cache.
  *
- * Setting the GetOption source to FIRDefault, if online, causes Firestore to
- * try to give a consistent (server-retrieved) snapshot, or else revert to the
- * cache to provide a value.
+ * Setting the FIRGetOption source to FIRSourceDefault, if online, causes
+ * Firestore to try to give a consistent (server-retrieved) snapshot, or else
+ * revert to the cache to provide a value.
  *
- * FIRServer causes Firestore to avoid the cache (generating an error if a
- * value cannot be retrieved from the server). The cache will be updated if the
- * RPC succeeds. Latency compensation still occurs (implying that if the cache
- * is more up to date, then it's values will be merged into the results).
+ * FIRSourceServer causes Firestore to avoid the cache (generating an error if
+ * a value cannot be retrieved from the server). The cache will be updated if
+ * the RPC succeeds. Latency compensation still occurs (implying that if the
+ * cache is more up to date, then it's values will be merged into the results).
  *
- * FIRCache causes Firestore to immediately return a value from the cache,
- * ignoring the server completely (implying that the returned value may be
- * stale with respect to the value on the server.) For a single document, the
- * get will fail if the document doesn't exist.
+ * FIRSourceCache causes Firestore to immediately return a value from the
+ * cache, ignoring the server completely (implying that the returned value may
+ * be stale with respect to the value on the server.) For a single document,
+ * the get will fail if the document doesn't exist.
  */
 typedef NS_ENUM(NSUInteger, FIRSource) {
   FIRSourceDefault,
