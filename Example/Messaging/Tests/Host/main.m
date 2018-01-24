1d65490f3fed5eefd101456f75991bf8d59cd00e
chliangGoogle <chliang@google.com>
2018-01-24 14:50:48 -0800

    add unit test for auto init function

---
+++ Example/Messaging/Tests/Host/main.m
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+#import "third_party/firebase/ios/Source/FirebaseMessaging/Tests/Host/AppDelegate.h"
+
+int main(int argc, char* argv[]) {
+  @autoreleasepool {
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
