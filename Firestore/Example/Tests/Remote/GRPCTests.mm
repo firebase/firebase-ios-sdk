b840f062aeec87650ccd917bf7accfaf2d4c61db
Marek Gilbert <mcg@google.com>
2018-01-23 23:31:15 -0800

    Add a simple, bare bones test
    This does nothing but verify that gRPC-C++ even compiles

---
+++ Firestore/Example/Tests/Remote/GRPCTests.mm
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <XCTest/XCTest.h>
+
+#include <grpc/grpc.h>
+#include <grpc++/channel.h>
+#include <grpc++/client_context.h>
+#include <grpc++/create_channel.h>
+#include <grpc++/security/credentials.h>
+
+@interface GRPCTests : XCTestCase
+@end
+
+@implementation GRPCTests
+
+- (void)testGRPC {
+  grpc::CreateChannel("localhost:50051",
+                      grpc::InsecureChannelCredentials());
+}
+
+@end
