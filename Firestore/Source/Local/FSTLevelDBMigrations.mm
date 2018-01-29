93a84b1ff9d75e80efd6c091b9a92d5e63ed0a22
Greg Soltis <gsoltis@google.com>
2018-01-29 13:49:38 -0800

    newlines, copyrights, assumptions

--- Firestore/Source/Local/FSTLevelDBMigrations.mm
+++ Firestore/Source/Local/FSTLevelDBMigrations.mm
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "Firestore/Source/Local/FSTLevelDBMigrations.h"
 
 #include <leveldb/db.h>
@@ -7,6 +23,8 @@
 #import "Firestore/Source/Local/FSTLevelDBKey.h"
 #import "Firestore/Source/Local/FSTLevelDBUtil.h"
 #import "Firestore/Source/Util/FSTAssert.h"
+
+NS_ASSUME_NONNULL_BEGIN
 
 using leveldb::DB;
 using leveldb::Status;
@@ -62,3 +80,5 @@ static void SaveVersion(DB *db, FSTLevelDBSchemaVersion version) {
 }
 
 @end
+
+NS_ASSUME_NONNULL_END
