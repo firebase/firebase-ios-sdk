b0a39db37c69b2236230179ba8706663eace0215
Ryan Wilson <wilsonryan@google.com>
2018-03-21 11:32:24 -0400

    Fix grammatical error.

--- Firebase/Core/Public/FIROptions.h
+++ Firebase/Core/Public/FIROptions.h
@@ -25,7 +25,7 @@ NS_SWIFT_NAME(FirebaseOptions)
 @interface FIROptions : NSObject <NSCopying>
 
 /**
- * Returns the default options. The first time this is called it contains synchronously reads
+ * Returns the default options. The first time this is called it synchronously reads
  * GoogleService-Info.plist from disk.
  */
 + (nullable FIROptions *)defaultOptions NS_SWIFT_NAME(defaultOptions());
