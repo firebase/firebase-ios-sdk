df92f1a4011eb35e1388f6d4c35e63879e47261c
rsgowman <rgowman@google.com>
2018-03-23 11:20:40 -0400

    Renames in serializer to reflect recent changes (#957)
    * Rename Writer::Encode* methods to Writer::Write*
    
    * Rename: s/stream/writer/ (approximately)
    
    but only where it applies to Writer (rather than pb_ostream_t).

--- Firestore/core/test/firebase/firestore/remote/serializer_test.cc
+++ Firestore/core/test/firebase/firestore/remote/serializer_test.cc
@@ -75,7 +75,7 @@ class SerializerTest : public ::testing::Test {
   }
 };
 
-TEST_F(SerializerTest, EncodesNullModelToBytes) {
+TEST_F(SerializerTest, WritesNullModelToBytes) {
   FieldValue model = FieldValue::NullValue();
 
   // TEXT_FORMAT_PROTO: 'null_value: NULL_VALUE'
@@ -83,7 +83,7 @@ TEST_F(SerializerTest, EncodesNullModelToBytes) {
   ExpectRoundTrip(model, bytes, FieldValue::Type::Null);
 }
 
-TEST_F(SerializerTest, EncodesBoolModelToBytes) {
+TEST_F(SerializerTest, WritesBoolModelToBytes) {
   struct TestCase {
     bool value;
     std::vector<uint8_t> bytes;
@@ -100,7 +100,7 @@ TEST_F(SerializerTest, EncodesBoolModelToBytes) {
   }
 }
 
-TEST_F(SerializerTest, EncodesIntegersModelToBytes) {
+TEST_F(SerializerTest, WritesIntegersModelToBytes) {
   // NB: because we're calculating the bytes via protoc (instead of
   // libprotobuf) we have to look at values from numeric_limits<T> ahead of
   // time. So these test cases make the following assumptions about
@@ -153,7 +153,7 @@ TEST_F(SerializerTest, EncodesIntegersModelToBytes) {
   }
 }
 
-TEST_F(SerializerTest, EncodesStringModelToBytes) {
+TEST_F(SerializerTest, WritesStringModelToBytes) {
   struct TestCase {
     std::string value;
     std::vector<uint8_t> bytes;
@@ -193,14 +193,14 @@ TEST_F(SerializerTest, EncodesStringModelToBytes) {
   }
 }
 
-TEST_F(SerializerTest, EncodesEmptyMapToBytes) {
+TEST_F(SerializerTest, WritesEmptyMapToBytes) {
   FieldValue model = FieldValue::ObjectValue({});
   // TEXT_FORMAT_PROTO: 'map_value: {}'
   std::vector<uint8_t> bytes{0x32, 0x00};
   ExpectRoundTrip(model, bytes, FieldValue::Type::Object);
 }
 
-TEST_F(SerializerTest, EncodesNestedObjectsToBytes) {
+TEST_F(SerializerTest, WritesNestedObjectsToBytes) {
   // As above, verify max int64_t value.
   EXPECT_EQ(9223372036854775807, std::numeric_limits<int64_t>::max());
   // TODO(rsgowman): link libprotobuf to the test suite and eliminate the
