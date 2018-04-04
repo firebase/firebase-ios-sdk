465e9a1f0ea9dc552a5bf4afbb164a433c6b5e62
Ryan Wilson <wilsonryan@google.com>
2018-04-04 10:56:23 -0400

    Update component versions for 4.12.0 (#1024)

--- Firebase/Core/FIROptions.m
+++ Firebase/Core/FIROptions.m
@@ -43,7 +43,7 @@ NSString *const kFIRIsSignInEnabled = @"IS_SIGNIN_ENABLED";
 NSString *const kFIRLibraryVersionID =
     @"4"     // Major version (one or more digits)
     @"00"    // Minor version (exactly 2 digits)
-    @"18"    // Build number (exactly 2 digits)
+    @"19"    // Build number (exactly 2 digits)
     @"000";  // Fixed "000"
 // Plist file name.
 NSString *const kServiceInfoFileName = @"GoogleService-Info";
--- FirebaseCore.podspec
+++ FirebaseCore.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'FirebaseCore'
-  s.version          = '4.0.18'
+  s.version          = '4.0.19'
   s.summary          = 'Firebase Core for iOS'
 
   s.description      = <<-DESC
