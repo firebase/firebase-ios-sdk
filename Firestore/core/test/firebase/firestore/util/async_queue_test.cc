9f746eb0e02df9695561d450837c8021bd01fbe3
Konstantin Varlamov <varconst@google.com>
2018-04-06 18:11:54 -0400

    dealing with hanging

--- Firestore/core/test/firebase/firestore/util/async_queue_test.cc
+++ Firestore/core/test/firebase/firestore/util/async_queue_test.cc
@@ -16,6 +16,7 @@
 
 #include "Firestore/core/src/firebase/firestore/util/async_queue.h"
 
+#include <stdlib.h>
 #include <chrono>
 #include <future>
 #include <string>
@@ -29,11 +30,16 @@ namespace util {
 
 namespace {
 
+namespace chr = std::chrono;
+
+chr::time_point<std::chrono::system_clock, chr::milliseconds> now() {
+  return chr::time_point_cast<chr::milliseconds>(chr::system_clock::now());
+}
+
 const auto kTimeout = std::chrono::seconds(5);
 
-class AsyncQueueTest : public ::testing::Test {
- public:
-  AsyncQueueTest() : signal_finished{[] {}} {
+struct TimeoutMixin {
+  TimeoutMixin() : signal_finished{[] {}} {
   }
 
   // Googletest doesn't contain built-in functionality to block until an async
@@ -45,111 +51,154 @@ class AsyncQueueTest : public ::testing::Test {
            std::future_status::ready;
   }
 
-  AsyncQueue queue;
   std::packaged_task<void()> signal_finished;
+};
+
+class ScheduleTest : public TimeoutMixin, public ::testing::Test {
+ public:
+  ScheduleTest() : start_time{now()} {
+  }
+
+  using ScheduleT = Schedule<int>;
+
+  int TryPop() {
+    int read = -1;
+    schedule.PopIfDue(&read);
+    return read;
+  }
+
+  ScheduleT schedule;
+  ScheduleT::TimePoint start_time;
+  int out = 0;
 };
 
 }  // namespace
 
 // Schedule tests
 
-TEST(ScheduleTest, Foo) {
-  namespace chr = std::chrono;
-  const auto now = [] {
-    return chr::time_point_cast<Schedule<int>::Duration>(
-        chr::system_clock::now());
-  };
-
-  Schedule<int> schedule;
+TEST_F(ScheduleTest, PopIfDue_Immediate) {
   EXPECT_FALSE(schedule.PopIfDue(nullptr));
-  schedule.Push(3, now());
-  schedule.Push(1, now());
-  schedule.Push(2, now());
+
+  schedule.Push(3, start_time);
+  schedule.Push(1, start_time);
+  schedule.Push(2, start_time);
   EXPECT_FALSE(schedule.empty());
 
-  int out = 0;
-  const auto pop = [&] {
-    const bool result = schedule.PopIfDue(&out);
-    EXPECT_TRUE(result);
-    return out;
-  };
-  EXPECT_EQ(pop(), 3);
-  EXPECT_EQ(pop(), 1);
-  EXPECT_EQ(pop(), 2);
+  EXPECT_EQ(TryPop(), 3);
+  EXPECT_EQ(TryPop(), 1);
+  EXPECT_EQ(TryPop(), 2);
   EXPECT_FALSE(schedule.PopIfDue(nullptr));
   EXPECT_TRUE(schedule.empty());
+}
 
-  out = 0;
-  auto time_point = now();
-  schedule.Push(1, time_point + chr::milliseconds(5));
-  schedule.Push(2, time_point + chr::milliseconds(3));
-  schedule.Push(3, time_point + chr::milliseconds(1));
+TEST_F(ScheduleTest, PopIfDue_Delayed) {
+  schedule.Push(1, start_time + chr::milliseconds(5));
+  schedule.Push(2, start_time + chr::milliseconds(3));
+  schedule.Push(3, start_time + chr::milliseconds(1));
+
   EXPECT_FALSE(schedule.PopIfDue(&out));
   std::this_thread::sleep_for(chr::milliseconds(5));
-  EXPECT_EQ(pop(), 3);
-  EXPECT_EQ(pop(), 2);
-  EXPECT_EQ(pop(), 1);
-  EXPECT_TRUE(schedule.empty());
 
-  out = 0;
-  time_point = now();
-  schedule.Push(1, time_point + chr::milliseconds(3));
+  EXPECT_EQ(TryPop(), 3);
+  EXPECT_EQ(TryPop(), 2);
+  EXPECT_EQ(TryPop(), 1);
+  EXPECT_TRUE(schedule.empty());
+}
+
+TEST_F(ScheduleTest, PopBlocking) {
+  schedule.Push(1, start_time + chr::milliseconds(3));
   EXPECT_FALSE(schedule.PopIfDue(&out));
+
   schedule.PopBlocking(&out);
   EXPECT_EQ(out, 1);
-  EXPECT_GE(now(), time_point + chr::milliseconds(3));
+  EXPECT_GE(now(), start_time + chr::milliseconds(3));
   EXPECT_TRUE(schedule.empty());
+}
 
-  out = 0;
-  schedule.Push(1, now());
+TEST_F(ScheduleTest, RemoveIf) {
+  schedule.Push(1, start_time);
   schedule.Push(2, now() + chr::minutes(1));
   EXPECT_TRUE(schedule.RemoveIf(&out, [](const int v) { return v == 1; }));
   EXPECT_EQ(out, 1);
   EXPECT_TRUE(schedule.RemoveIf(&out, [](const int v) { return v == 2; }));
   EXPECT_EQ(out, 2);
   EXPECT_TRUE(schedule.empty());
+}
+
+TEST_F(ScheduleTest, Ordering) {
+  schedule.Push(11, start_time + chr::milliseconds(5));
+  schedule.Push(1, start_time);
+  schedule.Push(2, start_time);
+  schedule.Push(9, start_time + chr::milliseconds(2));
+  schedule.Push(3, start_time);
+  schedule.Push(10, start_time + chr::milliseconds(3));
+  schedule.Push(12, start_time + chr::milliseconds(5));
+  schedule.Push(4, start_time);
+  schedule.Push(5, start_time);
+  schedule.Push(6, start_time);
+  schedule.Push(8, start_time + chr::milliseconds(1));
+  schedule.Push(7, start_time);
 
   std::vector<int> values;
   const auto append = [&] {
     values.push_back(0);
     schedule.PopBlocking(&values.back());
   };
-  time_point = now();
-  schedule.Push(11, time_point + chr::milliseconds(5));
-  schedule.Push(1, time_point);
-  schedule.Push(2, time_point);
-  schedule.Push(9, time_point + chr::milliseconds(2));
-  schedule.Push(3, time_point);
-  schedule.Push(10, time_point + chr::milliseconds(3));
-  schedule.Push(12, time_point + chr::milliseconds(5));
-  schedule.Push(4, time_point);
-  schedule.Push(5, time_point);
-  schedule.Push(6, time_point);
-  schedule.Push(8, time_point + chr::milliseconds(1));
-  schedule.Push(7, time_point);
   while (!schedule.empty()) {
     append();
   }
   const std::vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
   EXPECT_EQ(values, expected);
+}
 
-  out = 0;
-  std::async(std::launch::async, [&] {
-    schedule.Push(1, now());
+TEST_F(ScheduleTest, AddingEntryUnblocksEmptyQueue) {
+  const auto future = std::async(std::launch::async, [&] {
+    ASSERT_FALSE(schedule.PopIfDue(&out));
+    schedule.PopBlocking(&out);
+    EXPECT_EQ(out, 1);
+    signal_finished();
   });
-  schedule.PopBlocking(&out);
-  EXPECT_EQ(out, 1);
 
-  out = 0;
+  std::this_thread::sleep_for(chr::milliseconds(5));
+  schedule.Push(1, start_time);
+  // Unfortunately, the future returned from std::async blocks in its destructor
+  // until the async call is finished. If PopBlocking is buggy and hangs
+  // forever, the future's destructor will also hang forever. To avoid all tests
+  // freezing, the only thing to do is to abort, which skips destructors.
+  if (!WaitForTestToFinish()) {
+    ADD_FAILURE();
+    std::abort();
+  }
+}
+
+TEST_F(ScheduleTest, PopBlockingNoticesNewEntriesThatAreSooner) {
   schedule.Push(5, now() + chr::seconds(10));
-  std::async(std::launch::async, [&] {
-    schedule.Push(3, now());
+
+  const auto future = std::async(std::launch::async, [&] {
+      std::this_thread::sleep_for(chr::milliseconds(1));
+      schedule.Push(3, start_time);
+      if (!WaitForTestToFinish()) {
+        ADD_FAILURE();
+        std::abort();
+      }
   });
+
+  ASSERT_FALSE(schedule.PopIfDue(&out));
   schedule.PopBlocking(&out);
   EXPECT_EQ(out, 3);
+  signal_finished();
 }
 
 // AsyncQueue tests
+
+namespace {
+
+class AsyncQueueTest : public TimeoutMixin, public ::testing::Test {
+ public:
+  AsyncQueue queue;
+};
+
+}  // namespace
 
 TEST_F(AsyncQueueTest, Enqueue) {
   queue.Enqueue([&] { signal_finished(); });
@@ -204,6 +253,6 @@ TEST_F(AsyncQueueTest, DelayedOperationIsValidAfterTheOperationHasRun) {
   EXPECT_NO_THROW(delayed_operation.Cancel());
 }
 
-}  // namespace util
 }  // namespace firestore
+}  // namespace firebase
 }  // namespace firebase
