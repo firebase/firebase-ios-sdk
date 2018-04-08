d3196e4094882a39e74949339fcec5bab10d05a2
Konstantin Varlamov <varconst@google.com>
2018-04-08 17:22:55 -0400

    this certainly works

--- Firestore/core/test/firebase/firestore/util/autoid_test.cc
+++ Firestore/core/test/firebase/firestore/util/autoid_test.cc
@@ -22,9 +22,40 @@
 
 using firebase::firestore::util::CreateAutoId;
 
+struct Foo {
+  int uninit;
+};
+
+int foo(int i) {
+  char *x = (char*)malloc(10 * sizeof(char*));
+  free(x);
+
+  int* a = new int[10];
+    a[5] = 0;
+    if (a[i])
+      printf("xx\n");
+
+  return x[5];
+}
+
 TEST(AutoId, IsSane) {
+  const auto bad = []{
+    std::string pending = "obc";
+    pending += "d";
+    auto* ptr = &pending;
+    return ptr;
+  }();
+  EXPECT_TRUE(!bad->empty()) << "obc";
+  EXPECT_FALSE(!bad->empty()) << "obcd";
+  EXPECT_EQ(foo(0), 42);
+
+
   for (int i = 0; i < 50; i++) {
     std::string auto_id = CreateAutoId();
+
+    Foo foo;
+    EXPECT_EQ(foo.uninit, 42) << "obc";
+
     EXPECT_EQ(20u, auto_id.length());
     for (size_t pos = 0; pos < 20; pos++) {
       char c = auto_id[pos];
