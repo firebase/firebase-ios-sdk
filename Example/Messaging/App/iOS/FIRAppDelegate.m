42bc113754d2f9e2f0f6cf552916ae76149e717d
Paul Beusterien <paulbeusterien@google.com>
2018-04-09 09:11:21 -0700

    Travis static library testing (#1047)
    * Add Objective C example app for Messaging
    * Travis static library testing
    * static lib build fixes

--- Example/Messaging/App/iOS/FIRAppDelegate.m
+++ Example/Messaging/App/iOS/FIRAppDelegate.m
@@ -14,27 +14,30 @@
 
 #import "FIRAppDelegate.h"
 
+#import <FirebaseCore/FIRApp.h>
+
 @implementation FIRAppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  // Override point for customization after application launch.
+  [FIRApp configure];
   return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
   // Sent when the application is about to move from active to inactive state. This can occur for
   // certain types of temporary interruptions (such as an incoming phone call or SMS message) or
-  // when the user quits the application and it begins the transition to the background state. Use
-  // this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates.
-  // Games should use this method to pause the game.
+  // when the user quits the application and it begins the transition to the background state.
+  // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame
+  // rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
   // Use this method to release shared resources, save user data, invalidate timers, and store
   // enough application state information to restore your application to its current state in case
-  // it is terminated later. If your application supports background execution, this method is
-  // called instead of applicationWillTerminate: when the user quits.
+  // it is terminated later.
+  // If your application supports background execution, this method is called instead of
+  // applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
