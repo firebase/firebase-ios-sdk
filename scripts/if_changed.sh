20fb7d8f8bd36b5f59e9d5ba36a7d0a18c6f7645
Konstantin Varlamov <varconst@google.com>
2018-04-16 18:49:59 -0400

    Travis: trigger rebuild if Travis config or builds scripts have changed.

--- scripts/if_changed.sh
+++ scripts/if_changed.sh
@@ -66,6 +66,10 @@ else
   esac
 fi
 
+# Always rebuild if Travis configuration and/or build scripts changed.
+check_changes '^.travis.yml'
+check_changes '^scripts/(build|if_changed).sh'
+
 if [[ "$run" == true ]]; then
   "$@"
 else
