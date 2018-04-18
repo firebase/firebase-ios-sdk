e82190b7ca7e9ccb92a50b734742aa6f0bd89b2e
Paul Beusterien <paulbeusterien@google.com>
2018-04-17 21:28:45 -0700

    add copyrights

--- scripts/push-pods.sh
+++ scripts/push-pods.sh
@@ -1,4 +1,21 @@
-# script to push all the io2018 pods
+#!/usr/bin/env bash
+
+# Copyright 2018 Google
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Push all the io2018 pods
+
 # When bootstrapping a repo, FirebaseCore must be pushed first, then
 # FirebaseInstanceID, then FirebaseAnalytics, then the rest
 # Most of the warnings are tvOS specific. The Firestore one needs
