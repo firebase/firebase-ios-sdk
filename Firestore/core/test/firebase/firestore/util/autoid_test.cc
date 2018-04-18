a9fe85def32286357b3da8f4512dde91a9617bf6
Konstantin Varlamov <varconst@google.com>
2018-04-17 23:13:15 -0400

    Revert the bad test

--- Firestore/core/test/firebase/firestore/util/autoid_test.cc
+++ Firestore/core/test/firebase/firestore/util/autoid_test.cc
@@ -16,63 +16,15 @@
 
 #include "Firestore/core/src/firebase/firestore/util/autoid.h"
 
-#include <pthread.h>
 #include <cctype>
+
 #include "gtest/gtest.h"
-
-int Global;
-
-void *Thread1(void *x) {
-  Global++;
-  (void)x;
-  return NULL;
-}
-
-void *Thread2(void *x) {
-  Global--;
-  (void)x;
-  return NULL;
-}
-
-void bad_thread() {
-  pthread_t t[2];
-  pthread_create(&t[0], NULL, Thread1, NULL);
-  pthread_create(&t[1], NULL, Thread2, NULL);
-  pthread_join(t[0], NULL);
-  pthread_join(t[1], NULL);
-}
 
 using firebase::firestore::util::CreateAutoId;
 
-struct Foo {
-  int uninit;
-};
-
-int foo(int i) {
-  char *x = reinterpret_cast<char *>(malloc(10 * sizeof(char *)));
-  free(x);
-
-  int *a = new int[10];
-  a[5] = 0;
-  if (a[i]) printf("xx\n");
-
-  return x[5];
-}
-
-enum class Bar { X, Y };
-
 TEST(AutoId, IsSane) {
   for (int i = 0; i < 50; i++) {
-    int k = std::numeric_limits<int>::max();
-    k += i;
     std::string auto_id = CreateAutoId();
-
-    Foo foo;
-    if (foo.uninit == k) {
-      volatile Foo bar;
-      (void)bar;
-    }
-
     EXPECT_EQ(20u, auto_id.length());
     for (size_t pos = 0; pos < 20; pos++) {
       char c = auto_id[pos];
@@ -81,15 +33,4 @@ TEST(AutoId, IsSane) {
           << auto_id << "\"";
     }
   }
-  volatile auto bad = [] {
-    std::string pending = "obc";
-    pending += "d";
-    auto *ptr = &pending;
-    return ptr;
-  }();
-  (void)bad;
-  volatile auto f = foo(0);
-  (void)f;
-
-  bad_thread();
 }
