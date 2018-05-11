36d524c0438f0e39179f16080984c87f78b87555
Paul Beusterien <paulbeusterien@google.com>
2018-05-11 07:08:52 -0700

    comment review

--- scripts/if_cron.sh
+++ scripts/if_cron.sh
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Within Travis, runs the given command if the current project has changes
-# worth building.
+# Within Travis, check if running in a cron job.
 #
 # Examines the following Travis-supplied environment variables:
 #   - TRAVIS_EVENT_TYPE - to check if this is a cron job
