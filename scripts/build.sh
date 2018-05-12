20cdd7e79ec8a41d141f377b9bc020ad9ee0c691
Paul Beusterien <paulbeusterien@google.com>
2018-05-12 14:23:10 -0700

    Remove workaround and iPhone 8

--- scripts/build.sh
+++ scripts/build.sh
@@ -84,7 +84,7 @@ case "$platform" in
   iOS)
     xcb_flags=(
       -sdk 'iphonesimulator'
-      -destination 'platform=iOS Simulator,name=iPhone 7'
+      -destination 'platform=iOS Simulator,name=iPhone 8 Plus'
     )
     ;;
 
@@ -180,10 +180,6 @@ case "$product-$method-$platform" in
       cd Example
       sed -i -e 's/use_frameworks/\#use_frameworks/' Podfile
       pod update --no-repo-update
-      # Workarounds for https://github.com/CocoaPods/CocoaPods/issues/7592.
-      # Remove when updating to CocoaPods 1.5.1
-      sed -i -e 's/-l"FirebaseMessaging"//' "Pods/Target Support Files/Pods-Messaging_Tests_iOS/Pods-Messaging_Tests_iOS.debug.xcconfig"
-      sed -i -e 's/-l"FirebaseAuth-iOS" -l"FirebaseCore-iOS"//' "Pods/Target Support Files/Pods-Auth_Tests_iOS/Pods-Auth_Tests_iOS.debug.xcconfig"
       cd ..
       RunXcodebuild \
           -workspace 'Example/Firebase.xcworkspace' \
