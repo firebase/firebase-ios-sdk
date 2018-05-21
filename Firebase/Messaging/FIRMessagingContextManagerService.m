fe3b3183635630d7ab1abea6bb1695cb86fdcf9e
chliangGoogle <chliang@google.com>
2018-05-21 10:44:00 -0700

    fix

--- Firebase/Messaging/FIRMessagingContextManagerService.m
+++ Firebase/Messaging/FIRMessagingContextManagerService.m
@@ -132,7 +132,7 @@ typedef NS_ENUM(NSUInteger, FIRMessagingContextManagerMessageType) {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
   UILocalNotification *notification = [[UILocalNotification alloc] init];
-#pragma pop
+#pragma clang diagnostic pop
 
   // A great way to understand timezones and UILocalNotifications
   // http://stackoverflow.com/questions/18424569/understanding-uilocalnotification-timezone
