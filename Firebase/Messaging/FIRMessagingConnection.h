ab9c6f44a43a705df55db9cdd1aa2eef357e650e
Lingyi Zhang <lingyiz@google.com>
2018-06-25 14:41:54 -0700

    Remove heartbeat timestamp

--- Firebase/Messaging/FIRMessagingConnection.h
+++ Firebase/Messaging/FIRMessagingConnection.h
@@ -67,7 +67,6 @@ typedef NS_ENUM(NSUInteger, FIRMessagingConnectionCloseReason) {
  */
 @interface FIRMessagingConnection : NSObject
 
-@property(nonatomic, readwrite, assign) int64_t lastHeartbeatPingTimestamp;
 @property(nonatomic, readonly, assign) FIRMessagingConnectionState state;
 @property(nonatomic, readonly, copy) NSString *host;
 @property(nonatomic, readonly, assign) NSUInteger port;
