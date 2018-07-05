3ffb593c88337bebef51a5e49a53b6e193e71946
Marek Gilbert <mcg@google.com>
2018-07-05 10:30:23 -0700

    Add early exits to external project builds

--- cmake/external/grpc.cmake
+++ cmake/external/grpc.cmake
@@ -19,112 +19,110 @@ if(GRPC_ROOT)
   # If the user has supplied a GRPC_ROOT then just use it. Add an empty custom
   # target so that the superbuild dependencies still work.
   add_custom_target(grpc)
+  return()
+endif()
 
-else()
-  set(
-    GIT_SUBMODULES
-    third_party/boringssl
-  )
+set(
+  GIT_SUBMODULES
+  third_party/boringssl
+)
 
-  set(
-    CMAKE_ARGS
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-    -DBUILD_SHARED_LIBS:BOOL=OFF
-    -DgRPC_BUILD_TESTS:BOOL=OFF
+set(
+  CMAKE_ARGS
+  -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+  -DBUILD_SHARED_LIBS:BOOL=OFF
+  -DgRPC_BUILD_TESTS:BOOL=OFF
 
-    # TODO(rsgowman): We're currently building nanopb twice; once via grpc, and
-    # once via nanopb. The version from grpc is the one that actually ends up
-    # being used. We need to fix this such that either:
-    #   a) we instruct grpc to use our nanopb
-    #   b) we rely on grpc's nanopb instead of using our own.
-    # For now, we'll pass in the necessary nanopb cflags into grpc. (We require
-    # 16 bit fields. Without explicitly requesting this, nanopb uses 8 bit
-    # fields.)
-    -DCMAKE_C_FLAGS=-DPB_FIELD_16BIT
-    -DCMAKE_CXX_FLAGS=-DPB_FIELD_16BIT
-  )
+  # TODO(rsgowman): We're currently building nanopb twice; once via grpc, and
+  # once via nanopb. The version from grpc is the one that actually ends up
+  # being used. We need to fix this such that either:
+  #   a) we instruct grpc to use our nanopb
+  #   b) we rely on grpc's nanopb instead of using our own.
+  # For now, we'll pass in the necessary nanopb cflags into grpc. (We require
+  # 16 bit fields. Without explicitly requesting this, nanopb uses 8 bit
+  # fields.)
+  -DCMAKE_C_FLAGS=-DPB_FIELD_16BIT
+  -DCMAKE_CXX_FLAGS=-DPB_FIELD_16BIT
+)
 
 
-  ## c-ares
-  if(NOT c-ares_DIR)
-    set(c-ares_DIR ${FIREBASE_INSTALL_DIR}/lib/cmake/c-ares)
+## c-ares
+if(NOT c-ares_DIR)
+  set(c-ares_DIR ${FIREBASE_INSTALL_DIR}/lib/cmake/c-ares)
+endif()
+
+list(
+  APPEND CMAKE_ARGS
+  -DgRPC_CARES_PROVIDER:STRING=package
+  -Dc-ares_DIR:PATH=${c-ares_DIR}
+)
+
+
+## protobuf
+
+# Unlike other dependencies of gRPC, we control the protobuf version because we
+# have checked-in protoc outputs that must match the runtime.
+
+# The location where protobuf-config.cmake will be installed varies by platform
+if(NOT Protobuf_DIR)
+  if(WIN32)
+    set(Protobuf_DIR "${FIREBASE_INSTALL_DIR}/cmake")
+  else()
+    set(Protobuf_DIR "${FIREBASE_INSTALL_DIR}/lib/cmake/protobuf")
   endif()
+endif()
 
+list(
+  APPEND CMAKE_ARGS
+  -DgRPC_PROTOBUF_PROVIDER:STRING=package
+  -DgRPC_PROTOBUF_PACKAGE_TYPE:STRING=CONFIG
+  -DProtobuf_DIR:PATH=${Protobuf_DIR}
+)
+
+
+## zlib
+
+# cmake/external/zlib.cmake figures out whether or not to build zlib. Either
+# way, from the gRPC build's point of view it's a package.
+list(
+  APPEND CMAKE_ARGS
+  -DgRPC_ZLIB_PROVIDER:STRING=package
+)
+if(ZLIB_FOUND)
+  # Propagate possible user configuration to FindZLIB.cmake in the sub-build.
   list(
     APPEND CMAKE_ARGS
-    -DgRPC_CARES_PROVIDER:STRING=package
-    -Dc-ares_DIR:PATH=${c-ares_DIR}
+    -DZLIB_INCLUDE_DIR=${ZLIB_INCLUDE_DIR}
+    -DZLIB_LIBRARY=${ZLIB_LIBRARY}
   )
+endif()
 
 
-  ## protobuf
+ExternalProject_GitSource(
+  GRPC_GIT
+  GIT_REPOSITORY "https://github.com/grpc/grpc.git"
+  GIT_TAG "v1.8.3"
+  GIT_SUBMODULES ${GIT_SUBMODULES}
+)
 
-  # Unlike other dependencies of gRPC, we control the protobuf version because we
-  # have checked-in protoc outputs that must match the runtime.
+ExternalProject_Add(
+  grpc
+  DEPENDS
+    c-ares
+    protobuf
+    zlib
 
-  # The location where protobuf-config.cmake will be installed varies by platform
-  if(NOT Protobuf_DIR)
-    if(WIN32)
-      set(Protobuf_DIR "${FIREBASE_INSTALL_DIR}/cmake")
-    else()
-      set(Protobuf_DIR "${FIREBASE_INSTALL_DIR}/lib/cmake/protobuf")
-    endif()
-  endif()
+  ${GRPC_GIT}
 
-  list(
-    APPEND CMAKE_ARGS
-    -DgRPC_PROTOBUF_PROVIDER:STRING=package
-    -DgRPC_PROTOBUF_PACKAGE_TYPE:STRING=CONFIG
-    -DProtobuf_DIR:PATH=${Protobuf_DIR}
-  )
+  PREFIX ${PROJECT_BINARY_DIR}/external/grpc
 
+  CMAKE_ARGS
+    ${CMAKE_ARGS}
 
-  ## zlib
+  BUILD_COMMAND
+    ${CMAKE_COMMAND} --build . --target grpc
 
-  # cmake/external/zlib.cmake figures out whether or not to build zlib. Either
-  # way, from the gRPC build's point of view it's a package.
-  list(
-    APPEND CMAKE_ARGS
-    -DgRPC_ZLIB_PROVIDER:STRING=package
-  )
-  if(ZLIB_FOUND)
-    # Propagate possible user configuration to FindZLIB.cmake in the sub-build.
-    list(
-      APPEND CMAKE_ARGS
-      -DZLIB_INCLUDE_DIR=${ZLIB_INCLUDE_DIR}
-      -DZLIB_LIBRARY=${ZLIB_LIBRARY}
-    )
-  endif()
-
-
-  ExternalProject_GitSource(
-    GRPC_GIT
-    GIT_REPOSITORY "https://github.com/grpc/grpc.git"
-    GIT_TAG "v1.8.3"
-    GIT_SUBMODULES ${GIT_SUBMODULES}
-  )
-
-  ExternalProject_Add(
-    grpc
-    DEPENDS
-      c-ares
-      protobuf
-      zlib
-
-    ${GRPC_GIT}
-
-    PREFIX ${PROJECT_BINARY_DIR}/external/grpc
-
-    CMAKE_ARGS
-      ${CMAKE_ARGS}
-
-    BUILD_COMMAND
-      ${CMAKE_COMMAND} --build . --target grpc
-
-    UPDATE_COMMAND ""
-    TEST_COMMAND ""
-    INSTALL_COMMAND ""
-  )
-
-endif(GRPC_ROOT)
-
+  UPDATE_COMMAND ""
+  TEST_COMMAND ""
+  INSTALL_COMMAND ""
+)
--- cmake/external/leveldb.cmake
+++ cmake/external/leveldb.cmake
@@ -24,50 +24,50 @@ if(WIN32 OR LEVELDB_ROOT)
   #   https://github.com/google/leveldb/issues/363
   #   https://github.com/google/leveldb/issues/466
   add_custom_target(leveldb)
+  return()
+endif()
 
-else()
-  # Clean up warning output to reduce noise in the build
-  if(CMAKE_CXX_COMPILER_ID MATCHES "Clang|GNU")
-    set(
-      LEVELDB_CXX_FLAGS "\
-        -Wno-deprecated-declarations"
-    )
-  endif()
 
-  # Map CMake compiler configuration down onto the leveldb Makefile
+# Clean up warning output to reduce noise in the build
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang|GNU")
   set(
-    LEVELDB_OPT "\
-      $<$<CONFIG:Debug>:${CMAKE_CXX_FLAGS_DEBUG}> \
-      $<$<CONFIG:Release>:${CMAKE_CXX_FLAGS_RELEASE}>"
+    LEVELDB_CXX_FLAGS "\
+      -Wno-deprecated-declarations"
   )
+endif()
 
-  ExternalProject_Add(
-    leveldb
+# Map CMake compiler configuration down onto the leveldb Makefile
+set(
+  LEVELDB_OPT "\
+    $<$<CONFIG:Debug>:${CMAKE_CXX_FLAGS_DEBUG}> \
+    $<$<CONFIG:Release>:${CMAKE_CXX_FLAGS_RELEASE}>"
+)
 
-    DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
-    DOWNLOAD_NAME leveldb-v1.20.tar.gz
-    URL https://github.com/google/leveldb/archive/v1.20.tar.gz
-    URL_HASH SHA256=f5abe8b5b209c2f36560b75f32ce61412f39a2922f7045ae764a2c23335b6664
+ExternalProject_Add(
+  leveldb
 
-    PREFIX ${FIREBASE_BINARY_DIR}/external/leveldb
+  DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
+  DOWNLOAD_NAME leveldb-v1.20.tar.gz
+  URL https://github.com/google/leveldb/archive/v1.20.tar.gz
+  URL_HASH SHA256=f5abe8b5b209c2f36560b75f32ce61412f39a2922f7045ae764a2c23335b6664
 
-    # LevelDB's configuration is done in the Makefile
-    CONFIGURE_COMMAND ""
+  PREFIX ${FIREBASE_BINARY_DIR}/external/leveldb
 
-    # The Makefile-based build of leveldb does not support building
-    # out-of-source.
-    BUILD_IN_SOURCE ON
+  # LevelDB's configuration is done in the Makefile
+  CONFIGURE_COMMAND ""
 
-    # Only build the leveldb library skipping the tools and in-memory
-    # implementation we don't use.
-    BUILD_COMMAND
-      env CXXFLAGS=${LEVELDB_CXX_FLAGS} OPT=${LEVELDB_OPT}
-        make -j out-static/libleveldb.a
+  # The Makefile-based build of leveldb does not support building
+  # out-of-source.
+  BUILD_IN_SOURCE ON
 
-    INSTALL_DIR ${FIREBASE_INSTALL_DIR}
+  # Only build the leveldb library skipping the tools and in-memory
+  # implementation we don't use.
+  BUILD_COMMAND
+    env CXXFLAGS=${LEVELDB_CXX_FLAGS} OPT=${LEVELDB_OPT}
+      make -j out-static/libleveldb.a
 
-    INSTALL_COMMAND ""
-    TEST_COMMAND ""
-  )
+  INSTALL_DIR ${FIREBASE_INSTALL_DIR}
 
-endif(WIN32 OR LEVELDB_ROOT)
+  INSTALL_COMMAND ""
+  TEST_COMMAND ""
+)
