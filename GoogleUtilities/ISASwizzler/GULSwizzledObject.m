5ec926eb832cf0d5106f2b5d6ddda3d687cbb43a
Tejas Deshpande <tdeshpande@google.com>
2018-07-20 14:53:25 -0700

    Change how the GULSwizzledObject imports headers to be consistent.

--- GoogleUtilities/ISASwizzler/GULSwizzledObject.m
+++ GoogleUtilities/ISASwizzler/GULSwizzledObject.m
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "GULSwizzledObject.h"
-#import "GULObjectSwizzler.h"
+#import "Private/GULSwizzledObject.h"
+#import "Private/GULObjectSwizzler.h"
 
 NSString *kSwizzlerAssociatedObjectKey = @"gul_objectSwizzler";
 
