aed41f99897e3172a09b395acd79c9399462e69d
Greg Soltis <gsoltis@google.com>
2018-07-20 14:31:59 -0700

    Add failing test

--- Firestore/Example/Tests/Local/FSTLevelDBLRUGarbageCollectorTests.mm
+++ Firestore/Example/Tests/Local/FSTLevelDBLRUGarbageCollectorTests.mm
@@ -14,9 +14,21 @@
  * limitations under the License.
  */
 
-#import <Firestore/Source/Local/FSTLevelDB.h>
 #import "Firestore/Example/Tests/Local/FSTLRUGarbageCollectorTests.h"
+
+#import "Firestore/Source/Core/FSTQuery.h"
+#import "Firestore/Source/Local/FSTLevelDB.h"
+#import "Firestore/Source/Local/FSTLevelDBQueryCache.h"
+#import "Firestore/Source/Local/FSTLocalSerializer.h"
+#import "Firestore/Source/Local/FSTQueryData.h"
+#import "Firestore/Source/Remote/FSTSerializerBeta.h"
 #import "Firestore/Example/Tests/Local/FSTPersistenceTestHelpers.h"
+
+#include "Firestore/core/src/firebase/firestore/model/database_id.h"
+#include "Firestore/core/src/firebase/firestore/model/resource_path.h"
+
+using firebase::firestore::model::DatabaseId;
+using firebase::firestore::model::ResourcePath;
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -25,8 +37,51 @@ NS_ASSUME_NONNULL_BEGIN
 
 @implementation FSTLevelDBLRUGarbageCollectorTests
 
-- (id<FSTPersistence>)newPersistence {
+- (FSTLevelDB *)newPersistence {
   return [FSTPersistenceTestHelpers levelDBPersistence];
+}
+
+- (void)testHighestSequenceNumberPersisted {
+  DatabaseId database_id{"p", "d"};
+
+  FSTSerializerBeta *remoteSerializer = [[FSTSerializerBeta alloc] initWithDatabaseID:&database_id];
+  FSTLocalSerializer *serializer =
+          [[FSTLocalSerializer alloc] initWithRemoteSerializer:remoteSerializer];
+  NSString *dir = [FSTPersistenceTestHelpers levelDBDir];
+
+  FSTLevelDB *db1 = [[FSTLevelDB alloc] initWithDirectory:dir serializer:serializer];
+  NSError *error;
+  [db1 start:&error];
+  XCTAssertNil(error);
+  FSTLevelDBQueryCache *queryCache = db1.run("setup first db", [&]() -> FSTLevelDBQueryCache * {
+    FSTLevelDBQueryCache *queryCache = [db1 queryCache];
+    [queryCache start];
+    return queryCache;
+  });
+
+  XCTAssertEqual(0, queryCache.highestListenSequenceNumber);
+
+  FSTListenSequenceNumber expected = 1234;
+  db1.run("add query data", [&]() {
+    FSTQuery *query = [FSTQuery queryWithPath:ResourcePath{"some", "path"}];
+    FSTQueryData *queryData = [[FSTQueryData alloc] initWithQuery:query
+                                                         targetID:1
+                                             listenSequenceNumber:expected
+                                                          purpose:FSTQueryPurposeListen];
+    [queryCache addQueryData:queryData];
+  });
+
+  [db1 shutdown];
+  db1 = nil;
+
+  FSTLevelDB *db2 = [[FSTLevelDB alloc] initWithDirectory:dir serializer:serializer];
+  [db2 start:&error];
+  XCTAssertNil(error);
+  db2.run("verify sequence number", [&]() {
+    // We should remember the previous sequence number, and the next transaction should
+    // have a higher one.
+    XCTAssertGreaterThan(db2.currentSequenceNumber, expected);
+  });
 }
 
 @end
