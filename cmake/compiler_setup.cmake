1be2405ee9e615d53f58197c18973703952792fe
Gil <mcg@google.com>
2018-07-26 18:09:53 -0700

    Add CMake support for building under Visual Studio 2017 (#1588)
    * Add compiler flags for MSVC on VS2017
    
    * Force gtest to use the dynamic runtime on Windows.
    
    * Avoid setting flags that MSVC does not understand
    
    * Add compiler_id: cheap shortcuts for conditional features on a compiler
    
    * Fix googletest build under VS2017
    
    * Force disable Abseil's tests, which don't compile under VS2017.
    
    * Make Firestore CMake a self-contained build
    
    Now shell out to a sub-CMake to prepare depenencies without needing an
    explciit superbuild structure. This makes the Visual Studio and CLion
    projects driven from the CMake configuration actually usable.
    
    * Make LevelDB an optional dependency.
    
    * Move third-party dependencies to the top-level

--- cmake/compiler_setup.cmake
+++ cmake/compiler_setup.cmake
@@ -14,6 +14,8 @@
 
 # C++ Compiler setup
 
+include(compiler_id)
+
 # We use C++11
 set(CMAKE_CXX_STANDARD 11)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
@@ -23,15 +25,7 @@ if(CMAKE_GENERATOR STREQUAL "Ninja")
   set(NINJA ON)
 endif()
 
-if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-  set(CLANG ON)
-endif()
-
-if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-  set(GNU ON)
-endif()
-
-if(CLANG OR GNU)
+if(CXX_CLANG OR CXX_GNU)
   set(
     common_flags
     -Wall -Wextra -Werror
@@ -60,7 +54,7 @@ if(CLANG OR GNU)
     -Wstrict-prototypes
   )
 
-  if(CLANG)
+  if(CXX_CLANG)
     list(
       APPEND common_flags
      -Wconditional-uninitialized -Werror=return-type -Winfinite-recursion -Wmove
@@ -73,7 +67,7 @@ if(CLANG OR GNU)
 
   if(NINJA)
     # If building under Ninja, disable tty detection and force color output
-    if(CLANG OR GNU)
+    if(CXX_CLANG OR CXX_GNU)
       list(APPEND common_flags -fdiagnostics-color)
     endif()
   endif()
@@ -108,3 +102,29 @@ if(APPLE)
     -F${FIREBASE_INSTALL_DIR}/Frameworks
   )
 endif()
+
+if(MSVC)
+  set(
+    common_flags
+
+    # Cut down on symbol cruft in windows.h
+    /DWIN32_LEAN_AND_MEAN=1
+
+    # Specify at least Windows Vista/Server 2008 (required by gRPC)
+    /D_WIN32_WINNT=0x600
+
+    # Disable warnings that can't be easily addressed or are ignored by
+    # upstream projects.
+
+    # unary minus operator applied to unsigned type, result still unsigned
+    /wd4146
+  )
+endif()
+
+foreach(flag ${common_flags} ${c_flags})
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flag}")
+endforeach()
+
+foreach(flag ${common_flags} ${cxx_flags})
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}")
+endforeach()
