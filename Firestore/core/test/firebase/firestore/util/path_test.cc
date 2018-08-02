603467be825c5f7672e973bed6f77583e5d9fb55
Gil <mcg@google.com>
2018-08-01 17:38:33 -0700

    Rewrite Path to use native path character type (#1619)

--- Firestore/core/test/firebase/firestore/util/path_test.cc
+++ Firestore/core/test/firebase/firestore/util/path_test.cc
@@ -58,12 +58,49 @@ namespace util {
 //   * POSIX is more complicated than we need
 //   * std::filesystem is still too experimental (as of 2018-06-05)
 
-#define EXPECT_BASENAME_EQ(expected, source)                  \
-  do {                                                        \
-    EXPECT_EQ(std::string{expected}, Path::Basename(source)); \
+#define EXPECT_PATH_EQ(expected, actual)         \
+  do {                                           \
+    EXPECT_EQ(Path::FromUtf8(expected), actual); \
   } while (0)
 
-TEST(Path, Basename_NoSeparator) {
+#define EXPECT_PATH_EQ2(expected, actual)                        \
+  do {                                                           \
+    EXPECT_EQ(Path::FromUtf8(expected), Path::FromUtf8(actual)); \
+  } while (0)
+
+#define EXPECT_PATH_NE2(expected, actual)                        \
+  do {                                                           \
+    EXPECT_NE(Path::FromUtf8(expected), Path::FromUtf8(actual)); \
+  } while (0)
+
+TEST(PathTest, Equals) {
+  EXPECT_PATH_EQ2("", "");
+  EXPECT_PATH_EQ2("/", "/");
+  EXPECT_PATH_EQ2("a", "a/");  // Trailing slashes removed
+
+  EXPECT_PATH_NE2("", "/");
+  EXPECT_PATH_NE2("/", "a");
+  EXPECT_PATH_NE2("/a", "a");
+  EXPECT_PATH_NE2("/a", "/b");
+  EXPECT_PATH_NE2("/a", "/aa");
+  EXPECT_PATH_NE2("/a/b", "/a/");
+  EXPECT_PATH_NE2("/a/b", "/a//b");  // Internal slashes not removed
+
+#if defined(_WIN32)
+  EXPECT_PATH_EQ2("/", "\\");
+  EXPECT_PATH_EQ2("c:", "C:");
+  EXPECT_PATH_EQ2("C:/", "C:\\");
+  EXPECT_PATH_EQ2("c:\\", "C:\\");
+  EXPECT_PATH_EQ2("c:\\", "C:\\\\/\\");
+#endif
+}
+
+#define EXPECT_BASENAME_EQ(expected, source)                     \
+  do {                                                           \
+    EXPECT_PATH_EQ(expected, Path::FromUtf8(source).Basename()); \
+  } while (0)
+
+TEST(PathTest, Basename_NoSeparator) {
   // POSIX would require all of these to be ".".
   // python and libc++ agree this is "".
   EXPECT_BASENAME_EQ("", "");
@@ -73,7 +110,7 @@ TEST(Path, Basename_NoSeparator) {
   EXPECT_BASENAME_EQ("..", "..");
 }
 
-TEST(Path, Basename_LeadingSlash) {
+TEST(PathTest, Basename_LeadingSlash) {
   EXPECT_BASENAME_EQ("", "/");
   EXPECT_BASENAME_EQ("", "///");
   EXPECT_BASENAME_EQ("a", "/a");
@@ -84,7 +121,7 @@ TEST(Path, Basename_LeadingSlash) {
   EXPECT_BASENAME_EQ("..", "//..");
 }
 
-TEST(Path, Basename_IntermediateSlash) {
+TEST(PathTest, Basename_IntermediateSlash) {
   EXPECT_BASENAME_EQ("b", "/a/b");
   EXPECT_BASENAME_EQ("b", "/a//b");
   EXPECT_BASENAME_EQ("b", "//a/b");
@@ -95,7 +132,7 @@ TEST(Path, Basename_IntermediateSlash) {
   EXPECT_BASENAME_EQ("b", "//a/.//b");
 }
 
-TEST(Path, Basename_TrailingSlash) {
+TEST(PathTest, Basename_TrailingSlash) {
   // python: "a/b//" => ""
   // POSIX: "a/b//" => "b"
   // libc++ path::filename(): "a/b//" => "." (cppreference suggests "")
@@ -108,7 +145,7 @@ TEST(Path, Basename_TrailingSlash) {
   EXPECT_BASENAME_EQ("", "//a//b//");
 }
 
-TEST(Path, Basename_RelativePath) {
+TEST(PathTest, Basename_RelativePath) {
   EXPECT_BASENAME_EQ("b", "a/b");
   EXPECT_BASENAME_EQ("b", "a//b");
 
@@ -118,12 +155,12 @@ TEST(Path, Basename_RelativePath) {
   EXPECT_BASENAME_EQ("b", "a//.//b");
 }
 
-#define EXPECT_DIRNAME_EQ(expected, source)                  \
-  do {                                                       \
-    EXPECT_EQ(std::string{expected}, Path::Dirname(source)); \
+#define EXPECT_DIRNAME_EQ(expected, source)                     \
+  do {                                                          \
+    EXPECT_PATH_EQ(expected, Path::FromUtf8(source).Dirname()); \
   } while (0)
 
-TEST(Path, Dirname_NoSeparator) {
+TEST(PathTest, Dirname_NoSeparator) {
   // POSIX would require all of these to be ".".
   // python and libc++ agree this is "".
   EXPECT_DIRNAME_EQ("", "");
@@ -133,7 +170,7 @@ TEST(Path, Dirname_NoSeparator) {
   EXPECT_DIRNAME_EQ("", "..");
 }
 
-TEST(Path, Dirname_LeadingSlash) {
+TEST(PathTest, Dirname_LeadingSlash) {
   // POSIX says all "/".
   // python starts with "/" but does not strip trailing slashes.
   // libc++ path::parent_path() considers all of these be "", though
@@ -149,7 +186,7 @@ TEST(Path, Dirname_LeadingSlash) {
   EXPECT_DIRNAME_EQ("/", "//..");
 }
 
-TEST(Path, Dirname_IntermediateSlash) {
+TEST(PathTest, Dirname_IntermediateSlash) {
   EXPECT_DIRNAME_EQ("/a", "/a/b");
   EXPECT_DIRNAME_EQ("/a", "/a//b");
   EXPECT_DIRNAME_EQ("//a", "//a/b");
@@ -160,7 +197,7 @@ TEST(Path, Dirname_IntermediateSlash) {
   EXPECT_DIRNAME_EQ("//a/.", "//a/.//b");
 }
 
-TEST(Path, Dirname_TrailingSlash) {
+TEST(PathTest, Dirname_TrailingSlash) {
   // POSIX demands stripping trailing slashes before computing dirname, while
   // python and libc++ effectively seem to consider the path to contain an empty
   // path segment there.
@@ -173,7 +210,7 @@ TEST(Path, Dirname_TrailingSlash) {
   EXPECT_DIRNAME_EQ("//a//b", "//a//b//");
 }
 
-TEST(Path, Dirname_RelativePath) {
+TEST(PathTest, Dirname_RelativePath) {
   EXPECT_DIRNAME_EQ("a", "a/b");
   EXPECT_DIRNAME_EQ("a", "a//b");
 
@@ -183,56 +220,63 @@ TEST(Path, Dirname_RelativePath) {
   EXPECT_DIRNAME_EQ("a//.", "a//.//b");
 }
 
-TEST(Path, IsAbsolute) {
-  EXPECT_FALSE(Path::IsAbsolute(""));
-  EXPECT_TRUE(Path::IsAbsolute("/"));
-  EXPECT_TRUE(Path::IsAbsolute("//"));
-  EXPECT_TRUE(Path::IsAbsolute("/foo"));
-  EXPECT_FALSE(Path::IsAbsolute("foo"));
-  EXPECT_FALSE(Path::IsAbsolute("foo/bar"));
+TEST(PathTest, IsAbsolute) {
+  EXPECT_FALSE(Path::FromUtf8("").IsAbsolute());
+  EXPECT_TRUE(Path::FromUtf8("/").IsAbsolute());
+  EXPECT_TRUE(Path::FromUtf8("//").IsAbsolute());
+  EXPECT_TRUE(Path::FromUtf8("/foo").IsAbsolute());
+  EXPECT_FALSE(Path::FromUtf8("foo").IsAbsolute());
+  EXPECT_FALSE(Path::FromUtf8("foo/bar").IsAbsolute());
 }
 
-TEST(Path, Join_Absolute) {
-  EXPECT_EQ("/", Path::Join("/"));
+TEST(PathTest, Join_Absolute) {
+  EXPECT_PATH_EQ("/", Path::JoinUtf8("/"));
 
-  EXPECT_EQ("/", Path::Join("", "/"));
-  EXPECT_EQ("/", Path::Join("a", "/"));
-  EXPECT_EQ("/b", Path::Join("a", "/b"));
+  EXPECT_PATH_EQ("/", Path::JoinUtf8("", "/"));
+  EXPECT_PATH_EQ("/", Path::JoinUtf8("a", "/"));
+  EXPECT_PATH_EQ("/b", Path::JoinUtf8("a", "/b"));
 
   // Alternate root names should be preserved.
-  EXPECT_EQ("//", Path::Join("a", "//"));
-  EXPECT_EQ("//b", Path::Join("a", "//b"));
-  EXPECT_EQ("///b///", Path::Join("a", "///b///"));
+  EXPECT_PATH_EQ("//", Path::JoinUtf8("a", "//"));
+  EXPECT_PATH_EQ("//b", Path::JoinUtf8("a", "//b"));
+  EXPECT_PATH_EQ("///b///", Path::JoinUtf8("a", "///b///"));
 
-  EXPECT_EQ("/", Path::Join("/", "/"));
-  EXPECT_EQ("/b", Path::Join("/", "/b"));
-  EXPECT_EQ("//b", Path::Join("//host/a", "//b"));
-  EXPECT_EQ("//b", Path::Join("//host/a/", "//b"));
+  EXPECT_PATH_EQ("/", Path::JoinUtf8("/", "/"));
+  EXPECT_PATH_EQ("/b", Path::JoinUtf8("/", "/b"));
+  EXPECT_PATH_EQ("//b", Path::JoinUtf8("//host/a", "//b"));
+  EXPECT_PATH_EQ("//b", Path::JoinUtf8("//host/a/", "//b"));
 
-  EXPECT_EQ("/", Path::Join("/", ""));
-  EXPECT_EQ("/a", Path::Join("/", "a"));
-  EXPECT_EQ("/a/b/c", Path::Join("/", "a", "b", "c"));
-  EXPECT_EQ("/a/", Path::Join("/", "a/"));
-  EXPECT_EQ("/.", Path::Join("/", "."));
-  EXPECT_EQ("/..", Path::Join("/", ".."));
+  EXPECT_PATH_EQ("/", Path::JoinUtf8("/", ""));
+  EXPECT_PATH_EQ("/a", Path::JoinUtf8("/", "a"));
+  EXPECT_PATH_EQ("/a/b/c", Path::JoinUtf8("/", "a", "b", "c"));
+  EXPECT_PATH_EQ("/a/", Path::JoinUtf8("/", "a/"));
+  EXPECT_PATH_EQ("/.", Path::JoinUtf8("/", "."));
+  EXPECT_PATH_EQ("/..", Path::JoinUtf8("/", ".."));
 }
 
-TEST(Path, Join_Relative) {
-  EXPECT_EQ("", Path::Join(""));
+TEST(PathTest, Join_Relative) {
+  EXPECT_PATH_EQ("", Path::JoinUtf8(""));
 
-  EXPECT_EQ("", Path::Join("", "", "", ""));
-  EXPECT_EQ("a/b/c", Path::Join("a/b", "c"));
-  EXPECT_EQ("/c/d", Path::Join("a/b", "/c", "d"));
-  EXPECT_EQ("/c/d", Path::Join("a/b/", "/c", "d"));
+  EXPECT_PATH_EQ("", Path::JoinUtf8("", "", "", ""));
+  EXPECT_PATH_EQ("a/b/c", Path::JoinUtf8("a/b", "c"));
+  EXPECT_PATH_EQ("/c/d", Path::JoinUtf8("a/b", "/c", "d"));
+  EXPECT_PATH_EQ("/c/d", Path::JoinUtf8("a/b/", "/c", "d"));
 }
 
-TEST(Path, Join_Types) {
-  EXPECT_EQ("a/b", Path::Join(absl::string_view{"a"}, "b"));
-  EXPECT_EQ("a/b", Path::Join(std::string{"a"}, "b"));
+TEST(PathTest, Join_Types) {
+  EXPECT_PATH_EQ("a/b", Path::JoinUtf8(absl::string_view{"a"}, "b"));
+  EXPECT_PATH_EQ("a/b", Path::JoinUtf8(std::string{"a"}, "b"));
 
   std::string a_string{"a"};
-  EXPECT_EQ("a/b", Path::Join(a_string, "b"));
-  EXPECT_EQ("a", a_string);
+  EXPECT_PATH_EQ("a/b", Path::JoinUtf8(a_string, "b"));
+  EXPECT_PATH_EQ("a", Path::JoinUtf8(a_string));
+
+  EXPECT_PATH_EQ("a/b/c/d", Path::JoinUtf8(a_string, "b", Path::FromUtf8("c"),
+                                           absl::string_view{"d"}));
+
+  EXPECT_PATH_EQ("a/b/c/d",
+                 Path::JoinUtf8(Path::FromUtf8("a"), "b", Path::FromUtf8("c"),
+                                absl::string_view{"d"}));
 }
 
 }  // namespace util
