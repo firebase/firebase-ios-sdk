f28d8abcda858b9feff5bec8cf233309ceab8ad4
Marek Gilbert <mcg@google.com>
2018-08-04 16:31:10 -0700

    Add a Win32 filesystem implementation

--- Firestore/core/test/firebase/firestore/util/filesystem_test.cc
+++ Firestore/core/test/firebase/firestore/util/filesystem_test.cc
@@ -24,6 +24,7 @@
 #include "Firestore/core/src/firebase/firestore/util/autoid.h"
 #include "Firestore/core/src/firebase/firestore/util/log.h"
 #include "Firestore/core/src/firebase/firestore/util/path.h"
+#include "Firestore/core/src/firebase/firestore/util/string_win.h"
 #include "Firestore/core/test/firebase/firestore/util/status_test_util.h"
 #include "absl/strings/match.h"
 #include "absl/types/optional.h"
@@ -52,10 +53,18 @@ TEST(FilesystemTest, Exists) {
   EXPECT_FALSE(Exists(file));
 }
 
+#if defined(_WIN32)
+#define ASSERT_USEFUL_TEMP_DIR(dir)                          \
+  do {                                                       \
+    ASSERT_TRUE(absl::StartsWith(tmp.ToUtf8String(), "C:")); \
+  } while (0)
+
+#else
 #define ASSERT_USEFUL_TEMP_DIR(dir)                         \
   do {                                                      \
     ASSERT_TRUE(absl::StartsWith(tmp.ToUtf8String(), "/")); \
   } while (0)
+#endif  // defined(_WIN32)
 
 TEST(FilesystemTest, GetTempDir) {
   Path tmp = TempDir();
@@ -63,10 +72,33 @@ TEST(FilesystemTest, GetTempDir) {
 }
 
 absl::optional<std::string> GetEnv(const char* name) {
+#if defined(_WIN32)
+  // The required buffer size (not the length of the value)
+  size_t value_size = 0;
+  errno_t result = getenv_s(&value_size, nullptr, 0, name);
+  if (result) {
+    ADD_FAILURE() << "getenv_s failed with errno=" << result;
+    return absl::nullopt;
+  }
+  if (value_size == 0) return absl::nullopt;
+
+  std::string value(value_size, '\0');
+  result = getenv_s(&value_size, &value[0], value_size, name);
+  if (result) {
+    ADD_FAILURE() << "getenv_s failed with errno=" << result;
+    return absl::nullopt;
+  }
+
+  value.resize(value_size - 1);
+
+  return value;
+
+#else
   const char* value = getenv(name);
   if (!value) return absl::nullopt;
 
   return std::string{value};
+#endif
 }
 
 int SetEnv(const char* env_var, const char* value) {
