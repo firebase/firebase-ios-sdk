6eafb2577ada01a0cec1697c1c4530c604046215
Mina Farid <minafarid@google.com>
2018-08-09 14:28:24 -0400

    Add fuzzing target for creating ResourcePath objects (#1659)

---
+++ Firestore/core/test/firebase/firestore/fuzzing/resourcepath_fuzzer.cc
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cstddef>
+#include <cstdint>
+
+#include "Firestore/core/src/firebase/firestore/model/resource_path.h"
+
+using firebase::firestore::model::ResourcePath;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  const char* str_ptr = reinterpret_cast<const char*>(data);
+  absl::string_view str_view{str_ptr, size};
+
+  try {
+    ResourcePath rp = ResourcePath::FromString(str_view);
+  } catch (...) {
+    // Ignore caught errors and assertions.
+  }
+
+  return 0;
+}
