8d26f2092b6a244fc5e0765a2f4191f499ee3b7c
Konstantin Varlamov <varconst@google.com>
2018-08-13 19:29:42 -0400

    Initial

---
+++ Firestore/core/src/firebase/firestore/remote/buffered_writer.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2018 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_REMOTE_BUFFERED_WRITER_H
+#define FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_REMOTE_BUFFERED_WRITER_H
+
+#include <functional>
+#include <vector>
+
+#include <grpcpp/support/byte_buffer.h>
+
+namespace firebase {
+namespace firestore {
+namespace remote {
+
+// GRPC doesn't allow issuing a write operation before the previous write has
+// finished. This class helps keep track of pending writes and whether there
+// currently is a write operation in progress; it doesn't communicate with GRPC
+// directly and expects the following to function correctly:
+// - the constructor should be given a function that actually issues the GRPC
+//   write operation;
+// - each time a write operation finishes, `OnSuccessfulWrite` must be called.
+// The invariant is that `BufferedWriter` will never invoke the writing function
+// if `OnSuccessfulWrite` hasn't been called since the previous invocation.
+//
+// The main methods are `Enqueue` and `OnSuccessfulWrite`. `Enqueue` will invoke
+// the writing function immediately if there is no operation in progress;
+// otherwise, the given `bytes` will be stored in the buffer.
+// `OnSuccessfulWrite` will invoke the writing function with the next write in
+// buffer, as long as the buffer is not empty (in FIFO order).
+
+// `BufferedWriter` can be `Start`ed and `Stop`ped, which is expected to reflect
+// the state of the GRPC call. When `BufferedWriter` is not started, writes can
+// be enqueued, but the writing function will not be invoked. Once the
+// `BufferedWriter` is started, it will immediately invoke the writing function
+// if the buffer is non-empty. When the `BufferedWriter` is first created, it's
+// in the stopped state. `BufferedWriter` can be restarted.
+class BufferedWriter {
+ public:
+  using WriteFunction = std::function<void(grpc::ByteBuffer&&)>;
+
+  explicit BufferedWriter(WriteFunction&& write_func);
+
+  void Start();
+  void Stop();
+
+  bool empty() const {
+    return buffer_.empty();
+  }
+
+  bool IsStarted() const {
+    return is_started_;
+  }
+
+  void Enqueue(grpc::ByteBuffer&& bytes);
+  void OnSuccessfulWrite();
+
+  // Clears (but doesn't stop) the buffer. If there is an operation in progress,
+  // `OnSuccessfulWrite` must still be called for it.
+  void Clear();
+
+ private:
+  void TryWrite();
+
+  WriteFunction write_func_;
+
+  std::vector<grpc::ByteBuffer> buffer_;
+  bool has_pending_write_ = false;
+  bool is_started_ = false;
+};
+
+}  // namespace remote
+}  // namespace firestore
+}  // namespace firebase
+
+#endif  // FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_REMOTE_BUFFERED_WRITER_H
---
+++ Firestore/core/test/firebase/firestore/remote/buffered_writer_test.cc
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2018 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Firestore/core/src/firebase/firestore/remote/buffered_writer.h"
+
+#include "gtest/gtest.h"
+
+namespace firebase {
+namespace firestore {
+namespace remote {
+
+class BufferedWriterTest : public testing::Test {
+ public:
+  BufferedWriterTest()
+      : writer{[this](grpc::ByteBuffer&&) { ++writes_count; }} {
+  }
+
+  int writes_count = 0;
+  BufferedWriter writer;
+};
+
+TEST_F(BufferedWriterTest, CanDoImmediateWrites) {
+  EXPECT_EQ(writes_count, 0);
+
+  writer.Start();
+  writer.Enqueue({});
+  EXPECT_EQ(writes_count, 1);
+}
+
+TEST_F(BufferedWriterTest, CanDoBufferedWrites) {
+  EXPECT_EQ(writes_count, 0);
+
+  writer.Start();
+  writer.Enqueue({});
+  writer.Enqueue({});
+  writer.Enqueue({});
+  EXPECT_EQ(writes_count, 1);
+
+  writer.OnSuccessfulWrite();
+  EXPECT_EQ(writes_count, 2);
+
+  writer.OnSuccessfulWrite();
+  EXPECT_EQ(writes_count, 3);
+
+  // An extra call to `OnSuccessfulWrite` should be a no-op.
+  writer.OnSuccessfulWrite();
+  EXPECT_EQ(writes_count, 3);
+}
+
+TEST_F(BufferedWriterTest, CanEnqueueWritesBeforeStarting) {
+  writer.Enqueue({});
+  writer.Enqueue({});
+  EXPECT_EQ(writes_count, 0);
+
+  writer.Start();
+  EXPECT_EQ(writes_count, 1);
+  writer.OnSuccessfulWrite();
+  EXPECT_EQ(writes_count, 2);
+}
+
+TEST_F(BufferedWriterTest, CanStop) {
+  writer.Enqueue({});
+  writer.Enqueue({});
+  EXPECT_EQ(writes_count, 0);
+
+  writer.Start();
+  EXPECT_EQ(writes_count, 1);
+  writer.Stop();
+  writer.OnSuccessfulWrite();
+  EXPECT_EQ(writes_count, 1);
+}
+
+TEST_F(BufferedWriterTest, CanRestart) {
+  writer.Enqueue({});
+  writer.Enqueue({});
+  EXPECT_EQ(writes_count, 0);
+
+  writer.Start();
+  EXPECT_EQ(writes_count, 1);
+  writer.Stop();
+  writer.OnSuccessfulWrite();
+  EXPECT_EQ(writes_count, 1);
+  writer.Start();
+  EXPECT_EQ(writes_count, 2);
+}
+
+TEST_F(BufferedWriterTest, CanClear) {
+  writer.Enqueue({});
+  writer.Enqueue({});
+  writer.Enqueue({});
+  writer.Enqueue({});
+  EXPECT_EQ(writes_count, 0);
+
+  writer.Start();
+  EXPECT_EQ(writes_count, 1);
+  EXPECT_FALSE(writer.empty());
+  writer.Clear();
+  EXPECT_TRUE(writer.empty());
+
+  writer.Enqueue({});
+  // We still haven't acknowledged that the previous write finished, so the
+  // writer shouldn't do an immediate write. Clearing the writer shouldn't
+  // affect the writer still waiting for the previous operation to complete.
+  EXPECT_EQ(writes_count, 1);
+  writer.OnSuccessfulWrite();
+  EXPECT_EQ(writes_count, 2);
+
+  // The previously enqueued operations should be cleared.
+  writer.OnSuccessfulWrite();
+  writer.OnSuccessfulWrite();
+  writer.OnSuccessfulWrite();
+  EXPECT_EQ(writes_count, 2);
+}
+
+}  // namespace remote
+}  // namespace firestore
+}  // namespace firebase
