92be983060ab3d7d4411f1737c89d5fbfb7e8a16
Marek Gilbert <mcg@google.com>
2018-08-15 16:55:00 -0700

    FSTOnlineState -> OnlineState

--- Firestore/core/src/firebase/firestore/model/types.h
+++ Firestore/core/src/firebase/firestore/model/types.h
@@ -42,6 +42,37 @@ using ListenSequenceNumber = int64_t;
  */
 using TargetId = int32_t;
 
+/**
+ * Describes the online state of the Firestore client. Note that this does not
+ * indicate whether or not the remote store is trying to connect or not. This is
+ * primarily used by the View / EventManager code to change their behavior while
+ * offline (e.g. get() calls shouldn't wait for data from the server and
+ * snapshot events should set metadata.isFromCache=true).
+ */
+enum class OnlineState {
+  /**
+   * The Firestore client is in an unknown online state. This means the client
+   * is either not actively trying to establish a connection or it is currently
+   * trying to establish a connection, but it has not succeeded or failed yet.
+   * Higher-level components should not operate in offline mode.
+   */
+  Unknown,
+
+  /**
+   * The client is connected and the connections are healthy. This state is
+   * reached after a successful connection and there has been at least one
+   * successful message received from the backends.
+   */
+  Online,
+
+  /**
+   * The client is either trying to establish a connection but failing, or it
+   * has been explicitly marked offline via a call to `disableNetwork`.
+   * Higher-level components should operate in offline mode.
+   */
+  Offline
+};
+
 }  // namespace model
 }  // namespace firestore
 }  // namespace firebase
