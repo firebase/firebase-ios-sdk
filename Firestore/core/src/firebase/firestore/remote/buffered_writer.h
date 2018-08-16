2c72ef370d5b38fefcfeebfbb4bf8c0ff32d8825
Konstantin Varlamov <varconst@google.com>
2018-08-15 20:13:26 -0400

    More review feedback

--- Firestore/core/src/firebase/firestore/remote/buffered_writer.h
+++ Firestore/core/src/firebase/firestore/remote/buffered_writer.h
@@ -17,41 +17,37 @@
 #ifndef FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_REMOTE_BUFFERED_WRITER_H
 #define FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_REMOTE_BUFFERED_WRITER_H
 
-#include <functional>
+#include <memory>
 #include <queue>
 
-#include <grpcpp/support/byte_buffer.h>
+#include "Firestore/core/src/firebase/firestore/remote/grpc_operation.h"
 
 namespace firebase {
 namespace firestore {
 namespace remote {
 
 /**
- * `BufferedWriter` accepts `ByteBuffer`s ("writes") on its queue and writes
- * them one by one. Only one write may be in progress ("active") at any given
- * time.
+ * `BufferedWriter` accepts GRPC write operations ("writes") on its queue and
+ * writes them one by one. Only one write may be in progress ("active") at any
+ * given time.
  *
  * Writes are put on the queue using `Enqueue`; if no other write is currently
- * in progress, it will become active immediately, otherwise, it will put on the
- * queue. When a write becomes active, `WriteFunction` is invoked on it. A write
- * is active from the moment `WriteFunction` is invoked and until `DequeueNext`
- * is called on the `BufferedWriter`. `DequeueNext` makes the next write active,
- * if any.
+ * in progress, it will become active immediately, otherwise, it will be put on
+ * the queue. When a write becomes active, it is executed (via `Execute`);
+ * a write is active from the moment it is executed and until `DequeueNext` is
+ * called on the `BufferedWriter`. `DequeueNext` makes the next write active, if
+ * any.
  *
  * This class exists to help Firestore streams adhere to GRPC requirement that
  * only one write operation may be active at any given time.
  */
 class BufferedWriter {
  public:
-  using WriteFunction = std::function<void(grpc::ByteBuffer&&)>;
-
-  explicit BufferedWriter(WriteFunction&& write_func);
-
   bool empty() const {
     return queue_.empty();
   }
 
-  void Enqueue(grpc::ByteBuffer&& write);
+  void Enqueue(std::unique_ptr<GrpcOperation> write);
   void DequeueNext();
 
   // Doesn't affect the write that is currently in progress.
@@ -60,9 +56,7 @@ class BufferedWriter {
  private:
   void TryWrite();
 
-  WriteFunction write_func_;
-
-  std::queue<grpc::ByteBuffer> queue_;
+  std::queue<std::unique_ptr<GrpcOperation>> queue_;
   bool has_active_write_ = false;
 };
 
