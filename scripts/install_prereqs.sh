72edc15cd4d9686190369db4806494b7b3ca8d62
Marek Gilbert <mcg@google.com>
2018-08-18 08:47:15 -0700

    Make fuzzing run in the test stage
    Stages, even when cut off early with if_cron, still take about two
    minutes to run. The fuzz_test stage was waiting for all tests to
    complete before starting, which was artificially adding two minutes to
    every build.
    
    Use a different method to keep this visually distinct from the rest.

--- scripts/install_prereqs.sh
+++ scripts/install_prereqs.sh
@@ -35,7 +35,7 @@ case "$PROJECT-$PLATFORM-$METHOD" in
     bundle exec pod install --project-directory=GoogleUtilities/Example
     ;;
 
-  Firestore-*-xcodebuild)
+  Firestore-*-xcodebuild | Firestore-*-fuzz)
     gem install xcpretty
     bundle exec pod install --project-directory=Firestore/Example --repo-update
     ;;
