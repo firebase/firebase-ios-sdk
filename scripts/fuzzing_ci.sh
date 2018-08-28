54451096773f1207c13c7c9f7f88e5a02a2caeda
Konstantin Varlamov <varconst@google.com>
2018-08-28 00:29:21 -0400

    Reduce fuzz time to 25 minutes

--- scripts/fuzzing_ci.sh
+++ scripts/fuzzing_ci.sh
@@ -7,10 +7,10 @@
 # You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
-#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
@@ -20,8 +20,8 @@
 # from the project root directory. The script is intended to execute on travis
 # cron jobs, but can run locally (on a macOS) from the project root.
 
-# Total time allowed for fuzzing in seconds (40 minutes for now).
-readonly ALLOWED_TIME=2400
+# Total time allowed for fuzzing in seconds (25 minutes for now).
+readonly ALLOWED_TIME=1500
 
 # An invalid target that is used to retrieve the list of available targets in
 # the returned error message.
