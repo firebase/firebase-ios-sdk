7b5145bb0f1e95a48dc2c6c31e5394e7ebd34223
Konstantin Varlamov <varconst@google.com>
2018-08-29 17:57:06 -0400

    Initial

--- Firestore/core/src/firebase/firestore/remote/datastore.h
+++ Firestore/core/src/firebase/firestore/remote/datastore.h
@@ -17,10 +17,21 @@
 #ifndef FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_REMOTE_DATASTORE_H_
 #define FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_REMOTE_DATASTORE_H_
 
+#include <memory>
 #include <string>
 
+#include "Firestore/core/include/firebase/firestore/firestore_errors.h"
+#include "Firestore/core/src/firebase/firestore/core/database_info.h"
+#include "Firestore/core/src/firebase/firestore/remote/grpc_operation.h"
 #include "Firestore/core/src/firebase/firestore/remote/grpc_stream.h"
+#include "Firestore/core/src/firebase/firestore/remote/grpc_stream_observer.h"
+#include "Firestore/core/src/firebase/firestore/util/async_queue.h"
+#include "Firestore/core/src/firebase/firestore/util/executor.h"
 #include "Firestore/core/src/firebase/firestore/util/status.h"
+#include "absl/strings/string_view.h"
+#include "grpcpp/channel.h"
+#include "grpcpp/completion_queue.h"
+#include "grpcpp/generic/generic_stub.h"
 #include "grpcpp/support/status.h"
 
 namespace firebase {
@@ -29,9 +40,19 @@ namespace remote {
 
 class Datastore {
  public:
+  Datastore(util::AsyncQueue* firestore_queue,
+            const core::DatabaseInfo& database_info);
+
+  void Shutdown();
+
+  std::unique_ptr<GrpcStream> CreateGrpcStream(absl::string_view token,
+                                               absl::string_view path,
+                                               GrpcStreamObserver* observer);
+
+  static util::Status ConvertStatus(grpc::Status grpc_error);
+
   static std::string GetWhitelistedHeadersAsString(
       const GrpcStream::MetadataT& headers);
-  static util::Status ConvertStatus(grpc::Status grpc_error);
 
   Datastore(const Datastore& other) = delete;
   Datastore(Datastore&& other) = delete;
@@ -39,9 +60,25 @@ class Datastore {
   Datastore& operator=(Datastore&& other) = delete;
 
  private:
+  void PollGrpcQueue();
+
+  void EnsureValidGrpcStub();
+  std::shared_ptr<grpc::Channel> CreateGrpcChannel() const;
+  std::unique_ptr<grpc::ClientContext> CreateGrpcContext(
+      absl::string_view token) const;
+  std::unique_ptr<grpc::GenericClientAsyncReaderWriter> CreateGrpcReaderWriter(
+      grpc::ClientContext* context, absl::string_view path);
+
   static GrpcStream::MetadataT ExtractWhitelistedHeaders(
       const GrpcStream::MetadataT& headers);
 
+  util::AsyncQueue* firestore_queue_ = nullptr;
+  const core::DatabaseInfo* database_info_ = nullptr;
+
+  std::unique_ptr<util::internal::Executor> dedicated_executor_;
+  std::shared_ptr<grpc::Channel> grpc_channel_;
+  grpc::GenericStub grpc_stub_;
+  grpc::CompletionQueue grpc_queue_;
 };
 
 }  // namespace remote
--- Firestore/core/src/firebase/firestore/remote/datastore.mm
+++ Firestore/core/src/firebase/firestore/remote/datastore.mm
@@ -17,17 +17,41 @@
 #include "Firestore/core/src/firebase/firestore/remote/datastore.h"
 
 #include <algorithm>
+#include <fstream>
+#include <sstream>
 
-#include "Firestore/core/include/firebase/firestore/firestore_errors.h"
+#include <grpcpp/create_channel.h>
+#include "Firestore/core/src/firebase/firestore/auth/credentials_provider.h"
+#include "Firestore/core/src/firebase/firestore/auth/token.h"
+#include "Firestore/core/src/firebase/firestore/core/database_info.h"
+#include "Firestore/core/src/firebase/firestore/model/database_id.h"
+#include "Firestore/core/src/firebase/firestore/model/document_key.h"
+#include "Firestore/core/src/firebase/firestore/util/executor_libdispatch.h"
 #include "Firestore/core/src/firebase/firestore/util/hard_assert.h"
+#include "absl/memory/memory.h"
 #include "absl/strings/match.h"
-#include "absl/strings/string_view.h"
+
+#import "Firestore/Source/API/FIRFirestoreVersion.h"
 
 namespace firebase {
 namespace firestore {
 namespace remote {
 
+using auth::CredentialsProvider;
+using auth::Token;
+using core::DatabaseInfo;
+using model::DatabaseId;
+using model::DocumentKey;
+using util::StringFormat;
+using util::internal::ExecutorLibdispatch;
+
 namespace {
+
+std::unique_ptr<util::internal::Executor> CreateExecutor() {
+  auto queue = dispatch_queue_create("com.google.firebase.firestore.datastore",
+                                     DISPATCH_QUEUE_SERIAL);
+  return absl::make_unique<ExecutorLibdispatch>(queue);
+}
 
 absl::string_view ToStringView(grpc::string_ref grpc_str) {
   return {grpc_str.begin(), grpc_str.size()};
@@ -38,6 +62,102 @@ std::string ToString(grpc::string_ref grpc_str) {
 }
 
 }  // namespace
+
+const char *const kXGoogAPIClientHeader = "x-goog-api-client";
+const char *const kGoogleCloudResourcePrefix = "google-cloud-resource-prefix";
+
+Datastore::Datastore(util::AsyncQueue *firestore_queue,
+                     const core::DatabaseInfo &database_info)
+    : firestore_queue_{firestore_queue},
+      database_info_{&database_info},
+      grpc_channel_{CreateGrpcChannel()},
+      grpc_stub_{grpc_channel_},
+      dedicated_executor_{CreateExecutor()} {
+  dedicated_executor_->Execute([this] { PollGrpcQueue(); });
+}
+
+void Datastore::Shutdown() {
+  grpc_queue_.Shutdown();
+  // Drain the executor to make sure it extracted all the operations from gRPC
+  // completion queue.
+  dedicated_executor_->ExecuteBlocking([] {});
+}
+
+void Datastore::PollGrpcQueue() {
+  HARD_ASSERT(dedicated_executor_->IsCurrentExecutor(),
+              "PollGrpcQueue should only be called on the "
+              "dedicated Datastore executor");
+
+  void *tag = nullptr;
+  bool ok = false;
+  while (grpc_queue_.Next(&tag, &ok)) {
+    auto operation = static_cast<GrpcOperation *>(tag);
+    HARD_ASSERT(tag, "GRPC queue returned a null tag");
+    operation->Complete(ok);
+  }
+}
+
+std::shared_ptr<grpc::Channel> Datastore::CreateGrpcChannel() const {
+  return grpc::CreateChannel(
+      database_info_->host(),
+      grpc::SslCredentials(grpc::SslCredentialsOptions()));
+}
+
+void Datastore::EnsureValidGrpcStub() {
+  // TODO(varconst): find out in which cases a gRPC channel might shut down.
+  // This might be overkill.
+  if (!grpc_channel_ ||
+      grpc_channel_->GetState(false) == GRPC_CHANNEL_SHUTDOWN) {
+    grpc_channel_ = CreateGrpcChannel();
+    grpc_stub_ = grpc::GenericStub{grpc_channel_};
+  }
+}
+
+std::unique_ptr<GrpcStream> Datastore::CreateGrpcStream(
+    absl::string_view token,
+    absl::string_view path,
+    GrpcStreamObserver *observer) {
+  EnsureValidGrpcStub();
+
+  auto context = CreateGrpcContext(token);
+  auto reader_writer = CreateGrpcReaderWriter(context.get(), path);
+  return absl::make_unique<GrpcStream>(
+      std::move(context), std::move(reader_writer), observer, firestore_queue_);
+}
+
+std::unique_ptr<grpc::ClientContext> Datastore::CreateGrpcContext(
+    absl::string_view token) const {
+  auto context = absl::make_unique<grpc::ClientContext>();
+  if (token.data()) {
+    context->set_credentials(grpc::AccessTokenCredentials(token.data()));
+  }
+
+  // TODO(dimond): This should ideally also include the grpc version, however,
+  // gRPC defines the version as a macro, so it would be hardcoded based on
+  // version we have at compile time of the Firestore library, rather than the
+  // version available at runtime/at compile time by the user of the library.
+  //
+  // TODO(varconst): once C++ SDK is ready, this should be "gl-cpp" or similar.
+  context->AddMetadata(
+      kXGoogAPIClientHeader,
+      StringFormat("gl-objc/ fire/%s grpc/",
+                   reinterpret_cast<const char *>(FIRFirestoreVersionString)));
+
+  // This header is used to improve routing and project isolation by the
+  // backend.
+  model::DatabaseId db_id = database_info_->database_id();
+  context->AddMetadata(kGoogleCloudResourcePrefix,
+                       StringFormat("projects/%s/databases/%s",
+                                    db_id.project_id(), db_id.database_id()));
+  return context;
+}
+
+std::unique_ptr<grpc::GenericClientAsyncReaderWriter>
+Datastore::CreateGrpcReaderWriter(grpc::ClientContext *context,
+                                  absl::string_view path) {
+  return grpc_stub_.PrepareCall(context, path.data(), &grpc_queue_);
+}
+
 util::Status Datastore::ConvertStatus(grpc::Status from) {
   if (from.ok()) {
     return {};
@@ -60,7 +180,7 @@ GrpcStream::MetadataT Datastore::ExtractWhitelistedHeaders(
   GrpcStream::MetadataT whitelisted_headers;
   for (const auto &kv : headers) {
     auto found = std::find_if(std::begin(whitelist), std::end(whitelist),
-                              [&](const std::string& str) {
+                              [&](const std::string &str) {
                                 return str.size() == kv.first.size() &&
                                        absl::StartsWithIgnoreCase(
                                            str, ToStringView(kv.first));
