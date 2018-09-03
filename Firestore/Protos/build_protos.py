ada3cf345b41279b821ac2dde2e2cecfeac8333c
Marek Gilbert <mcg@google.com>
2018-09-03 01:32:48 -0700

    Generate Objective-C proto messages.

--- Firestore/Protos/build_protos.py
+++ Firestore/Protos/build_protos.py
@@ -57,6 +57,9 @@ def main():
       '--nanopb', action='store_true',
       help='Generates nanopb messages.')
   parser.add_argument(
+      '--objc', action='store_true',
+      help='Generates Objective-C messages.')
+  parser.add_argument(
       '--protos-dir', dest='protos_dir',
       help='Source directory containing .proto files.')
   parser.add_argument(
@@ -72,6 +75,9 @@ def main():
   parser.add_argument(
       '--include', '-I', dest='include', action='append', default=[],
       help='Adds INCLUDE to the proto path.')
+  parser.add_argument(
+      '--protoc-gen-grpc', dest='protoc_gen_grpc',
+      help='Location of the gRPC generator executable.')
   parser.add_argument(
       '--protoc-gen-nanopb', dest='protoc_gen_nanopb',
       help='Location of the nanopb generator executable.')
@@ -92,6 +98,10 @@ def main():
 
   if args.nanopb:
     NanopbGenerator(args, nanopb_proto_files).run()
+
+  proto_files = remove_well_known_protos(nanopb_proto_files)
+  if args.objc:
+    ObjcProtobufGenerator(args, proto_files).run()
 
 
 class NanopbGenerator(object):
@@ -114,12 +124,7 @@ class NanopbGenerator(object):
 
   def __run_generator(self, out_dir):
     """Invokes protoc using the nanopb plugin."""
-    cmd = [self.args.protoc]
-
-    include = self.args.include
-    if include is not None:
-      for path in include:
-        cmd.append('-I%s' % path)
+    cmd = protoc_command(self.args)
 
     gen = self.args.protoc_gen_nanopb
     if gen is not None:
@@ -133,14 +138,88 @@ class NanopbGenerator(object):
     cmd.append('--nanopb_out=%s:%s' % (nanopb_flags, out_dir))
 
     cmd.extend(self.proto_files)
+    run_protoc(self.args, cmd)
 
-    kwargs={}
-    if self.args.pythonpath:
-      env = os.environ.copy()
-      env['PYTHONPATH'] = self.args.pythonpath
-      kwargs['env'] = env
 
-    subprocess.check_call(cmd, **kwargs)
+class ObjcProtobufGenerator(object):
+  """Runs protoc for Objective-C."""
+
+  def __init__(self, args, proto_files):
+    self.args = args
+    self.proto_files = proto_files
+
+  def run(self):
+    objc_out = os.path.join(self.args.output_dir, 'objc')
+    mkdir(objc_out)
+
+    self.__run_generator(objc_out)
+    self.__stub_non_buildable_files(objc_out)
+
+    sources = collect_files(objc_out, '.h', '.m')
+    post_process_files(
+        sources,
+        add_copyright,
+        strip_trailing_whitespace,
+        objc_flatten_imports,
+        objc_strip_extension_registry
+    )
+
+  def __run_generator(self, out_dir):
+    """Invokes protoc using the objc and grpc plugins."""
+    cmd = protoc_command(self.args)
+
+    gen = self.args.protoc_gen_grpc
+    if gen is not None:
+      cmd.append('--plugin=protoc-gen-grpc=%s' % gen)
+
+    cmd.extend(['--objc_out=' + out_dir, '--grpc_out=' + out_dir])
+    cmd.extend(self.proto_files)
+    run_protoc(self.args, cmd)
+
+  def __stub_non_buildable_files(self, out_dir):
+    """Stub out generated files that make no sense."""
+
+    write_file(os.path.join(out_dir, 'google/api/Annotations.pbobjc.m'), [
+        'static int annotations_stub  __attribute__((unused,used)) = 0;\n'
+    ])
+
+    write_file(os.path.join(out_dir, 'google/api/Annotations.pbobjc.h'), [
+        '// Empty stub file\n'
+    ])
+
+
+def protoc_command(args):
+  """Composes the initial protoc command-line including its include path."""
+  cmd = [args.protoc]
+  if args.include is not None:
+    cmd.extend(['-I%s' % path for path in args.include])
+  return cmd
+
+
+def run_protoc(args, cmd):
+  kwargs={}
+  if args.pythonpath:
+    env = os.environ.copy()
+    env['PYTHONPATH'] = args.pythonpath
+    kwargs['env'] = env
+
+  subprocess.check_call(cmd, **kwargs)
+
+
+def remove_well_known_protos(filenames):
+  """Remove "well-known" protos for objc and cpp.
+
+  On those platforms we get these for free as a part of the protobuf runtime.
+  We only need them for nanopb.
+
+  Args:
+    filenames: A list of filenames, each naming a .proto file.
+
+  Returns:
+    The filenames with members of google/protobuf removed.
+  """
+
+  return [f for f in filenames if 'protos/google/protobuf/' not in f]
 
 
 def post_process_files(filenames, *processors):
@@ -181,6 +260,34 @@ def nanopb_rename_delete(lines):
   """
   delete_keyword = re.compile(r'\bdelete\b')
   return [delete_keyword.sub('delete_', line) for line in lines]
+
+
+def strip_trailing_whitespace(lines):
+  """Removes trailing whitespace from the given lines."""
+  return [line.rstrip() + '\n' for line in lines]
+
+
+def objc_flatten_imports(lines):
+  """Flattens the import statements for compatibility with CocoaPods."""
+
+  long_import = re.compile(r'#import ".*/')
+  return [long_import.sub('#import "', line) for line in lines]
+
+
+def objc_strip_extension_registry(lines):
+  """Removes extensionRegistry methods from the classes."""
+
+  skip = False
+  result = []
+  for line in lines:
+    if '+ (GPBExtensionRegistry*)extensionRegistry {' in line:
+      skip = True
+    if not skip:
+      result.append(line)
+    elif line == '}\n':
+      skip = False
+
+  return result
 
 
 def collect_files(root_dir, *extensions):
