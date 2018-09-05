1bf09fa1000bc61dcee69f805b87e016287e9e6f
Konstantin Varlamov <varconst@google.com>
2018-09-05 18:17:00 -0400

    Revert "Different approach to listener"
    This reverts commit a6d5da1b3ef14bf016eeff73977d5a204ff42ef7.

--- Firestore/core/src/firebase/firestore/remote/watch_stream.h
+++ Firestore/core/src/firebase/firestore/remote/watch_stream.h
@@ -41,19 +41,6 @@ namespace firebase {
 namespace firestore {
 namespace remote {
 
-class WatchStreamListener : public StreamListener {
-  public:
-    explicit WatchStreamListener(id delegate) : delegate_bridge_{delegate} {}
-
-    void OnOpen() override;
-    void OnClose(const util::Status& status) override;
-    void OnChange(FSTWatchChange* change,
-                              const model::SnapshotVersion& snapshot_version);
-
-  private:
-    bridge::WatchStreamDelegate delegate_bridge_;
-};
-
 /**
  * A `Stream` that implements the StreamingWatch RPC.
  *
@@ -88,16 +75,16 @@ class WatchStream : public Stream {
       Datastore* datastore, absl::string_view token) override;
   void FinishGrpcStream(GrpcStream* grpc_stream) override;
 
+  void DoOnStreamStart() override;
   util::Status DoOnStreamRead(const grpc::ByteBuffer& message) override;
-  StreamListener* GetStreamListener() override { return listener_.get(); }
+  void DoOnStreamFinish(const util::Status& status) override;
 
   std::string GetDebugName() const override {
     return "WatchStream";
   }
 
-  std::unique_ptr<WatchStreamListener> listener_;
-
   bridge::WatchStreamSerializer serializer_bridge_;
+  bridge::WatchStreamDelegate delegate_bridge_;
 };
 
 }  // namespace remote
