880d1190721f7a5d1245730bd990fc764808b96c
Marek Gilbert <mcg@google.com>
2018-09-06 23:11:16 -0700

    Review feedback

--- Firestore/Protos/nanopb_cpp_generator.py
+++ Firestore/Protos/nanopb_cpp_generator.py
@@ -22,14 +22,13 @@ from __future__ import print_function
 import sys
 
 import io
-import itertools
+import nanopb_generator as nanopb
 import os
 import os.path
+import shlex
 
-import nanopb_generator as nanopb
-
-import google.protobuf.text_format as text_format
-from google.protobuf.descriptor_pb2 import DescriptorProto, EnumDescriptorProto, FieldDescriptorProto
+if sys.platform == 'win32':
+  import msvcrt  # pylint: disable=g-import-not-at-top
 
 # The plugin_pb2 package loads descriptors on import, but doesn't defend
 # against multiple imports. Reuse the plugin package as loaded by the
@@ -40,7 +39,6 @@ plugin_pb2 = nanopb.plugin_pb2
 def main():
   # Parse request
   if sys.platform == 'win32':
-    import msvcrt
     # Set stdin and stdout to binary mode
     msvcrt.setmode(sys.stdin.fileno(), os.O_BINARY)
     msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
@@ -50,20 +48,26 @@ def main():
 
   # Generate code
   options = nanopb_parse_options(request)
-  other_files = nanopb_parse_files(request, options)
-  results = nanopb_generate(request, options, other_files)
-
-  response = plugin_pb2.CodeGeneratorResponse()
-  nanopb_write(results, response)
+  parsed_files = nanopb_parse_files(request, options)
+  results = nanopb_generate(request, options, parsed_files)
+  response = nanopb_write(results)
 
   # Write to stdout
-  io.open(sys.stdout.fileno(), "wb").write(response.SerializeToString())
+  io.open(sys.stdout.fileno(), 'wb').write(response.SerializeToString())
 
 
 def nanopb_parse_options(request):
-  import shlex
+  """Parses nanopb_generator command-line options from the given request.
+
+  Args:
+    request: A CodeGeneratorRequest passed by protoc.
+
+  Returns:
+    Nanopb's options object, obtained via optparser.
+  """
+  # Parse options the same was as nanopb_generator.main_plugin() does.
   args = shlex.split(request.parameter)
-  options, dummy = nanopb.optparser.parse_args(args)
+  options, _ = nanopb.optparser.parse_args(args)
 
   # Force certain options
   options.extension = '.nanopb'
@@ -80,28 +84,70 @@ def nanopb_parse_options(request):
 
 
 def nanopb_parse_files(request, options):
+  """Parses the files in the given request into nanopb ProtoFile objects.
+
+  Args:
+    request: A CodeGeneratorRequest, as passed by protoc.
+    options: The command-line options from nanopb_parse_options.
+
+  Returns:
+    A dictionary of filename to nanopb.ProtoFile objects, each one representing
+    the parsed form of a FileDescriptor in the request.
+  """
   # Process any include files first, in order to have them
   # available as dependencies
-  other_files = {}
+  parsed_files = {}
   for fdesc in request.proto_file:
-    other_files[fdesc.name] = nanopb.parse_file(fdesc.name, fdesc, options)
+    parsed_files[fdesc.name] = nanopb.parse_file(fdesc.name, fdesc, options)
 
-  return other_files
+  return parsed_files
 
 
-def nanopb_generate(request, options, other_files):
+def nanopb_generate(request, options, parsed_files):
+  """Generates C sources from the given parsed files.
+
+  Args:
+    request: A CodeGeneratorRequest, as passed by protoc.
+    options: The command-line options from nanopb_parse_options.
+    parsed_files: A dictionary of filename to nanopb.ProtoFile, as returned by
+      nanopb_parse_files().
+
+  Returns:
+    A list of nanopb output dictionaries, each one representing the code
+    generation reuslt for each file to generate. The output dictionaries have
+    the following form:
+
+        {
+          'headername': Name of header file, ending in .h,
+          'headerdata': Contents of the header file,
+          'sourcename': Name of the source code file, ending in .c,
+          'sourcedata': Contents of the source code file
+        }
+  """
   output = []
 
   for filename in request.file_to_generate:
     for fdesc in request.proto_file:
       if fdesc.name == filename:
-        results = nanopb.process_file(filename, fdesc, options, other_files)
+        results = nanopb.process_file(filename, fdesc, options, parsed_files)
         output.append(results)
 
   return output
 
 
-def nanopb_write(results, response):
+def nanopb_write(results):
+  """Translates nanopb output dictionaries to a CodeGeneratorResponse.
+
+  Args:
+    results: A list of generated source dictionaries, as returned by
+      nanopb_generate().
+
+  Returns:
+    A CodeGeneratorResponse describing the result of the code generation
+    process to protoc.
+  """
+  response = plugin_pb2.CodeGeneratorResponse()
+
   for result in results:
     f = response.file.add()
     f.name = result['headername']
@@ -110,6 +156,8 @@ def nanopb_write(results, response):
     f = response.file.add()
     f.name = result['sourcename']
     f.content = result['sourcedata']
+
+  return response
 
 
 if __name__ == '__main__':
