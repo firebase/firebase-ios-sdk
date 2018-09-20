c4d72c9d46ce9e5ac6d999d63183336f66bd6e88
Konstantin Varlamov <varconst@google.com>
2018-09-20 14:28:43 -0400

    Minor fix

--- Firestore/core/src/firebase/firestore/remote/datastore.mm
+++ Firestore/core/src/firebase/firestore/remote/datastore.mm
@@ -182,8 +182,9 @@ void Datastore::OnLookupDocumentsResponse(
   }
 
   Status parse_status;
+  std::vector<grpc::ByteBuffer> responses = std::move(result).ValueOrDie();
   NSArray<FSTMaybeDocument *> *docs = serializer_bridge_.MergeLookupResponses(
-      result.ValueOrDie(), &parse_status);
+      responses, &parse_status);
   if (parse_status.ok()) {
     completion(docs, nil);
   } else {
