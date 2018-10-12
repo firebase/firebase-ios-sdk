36eee0dcb67ffce5c884d96170be559e9842ead7
Firebase <firebase-noreply@google.com>
2018-10-12 12:27:14 -0400

    Firestore project: Import of API interfaces.   - 216873604 fix a style (clang-format) for GitHub sync. by zxu <zxu>   - 216859139 Implement the header of SetOptions, which will be of non-... by zxu <zxu>   - 216760495 Implement the JNI part of FieldPath: by zxu <zxu>   - 216580771 Implement the header of FieldPath, which will be of non-w... by zxu <zxu>   - 215276303 Implement the JNI call part of Transaction Android wrapper. by zxu <zxu>   - 215271876 Implement the header and common part of Transaction Andro... by zxu <zxu>   - 214817327 Implement the JNI call part of WriteBatch Android wrapper. by zxu <zxu>   - 214809271 Implement the header and common part of WriteBatch Androi... by zxu <zxu>   - 214660417 Implement the JNI call part of the DocumentChange Android... by zxu <zxu>   - 214648904 Implement the header and common part of DocumentChange An... by zxu <zxu>
    ORIGINAL_AUTHOR=Firebase <firebase-noreply>
    PiperOrigin-RevId: 216873604

--- Firestore/core/include/firebase/firestore/field_path.h
+++ Firestore/core/include/firebase/firestore/field_path.h
@@ -19,19 +19,71 @@
 
 #include <string>
 
+#if !defined(_STLPORT_VERSION)
+#include <initializer_list>
+#endif  // !defined(_STLPORT_VERSION)
+
 namespace firebase {
 namespace firestore {
 
-class FieldPathInternal;
+namespace model {
+class FieldPath;
+}  // namespace model
 
 /**
  * A FieldPath refers to a field in a document. The path may consist of a single
  * field name (referring to a top level field in the document), or a list of
  * field names (referring to a nested field in the document).
  */
-// TODO(zxu123): add more methods to complete the class and make it useful.
 class FieldPath {
- private:
+ public:
+  using FieldPathInternal = ::firebase::firestore::model::FieldPath;
+
+  /**
+   * Default constructor. This creates an invalid FieldPath. Attempting to
+   * perform any operations on this path will fail (and cause a crash) unless a
+   * valid FieldPath has been assigned to it.
+   */
+  FieldPath();
+
+#if !defined(_STLPORT_VERSION)
+  /**
+   * Creates a FieldPath from the provided field names. If more than one field
+   * name is provided, the path will point to a nested field in a document.
+   *
+   * @param field_names A list of field names.
+   */
+  FieldPath(std::initializer_list<std::string> field_names);
+#endif  // !defined(_STLPORT_VERSION)
+
+  /**
+   * Copy constructor.
+   */
+  FieldPath(const FieldPath& path);
+
+  /**
+   * Move constructor.
+   */
+  FieldPath(FieldPath&& path);
+
+  virtual ~FieldPath();
+
+  /**
+   * Copy assignment operator.
+   */
+  FieldPath& operator=(const FieldPath& path);
+
+  /**
+   * Move assignment operator.
+   */
+  FieldPath& operator=(FieldPath&& path);
+
+  /**
+   * A special sentinel FieldPath to refer to the ID of a document. It can be
+   * used in queries to sort or filter by the document ID.
+   */
+  static FieldPath DocumentId();
+
   /**
    * Parses a field path string into a FieldPath, treating dots as separators.
    *
@@ -40,8 +92,25 @@ class FieldPath {
    */
   static FieldPath FromDotSeparatedString(const std::string& path);
 
+  /**
+   * Returns a string representation of this FieldPath.
+   */
+  virtual std::string ToString() const;
+
+ protected:
+  explicit FieldPath(FieldPathInternal* internal);
+
+ private:
+  friend bool operator==(const FieldPath& lhs, const FieldPath& rhs);
+  friend bool operator!=(const FieldPath& lhs, const FieldPath& rhs);
+  friend bool operator<(const FieldPath& lhs, const FieldPath& rhs);
+  friend bool operator>(const FieldPath& lhs, const FieldPath& rhs);
+  friend bool operator<=(const FieldPath& lhs, const FieldPath& rhs);
+  friend bool operator>=(const FieldPath& lhs, const FieldPath& rhs);
+
   friend class Query;
   friend class QueryInternal;
+  friend class FieldPathConverter;
 
   FieldPathInternal* internal_ = nullptr;
 };
