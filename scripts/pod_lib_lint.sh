0a1c7d85e4d4c69c44aa0e0e328258d1bfd41a5a
Paul Beusterien <paulbeusterien@google.com>
2018-10-16 14:03:22 -0700

    AnalyticsInterop 1.1.0 is public so restore pod lib lint testing

--- scripts/pod_lib_lint.sh
+++ scripts/pod_lib_lint.sh
@@ -38,7 +38,6 @@ fi
 # manually pushed to a temporary Specs repo. See
 # https://guides.cocoapods.org/making/private-cocoapods.
 #ALT_SOURCES="--sources=https://github.com/paulb777/Specs.git,https://github.com/CocoaPods/Specs.git"
-ALT_SOURCES="--sources=https://github.com/paulb777/Specs.git,https://github.com/CocoaPods/Specs.git"
 
 podspec="$1"
 if [[ $# -gt 1 ]]; then
