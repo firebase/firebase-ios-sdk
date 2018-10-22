ab6df2efb7e3ae10efc4acfeb338d55e088216d9
Konstantin Varlamov <varconst@google.com>
2018-10-22 14:53:36 -0400

    Appease linter

--- Firestore/core/src/firebase/firestore/remote/grpc_root_certificate_finder_apple.mm
+++ Firestore/core/src/firebase/firestore/remote/grpc_root_certificate_finder_apple.mm
@@ -16,6 +16,8 @@
 
 #include "Firestore/core/src/firebase/firestore/remote/grpc_root_certificate_finder.h"
 
+#include <string>
+
 #include "Firestore/core/src/firebase/firestore/util/hard_assert.h"
 #include "Firestore/core/src/firebase/firestore/util/string_apple.h"
 
