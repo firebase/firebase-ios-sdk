a858b9a4d93564a7da2913299b2dc29827a62297
Paul Beusterien <paulbeusterien@google.com>
2019-01-24 16:47:27 -0800

    Codable Timestamp

--- Firestore/Swift/Source/Codable/Timestamp+Codable.swift
+++ Firestore/Swift/Source/Codable/Timestamp+Codable.swift
@@ -17,29 +17,46 @@
 import FirebaseFirestore
 
 /**
- * A protocol describing the encodable properties of a DocumentSnapshot.
+ * A protocol describing the encodable properties of a Timestamp.
  *
  * Note: this protocol exists as a workaround for the Swift compiler: if the Timestamp class
  * was extended directly to conform to Codable, the methods implementing the protocol would be need
  * to be marked required but that can't be done in an extension. Declaring the extension on the
  * protocol sidesteps this issue.
  */
-public protocol CodableTimestamp: Codable {
-  init(date: Date)
-  func dateValue() -> Date
+fileprivate protocol CodableTimestamp: Codable {
+  var seconds: Int64 { get }
+  var nanoseconds: Int32 { get }
+
+  init(seconds: Int64, nanoseconds: Int32)
 }
 
+/** The keys in a GeoPoint. Must match the properties of CodableGeoPoint. */
+fileprivate enum TimestampKeys: String, CodingKey {
+  case seconds
+  case nanoseconds
+}
+
+/**
+ * An extension of GeoPoint that implements the behavior of the Codable protocol.
+ *
+ * Note: this is implemented manually here because the Swift compiler can't synthesize these methods
+ * when declaring an extension to conform to Codable.
+ */
 extension CodableTimestamp {
-  var date: Date { return dateValue() }
   public init(from decoder: Decoder) throws {
-    let container = try decoder.singleValueContainer()
-    self.init(date: try container.decode(Date.self))
+    let container = try decoder.container(keyedBy: TimestampKeys.self)
+    let seconds = try container.decode(Int64.self, forKey: .seconds)
+    let nanoseconds = try container.decode(Int32.self, forKey: .nanoseconds)
+    self.init(seconds: seconds, nanoseconds: nanoseconds)
   }
 
   public func encode(to encoder: Encoder) throws {
-    var container = encoder.singleValueContainer()
-    try container.encode(dateValue())
+    var container = encoder.container(keyedBy: TimestampKeys.self)
+    try container.encode(seconds, forKey: .seconds)
+    try container.encode(nanoseconds, forKey: .nanoseconds)
   }
 }
 
+/** Extends GeoPoint to conform to Codable. */
 extension Timestamp: CodableTimestamp {}
