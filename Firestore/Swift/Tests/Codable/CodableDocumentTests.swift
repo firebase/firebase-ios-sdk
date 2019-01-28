ef120f954281819b02d6c636cfc80d331a0072d8
Paul Beusterien <paulbeusterien@google.com>
2019-01-28 10:18:01 -0800

    Fix unused build warning

--- Firestore/Swift/Tests/Codable/CodableDocumentTests.swift
+++ Firestore/Swift/Tests/Codable/CodableDocumentTests.swift
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
+import Foundation
 import FirebaseFirestore
 @testable import FirebaseFirestoreSwift
-import Foundation
 import XCTest
 
 class CodableDocumentTests: XCTestCase {
