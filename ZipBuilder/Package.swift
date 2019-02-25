ce1baac64a56069251d6eabb939637b920ac947d
Ryan Wilson <wilsonryan@google.com>
2019-02-25 13:11:43 -0500

    Fully ran style.sh

--- ZipBuilder/Package.swift
+++ ZipBuilder/Package.swift
@@ -4,13 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "ZipBuilder",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", .exact("1.2.0"))
-    ],
-    targets: [
-        .target(
-            name: "ZipBuilder",
-            dependencies: ["SwiftProtobuf"]),
-    ]
+  name: "ZipBuilder",
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-protobuf.git", .exact("1.2.0")),
+  ],
+  targets: [
+    .target(
+      name: "ZipBuilder",
+      dependencies: ["SwiftProtobuf"]
+    ),
+  ]
 )
