cb56f888593f42b0075a09d00fc8365a10e81dd7
Yue Wang <wangyue@google.com>
2019-03-25 13:25:22 -0700

    Support ERROR_INVALID_PROVIDER_ID in Firebase Auth.

--- Firebase/Auth/Source/FIRAuthErrorUtils.h
+++ Firebase/Auth/Source/FIRAuthErrorUtils.h
@@ -522,6 +522,12 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (NSError *)nullUserErrorWithMessage:(nullable NSString *)message;
 
+/** @fn invalidProviderIDError
+    @brief Constructs an @c NSError with the @c FIRAuthErrorCodeInvalidProviderID code.
+    @remarks This error indicates that the provider id given for the web operation is invalid.
+ */
++ (NSError *)invalidProviderIDError;
+
 /** @fn invalidDynamicLinkDomainErrorWithMessage:
     @brief Constructs an @c NSError with the code and message provided.
     @param message Error message from the backend, if any.
