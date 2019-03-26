097477acbca0cf9d56a4282d615ddf26a4a16b37
Marek Gilbert <mcg@google.com>
2019-03-26 13:19:04 -0700

    Add support for testing Firestore macOS on Travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -281,7 +281,7 @@ case "$product-$method-$platform" in
         test
     ;;
 
-  Firestore-xcodebuild-iOS)
+  Firestore-xcodebuild-*)
     RunXcodebuild \
         -workspace 'Firestore/Example/Firestore.xcworkspace' \
         -scheme "Firestore_Tests_$platform" \
@@ -304,7 +304,6 @@ case "$product-$method-$platform" in
         -scheme "Firestore_IntegrationTests_$platform" \
         "${xcb_flags[@]}" \
         build
-
     ;;
 
   Firestore-cmake-macOS)
