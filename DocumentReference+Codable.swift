7712528693e44156ca26a43ee4cae2dd7c4e88eb
1amageek <tmy0x3@icloud.com>
2019-03-27 02:29:25 +0900

    fix DocumentReference+Codable

--- DocumentReference+Codable.swift
+++ DocumentReference+Codable.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+/**
+ * A protocol describing the encodable properties of a Timestamp.
+ *
+ * Note: this protocol exists as a workaround for the Swift compiler: if the Timestamp class
+ * was extended directly to conform to Codable, the methods implementing the protocol would be need
+ * to be marked required but that can't be done in an extension. Declaring the extension on the
+ * protocol sidesteps this issue.
+ */
+private protocol CodableDocumentReference: Codable {
+
+}
+
+
+extension CodableDocumentReference {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        _ = try container.decode(DocumentReference.self)
+        throw FirestoreDecodingError.decodingIsNotSupported
+    }
+
+    public func encode(to encoder: Encoder) throws {
+//        var container = encoder.singleValueContainer()
+    }
+}
+
+extension DocumentReference: CodableDocumentReference { }
