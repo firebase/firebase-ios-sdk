c04e60379d86505198eb86c19cea5f0a69f8672b
Michael Haney <haneym@google.com>
2019-03-28 00:32:39 -0700

    Clean up generate_cct_protos.sh
    Use git instead of curl, rename zip to tar.gz, use readonly variables and proper variable expansion

--- GoogleDataTransportCCTSupport/GoogleDataTransportCCTSupport/ProtoSupport/generate_cct_protos.sh
+++ GoogleDataTransportCCTSupport/GoogleDataTransportCCTSupport/ProtoSupport/generate_cct_protos.sh
@@ -21,30 +21,22 @@
 # Dependencies: git, protobuf, python-protobuf, pyinstaller
 
 # From https://stackoverflow.com/a/246128/361918
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Current release of nanopb being used  to build the CCT protos
-NANOPB_ZIPFILE="https://github.com/nanopb/nanopb/archive/nanopb-0.3.9.2.tar.gz"
+readonly NANOPB_VERSION="0.3.9.2"
+readonly NANOPB_TEMPDIR="${DIR}/nanopb_temp"
 
-NANOPB_TEMPDIR="$DIR/nanopb_temp"
-
-rm -rf "$NANOPB_TEMPDIR"
+rm -rf "${NANOPB_TEMPDIR}"
 
 echo "Downloading nanopb..."
-mkdir -p "$NANOPB_TEMPDIR"
-curl -o "$NANOPB_TEMPDIR/nanopb_temp.zip" $NANOPB_ZIPFILE -O -J -L
-
-echo "Unzipping nanopb..."
-tar xf "$NANOPB_TEMPDIR/nanopb_temp.zip" -C "$NANOPB_TEMPDIR/" --strip-components=1
+git clone --branch "${NANOPB_VERSION}" https://github.com/nanopb/nanopb.git "${NANOPB_TEMPDIR}"
 
 echo "Building nanopb..."
-pushd "$NANOPB_TEMPDIR"
-git init > /dev/null
-git add * > /dev/null
-git commit -a -m "Initial commit." > /dev/null
-GIT_DESCRIPTION=`git describe --always`-macosx-x86
+pushd "${NANOPB_TEMPDIR}"
 ./tools/make_mac_package.sh
-NANOPB_BIN_DIR="dist/$GIT_DESCRIPTION"
+GIT_DESCRIPTION=`git describe --always`-macosx-x86
+NANOPB_BIN_DIR="dist/${GIT_DESCRIPTION}"
 popd
 
 echo "Removing existing CCT protos..."
@@ -52,11 +44,11 @@ rm -rf "GoogleDataTransportCCTSupport/GoogleDataTransportCCTSupport/Classes/Prot
 
 
 echo "Generating CCT protos..."
-python "$DIR"/proto_generator.py \
+python "${DIR}"/proto_generator.py \
   --nanopb \
   --protos_dir=GoogleDataTransportCCTSupport/GoogleDataTransportCCTSupport/Classes/Protos/ \
-  --pythonpath="$NANOPB_TEMPDIR/$NANOPB_BIN_DIR/generator" \
+  --pythonpath="${NANOPB_TEMPDIR}/${NANOPB_BIN_DIR}/generator" \
   --output_dir=GoogleDataTransportCCTSupport/GoogleDataTransportCCTSupport/Classes/Protogen/ \
   --include=GoogleDataTransportCCTSupport/GoogleDataTransportCCTSupport/Classes/Protos/
 
-rm -rf "$NANOPB_TEMPDIR"
+rm -rf "${NANOPB_TEMPDIR}"
