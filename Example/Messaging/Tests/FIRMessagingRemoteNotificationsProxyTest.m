6f19886b8aac8e5c7c293c51a7fa0d31e8c5efef
Maksym Malyhin <mmaksym@google.com>
2019-04-01 16:11:27 -0400

    Cleanup

--- Example/Messaging/Tests/FIRMessagingRemoteNotificationsProxyTest.m
+++ Example/Messaging/Tests/FIRMessagingRemoteNotificationsProxyTest.m
@@ -136,7 +136,6 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
   _mockUserNotificationCenter = OCMClassMock([UNUserNotificationCenter class]);
   OCMStub([_mockUserNotificationCenter currentNotificationCenter]).andReturn(_mockUserNotificationCenter);
-
 #endif
 }
 
