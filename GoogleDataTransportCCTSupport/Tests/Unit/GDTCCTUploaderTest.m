ed02e7008d5fcdfe4df0f66acdf94268f9d774b1
Michael Haney <haneym@google.com>
2019-04-01 18:49:59 -0700

    Implement the CCT prioritizer

--- GoogleDataTransportCCTSupport/Tests/Unit/GDTCCTUploaderTest.m
+++ GoogleDataTransportCCTSupport/Tests/Unit/GDTCCTUploaderTest.m
@@ -16,12 +16,67 @@
 
 #import <XCTest/XCTest.h>
 
+#import "GDTCCTEventGenerator.h"
+#import "GDTCCTNanopbHelpers.h"
+#import "GDTCCTTestServer.h"
 #import "GDTCCTUploader.h"
 
 @interface GDTCCTUploaderTest : XCTestCase
 
+/** An event generator for testing. */
+@property(nonatomic) GDTCCTEventGenerator *generator;
+
+/** The local HTTP server to use for testing. */
+@property(nonatomic) GDTCCTTestServer *testServer;
+
 @end
 
 @implementation GDTCCTUploaderTest
+
+- (void)setUp {
+  self.generator = [[GDTCCTEventGenerator alloc] init];
+  self.testServer = [[GDTCCTTestServer alloc] init];
+  [self.testServer registerLogBatchPath];
+  [self.testServer start];
+  XCTAssertTrue(self.testServer.isRunning);
+}
+
+- (void)tearDown {
+  [super tearDown];
+  [self.generator deleteGeneratedFilesFromDisk];
+  [self.testServer stop];
+}
+
+- (void)testUploadGivenConditions {
+  NSArray<GDTStoredEvent *> *storedEventsA = [self.generator generateTheFiveConsistentStoredEvents];
+  NSSet<GDTStoredEvent *> *storedEvents = [NSSet setWithArray:storedEventsA];
+
+  GDTUploadPackage *package = [[GDTUploadPackage alloc] init];
+  package.events = storedEvents;
+  GDTCCTUploader *uploader = [[GDTCCTUploader alloc] init];
+  uploader.serverURL = [self.testServer.serverURL URLByAppendingPathComponent:@"logBatch"];
+  __weak id weakSelf = self;
+  XCTestExpectation *responseSentExpectation = [self expectationWithDescription:@"response sent"];
+  self.testServer.responseCompletedBlock =
+      ^(GCDWebServerRequest *_Nonnull request, GCDWebServerResponse *_Nonnull response) {
+        // Redefining the self var addresses strong self capturing in the XCTAssert macros.
+        id self = weakSelf;
+        XCTAssertNotNil(self);
+        [responseSentExpectation fulfill];
+        XCTAssertEqual(response.statusCode, 200);
+        XCTAssertTrue(response.hasBody);
+      };
+  XCTestExpectation *uploadExpectation = [self expectationWithDescription:@"upload completes"];
+  [uploader uploadPackage:package
+               onComplete:^(GDTTarget target, GDTClock *_Nonnull nextUploadAttemptUTC,
+                            NSError *_Nullable uploadError) {
+                 [uploadExpectation fulfill];
+                 XCTAssertTrue(nextUploadAttemptUTC.timeMillis > [GDTClock snapshot].timeMillis);
+               }];
+  [self waitForExpectations:@[ responseSentExpectation, uploadExpectation ] timeout:30.0];
+  dispatch_sync(uploader.uploaderQueue, ^{
+    XCTAssertNil(uploader.currentTask);
+  });
+}
 
 @end
