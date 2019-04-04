2bd1a39b4be9c58ea88899a86915f0cef159570d
Maksym Malyhin <mmaksym@google.com>
2019-04-04 18:09:37 -0400

    Cocopoads test script [WIP]

---
+++ CocoapodsIntegrationTest/integrate_and_build.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+# Copyright 2018 Google
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+# 
+function runXcodebuild() {
+  parameters=(
+    -workspace 'CocoapodsIntegrationTest.xcworkspace'
+    -scheme 'CocoapodsIntegrationTest'
+    -sdk 'iphonesimulator'
+    -destination 'platform=iOS Simulator,name=iPhone 7'
+    CODE_SIGNING_REQUIRED=NO
+    clean
+    build
+  )
+
+  echo xcodebuild "${parameters[@]}"
+  xcodebuild "${parameters[@]}" | xcpretty; result=$?
+
+  exit $result
+}
+
+# Accepts path to Gemfile
+function prepareBundle() {
+  bundle update --gemfile="$@"
+}
+
+# 
+function prepareCocoapods() {
+  bundle update
+  bundle exec pod deintegrate
+  bundle exec pod update
+}
+
+for i in "$@"
+do
+  case $i in
+    --gemfile=*)
+    GEMFILE="${i#*=}"
+    shift
+    ;;
+    --podfile=*)
+    PODFILE="${i#*=}"
+    shift
+    ;;
+    *)
+      echo "Unknown option ${i}"
+      exit -1
+    ;;
+  esac
+done
+
+if [ -z ${GEMFILE+x} ]; then
+  echo "--gemfile=<path_to_gemfile> is a required parameter"
+  exit -1
+fi
+
+if [ -z ${PODFILE+x} ]; then
+  echo "--podfile=<path_to_podfile> is a required parameter"
+  exit -1
+fi
+
+echo "Gemfile  = ${GEMFILE}"
+echo "Podfile     = ${PODFILE}"
+
+pushd "$(dirname "$0")"
+
+prepareBundle "${Gemfile}"
+prepareCocoapods "${Podfile}"
+
+popd
