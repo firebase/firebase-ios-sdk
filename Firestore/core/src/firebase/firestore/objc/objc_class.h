1b24d11450702f18b42866f00553357d1dd3e5f2
Marek Gilbert <mcg@google.com>
2019-04-09 10:48:56 -0700

    Add an OBJC_CLASS macro
    This makes it possible to forward declare Objective-C types in plain C++
    headers.

---
+++ Firestore/core/src/firebase/firestore/objc/objc_class.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_OBJC_OBJC_CLASS_H_
+#define FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_OBJC_OBJC_CLASS_H_
+
+// The OBJC_CLASS macro defines a forward declaration for an Objective-C class
+// that's compatible both with Objective-C++ and regular C++. It's useful in
+// headers that reference Objective-C types as members of C++ classes that must
+// be usable from straight C++.
+//
+// Care must be taken to not use the forward declaration (even implicitly) in
+// any inline definitions in the header. Even though Objective-C object
+// pointers look like raw pointers, under ARC they're more like
+// std::shared_ptr, where assignments and copies all potentially change the
+// refcount of the pointee. When methods that affect the reference count are
+// compiled inline in regular C++ this additional behavior won't get compiled
+// in and the Objective-C reference counts will be off.
+//
+// Note that this may even appear to work, though it does so through undefined
+// behavior: inline definitions are deduplicated at link time, and if the
+// linker happens to choose a definition that was generated in an ARC-enabled
+// translation unit then that specific build will work.
+//
+// Concretely this means that any method manipulating an Objective-C object
+// pointer (constructors, destructors, getters, and setters) all must be
+// defined out of line to avoid problems where ARC does not see changes to the
+// reference.
+#if __OBJC__
+#define OBJC_CLASS(name) @class name
+
+#else
+// Forward declaration for struct objc_object from objc/objc.h
+#define OBJC_CLASS(name) using name = struct objc_object
+
+#endif  // __OBJC__
+
+#endif  // FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_OBJC_OBJC_CLASS_H_
