0c6a343bfbf2940ae001d3122b5ec0f8927dc56d
Paul Beusterien <paulbeusterien@google.com>
2019-04-10 15:50:55 -0700

    Temporarily restore setAnalyticsCollectionEnabled

--- Firebase/Core/Private/FIRAnalyticsConfiguration.h
+++ Firebase/Core/Private/FIRAnalyticsConfiguration.h
@@ -43,6 +43,10 @@ static NSString *const kFIRAnalyticsConfigurationSetSessionTimeoutIntervalNotifi
 /// Returns the shared instance of FIRAnalyticsConfiguration.
 + (FIRAnalyticsConfiguration *)sharedInstance;
 
+// Sets whether analytics collection is enabled for this app on this device. This setting is
+// persisted across app sessions. By default it is enabled.
+- (void)setAnalyticsCollectionEnabled:(BOOL)analyticsCollectionEnabled;
+
 /// Sets whether analytics collection is enabled for this app on this device, and a flag to persist
 /// the value or not. The setting should not be persisted if being set by the global data collection
 /// flag.
