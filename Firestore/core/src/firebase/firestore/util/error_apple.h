f66764423738f3c3544725d5f3e42762ab05393d
Marek Gilbert <mcg@google.com>
2019-04-11 09:44:42 -0700

    Make error_apple definitions out-of-line

--- Firestore/core/src/firebase/firestore/util/error_apple.h
+++ Firestore/core/src/firebase/firestore/util/error_apple.h
@@ -24,8 +24,9 @@
 
 #include "Firestore/core/include/firebase/firestore/firestore_errors.h"
 #include "Firestore/core/src/firebase/firestore/util/status.h"
-#include "Firestore/core/src/firebase/firestore/util/string_apple.h"
 #include "absl/strings/string_view.h"
+
+NS_ASSUME_NONNULL_BEGIN
 
 // The Cloud Firestore error domain. Keep in sync with FIRFirestoreErrors.h.
 // Exposed here to make it possible to build in CMake without bringing in the
@@ -38,47 +39,21 @@ namespace firestore {
 namespace util {
 
 // Translates a set of error_code and error_msg to an NSError.
-inline NSError* MakeNSError(const int64_t error_code,
-                            const absl::string_view error_msg,
-                            NSError* cause = nil) {
-  if (error_code == FirestoreErrorCode::Ok) {
-    return nil;
-  }
-
-  NSMutableDictionary<NSString*, id>* user_info =
-      [NSMutableDictionary dictionary];
-  user_info[NSLocalizedDescriptionKey] = WrapNSString(error_msg);
-  if (cause) {
-    user_info[NSUnderlyingErrorKey] = cause;
-  }
-
-  return [NSError errorWithDomain:FIRFirestoreErrorDomain
-                             code:static_cast<NSInteger>(error_code)
-                         userInfo:user_info];
-}
+NSError* MakeNSError(const int64_t error_code,
+                     const absl::string_view error_msg,
+                     NSError* cause = nil);
 
 inline NSError* MakeNSError(const util::Status& status) {
   return status.ToNSError();
 }
 
-inline Status MakeStatus(NSError* error) {
-  if (!error) {
-    return Status::OK();
-  }
-
-  HARD_ASSERT(error.domain == FIRFirestoreErrorDomain,
-              "Can only translate a Firestore error to a status");
-  auto error_code = static_cast<int>(error.code);
-  HARD_ASSERT(error_code >= FirestoreErrorCode::Cancelled &&
-                  error_code <= FirestoreErrorCode::Unauthenticated,
-              "Unknown error code");
-  return Status{static_cast<FirestoreErrorCode>(error_code),
-                MakeString(error.localizedDescription)};
-}
+Status MakeStatus(NSError* error);
 
 }  // namespace util
 }  // namespace firestore
 }  // namespace firebase
+
+NS_ASSUME_NONNULL_END
 
 #endif  // __OBJC__
 
--- Firestore/core/src/firebase/firestore/util/error_apple.mm
+++ Firestore/core/src/firebase/firestore/util/error_apple.mm
@@ -16,7 +16,53 @@
 
 #include "Firestore/core/src/firebase/firestore/util/error_apple.h"
 
-// NB: This is also defined in Firestore/Source/Public/FIRFirestoreErrors.h
+#include "Firestore/core/src/firebase/firestore/util/hard_assert.h"
+#include "Firestore/core/src/firebase/firestore/util/string_apple.h"
+
+// NB: This is also declared in Firestore/Source/Public/FIRFirestoreErrors.h
 // NOLINTNEXTLINE: public constant
 FOUNDATION_EXPORT NSString* const FIRFirestoreErrorDomain =
     @"FIRFirestoreErrorDomain";
+
+namespace firebase {
+namespace firestore {
+namespace util {
+
+// Translates a set of error_code and error_msg to an NSError.
+NSError* MakeNSError(const int64_t error_code,
+                     const absl::string_view error_msg,
+                     NSError* cause) {
+  if (error_code == FirestoreErrorCode::Ok) {
+    return nil;
+  }
+
+  NSMutableDictionary<NSString*, id>* user_info =
+      [NSMutableDictionary dictionary];
+  user_info[NSLocalizedDescriptionKey] = WrapNSString(error_msg);
+  if (cause) {
+    user_info[NSUnderlyingErrorKey] = cause;
+  }
+
+  return [NSError errorWithDomain:FIRFirestoreErrorDomain
+                             code:static_cast<NSInteger>(error_code)
+                         userInfo:user_info];
+}
+
+Status MakeStatus(NSError* error) {
+  if (!error) {
+    return Status::OK();
+  }
+
+  HARD_ASSERT(error.domain == FIRFirestoreErrorDomain,
+              "Can only translate a Firestore error to a status");
+  auto error_code = static_cast<int>(error.code);
+  HARD_ASSERT(error_code >= FirestoreErrorCode::Cancelled &&
+                  error_code <= FirestoreErrorCode::Unauthenticated,
+              "Unknown error code");
+  return Status{static_cast<FirestoreErrorCode>(error_code),
+                MakeString(error.localizedDescription)};
+}
+
+}  // namespace util
+}  // namespace firestore
+}  // namespace firebase
