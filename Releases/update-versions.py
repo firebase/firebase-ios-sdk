9d4d433b8b8f13db6cbff5c63888d7ac34104363
Paul Beusterien <paulbeusterien@google.com>
2019-04-11 17:26:52 -0700

    Skip tests on pod repo push

--- Releases/update-versions.py
+++ Releases/update-versions.py
@@ -205,8 +205,8 @@ def PushPodspecs(version_data):
     podspec = '{}.podspec'.format(pod)
     json = os.path.join(tmp_dir, '{}.json'.format(podspec))
     LogOrRun('pod ipc spec {} > {}'.format(podspec, json))
-    LogOrRun('pod repo push {} {}{}'.format(GetCpdcInternal(), json,
-                                            warnings_ok))
+    LogOrRun('pod repo push --skip-tests {} {}{}'.format(GetCpdcInternal(),
+                                                         json, warnings_ok))
   os.system('rm -rf {}'.format(tmp_dir))
 
 
