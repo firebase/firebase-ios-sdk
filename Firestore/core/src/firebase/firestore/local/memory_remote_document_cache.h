4c3fa1e04909c97d2d058ea58d8d0407208030fe
Michael Lehenbauer <mikelehen@google.com>
2019-04-17 15:36:26 -0700

    Set DerivePointerAlignment=false for Firestore cpp files.

--- Firestore/core/src/firebase/firestore/local/memory_remote_document_cache.h
+++ Firestore/core/src/firebase/firestore/local/memory_remote_document_cache.h
@@ -43,27 +43,27 @@ namespace local {
 
 class MemoryRemoteDocumentCache : public RemoteDocumentCache {
  public:
-  explicit MemoryRemoteDocumentCache(FSTMemoryPersistence *persistence);
+  explicit MemoryRemoteDocumentCache(FSTMemoryPersistence* persistence);
 
-  void Add(FSTMaybeDocument *document) override;
-  void Remove(const model::DocumentKey &key) override;
+  void Add(FSTMaybeDocument* document) override;
+  void Remove(const model::DocumentKey& key) override;
 
-  FSTMaybeDocument *_Nullable Get(const model::DocumentKey &key) override;
-  model::MaybeDocumentMap GetAll(const model::DocumentKeySet &keys) override;
-  model::DocumentMap GetMatching(FSTQuery *query) override;
+  FSTMaybeDocument* _Nullable Get(const model::DocumentKey& key) override;
+  model::MaybeDocumentMap GetAll(const model::DocumentKeySet& keys) override;
+  model::DocumentMap GetMatching(FSTQuery* query) override;
 
   std::vector<model::DocumentKey> RemoveOrphanedDocuments(
-      FSTMemoryLRUReferenceDelegate *reference_delegate,
+      FSTMemoryLRUReferenceDelegate* reference_delegate,
       model::ListenSequenceNumber upper_bound);
 
-  size_t CalculateByteSize(FSTLocalSerializer *serializer);
+  size_t CalculateByteSize(FSTLocalSerializer* serializer);
 
  private:
   /** Underlying cache of documents. */
   model::MaybeDocumentMap docs_;
 
   // This instance is owned by FSTMemoryPersistence; avoid a retain cycle.
-  __weak FSTMemoryPersistence *persistence_;
+  __weak FSTMemoryPersistence* persistence_;
 };
 
 }  // namespace local
