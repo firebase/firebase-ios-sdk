c33e28fe5751e74ebdc2cced1f224b045586cab0
Maksym Malyhin <mmaksym@google.com>
2019-04-17 10:01:42 -0400

    Notification Service extension for Messaging_Example_iOS app added to test FirebaseMessaging compatibility with extensions

---
+++ Example/Messaging/App/iOS/NotificationService/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  Messaging_NotificationService_iOS
+//
+//  Created by Maksym Malyhin on 2019-04-17.
+//  Copyright © 2019 Google. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
---
+++ Example/Messaging/App/iOS/NotificationService/NotificationService.m
@@ -0,0 +1,36 @@
+//
+//  NotificationService.m
+//  Messaging_NotificationService_iOS
+//
+//  Created by Maksym Malyhin on 2019-04-17.
+//  Copyright © 2019 Google. All rights reserved.
+//
+
+#import "NotificationService.h"
+
+@interface NotificationService ()
+
+@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
+@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+
+@end
+
+@implementation NotificationService
+
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+    self.contentHandler = contentHandler;
+    self.bestAttemptContent = [request.content mutableCopy];
+    
+    // Modify the notification content here...
+    self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ [modified]", self.bestAttemptContent.title];
+    
+    self.contentHandler(self.bestAttemptContent);
+}
+
+- (void)serviceExtensionTimeWillExpire {
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+  self.contentHandler(self.bestAttemptContent);
+}
+
+@end
