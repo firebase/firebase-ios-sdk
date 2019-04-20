cbd0bbcd3063b157e65c6fc11adf1519a6bd26f1
Michael Haney <mikehaney24@users.noreply.github.com>
2019-04-19 17:23:29 -0700

    Prefix the Library and Tests directories with GDT and GDTCCT (#2870)
    * Prefix the Library and Tests directories with GDT and GDTCCT
    
    This is to prevent overzealous rules in common_import_transformations.bara.sky from changing the libs' imports incorrectly.
    
    * Fix the check_whitespace exemption for GDTCCT
    
    * Actually fix the whitespace check this time
    
    * Fix actual trailing whitespace

--- scripts/check_whitespace.sh
+++ scripts/check_whitespace.sh
@@ -30,7 +30,7 @@ git grep "${options[@]}" -- \
     ':(exclude)Firestore/Protos/cpp' \
     ':(exclude)Firestore/Protos/objc' \
     ':(exclude)Firestore/third_party/abseil-cpp' \
-    ':(exclude)GoogleDataTransportCCTSupport/Library/Protogen/nanopb' \
+    ':(exclude)GoogleDataTransportCCTSupport/GDTCCTLibrary/Protogen/nanopb' \
     ':(exclude)GoogleDataTransportCCTSupport/ProtoSupport'
 
 if [[ $? == 0 ]]; then
