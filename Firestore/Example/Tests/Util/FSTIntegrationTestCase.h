570f4465de39574104f7a3c38f360f6699f58b50
Brian Chen <chenbrian@google.com>
2019-05-15 09:55:18 -0700

    Use shared_ptr for AsyncQueue and Executor (#2996)

--- Firestore/Example/Tests/Util/FSTIntegrationTestCase.h
+++ Firestore/Example/Tests/Util/FSTIntegrationTestCase.h
@@ -17,6 +17,8 @@
 #import <Foundation/Foundation.h>
 #import <XCTest/XCTest.h>
 
+#include <memory>
+
 #import "Firestore/Example/Tests/Util/XCTestCase+Await.h"
 
 #import "FIRFirestoreSource.h"
@@ -31,6 +33,8 @@
 @class FIRFirestoreSettings;
 @class FIRQuery;
 @class FSTEventAccumulator;
+
+namespace util = firebase::firestore::util;
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -101,7 +105,7 @@ extern "C" {
 
 - (void)enableNetwork;
 
-- (firebase::firestore::util::AsyncQueue *)queueForFirestore:(FIRFirestore *)firestore;
+- (const std::shared_ptr<util::AsyncQueue> &)queueForFirestore:(FIRFirestore *)firestore;
 
 /**
  * "Blocks" the current thread/run loop until the block returns YES.
@@ -113,6 +117,7 @@ extern "C" {
 
 @property(nonatomic, strong) FIRFirestore *db;
 @property(nonatomic, strong) FSTEventAccumulator *eventAccumulator;
+@property(nonatomic, strong) NSMutableArray<FIRFirestore *> *firestores;
 @end
 
 /** Converts the FIRQuerySnapshot to an NSArray containing the data of the documents in order. */
