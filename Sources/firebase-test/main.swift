696357c047bfcf3a2687c4c8af552d9427b49f69
Ryan Wilson <wilsonryan@google.com>
2019-05-17 09:46:45 -0400

    Beginning work to support Swift Package Manager.
    The folder structure is a little different, and still working out some
    Objective-C module issues but this seems to be the right approach for
    now.

---
+++ Sources/firebase-test/main.swift
@@ -0,0 +1,12 @@
+import Foundation
+import GoogleUtilities_Environment
+import GoogleUtilities_Logger
+
+print("Hello world!")
+print("Is app store receipt sandbox? Answer: \(GULAppEnvironmentUtil.isAppStoreReceiptSandbox())")
+print("Is from app store? Answer: \(GULAppEnvironmentUtil.isFromAppStore())")
+print("Is this the simulator? Answer: \(GULAppEnvironmentUtil.isSimulator())")
+print("Device model? Answer: \(GULAppEnvironmentUtil.deviceModel() ?? "NONE")")
+print("System version? Answer: \(GULAppEnvironmentUtil.systemVersion() ?? "NONE")")
+print("Is App extension? Answer: \(GULAppEnvironmentUtil.isAppExtension())")
+print("Is iOS 7 or higher? Answer: \(GULAppEnvironmentUtil.isIOS7OrHigher())")
