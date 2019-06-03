a9a412d24bf10ee11625d3310d70fbb95e187a78
Maksym Malyhin <mmaksym@google.com>
2019-06-03 15:22:07 -0400

    FIRInstallations.h - API definition

--- FirebaseInstallations/Source/Public/FIRInstallations.h
+++ FirebaseInstallations/Source/Public/FIRInstallations.h
@@ -16,9 +16,33 @@
 
 #import <Foundation/Foundation.h>
 
+// TODO: Replace by import.
+@class FIRAuthTokenResult;
+
+@class FIRApp;
+
 NS_ASSUME_NONNULL_BEGIN
 
+typedef void (^FIRInstallationsIDHandler)(NSString *__nullable identifier,
+                                          NSError *__nullable error)
+    NS_SWIFT_NAME(InstallationsIDHandler);
+
+typedef void (^FIRInstallationsTokenHandler)(FIRAuthTokenResult *__nullable tokenResult,
+                                             NSError *__nullable error)
+    NS_SWIFT_NAME(InstallationsTokenHandler);
+
+typedef void (^FIRInstallationsDeleteHandler)(NSError *__nullable error)
+    NS_SWIFT_NAME(InstallationsDeleteHandler);
+
 @interface FIRInstallations : NSObject
+
++ (FIRInstallations *)installationsWithApp:(FIRApp *)application;
+
+- (void)installationIDWithCompletion:(FIRInstallationsIDHandler)handler;
+
+- (void)authTokenWithCompletion:(FIRInstallationsTokenHandler)handler;
+
+- (void)deleteWithCompletion:(FIRInstallationsDeleteHandler)handler;
 
 @end
 
