bd2fba869451449bb977a617f849d674d790dc97
Maksym Malyhin <mmaksym@google.com>
2019-06-06 10:57:34 -0400

    Tests TODO update

--- FirebaseInstallations/Source/Tests/FIRInstallationsStoreTests.m
+++ FirebaseInstallations/Source/Tests/FIRInstallationsStoreTests.m
@@ -41,7 +41,8 @@
   self.store = [[FIRInstallationsStore alloc] initWithSecureStorage:self.mockSecureStorage
                                                         accessGroup:self.accessGroup];
 
-  // TODO: Replace real user defaults by an injected mock.
+  // TODO: Replace real user defaults by an injected mock or a test specific user defaults instance
+  // with a specific suite name.
   self.userDefaults =
       [[GULUserDefaults alloc] initWithSuiteName:kFIRInstallationsStoreUserDefaultsID];
 }
