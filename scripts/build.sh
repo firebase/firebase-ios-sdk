daab8e3d116bfb43def3109bc85764ee76cd94a2
Paul Beusterien <paulbeusterien@google.com>
2019-06-05 20:33:21 -0700

    Add FirebaseAuth test_spec (#3123)

--- scripts/build.sh
+++ scripts/build.sh
@@ -213,6 +213,25 @@ case "$product-$method-$platform" in
     fi
     ;;
 
+  Auth-xcodebuild-*)
+    if [[ "$TRAVIS_PULL_REQUEST" == "false" ||
+          "$TRAVIS_PULL_REQUEST_SLUG" == "$TRAVIS_REPO_SLUG" ]]; then
+      RunXcodebuild \
+        -workspace 'Example/Auth/AuthSample/AuthSample.xcworkspace' \
+        -scheme "Auth_ApiTests" \
+        "${xcb_flags[@]}" \
+        build \
+        test
+
+      RunXcodebuild \
+        -workspace 'Example/Auth/AuthSample/AuthSample.xcworkspace' \
+        -scheme "Auth_E2eTests" \
+        "${xcb_flags[@]}" \
+        build \
+        test
+    fi
+    ;;
+
   InAppMessaging-xcodebuild-iOS)
     RunXcodebuild \
         -workspace 'InAppMessaging/Example/InAppMessaging-Example-iOS.xcworkspace'  \
