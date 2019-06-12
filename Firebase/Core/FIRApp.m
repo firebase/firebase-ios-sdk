e3e0cc582fea9ccc3ca59fae126f2f723529d038
Michael Haney <haneym@google.com>
2019-06-12 12:31:59 -0700

    Put back some deleted symbols

--- Firebase/Core/FIRApp.m
+++ Firebase/Core/FIRApp.m
@@ -58,7 +58,10 @@ NSString *const kFIRGlobalAppDataCollectionEnabledDefaultsKeyFormat =
 NSString *const kFIRGlobalAppDataCollectionEnabledPlistKey =
     @"FirebaseDataCollectionDefaultEnabled";
 
+NSString *const kFIRAppDiagnosticsNotification = @"FIRAppDiagnosticsNotification";
+
 NSString *const kFIRAppDiagnosticsConfigurationTypeKey = @"ConfigType";
+NSString *const kFIRAppDiagnosticsErrorKey = @"Error";
 NSString *const kFIRAppDiagnosticsFIRAppKey = @"FIRApp";
 NSString *const kFIRAppDiagnosticsSDKNameKey = @"SDKName";
 NSString *const kFIRAppDiagnosticsSDKVersionKey = @"SDKVersion";
