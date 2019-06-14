da5abb1f7c70445d01706a3775d67e57cc905cd6
Hui Wu <wuandy@google.com>
2019-06-14 06:07:29 -0400

    FirestoreEncoder/Decoder renamed from stdlib JSONEncoder/Decoder.

--- Firestore/Swift/Source/Codable/third_party/FirestoreDecoder.swift
+++ Firestore/Swift/Source/Codable/third_party/FirestoreDecoder.swift
@@ -1,17 +1,4 @@
 //===----------------------------------------------------------------------===//
-//
-// This source file is part of the Swift.org open source project
-//
-// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-//
-//===----------------------------------------------------------------------===//
-
-
-//===----------------------------------------------------------------------===//
 // JSON Decoder
 //===----------------------------------------------------------------------===//
 
@@ -1494,35 +1481,4 @@ extension __JSONDecoder {
             return try type.init(from: self)
         }
     }
-}
-
-//===----------------------------------------------------------------------===//
-// Shared Key Types
-//===----------------------------------------------------------------------===//
-
-fileprivate struct _JSONKey : CodingKey {
-    public var stringValue: String
-    public var intValue: Int?
-
-    public init?(stringValue: String) {
-        self.stringValue = stringValue
-        self.intValue = nil
-    }
-
-    public init?(intValue: Int) {
-        self.stringValue = "\(intValue)"
-        self.intValue = intValue
-    }
-
-    public init(stringValue: String, intValue: Int?) {
-        self.stringValue = stringValue
-        self.intValue = intValue
-    }
-
-    fileprivate init(index: Int) {
-        self.stringValue = "Index \(index)"
-        self.intValue = index
-    }
-
-    fileprivate static let `super` = _JSONKey(stringValue: "super")!
 }
--- Firestore/Swift/Source/Codable/third_party/FirestoreEncoder.swift
+++ Firestore/Swift/Source/Codable/third_party/FirestoreEncoder.swift
@@ -1010,6 +1010,36 @@ fileprivate class __JSONReferencingEncoder : __JSONEncoder {
     }
 }
 
+//===----------------------------------------------------------------------===//
+// Shared Key Types
+//===----------------------------------------------------------------------===//
+
+fileprivate struct _JSONKey : CodingKey {
+    public var stringValue: String
+    public var intValue: Int?
+
+    public init?(stringValue: String) {
+        self.stringValue = stringValue
+        self.intValue = nil
+    }
+
+    public init?(intValue: Int) {
+        self.stringValue = "\(intValue)"
+        self.intValue = intValue
+    }
+
+    public init(stringValue: String, intValue: Int?) {
+        self.stringValue = stringValue
+        self.intValue = intValue
+    }
+
+    fileprivate init(index: Int) {
+        self.stringValue = "Index \(index)"
+        self.intValue = index
+    }
+
+    fileprivate static let `super` = _JSONKey(stringValue: "super")!
+}
 
 //===----------------------------------------------------------------------===//
 // Shared ISO8601 Date Formatter
