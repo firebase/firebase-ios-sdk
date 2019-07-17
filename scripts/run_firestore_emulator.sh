a4322d34b9d48ff70ea42ff249328f70ae76d1ed
Marek Gilbert <mcg@google.com>
2019-07-17 13:56:56 -0700

    Upgrade to Firestore Emulator 1.6.2
    This just brings us up-to-date without any functional changes as far as
    the SDK is concerned.

--- scripts/run_firestore_emulator.sh
+++ scripts/run_firestore_emulator.sh
@@ -20,7 +20,7 @@
 
 set -euo pipefail
 
-VERSION='1.6.1'
+VERSION='1.6.2'
 FILENAME="cloud-firestore-emulator-v${VERSION}.jar"
 URL="https://storage.googleapis.com/firebase-preview-drop/emulator/${FILENAME}"
 
