3b0ed0ee4af4e47712fe759feaec1343c08d1e2e
Paul Beusterien <paulbeusterien@google.com>
2019-08-05 19:30:54 -0700

    Open Source ABTesting (#3507)
    * CI for ABTesting
    * README update

---
+++ FirebaseABTesting/README.md
@@ -0,0 +1,11 @@
+# Firebase ABTesting SDK for iOS
+
+A/B testing is a Firebase service that lets you run experiments across users of
+your iOS and Android apps. It lets you learn how well one or more changes to
+your app work with a smaller set of users before you roll out changes to all
+users. You can run experiments to find the most effective ways to use
+Firebase Cloud Messaging and Firebase Remote Config in your app.
+
+Please visit [our developer site]
+(https://firebase.google.com/docs/ab-testing/) for integration instructions,
+documentations, support information, and terms of service.
---
+++ FirebaseABTesting/Tests/Unit/ABTFakeFIRAConditionalUserPropertyController.h
@@ -0,0 +1,46 @@
+// Copyright 2019 Google
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import <Foundation/Foundation.h>
+
+#ifdef COCOAPODS
+#import <FirebaseAnalyticsInterop/FIRAnalyticsInterop.h>
+#else
+#import "third_party/firebase/ios/Releases/FirebaseInterop/Analytics/Public/FIRAnalyticsInterop.h"
+#endif
+
+@class FIRAConditionalUserProperty;
+@class FIRAEvent;
+
+/// Fake Firebase Analytics Conditional User Property Controller Class.
+/// This is a lightweight class to test experiments set and clean, events logging in unit tests.
+@interface ABTFakeFIRAConditionalUserPropertyController : NSObject
+
+/// Returns the FIRAConditionalUserPropertyController singleton.
++ (instancetype)sharedInstance;
+- (void)setConditionalUserProperty:(NSDictionary<NSString *, id> *)conditionalUserProperty;
+- (void)clearConditionalUserPropertyWithName:(NSString *)conditionalUserPropertyName;
+- (NSArray<FIRAConditionalUserProperty *> *)
+    conditionalUserPropertiesWithNamePrefix:(NSString *)namePrefix
+                             filterByOrigin:(NSString *)origin;
+- (NSInteger)maxUserPropertiesForOrigin:(NSString *)origin;
+- (void)resetExperiments;
+@end
+
+@interface FakeAnalytics : NSObject <FIRAnalyticsInterop> {
+  ABTFakeFIRAConditionalUserPropertyController *_fakeController;
+}
+- (instancetype)initWithFakeController:
+    (ABTFakeFIRAConditionalUserPropertyController *)fakeController;
+@end
