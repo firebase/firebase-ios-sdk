93afb2057b6f8255854c928cde89343bbac9b93d
Marek Gilbert <mcg@google.com>
2019-08-23 16:14:14 -0700

    Port ViewDocumentChanges to C++

---
+++ Firestore/core/src/firebase/firestore/core/view.cc
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Firestore/core/src/firebase/firestore/core/view.h"
+
+#include <utility>
+
+namespace firebase {
+namespace firestore {
+namespace core {
+
+// MARK: - ViewDocumentChanges
+
+ViewDocumentChanges::ViewDocumentChanges(model::DocumentSet new_documents,
+                                         DocumentViewChangeSet changes,
+                                         model::DocumentKeySet mutated_keys,
+                                         bool needs_refill)
+    : document_set_(std::move(new_documents)),
+      change_set_(std::move(changes)),
+      mutated_keys_(std::move(mutated_keys)),
+      needs_refill_(needs_refill) {
+}
+
+}  // namespace core
+}  // namespace firestore
+}  // namespace firebase
---
+++ Firestore/core/src/firebase/firestore/core/view.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2019 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_CORE_VIEW_H_
+#define FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_CORE_VIEW_H_
+
+#include "Firestore/core/src/firebase/firestore/core/view_snapshot.h"
+#include "Firestore/core/src/firebase/firestore/model/document_key_set.h"
+#include "Firestore/core/src/firebase/firestore/model/document_set.h"
+
+namespace firebase {
+namespace firestore {
+namespace core {
+
+/** The result of applying a set of doc changes to a view. */
+class ViewDocumentChanges {
+ public:
+  ViewDocumentChanges(model::DocumentSet new_documents,
+                      DocumentViewChangeSet changes,
+                      model::DocumentKeySet mutated_keys,
+                      bool needs_refill);
+
+  /** The new set of docs that should be in the view. */
+  const model::DocumentSet& document_set() const {
+    return document_set_;
+  }
+
+  /** The diff of this these docs with the previous set of docs. */
+  const core::DocumentViewChangeSet& change_set() const {
+    return change_set_;
+  }
+
+  const model::DocumentKeySet& mutated_keys() const {
+    return mutated_keys_;
+  }
+
+  /**
+   * Whether the set of documents passed in was not sufficient to calculate the
+   * new state of the view and there needs to be another pass based on the local
+   * cache.
+   */
+  bool needs_refill() const {
+    return needs_refill_;
+  }
+
+ private:
+  model::DocumentSet document_set_;
+  core::DocumentViewChangeSet change_set_;
+  model::DocumentKeySet mutated_keys_;
+  bool needs_refill_ = false;
+};
+
+}  // namespace core
+}  // namespace firestore
+}  // namespace firebase
+
+#endif  // FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_CORE_VIEW_H_
