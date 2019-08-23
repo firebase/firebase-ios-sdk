b0de7d748c8fd181c00ae62f77566c343bc7b44c
Paul Beusterien <paulbeusterien@google.com>
2019-08-23 10:47:26 -0700

    style

--- FirebaseRemoteConfig/Tests/Unit/RCNConfigTest.m
+++ FirebaseRemoteConfig/Tests/Unit/RCNConfigTest.m
@@ -20,13 +20,13 @@
 
 #import <FirebaseInstanceID/FIRInstanceID+Private.h>
 #import <FirebaseInstanceID/FIRInstanceIDCheckinPreferences.h>
+#import <OCMock/OCMock.h>
 #import "FirebaseRemoteConfig/Sources/RCNConfigConstants.h"
 #import "FirebaseRemoteConfig/Sources/RCNConfigContent.h"
 #import "FirebaseRemoteConfig/Sources/RCNConfigExperiment.h"
 #import "FirebaseRemoteConfig/Sources/RCNConfigFetch.h"
 #import "FirebaseRemoteConfig/Sources/RCNConfigValue_Internal.h"
 #import "FirebaseRemoteConfig/Tests/Unit/RCNTestUtilities.h"
-#import <OCMock/OCMock.h>
 
 static NSString *const RCNFakeSenderID = @"855865492447";
 static NSString *const RCNFakeToken = @"ctToAh17Exk:"
