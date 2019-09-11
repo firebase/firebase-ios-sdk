e2da82af6b54e61b87a4200591f7e72271014564
Mandar <dmandar@users.noreply.github.com>
2019-09-10 22:03:20 -0700

    Fix style.

--- FirebaseRemoteConfig/Tests/Unit/RCNConfigDBManagerTest.m
+++ FirebaseRemoteConfig/Tests/Unit/RCNConfigDBManagerTest.m
@@ -20,11 +20,11 @@
 
 #import <FirebaseCore/FIRAppInternal.h>
 #import <OCMock/OCMock.h>
+#import "FirebaseRemoteConfig/Sources/Private/RCNConfigSettings.h"
 #import "FirebaseRemoteConfig/Sources/RCNConfigConstants.h"
 #import "FirebaseRemoteConfig/Sources/RCNConfigContent.h"
 #import "FirebaseRemoteConfig/Sources/RCNConfigDBManager.h"
 #import "FirebaseRemoteConfig/Sources/RCNConfigDefines.h"
-#import "FirebaseRemoteConfig/Sources/Private/RCNConfigSettings.h"
 #import "FirebaseRemoteConfig/Tests/Unit/RCNTestUtilities.h"
 
 @interface RCNConfigDBManager (Test)
