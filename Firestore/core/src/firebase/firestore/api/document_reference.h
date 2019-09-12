e63534cb1779ba4636f4157414315f3b45f96e0a
Brian Chen <chenbrian@google.com>
2019-09-12 14:42:17 -0700

    fix style

--- Firestore/core/src/firebase/firestore/api/document_reference.h
+++ Firestore/core/src/firebase/firestore/api/document_reference.h
@@ -28,8 +28,8 @@
 #include "Firestore/core/src/firebase/firestore/model/resource_path.h"
 #include "Firestore/core/src/firebase/firestore/util/nullability.h"
 #include "Firestore/core/src/firebase/firestore/util/status.h"
-#include "Firestore/core/src/firebase/firestore/util/statusor_callback.h"
 #include "Firestore/core/src/firebase/firestore/util/status_fwd.h"
+#include "Firestore/core/src/firebase/firestore/util/statusor_callback.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
