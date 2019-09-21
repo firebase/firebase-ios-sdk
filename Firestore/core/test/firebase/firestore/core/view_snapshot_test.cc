2cf6cc08ed8b889410a1efb97ab91481c36aab70
Marek Gilbert <mcg@google.com>
2019-09-21 10:59:39 -0700

    view_snapshot_test: automated changes

--- Firestore/core/test/firebase/firestore/core/view_snapshot_test.cc
+++ Firestore/core/test/firebase/firestore/core/view_snapshot_test.cc
@@ -14,126 +14,129 @@
  * limitations under the License.
  */
 
-#import <XCTest/XCTest.h>
-
 #include <vector>
 
 #include "Firestore/core/src/firebase/firestore/core/view_snapshot.h"
 #include "Firestore/core/src/firebase/firestore/model/document_set.h"
 #include "Firestore/core/test/firebase/firestore/testutil/testutil.h"
+#include "gtest/gtest.h"
 
-namespace core = firebase::firestore::core;
-namespace testutil = firebase::firestore::testutil;
-using firebase::firestore::core::DocumentViewChange;
-using firebase::firestore::core::DocumentViewChangeSet;
-using firebase::firestore::core::ViewSnapshot;
-using firebase::firestore::model::Document;
-using firebase::firestore::model::DocumentComparator;
-using firebase::firestore::model::DocumentKeySet;
-using firebase::firestore::model::DocumentSet;
-using firebase::firestore::model::DocumentState;
-using firebase::firestore::testutil::Query;
+namespace firebase {
+namespace firestore {
+namespace core {
+
+using model::Document;
+using model::DocumentComparator;
+using model::DocumentKeySet;
+using model::DocumentSet;
+using model::DocumentState;
+using testutil::Query;
 
 using testutil::Doc;
 using testutil::Map;
 
-NS_ASSUME_NONNULL_BEGIN
-
-@interface FSTViewSnapshotTests : XCTestCase
-@end
-
-@implementation FSTViewSnapshotTests
-
-- (void)testDocumentChangeConstructor {
+TEST(ViewSnapshotTest, DocumentChangeConstructor) {
   Document doc = Doc("a/b", 0, Map());
   DocumentViewChange::Type type = DocumentViewChange::Type::Modified;
   DocumentViewChange change{doc, type};
-  XCTAssertEqual(change.document(), doc);
-  XCTAssertEqual(change.type(), type);
+  ASSERT_EQ(change.document(), doc);
+  ASSERT_EQ(change.type(), type);
 }
 
-- (void)testTrack {
+TEST(ViewSnapshotTest, Track) {
   DocumentViewChangeSet set;
 
-  Document docAdded = Doc("a/1", 0, Map());
-  Document docRemoved = Doc("a/2", 0, Map());
-  Document docModified = Doc("a/3", 0, Map());
+  Document doc_added = Doc("a/1", 0, Map());
+  Document doc_removed = Doc("a/2", 0, Map());
+  Document doc_modified = Doc("a/3", 0, Map());
 
-  Document docAddedThenModified = Doc("b/1", 0, Map());
-  Document docAddedThenRemoved = Doc("b/2", 0, Map());
-  Document docRemovedThenAdded = Doc("b/3", 0, Map());
-  Document docModifiedThenRemoved = Doc("b/4", 0, Map());
-  Document docModifiedThenModified = Doc("b/5", 0, Map());
+  Document doc_added_then_modified = Doc("b/1", 0, Map());
+  Document doc_added_then_removed = Doc("b/2", 0, Map());
+  Document doc_removed_then_added = Doc("b/3", 0, Map());
+  Document doc_modified_then_removed = Doc("b/4", 0, Map());
+  Document doc_modified_then_modified = Doc("b/5", 0, Map());
 
-  set.AddChange(DocumentViewChange{docAdded, DocumentViewChange::Type::Added});
-  set.AddChange(DocumentViewChange{docRemoved, DocumentViewChange::Type::Removed});
-  set.AddChange(DocumentViewChange{docModified, DocumentViewChange::Type::Modified});
-  set.AddChange(DocumentViewChange{docAddedThenModified, DocumentViewChange::Type::Added});
-  set.AddChange(DocumentViewChange{docAddedThenModified, DocumentViewChange::Type::Modified});
-  set.AddChange(DocumentViewChange{docAddedThenRemoved, DocumentViewChange::Type::Added});
-  set.AddChange(DocumentViewChange{docAddedThenRemoved, DocumentViewChange::Type::Removed});
-  set.AddChange(DocumentViewChange{docRemovedThenAdded, DocumentViewChange::Type::Removed});
-  set.AddChange(DocumentViewChange{docRemovedThenAdded, DocumentViewChange::Type::Added});
-  set.AddChange(DocumentViewChange{docModifiedThenRemoved, DocumentViewChange::Type::Modified});
-  set.AddChange(DocumentViewChange{docModifiedThenRemoved, DocumentViewChange::Type::Removed});
-  set.AddChange(DocumentViewChange{docModifiedThenModified, DocumentViewChange::Type::Modified});
-  set.AddChange(DocumentViewChange{docModifiedThenModified, DocumentViewChange::Type::Modified});
+  set.AddChange(DocumentViewChange{doc_added, DocumentViewChange::Type::Added});
+  set.AddChange(
+      DocumentViewChange{doc_removed, DocumentViewChange::Type::Removed});
+  set.AddChange(
+      DocumentViewChange{doc_modified, DocumentViewChange::Type::Modified});
+  set.AddChange(DocumentViewChange{doc_added_then_modified,
+                                   DocumentViewChange::Type::Added});
+  set.AddChange(DocumentViewChange{doc_added_then_modified,
+                                   DocumentViewChange::Type::Modified});
+  set.AddChange(DocumentViewChange{doc_added_then_removed,
+                                   DocumentViewChange::Type::Added});
+  set.AddChange(DocumentViewChange{doc_added_then_removed,
+                                   DocumentViewChange::Type::Removed});
+  set.AddChange(DocumentViewChange{doc_removed_then_added,
+                                   DocumentViewChange::Type::Removed});
+  set.AddChange(DocumentViewChange{doc_removed_then_added,
+                                   DocumentViewChange::Type::Added});
+  set.AddChange(DocumentViewChange{doc_modified_then_removed,
+                                   DocumentViewChange::Type::Modified});
+  set.AddChange(DocumentViewChange{doc_modified_then_removed,
+                                   DocumentViewChange::Type::Removed});
+  set.AddChange(DocumentViewChange{doc_modified_then_modified,
+                                   DocumentViewChange::Type::Modified});
+  set.AddChange(DocumentViewChange{doc_modified_then_modified,
+                                   DocumentViewChange::Type::Modified});
 
   std::vector<DocumentViewChange> changes = set.GetChanges();
-  XCTAssertEqual(changes.size(), 7);
+  ASSERT_EQ(changes.size(), 7);
 
-  XCTAssertEqual(changes[0].document(), docAdded);
-  XCTAssertEqual(changes[0].type(), DocumentViewChange::Type::Added);
+  ASSERT_EQ(changes[0].document(), doc_added);
+  ASSERT_EQ(changes[0].type(), DocumentViewChange::Type::Added);
 
-  XCTAssertEqual(changes[1].document(), docRemoved);
-  XCTAssertEqual(changes[1].type(), DocumentViewChange::Type::Removed);
+  ASSERT_EQ(changes[1].document(), doc_removed);
+  ASSERT_EQ(changes[1].type(), DocumentViewChange::Type::Removed);
 
-  XCTAssertEqual(changes[2].document(), docModified);
-  XCTAssertEqual(changes[2].type(), DocumentViewChange::Type::Modified);
+  ASSERT_EQ(changes[2].document(), doc_modified);
+  ASSERT_EQ(changes[2].type(), DocumentViewChange::Type::Modified);
 
-  XCTAssertEqual(changes[3].document(), docAddedThenModified);
-  XCTAssertEqual(changes[3].type(), DocumentViewChange::Type::Added);
+  ASSERT_EQ(changes[3].document(), doc_added_then_modified);
+  ASSERT_EQ(changes[3].type(), DocumentViewChange::Type::Added);
 
-  XCTAssertEqual(changes[4].document(), docRemovedThenAdded);
-  XCTAssertEqual(changes[4].type(), DocumentViewChange::Type::Modified);
+  ASSERT_EQ(changes[4].document(), doc_removed_then_added);
+  ASSERT_EQ(changes[4].type(), DocumentViewChange::Type::Modified);
 
-  XCTAssertEqual(changes[5].document(), docModifiedThenRemoved);
-  XCTAssertEqual(changes[5].type(), DocumentViewChange::Type::Removed);
+  ASSERT_EQ(changes[5].document(), doc_modified_then_removed);
+  ASSERT_EQ(changes[5].type(), DocumentViewChange::Type::Removed);
 
-  XCTAssertEqual(changes[6].document(), docModifiedThenModified);
-  XCTAssertEqual(changes[6].type(), DocumentViewChange::Type::Modified);
+  ASSERT_EQ(changes[6].document(), doc_modified_then_modified);
+  ASSERT_EQ(changes[6].type(), DocumentViewChange::Type::Modified);
 }
 
-- (void)testViewSnapshotConstructor {
+TEST(ViewSnapshotTest, ViewSnapshotConstructor) {
   core::Query query = Query("a");
   DocumentSet documents = DocumentSet{DocumentComparator::ByKey()};
-  DocumentSet oldDocuments = documents;
+  DocumentSet old_documents = documents;
   documents = documents.insert(Doc("c/a", 1, Map()));
-  std::vector<DocumentViewChange> documentChanges{
-      DocumentViewChange{Doc("c/a", 1, Map()), DocumentViewChange::Type::Added}};
+  std::vector<DocumentViewChange> document_changes{DocumentViewChange{
+      Doc("c/a", 1, Map()), DocumentViewChange::Type::Added}};
 
-  bool fromCache = true;
-  DocumentKeySet mutatedKeys;
-  bool syncStateChanged = true;
+  bool from_cache = true;
+  DocumentKeySet mutated_keys;
+  bool sync_state_changed = true;
 
   ViewSnapshot snapshot{query,
                         documents,
-                        oldDocuments,
-                        documentChanges,
-                        mutatedKeys,
-                        fromCache,
-                        syncStateChanged,
+                        old_documents,
+                        document_changes,
+                        mutated_keys,
+                        from_cache,
+                        sync_state_changed,
                         /*excludes_metadata_changes=*/false};
 
-  XCTAssertEqual(snapshot.query(), query);
-  XCTAssertEqual(snapshot.documents(), documents);
-  XCTAssertEqual(snapshot.old_documents(), oldDocuments);
-  XCTAssertEqual(snapshot.document_changes(), documentChanges);
-  XCTAssertEqual(snapshot.from_cache(), fromCache);
-  XCTAssertEqual(snapshot.mutated_keys(), mutatedKeys);
-  XCTAssertEqual(snapshot.sync_state_changed(), syncStateChanged);
+  ASSERT_EQ(snapshot.query(), query);
+  ASSERT_EQ(snapshot.documents(), documents);
+  ASSERT_EQ(snapshot.old_documents(), old_documents);
+  ASSERT_EQ(snapshot.document_changes(), document_changes);
+  ASSERT_EQ(snapshot.from_cache(), from_cache);
+  ASSERT_EQ(snapshot.mutated_keys(), mutated_keys);
+  ASSERT_EQ(snapshot.sync_state_changed(), sync_state_changed);
 }
 
-@end
-
-NS_ASSUME_NONNULL_END
+}  // namespace core
+}  // namespace firestore
+}  // namespace firebase
