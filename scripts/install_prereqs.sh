2b313d4e34db91faacf436547ab4da4b5bdbe1cc
Paul Beusterien <paulbeusterien@google.com>
2019-09-30 14:06:48 -0700

    Fabc

--- scripts/install_prereqs.sh
+++ scripts/install_prereqs.sh
@@ -88,7 +88,7 @@ case "$PROJECT-$PLATFORM-$METHOD" in
     install_secrets
     ;;
 
-  FIAM-iOS-xcodebuild)
+  Fabc-iOS-xcodebuild)
     gem install xcpretty
     bundle exec pod install --project-directory=InAppMessagingDisplay/Example --repo-update
     bundle exec pod install --project-directory=InAppMessaging/Example --repo-update
