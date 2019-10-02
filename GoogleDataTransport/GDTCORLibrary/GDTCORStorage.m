635175449791595bae2cda0a3fe0f5292d064758
Ryan Wilson <wilsonryan@google.com>
2019-10-01 22:23:41 -0400

    Clarify comment for starting the background tasks.

--- GoogleDataTransport/GDTCORLibrary/GDTCORStorage.m
+++ GoogleDataTransport/GDTCORLibrary/GDTCORStorage.m
@@ -227,7 +227,8 @@ static NSString *GDTCORStoragePath() {
 
 - (void)appWillBackground:(GDTCORApplication *)app {
   dispatch_async(_storageQueue, ^{
-    // Create an immediate background task to run until the end of the current queue of work.
+    // Immediately request a background task to run until the end of the current queue of work, and
+    // cancel it once the work is done.
     __block GDTCORBackgroundIdentifier bgID =
         [app beginBackgroundTaskWithName:@"GDTStorage"
                        expirationHandler:^{
