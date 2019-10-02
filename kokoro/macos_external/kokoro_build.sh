8d2d40556c8db5001ea2f4a545b1e3ff56c5ec7b
Morgan Chen <morganchen12@gmail.com>
2019-10-02 11:57:10 -0700

    Add kokoro files

---
+++ kokoro/macos_external/kokoro_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+# Display commands being run.
+set -x
+
+
+# Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
+# The final directory name in this path is determined by the scm name specified
+# in the job configuration.
+cd ${KOKORO_ARTIFACTS_DIR}/github/firebase-ios-sdk
+bundle install
+./test.sh
