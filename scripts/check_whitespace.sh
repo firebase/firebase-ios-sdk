99b56a7acbd12ff375987ac129b69f481b3c67f4
Paul Beusterien <paulbeusterien@google.com>
2019-10-07 15:51:42 -0700

    allow trailing whitespace in NOTICES

--- scripts/check_whitespace.sh
+++ scripts/check_whitespace.sh
@@ -28,6 +28,7 @@ options=(
 git grep "${options[@]}" -- \
     ':(exclude)Firebase/CoreDiagnostics/FIRCDLibrary/Protogen/nanopb' \
     ':(exclude)Firebase/CoreDiagnostics/ProtoSupport' \
+    ':(exclude)Firebase/Firebase/NOTICES' \
     ':(exclude)Firebase/InAppMessaging/ProtoSupport' \
     ':(exclude)Firebase/InAppMessaging/Analytics/Protogen/nanopb' \
     ':(exclude)Firestore/Protos/nanopb' \
