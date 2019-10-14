ded8ec1bfaffbac97cb64d3acb5b11c79c459bd1
Chuan Ren <renkelvin@gmail.com>
2019-10-14 16:42:46 -0700

    Initial changes

--- Firebase/Auth/Source/AuthProvider/OAuth/FIROAuthProvider.m
+++ Firebase/Auth/Source/AuthProvider/OAuth/FIROAuthProvider.m
@@ -71,6 +71,7 @@ static NSString *const kAuthTypeSignInWithRedirect = @"signInWithRedirect";
                                     accessToken:(nullable NSString *)accessToken {
   return [[FIROAuthCredential alloc] initWithProviderID:providerID
                                                 IDToken:IDToken
+                                               rawNonce:nil
                                             accessToken:accessToken
                                                  secret:nil
                                            pendingToken:nil];
@@ -80,7 +81,31 @@ static NSString *const kAuthTypeSignInWithRedirect = @"signInWithRedirect";
                                      accessToken:(NSString *)accessToken {
   return [[FIROAuthCredential alloc] initWithProviderID:providerID
                                                 IDToken:nil
+                                               rawNonce:nil
                                             accessToken:accessToken
+                                                 secret:nil
+                                           pendingToken:nil];
+}
+
++ (FIROAuthCredential *)credentialWithProviderID:(NSString *)providerID
+                                         IDToken:(NSString *)IDToken
+                                        rawNonce:(nullable NSString *)rawNonce
+                                     accessToken:(nullable NSString *)accessToken {
+  return [[FIROAuthCredential alloc] initWithProviderID:providerID
+                                                IDToken:IDToken
+                                               rawNonce:rawNonce
+                                            accessToken:accessToken
+                                                 secret:nil
+                                           pendingToken:nil];
+}
+
++ (FIROAuthCredential *)credentialWithProviderID:(NSString *)providerID
+                                         IDToken:(NSString *)IDToken
+                                        rawNonce:(nullable NSString *)rawNonce {
+  return [[FIROAuthCredential alloc] initWithProviderID:providerID
+                                                IDToken:IDToken
+                                               rawNonce:rawNonce
+                                            accessToken:nil
                                                  secret:nil
                                            pendingToken:nil];
 }
