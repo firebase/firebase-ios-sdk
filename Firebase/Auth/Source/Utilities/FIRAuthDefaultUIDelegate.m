e62d05b4f1bfbc49448e6ea3f081dea3d76f6471
Chuan Ren <renkelvin@gmail.com>
2019-10-16 17:04:33 -0700

    Remove !@available notation (#4105)

--- Firebase/Auth/Source/Utilities/FIRAuthDefaultUIDelegate.m
+++ Firebase/Auth/Source/Utilities/FIRAuthDefaultUIDelegate.m
@@ -86,27 +86,13 @@ NS_ASSUME_NONNULL_BEGIN
         }
       }
     }
-  }
-#elif __TV_OS_VERSION_MAX_ALLOWED >= 130000
-  if (@available(iOS 13.0, tvOS 13.0, *)) {
-    UIApplication *application = [applicationClass sharedApplication];
-    NSSet<UIScene *> * connectedScenes = application.connectedScenes;
-    for (UIScene *scene in connectedScenes) {
-      if ([scene isKindOfClass:[UIWindowScene class]]) {
-        UIWindowScene *windowScene = (UIWindowScene *)scene;
-        for (UIWindow *window in windowScene.windows) {
-          if (window.isKeyWindow) {
-            topViewController = window.rootViewController;
-          }
-        }
-      }
-    }
-  }
-#else
-  if (!@available(iOS 13.0, tvOS 13.0, *)) {
+  } else {
     UIApplication *application = [applicationClass sharedApplication];
     topViewController = application.keyWindow.rootViewController;
   }
+#else
+  UIApplication *application = [applicationClass sharedApplication];
+  topViewController = application.keyWindow.rootViewController;
 #endif
 
   while (true){
