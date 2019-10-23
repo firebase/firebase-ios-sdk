18d4bb7bc21de0307096705664c8f56b318e7744
Maksym Malyhin <mmaksym@google.com>
2019-10-23 15:41:47 -0400

    ./scripts/style.sh

--- FirebaseInstallations/Source/Tests/Unit/FIRInstallationsSingleOperationPromiseCacheTests.m
+++ FirebaseInstallations/Source/Tests/Unit/FIRInstallationsSingleOperationPromiseCacheTests.m
@@ -1,24 +1,24 @@
 /*
-* Copyright 2019 Google
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2019 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import <XCTest/XCTest.h>
 
-#import "FBLPromise+Testing.h"
-#import "FBLPromise+Delay.h"
 #import "FBLPromise+Await.h"
+#import "FBLPromise+Delay.h"
+#import "FBLPromise+Testing.h"
 
 #import "FIRInstallationsSingleOperationPromiseCache.h"
 
@@ -37,10 +37,12 @@
 }
 
 - (void)assertRaceConditionWithParallelOperationCount:(NSInteger)count {
-  FIRInstallationsSingleOperationPromiseCache *promiseCache = [[FIRInstallationsSingleOperationPromiseCache alloc] initWithNewOperationHandler:^FBLPromise * _Nonnull {
-    [NSThread sleepForTimeInterval:0.001];
-    return [[FBLPromise resolvedWith:[[NSObject alloc] init]] delay:0.001];
-  }];
+  FIRInstallationsSingleOperationPromiseCache *promiseCache =
+      [[FIRInstallationsSingleOperationPromiseCache alloc]
+          initWithNewOperationHandler:^FBLPromise *_Nonnull {
+            [NSThread sleepForTimeInterval:0.001];
+            return [[FBLPromise resolvedWith:[[NSObject alloc] init]] delay:0.001];
+          }];
 
   XCTestExpectation *expectation = [self expectationWithDescription:@""];
   expectation.expectedFulfillmentCount = count;
@@ -60,7 +62,7 @@
   XCTAssert(FBLWaitForPromisesWithTimeout(10));
   XCTAssertNil([promiseCache getExistingPendingPromise]);
 
-  [self waitForExpectations:@[expectation] timeout:10];
+  [self waitForExpectations:@[ expectation ] timeout:10];
 }
 
 @end
