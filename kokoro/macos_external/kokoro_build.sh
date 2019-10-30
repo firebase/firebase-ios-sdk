7cc66bb4f60b6b86c03cb9a96de16e884e935abd
Morgan Chen <morganchen12@gmail.com>
2019-10-30 10:02:48 -0700

    add export LC_CTYPE=en_US.UTF-8

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -5,6 +5,7 @@ set -e
 # Display commands being run.
 set -x
 
+export LC_CTYPE=en_US.UTF-8
 
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
 # The final directory name in this path is determined by the scm name specified
