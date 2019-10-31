767323eb24068084b92a56e9c7f6141fa7e02e0f
Morgan Chen <morganchen12@gmail.com>
2019-10-31 16:05:00 -0700

    Add correct folder structure and better docs

---
+++ kokoro/macos_external/continuous.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+# Display commands being run.
+set -x
+
+# Placeholder script to test GitHub webhooks. Eventually this script
+# should be replaced by one job per test target.
+
+exit 0
