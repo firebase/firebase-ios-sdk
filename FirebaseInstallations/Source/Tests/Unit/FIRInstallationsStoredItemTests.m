76b47085c907935da3ab7f97bcedc9453a8ba31a
Maksym Malyhin <mmaksym@google.com>
2019-11-04 11:45:17 -0500

    FIS: Use IID auth token to authorize IID migration. (#4224)
    * `FIRInstallationsIIDTokenStore` introduced. `FIRInstallationsIIDCheckinStore` removed.
    
    * FIRInstallationsIIDTokenStoreTests
    
    * Run ./scripts/style.sh
    
    * FIRInstallationsIIDTokenStore implementation.
    
    * ./scripts/style.sh
    
    * Fix tvOS and macOS availability.
    
    * Rename IIDAuthToken -> IIDDefaultToken
    
    * FIRInstallationsIIDTokenStoreTests: fix flaky IID token removal
    
    * ./scripts/style.sh

--- FirebaseInstallations/Source/Tests/Unit/FIRInstallationsStoredItemTests.m
+++ FirebaseInstallations/Source/Tests/Unit/FIRInstallationsStoredItemTests.m
@@ -19,7 +19,6 @@
 #import "FIRKeyedArchivingUtils.h"
 
 #import "FIRInstallationsStoredAuthToken.h"
-#import "FIRInstallationsStoredIIDCheckin.h"
 #import "FIRInstallationsStoredItem.h"
 
 @interface FIRInstallationsStoredItemTests : XCTestCase
@@ -39,7 +38,7 @@
   item.refreshToken = @"refresh-token";
   item.authToken = authToken;
   item.registrationStatus = FIRInstallationStatusRegistered;
-  item.IIDCheckin = [self createIIDCheckin];
+  item.IIDDefaultToken = @"IIDTokenValue";
 
   NSError *error;
   NSData *archivedItem = [FIRKeyedArchivingUtils archivedDataWithRootObject:item error:&error];
@@ -56,13 +55,7 @@
   XCTAssertEqualObjects(unarchivedItem.authToken.token, item.authToken.token);
   XCTAssertEqualObjects(unarchivedItem.authToken.expirationDate, item.authToken.expirationDate);
   XCTAssertEqual(unarchivedItem.registrationStatus, item.registrationStatus);
-  XCTAssertEqualObjects(unarchivedItem.IIDCheckin.deviceID, item.IIDCheckin.deviceID);
-  XCTAssertEqualObjects(unarchivedItem.IIDCheckin.secretToken, item.IIDCheckin.secretToken);
-}
-
-- (FIRInstallationsStoredIIDCheckin *)createIIDCheckin {
-  return [[FIRInstallationsStoredIIDCheckin alloc] initWithDeviceID:@"IIDDeviceID"
-                                                        secretToken:@"IIDSecretToken"];
+  XCTAssertEqualObjects(unarchivedItem.IIDDefaultToken, item.IIDDefaultToken);
 }
 
 @end
