b938074af866dd3b388de93f6742dd56c3fa5652
Marek Gilbert <mcg@google.com>
2019-11-03 19:34:27 -0600

    Rewrite FSTLevelDBTransactionTests in C++

--- Firestore/core/test/firebase/firestore/local/leveldb_transaction_test.cc
+++ Firestore/core/test/firebase/firestore/local/leveldb_transaction_test.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Google
+ * Copyright 2019 Google
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,15 +14,10 @@
  * limitations under the License.
  */
 
-#import <XCTest/XCTest.h>
+#include "Firestore/core/src/firebase/firestore/local/leveldb_transaction.h"
 
 #include <memory>
 #include <string>
-
-// This is out of order to satisfy the linter, which doesn't realize this is
-// the header corresponding to this test.
-// TODO(wilhuff): move this to the top once the test filename matches
-#include "Firestore/core/src/firebase/firestore/local/leveldb_transaction.h"
 
 #include "Firestore/Protos/nanopb/firestore/local/mutation.nanopb.h"
 #include "Firestore/Protos/nanopb/firestore/local/target.nanopb.h"
@@ -32,76 +27,72 @@
 #include "Firestore/core/src/firebase/firestore/nanopb/reader.h"
 #include "Firestore/core/src/firebase/firestore/util/path.h"
 #include "Firestore/core/test/firebase/firestore/local/persistence_testing.h"
+#include "Firestore/core/test/firebase/firestore/util/status_testing.h"
 #include "absl/strings/string_view.h"
+#include "gtest/gtest.h"
 #include "leveldb/db.h"
 
-NS_ASSUME_NONNULL_BEGIN
+namespace firebase {
+namespace firestore {
+namespace local {
 
-using firebase::firestore::firestore_client_Target;
-using firebase::firestore::firestore_client_WriteBatch;
-using firebase::firestore::local::LevelDbDir;
-using firebase::firestore::local::LevelDbMutationKey;
-using firebase::firestore::local::LevelDbTransaction;
-using firebase::firestore::nanopb::ByteString;
-using firebase::firestore::nanopb::Message;
-using firebase::firestore::nanopb::StringReader;
-using firebase::firestore::util::Path;
 using leveldb::DB;
 using leveldb::Options;
 using leveldb::ReadOptions;
 using leveldb::Status;
 using leveldb::WriteOptions;
+using nanopb::ByteString;
+using nanopb::Message;
+using nanopb::StringReader;
+using util::Path;
 
-@interface FSTLevelDBTransactionTests : XCTestCase
-@end
+class LevelDbTransactionTest : public testing::Test {
+ protected:
+  void SetUp() override;
 
-@implementation FSTLevelDBTransactionTests {
-  std::unique_ptr<DB> _db;
-}
+  std::unique_ptr<DB> db_;
+};
 
-- (void)setUp {
+void LevelDbTransactionTest::SetUp() {
   Options options;
   options.error_if_exists = true;
   options.create_if_missing = true;
 
   Path dir = LevelDbDir();
-  DB *db;
+  DB* db = nullptr;
   Status status = DB::Open(options, dir.ToUtf8String(), &db);
-  XCTAssert(status.ok(), @"Failed to create db: %s", status.ToString().c_str());
-  _db.reset(db);
+  ASSERT_TRUE(status.ok()) << "Failed to create db: "
+                           << status.ToString().c_str();
+  db_.reset(db);
 }
 
-- (void)tearDown {
-  _db.reset();
-}
-
-- (void)testCreateTransaction {
-  LevelDbTransaction transaction(_db.get(), "testCreateTransaction");
+TEST_F(LevelDbTransactionTest, CreateTransaction) {
+  LevelDbTransaction transaction(db_.get(), "CreateTransaction");
   std::string key = "key1";
 
   transaction.Put(key, "value");
   auto iter = transaction.NewIterator();
   iter->Seek(key);
-  XCTAssertEqual(key, iter->key());
+  ASSERT_EQ(key, iter->key());
   iter->Next();
-  XCTAssertFalse(iter->Valid());
+  ASSERT_FALSE(iter->Valid());
 }
 
-- (void)testCanReadCommittedAndMutations {
+TEST_F(LevelDbTransactionTest, CanReadCommittedAndMutations) {
   const std::string committed_key1 = "c_key1";
   const std::string committed_value1 = "c_value1";
-  const WriteOptions &writeOptions = LevelDbTransaction::DefaultWriteOptions();
+  const WriteOptions& write_options = LevelDbTransaction::DefaultWriteOptions();
   // add two things committed, mutate one, add another mutation
   // verify you can get the original committed, the mutation, and the addition
-  Status status = _db->Put(writeOptions, committed_key1, committed_value1);
-  XCTAssertTrue(status.ok());
+  Status status = db_->Put(write_options, committed_key1, committed_value1);
+  ASSERT_TRUE(status.ok());
 
   const std::string committed_key2 = "c_key2";
   const std::string committed_value2 = "c_value2";
-  status = _db->Put(writeOptions, committed_key2, committed_value2);
-  XCTAssertTrue(status.ok());
+  status = db_->Put(write_options, committed_key2, committed_value2);
+  ASSERT_TRUE(status.ok());
 
-  LevelDbTransaction transaction(_db.get(), "testCanReadCommittedAndMutations");
+  LevelDbTransaction transaction(db_.get(), "CanReadCommittedAndMutations");
   const std::string mutation_key1 = "m_key1";
   const std::string mutation_value1 = "m_value1";
   transaction.Put(mutation_key1, mutation_value1);
@@ -112,120 +103,123 @@ using leveldb::WriteOptions;
 
   std::string value;
   status = transaction.Get(committed_key1, &value);
-  XCTAssertTrue(status.ok());
-  XCTAssertEqual(value, committed_value1);
+  ASSERT_TRUE(status.ok());
+  ASSERT_EQ(value, committed_value1);
 
   status = transaction.Get(mutation_key1, &value);
-  XCTAssertTrue(status.ok());
-  XCTAssertEqual(value, mutation_value1);
+  ASSERT_TRUE(status.ok());
+  ASSERT_EQ(value, mutation_value1);
 
   status = transaction.Get(committed_key2, &value);
-  XCTAssertTrue(status.ok());
-  XCTAssertEqual(value, mutation_value2);
+  ASSERT_TRUE(status.ok());
+  ASSERT_EQ(value, mutation_value2);
 }
 
-- (void)testDeleteCommitted {
+TEST_F(LevelDbTransactionTest, DeleteCommitted) {
   // add something committed, delete it, verify you can't read it
   for (int i = 0; i < 3; ++i) {
-    Status status = _db->Put(LevelDbTransaction::DefaultWriteOptions(), "key_" + std::to_string(i),
-                             "value_" + std::to_string(i));
-    XCTAssertTrue(status.ok());
+    Status status =
+        db_->Put(LevelDbTransaction::DefaultWriteOptions(),
+                 "key_" + std::to_string(i), "value_" + std::to_string(i));
+    ASSERT_TRUE(status.ok());
   }
-  LevelDbTransaction transaction(_db.get(), "testDeleteCommitted");
+  LevelDbTransaction transaction(db_.get(), "DeleteCommitted");
   transaction.Put("key_1", "new_value");
   std::string value;
   Status status = transaction.Get("key_1", &value);
-  XCTAssertTrue(status.ok());
-  XCTAssertEqual(value, "new_value");
+  ASSERT_TRUE(status.ok());
+  ASSERT_EQ(value, "new_value");
 
   transaction.Delete("key_1");
   status = transaction.Get("key_1", &value);
-  XCTAssertTrue(status.IsNotFound());
+  ASSERT_TRUE(status.IsNotFound());
 
   LevelDbTransaction::Iterator iter(&transaction);
   iter.Seek("");
-  XCTAssertEqual(iter.key(), "key_0");
+  ASSERT_EQ(iter.key(), "key_0");
   iter.Next();
-  XCTAssertEqual(iter.key(), "key_2");
+  ASSERT_EQ(iter.key(), "key_2");
   iter.Next();
-  XCTAssertFalse(iter.Valid());
+  ASSERT_FALSE(iter.Valid());
 }
 
-- (void)testMutateDeleted {
+TEST_F(LevelDbTransactionTest, MutateDeleted) {
   // delete something, then mutate it, then read it.
   // Also include an actual deletion
   for (int i = 0; i < 4; ++i) {
-    Status status = _db->Put(LevelDbTransaction::DefaultWriteOptions(), "key_" + std::to_string(i),
-                             "value_" + std::to_string(i));
-    XCTAssertTrue(status.ok());
+    Status status =
+        db_->Put(LevelDbTransaction::DefaultWriteOptions(),
+                 "key_" + std::to_string(i), "value_" + std::to_string(i));
+    ASSERT_TRUE(status.ok());
   }
   std::string value;
-  LevelDbTransaction transaction(_db.get(), "testMutateDeleted");
+  LevelDbTransaction transaction(db_.get(), "MutateDeleted");
   transaction.Delete("key_1");
   Status status = transaction.Get("key_1", &value);
-  XCTAssertTrue(status.IsNotFound());
+  ASSERT_TRUE(status.IsNotFound());
 
   transaction.Put("key_1", "new_value");
   status = transaction.Get("key_1", &value);
-  XCTAssertTrue(status.ok());
-  XCTAssertEqual(value, "new_value");
+  ASSERT_TRUE(status.ok());
+  ASSERT_EQ(value, "new_value");
 
   transaction.Delete("key_3");
 
   LevelDbTransaction::Iterator iter(&transaction);
   iter.Seek("");
-  XCTAssertEqual(iter.key(), "key_0");
+  ASSERT_EQ(iter.key(), "key_0");
   iter.Next();
-  XCTAssertEqual(iter.key(), "key_1");
-  XCTAssertEqual(iter.value(), "new_value");
+  ASSERT_EQ(iter.key(), "key_1");
+  ASSERT_EQ(iter.value(), "new_value");
   iter.Next();
-  XCTAssertEqual(iter.key(), "key_2");
+  ASSERT_EQ(iter.key(), "key_2");
   iter.Next();
-  XCTAssertFalse(iter.Valid());
+  ASSERT_FALSE(iter.Valid());
 
   // Commit, then check underlying db.
   transaction.Commit();
 
-  const ReadOptions &readOptions = LevelDbTransaction::DefaultReadOptions();
-  status = _db->Get(readOptions, "key_0", &value);
-  XCTAssertTrue(status.ok());
-  XCTAssertEqual("value_0", value);
+  const ReadOptions& read_options = LevelDbTransaction::DefaultReadOptions();
+  status = db_->Get(read_options, "key_0", &value);
+  ASSERT_TRUE(status.ok());
+  ASSERT_EQ("value_0", value);
 
-  status = _db->Get(readOptions, "key_1", &value);
-  XCTAssertTrue(status.ok());
-  XCTAssertEqual("new_value", value);
+  status = db_->Get(read_options, "key_1", &value);
+  ASSERT_TRUE(status.ok());
+  ASSERT_EQ("new_value", value);
 
-  status = _db->Get(readOptions, "key_2", &value);
-  XCTAssertTrue(status.ok());
-  XCTAssertEqual("value_2", value);
+  status = db_->Get(read_options, "key_2", &value);
+  ASSERT_TRUE(status.ok());
+  ASSERT_EQ("value_2", value);
 
-  status = _db->Get(readOptions, "key_3", &value);
-  XCTAssertTrue(status.IsNotFound());
+  status = db_->Get(read_options, "key_3", &value);
+  ASSERT_TRUE(status.IsNotFound());
 }
 
-- (void)testProtobufSupport {
-  LevelDbTransaction transaction(_db.get(), "testProtobufSupport");
+TEST_F(LevelDbTransactionTest, ProtobufSupport) {
+  LevelDbTransaction transaction(db_.get(), "ProtobufSupport");
 
   Message<firestore_client_Target> target;
   target->target_id = 1;
   target->last_listen_sequence_number = 2;
 
-  std::string key("theKey");
+  std::string key("the_key");
   transaction.Put(key, target);
 
   std::string value;
-  Status status = transaction.Get("theKey", &value);
+  Status status = transaction.Get("the_key", &value);
 
   ByteString bytes{value};
   StringReader reader{bytes};
   auto parsed = Message<firestore_client_Target>::TryParse(&reader);
-  XCTAssertTrue(reader.ok());
-  XCTAssertEqual(target->target_id, parsed->target_id);
-  XCTAssertEqual(target->last_listen_sequence_number, parsed->last_listen_sequence_number);
+  ASSERT_TRUE(reader.ok());
+  ASSERT_EQ(target->target_id, parsed->target_id);
+  ASSERT_EQ(target->last_listen_sequence_number,
+            parsed->last_listen_sequence_number);
 }
 
-- (void)testCanIterateAndDelete {
-  LevelDbTransaction transaction(_db.get(), "testCanIterateAndDelete");
+TEST_F(LevelDbTransactionTest, CanIterateAndDelete) {
+  LevelDbTransaction transaction(db_.get(), "CanIterateAndDelete");
 
   for (int i = 0; i < 4; ++i) {
     transaction.Put("key_" + std::to_string(i), "value_" + std::to_string(i));
@@ -234,85 +228,92 @@ using leveldb::WriteOptions;
   auto it = transaction.NewIterator();
   it->Seek("key_0");
   for (int i = 0; i < 4; ++i) {
-    XCTAssertTrue(it->Valid());
+    ASSERT_TRUE(it->Valid());
     absl::string_view key = it->key();
     std::string expected = "key_" + std::to_string(i);
-    XCTAssertEqual(expected, key);
+    ASSERT_EQ(expected, key);
     transaction.Delete(key);
     it->Next();
   }
 }
 
-- (void)testCanIterateFromDeletionToCommitted {
+TEST_F(LevelDbTransactionTest, CanIterateFromDeletionToCommitted) {
   // Write keys key_0 and key_1
   for (int i = 0; i < 2; ++i) {
-    Status status = _db->Put(LevelDbTransaction::DefaultWriteOptions(), "key_" + std::to_string(i),
-                             "value_" + std::to_string(i));
-    XCTAssertTrue(status.ok());
+    Status status =
+        db_->Put(LevelDbTransaction::DefaultWriteOptions(),
+                 "key_" + std::to_string(i), "value_" + std::to_string(i));
+    ASSERT_TRUE(status.ok());
   }
 
-  // Create a transaction, iterate, deleting key_0. Verify we still iterate key_1.
-  LevelDbTransaction transaction(_db.get(), "testCanIterateFromDeletionToCommitted");
+  // Create a transaction, iterate, deleting key_0. Verify we still iterate
+  // key_1.
+  LevelDbTransaction transaction(db_.get(),
+                                 "CanIterateFromDeletionToCommitted");
   auto it = transaction.NewIterator();
   it->Seek("key_0");
-  XCTAssertTrue(it->Valid());
-  XCTAssertEqual("key_0", it->key());
+  ASSERT_TRUE(it->Valid());
+  ASSERT_EQ("key_0", it->key());
   transaction.Delete("key_0");
   it->Next();
-  XCTAssertTrue(it->Valid());
-  XCTAssertEqual("key_1", it->key());
+  ASSERT_TRUE(it->Valid());
+  ASSERT_EQ("key_1", it->key());
   it->Next();
-  XCTAssertFalse(it->Valid());
+  ASSERT_FALSE(it->Valid());
 }
 
-- (void)testDeletingAheadOfAnIterator {
+TEST_F(LevelDbTransactionTest, DeletingAheadOfAnIterator) {
   // Write keys
   for (int i = 0; i < 4; ++i) {
-    Status status = _db->Put(LevelDbTransaction::DefaultWriteOptions(), "key_" + std::to_string(i),
-                             "value_" + std::to_string(i));
-    XCTAssertTrue(status.ok());
+    Status status =
+        db_->Put(LevelDbTransaction::DefaultWriteOptions(),
+                 "key_" + std::to_string(i), "value_" + std::to_string(i));
+    ASSERT_TRUE(status.ok());
   }
 
-  // Create a transaction, iterate to key_1, delete key_2. Verify we still iterate key_3.
-  LevelDbTransaction transaction(_db.get(), "testDeletingAheadOfAnIterator");
+  // Create a transaction, iterate to key_1, delete key_2. Verify we still
+  // iterate key_3.
+  LevelDbTransaction transaction(db_.get(), "DeletingAheadOfAnIterator");
   auto it = transaction.NewIterator();
   it->Seek("key_0");
-  XCTAssertTrue(it->Valid());
-  XCTAssertEqual("key_0", it->key());
+  ASSERT_TRUE(it->Valid());
+  ASSERT_EQ("key_0", it->key());
   it->Next();
-  XCTAssertTrue(it->Valid());
-  XCTAssertEqual("key_1", it->key());
+  ASSERT_TRUE(it->Valid());
+  ASSERT_EQ("key_1", it->key());
   transaction.Delete("key_2");
   it->Next();
-  XCTAssertTrue(it->Valid());
-  XCTAssertEqual("key_3", it->key());
-  XCTAssertTrue(it->Valid());
+  ASSERT_TRUE(it->Valid());
+  ASSERT_EQ("key_3", it->key());
+  ASSERT_TRUE(it->Valid());
   it->Next();
-  XCTAssertFalse(it->Valid());
+  ASSERT_FALSE(it->Valid());
 }
 
-- (void)testToString {
+TEST_F(LevelDbTransactionTest, ToString) {
   std::string key = LevelDbMutationKey::Key("user1", 42);
   Message<firestore_client_WriteBatch> message;
   message->batch_id = 42;
 
-  LevelDbTransaction transaction(_db.get(), "testToString");
+  LevelDbTransaction transaction(db_.get(), "ToString");
   std::string description = transaction.ToString();
-  XCTAssertEqual(description, "<LevelDbTransaction testToString: 0 changes (0 bytes):>");
+  ASSERT_EQ(description, "<LevelDbTransaction ToString: 0 changes (0 bytes):>");
 
   transaction.Put(key, message);
   description = transaction.ToString();
-  XCTAssertEqual(description, "<LevelDbTransaction testToString: 1 changes (2 bytes):\n"
-                              "  - Put [mutation: user_id=user1 batch_id=42] (2 bytes)>");
+  ASSERT_EQ(description,
+            "<LevelDbTransaction ToString: 1 changes (2 bytes):\n"
+            "  - Put [mutation: user_id=user1 batch_id=42] (2 bytes)>");
 
   std::string key2 = LevelDbMutationKey::Key("user1", 43);
   transaction.Delete(key2);
   description = transaction.ToString();
-  XCTAssertEqual(description, "<LevelDbTransaction testToString: 2 changes (2 bytes):\n"
-                              "  - Delete [mutation: user_id=user1 batch_id=43]\n"
-                              "  - Put [mutation: user_id=user1 batch_id=42] (2 bytes)>");
+  ASSERT_EQ(description,
+            "<LevelDbTransaction ToString: 2 changes (2 bytes):\n"
+            "  - Delete [mutation: user_id=user1 batch_id=43]\n"
+            "  - Put [mutation: user_id=user1 batch_id=42] (2 bytes)>");
 }
 
-@end
-
-NS_ASSUME_NONNULL_END
+}  // namespace local
+}  // namespace firestore
+}  // namespace firebase
