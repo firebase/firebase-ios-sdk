c288ab16ca6d5650f5bee4152c54da9b722d7e3e
Gil <mcg@google.com>
2019-11-06 12:22:24 -0800

    Fix missing include for StatusOr

--- Firestore/core/src/firebase/firestore/util/filesystem.h
+++ Firestore/core/src/firebase/firestore/util/filesystem.h
@@ -22,6 +22,7 @@
 
 #include "Firestore/core/src/firebase/firestore/util/path.h"
 #include "Firestore/core/src/firebase/firestore/util/status.h"
+#include "Firestore/core/src/firebase/firestore/util/statusor.h"
 
 namespace firebase {
 namespace firestore {
