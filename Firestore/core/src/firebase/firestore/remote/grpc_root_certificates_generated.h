0160e5894a7087084598af0185fb139f55a5ea56
varconst <varconsteq@gmail.com>
2019-11-18 15:41:55 -0500

    First iteration

---
+++ Firestore/core/src/firebase/firestore/remote/grpc_root_certificates_generated.h
@@ -0,0 +1,20 @@
+// Copyright 2018 Google Inc. All Rights Reserved.
+
+#ifndef _USERS_VARCONST_FIREBASE_IOS_SDK_FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_REMOTE_GRPC_ROOT_CERTIFICATES_GENERATED_H
+#define _USERS_VARCONST_FIREBASE_IOS_SDK_FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_REMOTE_GRPC_ROOT_CERTIFICATES_GENERATED_H
+
+#include <cstdlib>
+
+namespace firebase {
+namespace firestore {
+namespace remote {
+
+extern const size_t grpc_root_certificates_generated_size;
+extern const unsigned char grpc_root_certificates_generated_data[];
+extern const char roots_filename[];
+
+}  // namespace remote
+}  // namespace firestore
+}  // namespace firebase
+
+#endif  // _USERS_VARCONST_FIREBASE_IOS_SDK_FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_REMOTE_GRPC_ROOT_CERTIFICATES_GENERATED_H
