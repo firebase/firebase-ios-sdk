71b09774d3dcf11877d873b7cf9a248ef60f514d
Gil <mcg@google.com>
2019-11-26 15:43:42 -0800

    Fix TSAN test failures under CMake (#4396)

--- cmake/FindASANDylib.cmake
+++ cmake/FindASANDylib.cmake
@@ -12,10 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Finds `libclang_rt.asan_osx_dynamic.dylib` on Apple platform, it is typically
-# under ${CMAKE_CXX_COMPILER}/../lib.
-get_filename_component(compiler_bin_dir ${CMAKE_CXX_COMPILER} DIRECTORY)
-get_filename_component(compiler_dir ${compiler_bin_dir} DIRECTORY)
+if(APPLE AND CXX_CLANG)
+  # Sanitizers in Clang are supported by a specific runtime and Clang
+  # automatically links against the right runtime. When building staticly
+  # linked binaries, the resulting binary will contain the runtime. Dynamically
+  # linked binaries will link against the dynamic version of the library.
+  #
+  # xctest bundles are necessarily dynamicly linked, but linked in such a way
+  # that the sanitizer runtime is assumed to be on the @rpath. This finds the
+  # clang runtimes so that they can be supplied as needed.
+  get_filename_component(compiler_bin_dir ${CMAKE_CXX_COMPILER} DIRECTORY)
+  get_filename_component(compiler_dir ${compiler_bin_dir} DIRECTORY)
 
-file(GLOB_RECURSE ASAN_DYLIB
-  ${compiler_dir}/libclang_rt.asan_osx_dynamic.dylib)
+  if(WITH_ASAN)
+    file(
+      GLOB_RECURSE CLANG_ASAN_DYLIB
+      ${compiler_dir}/libclang_rt.asan_osx_dynamic.dylib
+    )
+  endif()
+
+  if(WITH_TSAN)
+    file(
+      GLOB_RECURSE CLANG_TSAN_DYLIB
+      ${compiler_dir}/libclang_rt.tsan_osx_dynamic.dylib
+    )
+  endif()
+endif(APPLE AND CXX_CLANG)
