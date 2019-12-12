fa1d31205c1b006edaea38970385f2d059bb6716
Chuan Ren <renkelvin@gmail.com>
2019-12-11 16:07:00 -0800

    ~

--- Firebase/Auth/Source/Auth/FIRAuth.m
+++ Firebase/Auth/Source/Auth/FIRAuth.m
@@ -13,8 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
+// TEST
 #import <Foundation/Foundation.h>
 
 #import "FIRAuth_Internal.h"
