4c70388ebc9b9806c696c4a8270ae6b05dac2c6b
Paul Beusterien <paulbeusterien@google.com>
2019-12-30 13:39:43 -0800

    Secrets are needed for installations tvOS

--- scripts/install_prereqs.sh
+++ scripts/install_prereqs.sh
@@ -25,7 +25,8 @@ function install_secrets() {
   # Set up secrets for integration tests and metrics collection. This does not work for pull
   # requests from forks. See
   # https://docs.travis-ci.com/user/pull-requests#pull-requests-and-security-restrictions
-  if [[ ! -z $encrypted_d6a88994a5ab_key ]]; then
+  if [[ ! -z $encrypted_d6a88994a5ab_key && $secrets_installed -ne true ]]; then
+    secrets_installed=true
     openssl aes-256-cbc -K $encrypted_5dda5f491369_key -iv $encrypted_5dda5f491369_iv \
     -in scripts/travis-encrypted/Secrets.tar.enc \
     -out scripts/travis-encrypted/Secrets.tar -d
@@ -86,6 +87,7 @@ case "$PROJECT-$PLATFORM-$METHOD" in
     ;;
 
   Installations-*)
+    install_secrets
     ;;
 
   InAppMessaging-*-xcodebuild)
