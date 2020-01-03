7563fe35b43a548806785a0d2c1f95c2829fdd37
Paul Beusterien <paulbeusterien@google.com>
2020-01-03 07:56:43 -0800

    Start GitHub Actions (#4431)

--- scripts/check.sh
+++ scripts/check.sh
@@ -246,14 +246,16 @@ fi
 
 # If there are changes to the Firestore project, ensure they're ordered
 # correctly to minimize conflicts.
-if ! git diff --quiet "${START_SHA}" -- Firestore; then
-  sync_project_cmd=("${top_dir}/scripts/sync_project.rb")
-  if [[ "${TEST_ONLY}" == true ]]; then
-    sync_project_cmd+=(--test-only)
-  fi
-  "${sync_project_cmd[@]}"
-  if ! git diff --quiet; then
-    maybe_commit "sync_project.rb generated changes"
+if [ -z "${GITHUB_WORKFLOW-}" ]; then
+  if ! git diff --quiet "${START_SHA}" -- Firestore; then
+    sync_project_cmd=("${top_dir}/scripts/sync_project.rb")
+    if [[ "${TEST_ONLY}" == true ]]; then
+      sync_project_cmd+=(--test-only)
+    fi
+    "${sync_project_cmd[@]}"
+    if ! git diff --quiet; then
+      maybe_commit "sync_project.rb generated changes"
+    fi
   fi
 fi
 
