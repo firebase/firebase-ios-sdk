339c8404d0d6ac92c13338f8f4a4ce38b42d5c1c
Sebastian Schmidt <mrschmidt@google.com>
2020-01-08 09:25:30 -0800

    Revert "Add +[FIRServerValues increment:] (#4328)" (#4627)
    This reverts commit 1cf8a944d78040749d96a0b82b608495b0bcc717.

--- Firebase/Database/Core/FServerValues.m
+++ Firebase/Database/Core/FServerValues.m
@@ -20,112 +20,37 @@
 #import "FLeafNode.h"
 #import "FSnapshotUtilities.h"
 
-const NSString *kTimestamp = @"timestamp";
-const NSString *kIncrement = @"increment";
-
-BOOL canBeRepresentedAsLong(NSNumber *num) {
-    switch (num.objCType[0]) {
-    case 'f': // float; fallthrough
-    case 'd': // double
-        return NO;
-    case 'L': // unsigned long; fallthrough
-    case 'Q': // unsigned long long; fallthrough
-        // Only use ulong(long) if there isn't an overflow.
-        if (num.unsignedLongLongValue > LONG_MAX) {
-            return NO;
-        }
-    }
-    return YES;
-}
-
-@interface FServerValues ()
-+ (id)resolveScalarServerOp:(NSString *)op
-           withServerValues:(NSDictionary *)serverValues;
-+ (id)resolveComplexServerOp:(NSDictionary *)op
-                withExisting:(id<FNode>)existing
-                serverValues:(NSDictionary *)serverValues;
-@end
-
 @implementation FServerValues
 
 + (NSDictionary *)generateServerValues:(id<FClock>)clock {
     long long millis = (long long)([clock currentTime] * 1000);
-    return @{kTimestamp : [NSNumber numberWithLongLong:millis]};
+    return @{@"timestamp" : [NSNumber numberWithLongLong:millis]};
 }
 
 + (id)resolveDeferredValue:(id)val
-              withExisting:(id<FNode>)existing
-              serverValues:(NSDictionary *)serverValues {
-    if (![val isKindOfClass:[NSDictionary class]]) {
-        return val;
-    }
-    NSDictionary *dict = val;
-    id op = dict[kServerValueSubKey];
-
-    if (op == nil) {
-        return val;
-    } else if ([op isKindOfClass:NSString.class]) {
-        return [FServerValues resolveScalarServerOp:op
-                                   withServerValues:serverValues];
-    } else if ([op isKindOfClass:NSDictionary.class]) {
-        return [FServerValues resolveComplexServerOp:op
-                                        withExisting:existing
-                                        serverValues:serverValues];
+          withServerValues:(NSDictionary *)serverValues {
+    if ([val isKindOfClass:[NSDictionary class]]) {
+        NSDictionary *dict = val;
+        if (dict[kServerValueSubKey] != nil) {
+            NSString *serverValueType = [dict objectForKey:kServerValueSubKey];
+            if (serverValues[serverValueType] != nil) {
+                return [serverValues objectForKey:serverValueType];
+            } else {
+                // TODO: Throw unrecognizedServerValue error here
+            }
+        }
     }
     return val;
 }
 
-+ (id)resolveScalarServerOp:(NSString *)op
-           withServerValues:(NSDictionary *)serverValues {
-    return serverValues[op];
-}
-
-+ (id)resolveComplexServerOp:(NSDictionary *)op
-                withExisting:(id<FNode>)existing
-                serverValues:(NSDictionary *)serverValues {
-    // Only increment is supported as of now
-    if (op[kIncrement] == nil) {
-        return nil;
-    }
-
-    // Incrementing a non-number sets the value to the incremented amount
-    NSNumber *delta = op[kIncrement];
-    if (![existing isLeafNode]) {
-        return delta;
-    }
-    FLeafNode *existingLeaf = existing;
-    if (![existingLeaf.value isKindOfClass:NSNumber.class]) {
-        return delta;
-    }
-
-    NSNumber *existingNum = existingLeaf.value;
-    BOOL incrLong = canBeRepresentedAsLong(delta);
-    BOOL baseLong = canBeRepresentedAsLong(existingNum);
-
-    if (incrLong && baseLong) {
-        long x = delta.longValue;
-        long y = existingNum.longValue;
-        long r = x + y;
-
-        // See "Hacker's Delight" 2-12: Overflow if both arguments have the
-        // opposite sign of the result
-        if (((x ^ r) & (y ^ r)) >= 0) {
-            return @(r);
-        }
-    }
-    return @(delta.doubleValue + existingNum.doubleValue);
-}
-
 + (FCompoundWrite *)resolveDeferredValueCompoundWrite:(FCompoundWrite *)write
-                                         withExisting:(id<FNode>)existing
-                                         serverValues:
-                                             (NSDictionary *)serverValues {
+                                     withServerValues:
+                                         (NSDictionary *)serverValues {
     __block FCompoundWrite *resolved = write;
     [write enumerateWrites:^(FPath *path, id<FNode> node, BOOL *stop) {
       id<FNode> resolvedNode =
           [FServerValues resolveDeferredValueSnapshot:node
-                                         withExisting:existing
-                                         serverValues:serverValues];
+                                     withServerValues:serverValues];
       // Node actually changed, use pointer inequality here
       if (resolvedNode != node) {
           resolved = [resolved addWrite:resolvedNode atPath:path];
@@ -135,8 +60,7 @@ BOOL canBeRepresentedAsLong(NSNumber *num) {
 }
 
 + (id)resolveDeferredValueTree:(FSparseSnapshotTree *)tree
-                  withExisting:(id<FNode>)existing
-                  serverValues:(NSDictionary *)serverValues {
+              withServerValues:(NSDictionary *)serverValues {
     FSparseSnapshotTree *resolvedTree = [[FSparseSnapshotTree alloc] init];
     [tree
         forEachTreeAtPath:[FPath empty]
@@ -145,28 +69,22 @@ BOOL canBeRepresentedAsLong(NSNumber *num) {
                              rememberData:
                                  [FServerValues
                                      resolveDeferredValueSnapshot:node
-                                                     withExisting:
-                                                         [existing
-                                                             getChild:path]
-                                                     serverValues:serverValues]
+                                                 withServerValues:serverValues]
                                    onPath:path];
                        }];
     return resolvedTree;
 }
 
 + (id<FNode>)resolveDeferredValueSnapshot:(id<FNode>)node
-                             withExisting:(id<FNode>)existing
-                             serverValues:(NSDictionary *)serverValues {
+                         withServerValues:(NSDictionary *)serverValues {
     id priorityVal =
         [FServerValues resolveDeferredValue:[[node getPriority] val]
-                               withExisting:existing.getPriority
-                               serverValues:serverValues];
+                           withServerValues:serverValues];
     id<FNode> priority = [FSnapshotUtilities nodeFrom:priorityVal];
 
     if ([node isLeafNode]) {
         id value = [self resolveDeferredValue:[node val]
-                                 withExisting:existing
-                                 serverValues:serverValues];
+                             withServerValues:serverValues];
         if (![value isEqual:[node val]] ||
             ![priority isEqual:[node getPriority]]) {
             return [[FLeafNode alloc] initWithValue:value
@@ -182,10 +100,9 @@ BOOL canBeRepresentedAsLong(NSNumber *num) {
 
         [node enumerateChildrenUsingBlock:^(NSString *childKey,
                                             id<FNode> childNode, BOOL *stop) {
-          id newChildNode = [FServerValues
-              resolveDeferredValueSnapshot:childNode
-                              withExisting:[existing getImmediateChild:childKey]
-                              serverValues:serverValues];
+          id newChildNode =
+              [FServerValues resolveDeferredValueSnapshot:childNode
+                                         withServerValues:serverValues];
           if (![newChildNode isEqual:childNode]) {
               newNode = [newNode updateImmediateChild:childKey
                                          withNewChild:newChildNode];
