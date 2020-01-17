f62ba0fdf434778c32990d101c921fcfd4a6a963
Mandar <dmandar@users.noreply.github.com>
2020-01-17 11:20:50 -0800

    Remove test - will be added later.

--- FirebaseSegmentation/Tests/Unit/SEGInitializationTests.m
+++ FirebaseSegmentation/Tests/Unit/SEGInitializationTests.m
@@ -26,17 +26,11 @@
 - (void)setUp {
   // Put setup code here. This method is called before the invocation of each test method in the
   // class.
-  [FIRApp configure];
 }
 
 - (void)tearDown {
   // Put teardown code here. This method is called after the invocation of each test method in the
   // class.
-}
-
-- (void)testExample {
-  FIRSegmentation *segmentation = [FIRSegmentation segmentation];
-  XCTAssertNotNil(segmentation);
 }
 
 @end
