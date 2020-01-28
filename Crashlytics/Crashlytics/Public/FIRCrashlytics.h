51e70cae66dedd5121b538cf884e45829ede8785
samedson <samedson@google.com>
2020-01-28 10:12:53 -0500

    warn => warning

--- Crashlytics/Crashlytics/Public/FIRCrashlytics.h
+++ Crashlytics/Crashlytics/Public/FIRCrashlytics.h
@@ -15,7 +15,7 @@
 #import <Foundation/Foundation.h>
 
 #if __has_include(<Crashlytics/Crashlytics.h>)
-#warn "FirebaseCrashlytics and Crashlytics are not compatible in the same app because including multiple crash reporters can cause problems when registering exception handlers."
+#warning "FirebaseCrashlytics and Crashlytics are not compatible in the same app because including multiple crash reporters can cause problems when registering exception handlers."
 #endif
 
 NS_ASSUME_NONNULL_BEGIN
