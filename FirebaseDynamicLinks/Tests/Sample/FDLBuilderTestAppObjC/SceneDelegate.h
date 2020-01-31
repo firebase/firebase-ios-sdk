2c395ad7b9dddce2400622085f523ffa1a78cb71
Mandar <dmandar@users.noreply.github.com>
2020-01-30 17:16:52 -0800

    Get the FDL app running with the new provisioning profile, entitlements and info.plist entries.

---
+++ FirebaseDynamicLinks/Tests/Sample/FDLBuilderTestAppObjC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FDLBuilderTestAppObjC
+//
+//  Created by Mandar Deolalikar on 1/30/20.
+//  Copyright © 2020 Google Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
