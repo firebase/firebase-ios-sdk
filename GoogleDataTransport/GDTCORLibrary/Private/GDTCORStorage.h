a73b517c86030421f0ecea1017774554ed87894d
Michael Haney <haneym@google.com>
2020-01-31 17:31:02 -0800

    Add a storage completion handler to the event transport pipeline

--- GoogleDataTransport/GDTCORLibrary/Private/GDTCORStorage.h
+++ GoogleDataTransport/GDTCORLibrary/Private/GDTCORStorage.h
@@ -36,8 +36,10 @@ NS_ASSUME_NONNULL_BEGIN
  * a GDTCORStoredEvent instance.
  *
  * @param event The event to store.
+ * @param completion A block to run when an event was written to disk or dropped.
  */
-- (void)storeEvent:(GDTCOREvent *)event;
+- (void)storeEvent:(GDTCOREvent *)event
+        onComplete:(void (^)(BOOL wasWritten, NSError *error))completion;
 
 /** Removes a set of events from storage specified by their hash.
  *
