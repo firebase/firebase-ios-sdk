f138a75a33cde26a58e586274e2dc7f8618fd924
Michael Haney <mikehaney24@users.noreply.github.com>
2020-02-03 08:15:31 -0800

    Add a storage completion handler to the event transport pipeline (#4794)

--- GoogleDataTransport/GDTCORLibrary/GDTCORTransformer.m
+++ GoogleDataTransport/GDTCORLibrary/GDTCORTransformer.m
@@ -47,8 +47,13 @@
 }
 
 - (void)transformEvent:(GDTCOREvent *)event
-      withTransformers:(NSArray<id<GDTCOREventTransformer>> *)transformers {
+      withTransformers:(NSArray<id<GDTCOREventTransformer>> *)transformers
+            onComplete:(nonnull void (^)(BOOL wasWritten, NSError *error))completion {
   GDTCORAssert(event, @"You can't write a nil event");
+  if (!completion) {
+    completion = ^(BOOL wasWritten, NSError *_Nullable error) {
+    };
+  }
 
   __block GDTCORBackgroundIdentifier bgID = GDTCORBackgroundIdentifierInvalid;
   bgID = [[GDTCORApplication sharedApplication]
@@ -64,15 +69,17 @@
         GDTCORLogDebug("Applying a transformer to event %@", event);
         transformedEvent = [transformer transform:transformedEvent];
         if (!transformedEvent) {
+          completion(NO, nil);
           return;
         }
       } else {
         GDTCORLogError(GDTCORMCETransformerDoesntImplementTransform,
                        @"Transformer doesn't implement transform: %@", transformer);
+        completion(NO, nil);
         return;
       }
     }
-    [self.storageInstance storeEvent:transformedEvent];
+    [self.storageInstance storeEvent:transformedEvent onComplete:completion];
 
     // The work is done, cancel the background task if it's valid.
     [[GDTCORApplication sharedApplication] endBackgroundTask:bgID];
