7f84e84bf959ae0f79431fa922d663f40bf952ab
Gil <mcg@google.com>
2020-02-04 13:38:59 -0800

    Fixes for issues discovered in the Unity desktop build (#4795)
    * Make Filesystem's destructor virtual
    
    * Avoid capturing a strong reference to FirestoreClient in GC tasks.
    
    This prevents this task from keeping the FirestoreClient alive even
    after the api::Firestore instance has been destroyed.
    
    * Ensure that FirestoreClient is terminated
    
    In the public C++ API, it's possible to delete the owning FirebaseApp
    which triggers the destruction of the api::Firestore object even though
    we haven't called Terminate.
    
    This makes it such that api::Firestore calls a blocking Terminate call
    on FirestoreClient in its destructor.
    
    Additionally, change AsyncQueue to always run the operation enqueued on
    shutdown, even if already shutdown. Make FirestoreClient capable of
    short circuiting the termination work for itself. This makes terminate
    idempotent in a more staightforward way.
    
    * Clean up excess logging
    
    * Remove defintion of weak_from_this
    
    * Review feedback
    
    * Make AsyncQueue extend std::enable_shared_from_this
    
    Also hide the AsyncQueue's public constructor and force all users to
    manage their reference to it via std::shared_ptr.
    
    * Handle Executor shutdown from within its own pool

--- Firestore/core/src/firebase/firestore/util/executor_std.cc
+++ Firestore/core/src/firebase/firestore/util/executor_std.cc
@@ -40,7 +40,8 @@ std::string ThreadIdToString(const std::thread::id thread_id) {
 
 // MARK: - ExecutorStd
 
-ExecutorStd::ExecutorStd(int threads) {
+ExecutorStd::ExecutorStd(int threads)
+    : shutting_down_(std::make_shared<std::atomic<bool>>()) {
   HARD_ASSERT(threads > 0);
 
   // Somewhat counter-intuitively, constructor of `std::atomic` assigns the
@@ -49,14 +50,14 @@ ExecutorStd::ExecutorStd(int threads) {
   // See [this thread](https://stackoverflow.com/questions/25609858) for context
   // on the constructor.
   current_id_ = 0;
-  shutting_down_ = false;
+  *shutting_down_ = false;
   for (int i = 0; i < threads; ++i) {
     worker_thread_pool_.emplace_back(&ExecutorStd::PollingThread, this);
   }
 }
 
 ExecutorStd::~ExecutorStd() {
-  shutting_down_ = true;
+  *shutting_down_ = true;
 
   // Make sure the worker threads are not blocked, so that the call to `join`
   // doesn't hang. It's not deterministic which thread will pick up an entry,
@@ -66,7 +67,14 @@ ExecutorStd::~ExecutorStd() {
   }
 
   for (std::thread& thread : worker_thread_pool_) {
-    thread.join();
+    // If the current thread is running this destructor, we can't join the
+    // thread. Instead detach it and rely on PollingThread to notice that
+    // *shutting_down_ is now true.
+    if (std::this_thread::get_id() == thread.get_id()) {
+      thread.detach();
+    } else {
+      thread.join();
+    }
   }
 }
 
@@ -105,7 +113,10 @@ ExecutorStd::Id ExecutorStd::PushOnSchedule(Operation&& operation,
 }
 
 void ExecutorStd::PollingThread() {
-  while (!shutting_down_) {
+  // Keep a local shared_ptr here to ensure that the atomic pointed to by
+  // shutting_down_ remains valid even after the destruction of the executor.
+  std::shared_ptr<std::atomic<bool>> local_shutting_down = shutting_down_;
+  while (!*local_shutting_down) {
     Entry entry = schedule_.PopBlocking();
     if (entry.tagged.operation) {
       entry.tagged.operation();
