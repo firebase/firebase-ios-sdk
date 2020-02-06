51474ea43d3d1f4781d3b58633e13fa247508ded
Pranav Rajgopal <pranavrajgopal@google.com>
2020-02-06 17:46:40 -0500

    Initial commit for Firebase App Distribution New Build Alerts SDK (with sample test app)

---
+++ FirebaseAppDistribution/Sources/FIRAppDistribution.m
@@ -0,0 +1,90 @@
+// Copyright 2019 Google
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "FIRAppDistribution.h"
+
+#import <FirebaseCore/FIRAppInternal.h>
+#import <FirebaseCore/FIRComponent.h>
+#import <FirebaseCore/FIRComponentContainer.h>
+
+/// Empty protocol to register with FirebaseCore's component system.
+@protocol FIRAppDistributionInstanceProvider <NSObject>
+@end
+
+@interface FIRAppDistribution () <FIRLibrary, FIRAppDistributionInstanceProvider>
+
+@end
+
+@implementation FIRAppDistribution
+
+#pragma mark - Singleton Support
+
+- (instancetype)initWithApp:(FIRApp *)app
+                    appInfo:(NSDictionary *)appInfo {
+  self = [super init];
+
+  if (self) {
+      // Save any properties here
+      NSLog(@"APP DISTRIBUTION STARTED UP!");
+  }
+
+  return self;
+}
+
++ (void)load {
+  [FIRApp registerInternalLibrary:(Class<FIRLibrary>)self
+                         withName:@"firebase-appdistribution"
+                      withVersion:@"0.0.0"]; //TODO: Get version from podspec
+}
+
++ (NSArray<FIRComponent *> *)componentsToRegister {
+
+  FIRComponentCreationBlock creationBlock =
+      ^id _Nullable(FIRComponentContainer *container, BOOL *isCacheable) {
+    if (!container.app.isDefaultApp) {
+      NSLog(@"App Distribution must be used with the default Firebase app.");
+      return nil;
+    }
+
+    *isCacheable = YES;
+
+    return [[FIRAppDistribution alloc] initWithApp:container.app
+                                       appInfo:NSBundle.mainBundle.infoDictionary];
+  };
+
+  FIRComponent *component =
+      [FIRComponent componentWithProtocol:@protocol(FIRAppDistributionInstanceProvider)
+                      instantiationTiming:FIRInstantiationTimingEagerInDefaultApp
+                             dependencies:@[]
+                            creationBlock:creationBlock];
+  return @[ component ];
+}
+
++ (instancetype)appDistribution {
+  // The container will return the same instance since isCacheable is set
+
+  FIRApp *defaultApp = [FIRApp defaultApp];  // Missing configure will be logged here.
+
+  // Get the instance from the `FIRApp`'s container. This will create a new instance the
+  // first time it is called, and since `isCacheable` is set in the component creation
+  // block, it will return the existing instance on subsequent calls.
+  id<FIRAppDistributionInstanceProvider> instance =
+    FIR_COMPONENT(FIRAppDistributionInstanceProvider, defaultApp.container);
+
+  // In the component creation block, we return an instance of `FIRAppDistribution`. Cast it and
+  // return it.
+  return (FIRAppDistribution *)instance;
+}
+
+@end
