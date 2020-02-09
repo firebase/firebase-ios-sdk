39b02978b410b31212d9d5614e747506f4d6ecb2
Marek Gilbert <mcg@google.com>
2020-02-08 17:18:50 -0800

    Capture and show raw xcodebuild log on failure

--- scripts/build.sh
+++ scripts/build.sh
@@ -115,14 +115,16 @@ function RunXcodebuild() {
     xcpretty_cmd+=(-f $(xcpretty-travis-formatter))
   fi
 
-  xcodebuild "$@" | "${xcpretty_cmd[@]}"; result=$?
+  xcodebuild "$@" | tee xcodebuild.log | "${xcpretty_cmd[@]}"; result=$?
   if [[ $result == 65 ]]; then
     echo "xcodebuild exited with 65, retrying" 1>&2
     sleep 5
 
-    xcodebuild "$@" | "${xcpretty_cmd[@]}"; result=$?
+    xcodebuild "$@" | tee xcodebuild.log | "${xcpretty_cmd[@]}"; result=$?
   fi
   if [[ $result != 0 ]]; then
+    echo "xcodebuild exited with $result; raw log follows" 1>&2
+    cat xcodebuild.log
     exit $result
   fi
 }
