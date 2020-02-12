f5973800b91b1ce0fa6ec58c64122ce5feb9544a
Doudou Nan <doudounan@google.com>
2020-02-12 15:17:06 -0800

    Set fileURL in COREvent nullable

--- GoogleDataTransport/GDTCORLibrary/Public/GDTCOREvent.h
+++ GoogleDataTransport/GDTCORLibrary/Public/GDTCOREvent.h
@@ -64,7 +64,7 @@ typedef NS_ENUM(NSInteger, GDTCOREventQoS) {
 
 /** The fileURL of dataObject in disk. set when store the transport bytes to
  * disk,*/
-@property(readonly, nonatomic) NSURL *fileURL;
+@property(nullable, readonly, nonatomic) NSURL *fileURL;
 
 /** A dictionary provided to aid prioritizers by allowing the passing of arbitrary data. It will be
  * retained by a copy in -copy, but not used for -hash.
