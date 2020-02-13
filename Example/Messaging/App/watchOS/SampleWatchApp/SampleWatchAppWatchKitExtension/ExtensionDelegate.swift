518e83acf0481aa17ec0edb15253a553110467d5
Chen Liang <chliang@google.com>
2020-02-13 11:19:53 -0800

    remove method stubs

--- Example/Messaging/App/watchOS/SampleWatchApp/SampleWatchAppWatchKitExtension/ExtensionDelegate.swift
+++ Example/Messaging/App/watchOS/SampleWatchApp/SampleWatchAppWatchKitExtension/ExtensionDelegate.swift
@@ -43,15 +43,6 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate, MessagingDelegate {
     Messaging.messaging().apnsToken = deviceToken
   }
 
-  func applicationDidBecomeActive() {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-  }
-
-  func applicationWillResignActive() {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, etc.
-  }
-
   func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
     // Sent when the system needs to launch the application in the background to process tasks. Tasks arrive in a set, so loop through and process each one.
     for task in backgroundTasks {
