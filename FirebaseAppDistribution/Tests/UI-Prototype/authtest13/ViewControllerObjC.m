694b91f6524854393fc6ec488beaa0c5dd5d19e6
Jeremy Durham <jeremydurham@google.com>
2020-02-19 13:43:29 -0500

    First pass at API

---
+++ FirebaseAppDistribution/Tests/UI-Prototype/authtest13/ViewControllerObjC.m
@@ -0,0 +1,38 @@
+//
+//  ViewControllerObjC.m
+//  authtest13
+//
+//  Created by Jeremy Durham on 2/19/20.
+//  Copyright © 2020 Pranav Rajgopal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ViewControllerObjC.h"
+#import <FIRAppDistribution.h>
+
+@implementation ViewControllerObjC
+
+- (void)viewDidLoad:(BOOL)animated{
+    [super viewDidLoad];
+    [[FIRAppDistribution appDistribution] checkForUpdateWithCompletion:^(FIRAppDistributionRelease * _Nullable release, NSError * _Nullable error) {
+        NSLog(@"%@", release);
+        if (release) {
+            UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"New Version Available"
+                                                                             message:[NSString stringWithFormat:@"Version %@ (%@) is available.",
+                                                                                      release.bundleShortVersion, release.bundleVersion]
+                                                                      preferredStyle:UIAlertControllerStyleAlert];
+            
+            UIAlertAction *updateAction = [UIAlertAction actionWithTitle:@"Update" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){
+            }];
+            UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){
+            }];
+            
+            [alert addAction:updateAction];
+            [alert addAction:cancelAction];
+            
+            [self presentViewController:alert animated:YES completion:nil];
+        }
+    }];
+}
+
+@end
