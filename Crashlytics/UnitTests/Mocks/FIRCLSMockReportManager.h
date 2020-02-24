d820aad3fb8c42d95dab4937e416f769dbac01fe
Maksym Malyhin <mmaksym@google.com>
2020-02-24 10:46:09 -0500

    Crashlytics SDK: use Firebase Installations instead of Instance ID (#4954)
    * WIP
    
    * Replace Instance ID by FIS
    
    * Crashlytics podspec: replace FirebaseInstanceID by FirebaseInstallations
    
    * Run  ./scripts/style.sh
    
    * Cleanup and copyright update.

--- Crashlytics/UnitTests/Mocks/FIRCLSMockReportManager.h
+++ Crashlytics/UnitTests/Mocks/FIRCLSMockReportManager.h
@@ -23,7 +23,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface FIRCLSMockReportManager : FIRCLSReportManager
 
 - (instancetype)initWithFileManager:(FIRCLSFileManager *)fileManager
-                         instanceID:(FIRInstanceID *)instanceID
+                      installations:(FIRInstallations *)installations
                           analytics:(nullable id<FIRAnalyticsInterop>)analytics
                         googleAppID:(NSString *)googleAppID
                         dataArbiter:(FIRCLSDataCollectionArbiter *)dataArbiter
@@ -31,7 +31,7 @@ NS_ASSUME_NONNULL_BEGIN
     NS_DESIGNATED_INITIALIZER;
 
 - (instancetype)initWithFileManager:(FIRCLSFileManager *)fileManager
-                         instanceID:(FIRInstanceID *)instanceID
+                      installations:(FIRInstallations *)instanceID
                           analytics:(nullable id<FIRAnalyticsInterop>)analytics
                         googleAppID:(NSString *)googleAppID
                         dataArbiter:(FIRCLSDataCollectionArbiter *)dataArbiter NS_UNAVAILABLE;
