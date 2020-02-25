d262ad6465ceed064df5ba7462bc53a2a830053e
Jeremy Durham <jeremydurham@google.com>
2020-02-25 13:43:48 -0500

    Add Delegate swizzling and continue working on SDK

---
+++ FirebaseAppDistribution/Sources/Public/FIRAppDistributionAppDelegateInterceptor.h
@@ -0,0 +1,26 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol OIDExternalUserAgentSession;
+
+/// An instance of this class is meant to be registered as an AppDelegate interceptor, and
+/// implements the logic that my SDK needs to perform when certain app delegate methods are invoked.
+@interface FIRAppDistributionAppDelegatorInterceptor : NSObject <UIApplicationDelegate>
+
+/// Returns the MYAppDelegateInterceptor singleton.
+/// Always register just this singleton as the app delegate interceptor. This instance is
+/// retained. The App Delegate Swizzler only retains weak references and so this is needed.
++ (instancetype)sharedInstance;
+
+/*! @brief The authorization flow session which receives the return URL from \SFSafariViewController.
+    @discussion We need to store this in the app delegate as it's that delegate which receives the
+        incoming URL on UIApplicationDelegate.application:openURL:options:. This property will be
+        nil, except when an authorization flow is in progress.
+ */
+@property(nonatomic, strong, nullable) id<OIDExternalUserAgentSession> currentAuthorizationFlow;
+
+@end
+
+NS_ASSUME_NONNULL_END
