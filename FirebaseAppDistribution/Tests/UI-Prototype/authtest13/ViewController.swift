a005ffcb20485d4ad1378d53d8b96cdeb8a1eba3
Jeremy Durham <jeremydurham@google.com>
2020-02-25 13:43:48 -0500

    Add Delegate swizzling and continue working on SDK

--- FirebaseAppDistribution/Tests/UI-Prototype/authtest13/ViewController.swift
+++ FirebaseAppDistribution/Tests/UI-Prototype/authtest13/ViewController.swift
@@ -12,26 +12,28 @@ import SafariServices
 class ViewController: UIViewController {
     
     var safariViewController: SFSafariViewController?
-
+    
     override func viewDidLoad() {
+        print("Loaded!")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
-//        let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate
+        //        let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate
         
         appDelegate.mainViewController = self
-        AppDistribution.appDistribution().checkForUpdate(completion: { release, error in
+        
+        AppDistribution.appDistribution().checkForUpdate(view: self, completion: { release, error in
             guard let release = release else {
                 return
             }
-
+            
             let uialert = UIAlertController(title: "New Version Available", message: "Version \(release.bundleShortVersion) (\(release.bundleVersion)) is available.", preferredStyle: .alert)
             
             uialert.addAction(UIAlertAction(title: "Update", style: UIAlertAction.Style.default) {
-                UIAlertAction in
+                alert in UIApplication.shared.open(release.downloadUrl)
             })
             uialert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel) {
-                UIAlertAction in
+                alert in
             })
             self.present(uialert, animated: true, completion: nil)
         })
@@ -46,18 +48,18 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touch(_ sender: Any) {
-        print("this works!!!")
-        if let url = URL(string: "https://appdistribution.firebase.dev/app_distro/projects/5e20b15eccbee769cb4582ee") {
-            let config = SFSafariViewController.Configuration()
-            config.entersReaderIfAvailable = true
-
-            let vc = SFSafariViewController(url: url, configuration: config)
-            safariViewController = vc
-            present(vc, animated: true)
-        }
-////
-//        guard let url = URL(string: "https://tinyurl.com/ua8tka3") else { return}
-//        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        //        print("this works!!!")
+        //        if let url = URL(string: "https://appdistribution.firebase.dev/app_distro/projects/5e20b15eccbee769cb4582ee") {
+        //            let config = SFSafariViewController.Configuration()
+        //            config.entersReaderIfAvailable = true
+        //
+        //            let vc = SFSafariViewController(url: url, configuration: config)
+        //            safariViewController = vc
+        //            present(vc, animated: true)
+        //        }
+        ////
+        //        guard let url = URL(string: "https://tinyurl.com/ua8tka3") else { return}
+        //        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
 }
 
