7b8c44d99085171a853a812b18c9599eedc3cbbe
Jason Hu <jasonhu@google.com>
2020-03-05 11:37:42 -0500

    Fix TODO message

--- Crashlytics/Crashlytics/Models/FIRCLSInternalReport.m
+++ Crashlytics/Crashlytics/Models/FIRCLSInternalReport.m
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO: This class should be removed the uploading of reports via GoogleDataTransport is no longer
-// an experiment
+// TODO: Remove this class after the uploading of reports via GoogleDataTransport is no longer an
+// experiment
 
 #import "FIRCLSInternalReport.h"
 
