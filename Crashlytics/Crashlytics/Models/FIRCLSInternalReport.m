9fa46323d3780a8888005222b1b707c5bc83384a
Jason Hu <jasonhu@google.com>
2020-03-05 11:35:09 -0500

    Fix issue where Crash Free Users does not work with new GDT changes

--- Crashlytics/Crashlytics/Models/FIRCLSInternalReport.m
+++ Crashlytics/Crashlytics/Models/FIRCLSInternalReport.m
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// TODO: This class should be removed the uploading of reports via GoogleDataTransport is no longer
+// an experiment
+
 #import "FIRCLSInternalReport.h"
 
 #import "FIRCLSFile.h"
