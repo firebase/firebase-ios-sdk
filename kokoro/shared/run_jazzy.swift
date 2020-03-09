caa673558de3f8fbbff8a5a0f28c4d58a37fcc38
Morgan Chen <morganchen12@gmail.com>
2020-03-09 16:17:08 -0700

    run style

--- kokoro/shared/run_jazzy.swift
+++ kokoro/shared/run_jazzy.swift
@@ -4,15 +4,15 @@ import Foundation
 
 @discardableResult
 func shell(_ args: String...) -> Int32 {
-    let task = Process()
-    task.launchPath = "usr/bin/env"
-    task.arguments = ["bash", "-c"] + args
-    task.launch()
-    task.waitUntilExit()
-    return task.terminationStatus
+  let task = Process()
+  task.launchPath = "usr/bin/env"
+  task.arguments = ["bash", "-c"] + args
+  task.launch()
+  task.waitUntilExit()
+  return task.terminationStatus
 }
 
 let jazzyCheck = shell("jazzy --version")
 guard jazzyCheck == 0 else {
-    exit(jazzyCheck)
+  exit(jazzyCheck)
 }
