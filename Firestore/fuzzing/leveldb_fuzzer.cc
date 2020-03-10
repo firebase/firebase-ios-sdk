5ff8725b737682ea561366027bbe53600b688148
varconst <varconsteq@gmail.com>
2020-03-10 19:12:55 -0400

    Try fix

--- Firestore/fuzzing/leveldb_fuzzer.cc
+++ Firestore/fuzzing/leveldb_fuzzer.cc
@@ -34,7 +34,7 @@ using firebase::firestore::model::BatchId;
 using firebase::firestore::model::ResourcePath;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  const char* str_ptr = reinterpret_cast<const char*>(dataOBC);
+  const char* str_ptr = reinterpret_cast<const char*>(data);
   std::string str{str_ptr, size};
   leveldb::Slice slice = firebase::firestore::local::MakeSlice(str);
 
