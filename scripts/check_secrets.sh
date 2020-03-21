3c094637666a4680be38908021186db121881959
Paul Beusterien <paulbeusterien@google.com>
2020-03-21 08:06:16 -0700

    Fix cron and maybe fork

--- scripts/check_secrets.sh
+++ scripts/check_secrets.sh
@@ -32,12 +32,11 @@ check_secrets()
   fi
   # GitHub Actions: Secrets are available if we're not running on a fork.
   # See https://help.github.com/en/actions/automating-your-workflow-with-github-actions/using-environment-variables
-  # TODO- These are both set in the master repo even thought the docs say otherwise.
-  # Investigate their values in a fork.
+  # TODO- Both GITHUB_BASE_REF and GITHUB_HEAD_REF are set in master repo
+  # PRs even thought the docs say otherwise. They are not set in cron jobs on master.
+  # Investigate how do to distinguish fork PRs from master repo PRs.
   if [[ -n "${GITHUB_WORKFLOW:-}" ]]; then
-    if [[ -n "$GITHUB_BASE_REF" ]]; then
-      return 0
-    fi
+    return 0
   fi
   return 1
 }
