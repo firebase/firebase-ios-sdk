aa03a91b6bcb725f35814c22d099fc8567cd5d26
Jeremy Durham <jeremydurham@google.com>
2020-03-23 09:43:16 -0400

    Wiring up talking to the tester API

--- FirebaseAppDistribution/Sources/FIRAppDistributionAppDelegateInterceptor.m
+++ FirebaseAppDistribution/Sources/FIRAppDistributionAppDelegateInterceptor.m
@@ -24,43 +24,6 @@
             openURL:(NSURL *)URL
             options:(NSDictionary<NSString *, id> *)options {
 
-  //[MYInterestingClass doSomething];
-
-  // Results of this are ORed and NO doesn't affect other delegate interceptors' result.
   return NO;
 }
-
-//- (BOOL)application:(UIApplication *)application
-//            openURL:(NSURL *)URL
-//  sourceApplication:(NSString *)sourceApplication
-//         annotation:(id)annotation {
-//
-//    //[MYInterestingClass doSomething];
-//
-//  // Results of this are ORed and NO doesn't affect other delegate interceptors' result.
-//  return NO;
-//}
-//
-//#pragma mark - Network overridden handler methods
-//
-//- (void)application:(UIApplication *)application
-//    handleEventsForBackgroundURLSession:(NSString *)identifier
-//                      completionHandler:(void (^)(void))completionHandler {
-//
-//  // Note: Interceptors are not responsible for (and should not) call the completion handler.
-//  //[MYInterestingClass doSomething];
-//}
-//
-//#pragma mark - User Activities overridden handler methods
-//
-//- (BOOL)application:(UIApplication *)application
-//    continueUserActivity:(NSUserActivity *)userActivity
-//      restorationHandler:(void (^)(NSArray *restorableObjects))restorationHandler {
-//
-//  //[MYInterestingClass doSomething];
-//
-//  // Results of this are ORed and NO doesn't affect other delegate interceptors' result.
-//  return NO;
-//}
-
 @end
