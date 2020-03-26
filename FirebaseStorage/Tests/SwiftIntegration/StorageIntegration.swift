b8876bdc974667c2a6fca1202f8296b757414e69
Paul Beusterien <paulbeusterien@google.com>
2020-03-25 17:28:29 -0700

    style

--- FirebaseStorage/Tests/SwiftIntegration/StorageIntegration.swift
+++ FirebaseStorage/Tests/SwiftIntegration/StorageIntegration.swift
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import XCTest
 @testable import FirebaseCore
 @testable import FirebaseStorage
+import XCTest
 
 class StorageIntegration: XCTestCase {
   var app: FirebaseApp?
