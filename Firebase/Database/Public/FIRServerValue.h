059c24fdb1aa7cfbfdce6e86c09d932a1a489bfb
Sebastian Schmidt <mrschmidt@google.com>
2020-04-02 15:39:16 -0700

    Make ServerValue.increment() public (#5256)

--- Firebase/Database/Public/FIRServerValue.h
+++ Firebase/Database/Public/FIRServerValue.h
@@ -31,6 +31,23 @@ NS_SWIFT_NAME(ServerValue)
  */
 + (NSDictionary *)timestamp;
 
+/**
+ * Returns a placeholder value that can be used to atomically increment the
+ * current database value by the provided delta.
+ *
+ * The delta must be an long or a double value. If the current value is not an
+ * integer or double, or if the data does not yet exist, the transformation will
+ * set the data to the delta value. If either of the delta value or the existing
+ * data are doubles, both values will be interpreted as doubles. Double
+ * arithmetic and representation of double values follow IEEE 754 semantics. If
+ * there is positive/negative integer overflow, the sum is calculated as a  a
+ * double.
+ *
+ * @param delta the amount to modify the current value atomically.
+ * @return a placeholder value for modifying data atomically server-side.
+ */
++ (NSDictionary *)increment:(NSNumber *)delta NS_SWIFT_NAME(increment(_:));
+
 @end
 
 NS_ASSUME_NONNULL_END
