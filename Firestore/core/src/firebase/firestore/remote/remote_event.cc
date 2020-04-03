9a3d9c63c47b2ae5a570ac31a9ea9f3419b43c75
Brian Chen <chenbrian@google.com>
2020-04-03 13:35:44 -0700

    remove usage of reserve

--- Firestore/core/src/firebase/firestore/remote/remote_event.cc
+++ Firestore/core/src/firebase/firestore/remote/remote_event.cc
@@ -200,7 +200,6 @@ std::vector<TargetId> WatchChangeAggregator::GetTargetIds(
   }
 
   std::vector<TargetId> result;
-  result.reserve(target_states_.size());
   for (const auto& entry : target_states_) {
     if (IsActiveTarget(entry.first)) {
       result.push_back(entry.first);
