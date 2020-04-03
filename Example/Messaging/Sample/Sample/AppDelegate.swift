5d7dc0340d9398afef3f964de6550550c482b674
Chen Liang <chliang@google.com>
2020-04-03 16:11:43 -0700

    add minimum funciton to enable fcm

---
+++ Example/Messaging/Sample/Sample/AppDelegate.swift
@@ -0,0 +1,93 @@
+// Copyright 2020 Google
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import UIKit
+import CoreData
+import FirebaseCore
+import FirebaseMessaging
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
+
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    Messaging.messaging().delegate = self
+    
+    let center = UNUserNotificationCenter.current()
+    center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+      if error != nil {
+        print ("error requesting notification permission %@\n", error ?? "")
+      }
+    }
+    application.registerForRemoteNotifications()
+    return true
+  }
+
+  // MARK: UISceneSession Lifecycle
+
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+
+  // MARK: - Core Data stack
+
+  lazy var persistentContainer: NSPersistentContainer = {
+      /*
+       The persistent container for the application. This implementation
+       creates and returns a container, having loaded the store for the
+       application to it. This property is optional since there are legitimate
+       error conditions that could cause the creation of the store to fail.
+      */
+      let container = NSPersistentContainer(name: "Sample")
+      container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+          if let error = error as NSError? {
+              // Replace this implementation with code to handle the error appropriately.
+              // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+               
+              /*
+               Typical reasons for an error here include:
+               * The parent directory does not exist, cannot be created, or disallows writing.
+               * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+               * The device is out of space.
+               * The store could not be migrated to the current model version.
+               Check the error message to determine what the actual problem was.
+               */
+              fatalError("Unresolved error \(error), \(error.userInfo)")
+          }
+      })
+      return container
+  }()
+
+  // MARK: - Core Data Saving support
+
+  func saveContext () {
+      let context = persistentContainer.viewContext
+      if context.hasChanges {
+          do {
+              try context.save()
+          } catch {
+              // Replace this implementation with code to handle the error appropriately.
+              // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+              let nserror = error as NSError
+              fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+          }
+      }
+  }
+
+}
+
