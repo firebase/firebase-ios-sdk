1418b8eb398a99e03e935aff3f1eb09728805528
Chen Liang <chliang@google.com>
2020-04-03 17:34:21 -0700

    add iid/token methods

--- Example/Messaging/Sample/Sample/ContentView.swift
+++ Example/Messaging/Sample/Sample/ContentView.swift
@@ -13,11 +13,57 @@
 // limitations under the License.
 
 import SwiftUI
+import FirebaseCore
+import FirebaseMessaging
+import FirebaseInstanceID
 
 struct ContentView: View {
+  @State private var token = Messaging.messaging().fcmToken
+  @State private var iid = ""
+  
     var body: some View {
-        Text("Hello, World!")
+      
+      VStack {
+        HStack {
+          Button("get ID and token", action: getToken).padding()
+          Button("deleteToken", action: deleteToken).padding()
+          Button("deleteID", action: deleteID).padding()
+        }
+        Text("IID:\n" + (self.iid)).padding(10)
+        Text("Token:\n" + (self.token ?? "")).padding(10)
+      }
     }
+  func getToken() {
+    InstanceID.instanceID().instanceID { (result, error) in
+      if result != nil && error == nil {
+        self.token = result?.token
+        self.iid = result?.instanceID ?? ""
+      }
+    }
+  }
+  
+  func deleteToken() {
+    Messaging.messaging().deleteFCMToken(forSenderID: FirebaseApp.app()?.options.gcmSenderID ?? "") { (error) in
+      if (error != nil) {
+        print ("Failed delete token: ", error ?? "")
+        return
+      }
+      self.token = ""
+    }
+  }
+  
+  func deleteID() {
+    InstanceID.instanceID().deleteID { (error) in
+      if error != nil {
+        print ("Failed delete ID: ", error ?? "")
+        return
+      }
+    }
+  }
+  
+  func OnTokenRefresh() {
+    self.token = Messaging.messaging().fcmToken
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- Example/Messaging/Sample/Sample/SceneDelegate.swift
+++ Example/Messaging/Sample/Sample/SceneDelegate.swift
@@ -19,7 +19,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
 
-
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
