49aa99f8656dbeb42e5b970a0ba8882fc93118df
Doudou Nan <doudounan@google.com>
2020-04-08 09:44:53 -0700

    Add companion watchos test app targets

---
+++ GoogleDataTransportCCTSupport/GDTCCTWatchOSTestApp/GDTCCTiOSTestAppForCompanionWatchApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GDTCCTiOSTestAppForCompanionWatchApp
+//
+//  Created by Doudou Nan on 4/7/20.
+//  Copyright © 2020 Google. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
