95b85174b85f2570fdbe661bf7568542e46f5ab6
Maksym Malyhin <mmaksym@google.com>
2020-04-09 15:41:59 -0400

    GULKeychainUtils API docs

--- GoogleUtilities/Environment/Public/GULKeychainUtils.h
+++ GoogleUtilities/Environment/Public/GULKeychainUtils.h
@@ -23,14 +23,37 @@ FOUNDATION_EXPORT NSString *const kGULKeychainUtilsErrorDomain;
 /// Helper functions to access Keychain.
 @interface GULKeychainUtils : NSObject
 
-// TODO: Add API docs.
+/** Fetches a keychain item data matching to the provided query.
+ *  @param query A dictionary with Keychain query parameters. See docs for `SecItemCopyMatching` for
+ * details.
+ *  @param outError A pointer to `NSError` instance or `NULL`. The instance at `outError` will be
+ * assigned with an error if there is.
+ *  @returns Data for the first Keychain Item matching the provided query or `nil` if there is not
+ * such an item (`outError` will be `nil` in this case) or an error occurred.
+ */
 + (nullable NSData *)getItemWithQuery:(NSDictionary *)query
                                 error:(NSError *_Nullable *_Nullable)outError;
 
+/** Stores data to a Keychain Item matching to the provided query. An existing Keychain Item
+ * matching the query parameters will be updated or a new will be created.
+ *  @param item A Keychain Item data to store.
+ *  @param query A dictionary with Keychain query parameters. See docs for `SecItemAdd` and
+ * `SecItemUpdate` for details.
+ *  @param outError A pointer to `NSError` instance or `NULL`. The instance at `outError` will be
+ * assigned with an error if there is.
+ *  @returns `YES` when data was successfully stored, `NO` otherwise.
+ */
 + (BOOL)setItem:(NSData *)item
       withQuery:(NSDictionary *)query
           error:(NSError *_Nullable *_Nullable)outError;
 
+/** Removes a Keychain Item matching to the provided query.
+ *  @param query A dictionary with Keychain query parameters. See docs for `SecItemDelete` for
+ * details.
+ *  @param outError A pointer to `NSError` instance or `NULL`. The instance at `outError` will be
+ * assigned with an error if there is.
+ *  @returns `YES` if the item was removed successfully or doesn't exist, `NO` otherwise.
+ */
 + (BOOL)removeItemWithQuery:(NSDictionary *)query error:(NSError *_Nullable *_Nullable)outError;
 
 @end
