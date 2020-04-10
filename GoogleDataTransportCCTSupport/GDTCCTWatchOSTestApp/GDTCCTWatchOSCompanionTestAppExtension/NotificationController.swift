6c65d5458bd37099ba45ceca21263dd7da899456
Doudou Nan <doudounan@google.com>
2020-04-10 15:52:34 -0700

    remove unused methods in controllers

--- GoogleDataTransportCCTSupport/GDTCCTWatchOSTestApp/GDTCCTWatchOSCompanionTestAppExtension/NotificationController.swift
+++ GoogleDataTransportCCTSupport/GDTCCTWatchOSTestApp/GDTCCTWatchOSCompanionTestAppExtension/NotificationController.swift
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google
+ * Copyright 2020 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,23 +22,6 @@ class NotificationController: WKUserNotificationInterfaceController {
   override init() {
     // Initialize variables here.
     super.init()
-
     // Configure interface objects here.
-  }
-
-  override func willActivate() {
-    // This method is called when watch view controller is about to be visible to user
-    super.willActivate()
-  }
-
-  override func didDeactivate() {
-    // This method is called when watch view controller is no longer visible
-    super.didDeactivate()
-  }
-
-  override func didReceive(_ notification: UNNotification) {
-    // This method is called when a notification needs to be presented.
-    // Implement it if you use a dynamic notification interface.
-    // Populate your dynamic notification interface as quickly as possible.
   }
 }
