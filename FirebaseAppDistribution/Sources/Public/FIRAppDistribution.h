866ac975dbd71753935c00a9dcc2ff3da948333b
Jeremy Durham <jeremydurham@google.com>
2020-04-15 01:12:09 -0400

    Replace include with class forward

--- FirebaseAppDistribution/Sources/Public/FIRAppDistribution.h
+++ FirebaseAppDistribution/Sources/Public/FIRAppDistribution.h
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "FIRAppDistributionRelease.h"
-
+@class FIRAppDistributionRelease;
 #import <AppAuth/AppAuth.h>
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
