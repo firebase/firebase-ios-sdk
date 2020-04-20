3c1033de53befd366600c6896e49857de7a00afd
samedson <samedson@google.com>
2020-04-20 11:56:49 -0400

    Period

--- Crashlytics/Crashlytics/Controllers/FIRCLSReportManager.m
+++ Crashlytics/Crashlytics/Controllers/FIRCLSReportManager.m
@@ -527,7 +527,7 @@ static void (^reportSentCallback)(void);
   // failing computeExecutableInfo. This can happen if the user sets the
   // Exported Symbols File in Build Settings, and leaves off the one symbol
   // that Crashlytics needs, "__mh_execute_header" (wich is defined in mach-o/ldsyms.h as
-  // _MH_EXECUTE_SYM) From https://github.com/firebase/firebase-ios-sdk/issues/5020
+  // _MH_EXECUTE_SYM). From https://github.com/firebase/firebase-ios-sdk/issues/5020
   if (!self.appIDModel) {
     FIRCLSErrorLog(
         @"Crashlytics could not find the main file symbol, and cannot start up. This can happen "
