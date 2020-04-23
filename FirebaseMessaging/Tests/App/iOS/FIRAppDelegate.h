eef949b377ea418d1b5d49f720f635337b8b7d42
Chen Liang <chliang@google.com>
2020-04-22 17:36:57 -0700

    update UIKit import syntax

--- FirebaseMessaging/Tests/App/iOS/FIRAppDelegate.h
+++ FirebaseMessaging/Tests/App/iOS/FIRAppDelegate.h
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-@import UIKit;
+#import <UIKit/UIKit.h>
 
 @interface FIRAppDelegate : UIResponder <UIApplicationDelegate>
 
--- FirebaseMessaging/Tests/App/iOS/main.m
+++ FirebaseMessaging/Tests/App/iOS/main.m
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@import UIKit;
+#import <UIKit/UIKit.h>
 #import "FIRAppDelegate.h"
 
 int main(int argc, char* argv[]) {
