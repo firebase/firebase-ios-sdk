d5fa470ab44cae2e083094143d280836ab45f922
Peter Friese <peter@peterfriese.de>
2020-04-28 17:30:50 +0200

    First working version of Query.getDocuments() publisher

--- Firestore/Swift/Source/Combine/Query+Combine.swift
+++ Firestore/Swift/Source/Combine/Query+Combine.swift
@@ -20,59 +20,75 @@
 import Foundation
 import FirebaseFirestore
 
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
 extension Query {
 
-  @available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
-  struct DocumentPublisher<Output: DocumentSnapshot>: Publisher {
-    typealias Output = DocumentSnapshot
-    typealias Failure = Error
-    
-    private let query: Query
-    
-    init(query: Query) {
-      self.query = query
-    }
-    
-    func receive<S>(subscriber: S) where S : Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
-      let subscription = DocumentSubscription(subscriber: subscriber, query: query)
-      subscriber.receive(subscription: subscription)
-    }
-    
-  }
-  
-  @available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
-  public func getDocuments() -> AnyPublisher<DocumentSnapshot, Error> {
-    return DocumentPublisher(query: self).eraseToAnyPublisher()
-  }
-
-  @available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
-  public func dummyPublisher() -> AnyPublisher<Int, Never> {
-    return Just(42).eraseToAnyPublisher()
-  }  
-  
-}
-
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
-extension Query.DocumentPublisher {
-  
-  @available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
-  class DocumentSubscription<S: Subscriber>: Subscription where S.Input == DocumentSnapshot, S.Failure == Error {
-    private let query: Query
-    private var subscriber: S?
-    
-    init(subscriber: S, query: Query) {
-      self.subscriber = subscriber
-      self.query = query
-    }
-    
-    func request(_ demand: Subscribers.Demand) {
-      if (demand > 0) {
+  /**
+   * Reads the documents matching this query.
+   *
+   * This method returns a publisher that yields an array
+   * of `DocumentSnapshot`s, allowing the user to easily iterate over
+   * the documents themselves:
+   *
+   * ```
+   * let noBooks = [Book]()
+   * db.collection("books").getDocuments()
+   *   .map { querySnapshot in
+   *     querySnapshot.documents.compactMap { (queryDocumentSnapshot) in
+   *       return try? queryDocumentSnapshot.data(as: Book.self)
+   *     }
+   *   }
+   *   .replaceError(with: noBooks)
+   *   .assign(to: \.books, on: self)
+   *   .store(in: &cancellables)
+   * ```
+   */
+  public func getDocuments() -> AnyPublisher<QuerySnapshot, Error> {
+    Future<QuerySnapshot, Error> { [weak self] promise in
+      self?.getDocuments { (querySnapshot, error) in
+        if let error = error {
+          promise(.failure(error))
+        }
+        else if let querySnapshot = querySnapshot {
+          promise(.success(querySnapshot))
+        }
       }
     }
-    
-    func cancel() {
-      subscriber = nil
+    .eraseToAnyPublisher()
+  }
+  
+  /**
+   * Reads the documents matching this query.
+   *
+   * This method returns a publisher that yields an array
+   * of `DocumentSnapshot`s, allowing the user to easily iterate over
+   * the documents themselves:
+   *
+   * ```
+   * let noBooks = [Book]()
+   * db.collection("books").getDocuments2()
+   *   .map { documentSnapshots in
+   *     documentSnapshots.compactMap { documentSnapshot -> Book? in
+   *       return try? documentSnapshot.data(as: Book.self)
+   *     }
+   *   }
+   *   .replaceError(with: noBooks)
+   *   .assign(to: \.books, on: self)
+   *   .store(in: &cancellables)
+   * ```
+   */
+  public func getDocuments2() -> AnyPublisher<[DocumentSnapshot], Error> {
+    Future<[DocumentSnapshot], Error> { [weak self] promise in
+      self?.getDocuments(completion: { (querySnapshot, error) in
+        if let error = error {
+          promise(.failure(error))
+        }
+        else if let querySnapshot = querySnapshot {
+          promise(.success(querySnapshot.documents))
+        }
+      })
     }
+    .eraseToAnyPublisher()
   }
 
 }
