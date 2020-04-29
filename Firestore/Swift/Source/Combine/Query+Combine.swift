9f87419bdcc96db20f9299e83b914d9d2c0c346e
Peter Friese <peter@peterfriese.de>
2020-04-29 12:18:54 +0200

    Update code style / fix linting

--- Firestore/Swift/Source/Combine/Query+Combine.swift
+++ Firestore/Swift/Source/Combine/Query+Combine.swift
@@ -16,79 +16,78 @@
 
 #if canImport(Combine)
   import Combine
-#endif
-import Foundation
-import FirebaseFirestore
 
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
-extension Query {
+  import Foundation
+  import FirebaseFirestore
 
-  /**
-   * Reads the documents matching this query.
-   *
-   * This method returns a publisher that yields an array
-   * of `QuerySnapshot`s, requiring the user to extract the underlying
-   * `DocumentSnapshot`s before using them:
-   *
-   * ```
-   * let noBooks = [Book]()
-   * db.collection("books").getDocuments()
-   *   .map { querySnapshot in
-   *     querySnapshot.documents.compactMap { (queryDocumentSnapshot) in
-   *       return try? queryDocumentSnapshot.data(as: Book.self)
-   *     }
-   *   }
-   *   .replaceError(with: noBooks)
-   *   .assign(to: \.books, on: self)
-   *   .store(in: &cancellables)
-   * ```
-   */
-  public func getDocuments() -> AnyPublisher<QuerySnapshot, Error> {
-    Future<QuerySnapshot, Error> { [weak self] promise in
-      self?.getDocuments { (querySnapshot, error) in
-        if let error = error {
-          promise(.failure(error))
-        }
-        else if let querySnapshot = querySnapshot {
-          promise(.success(querySnapshot))
+  @available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
+  extension Query {
+    /**
+     * Reads the documents matching this query.
+     *
+     * This method returns a publisher that yields an array
+     * of `QuerySnapshot`s, requiring the user to extract the underlying
+     * `DocumentSnapshot`s before using them:
+     *
+     * ```
+     * let noBooks = [Book]()
+     * db.collection("books").getDocuments()
+     *   .map { querySnapshot in
+     *     querySnapshot.documents.compactMap { (queryDocumentSnapshot) in
+     *       return try? queryDocumentSnapshot.data(as: Book.self)
+     *     }
+     *   }
+     *   .replaceError(with: noBooks)
+     *   .assign(to: \.books, on: self)
+     *   .store(in: &cancellables)
+     * ```
+     */
+    public func getDocuments() -> AnyPublisher<QuerySnapshot, Error> {
+      Future<QuerySnapshot, Error> { [weak self] promise in
+        self?.getDocuments { querySnapshot, error in
+          if let error = error {
+            promise(.failure(error))
+          } 
+          else if let querySnapshot = querySnapshot {
+            promise(.success(querySnapshot))
+          }
         }
       }
+      .eraseToAnyPublisher()
     }
-    .eraseToAnyPublisher()
-  }
-  
-  /**
-   * Reads the documents matching this query.
-   *
-   * This method returns a publisher that yields an array
-   * of `DocumentSnapshot`s, allowing the user to easily iterate over
-   * the documents themselves:
-   *
-   * ```
-   * let noBooks = [Book]()
-   * db.collection("books").getDocuments2()
-   *   .map { documentSnapshots in
-   *     documentSnapshots.compactMap { documentSnapshot -> Book? in
-   *       return try? documentSnapshot.data(as: Book.self)
-   *     }
-   *   }
-   *   .replaceError(with: noBooks)
-   *   .assign(to: \.books, on: self)
-   *   .store(in: &cancellables)
-   * ```
-   */
-  public func getDocuments2() -> AnyPublisher<[DocumentSnapshot], Error> {
-    Future<[DocumentSnapshot], Error> { [weak self] promise in
-      self?.getDocuments(completion: { (querySnapshot, error) in
-        if let error = error {
-          promise(.failure(error))
-        }
-        else if let querySnapshot = querySnapshot {
-          promise(.success(querySnapshot.documents))
-        }
-      })
-    }
-    .eraseToAnyPublisher()
-  }
 
-}
+    /**
+     * Reads the documents matching this query.
+     *
+     * This method returns a publisher that yields an array
+     * of `DocumentSnapshot`s, allowing the user to easily iterate over
+     * the documents themselves:
+     *
+     * ```
+     * let noBooks = [Book]()
+     * db.collection("books").getDocuments2()
+     *   .map { documentSnapshots in
+     *     documentSnapshots.compactMap { documentSnapshot -> Book? in
+     *       return try? documentSnapshot.data(as: Book.self)
+     *     }
+     *   }
+     *   .replaceError(with: noBooks)
+     *   .assign(to: \.books, on: self)
+     *   .store(in: &cancellables)
+     * ```
+     */
+    public func getDocuments2() -> AnyPublisher<[DocumentSnapshot], Error> {
+      Future<[DocumentSnapshot], Error> { [weak self] promise in
+        self?.getDocuments(completion: { querySnapshot, error in
+          if let error = error {
+            promise(.failure(error))
+          } 
+          else if let querySnapshot = querySnapshot {
+            promise(.success(querySnapshot.documents))
+          }
+        })
+      }
+      .eraseToAnyPublisher()
+    }
+  }
+#endif
