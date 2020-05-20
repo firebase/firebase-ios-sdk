02a9b2a44ba07408eaf0b13193cf5ec976895df6
Paul Beusterien <paulbeusterien@google.com>
2020-05-20 12:19:31 -0700

    style

--- FirebaseStorageSwift/Tests/Integration/StorageIntegration.swift
+++ FirebaseStorageSwift/Tests/Integration/StorageIntegration.swift
@@ -561,7 +561,6 @@ class StorageIntegration: XCTestCase {
     waitForExpectations()
   }
 
-
   private func waitForExpectations() {
     let kFIRStorageIntegrationTestTimeout = 60.0
     waitForExpectations(timeout: kFIRStorageIntegrationTestTimeout,
