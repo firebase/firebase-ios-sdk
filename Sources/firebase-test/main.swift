0a326e5d5290399cb58d333f7e124bf2813a99c5
Paul Beusterien <paulbeusterien@google.com>
2020-05-30 07:32:53 -0700

    swift run firebase-test for Core and Storage

--- Sources/firebase-test/main.swift
+++ Sources/firebase-test/main.swift
@@ -1,10 +1,10 @@
 import Foundation
 // import Firebase // TODO mac wants module imports, iOS requires non-modular imports in Firebase.h
-//import FirebaseCore
+import FirebaseCore
 // import FirebaseAuth
 // import FirebaseFunctions
 // import FirebaseInstanceID
-//import FirebaseStorage
+import FirebaseStorage
 import GoogleUtilities_Environment
 import GoogleUtilities_Logger
 
@@ -17,8 +17,8 @@ print("System version? Answer: \(GULAppEnvironmentUtil.systemVersion() ?? "NONE"
 print("Is App extension? Answer: \(GULAppEnvironmentUtil.isAppExtension())")
 print("Is iOS 7 or higher? Answer: \(GULAppEnvironmentUtil.isIOS7OrHigher())")
 
-//print("Is there a default app? Answer: \(FirebaseApp.app() != nil)")
+print("Is there a default app? Answer: \(FirebaseApp.app() != nil)")
 
-//print("Storage Version String? Answer: \(StorageVersionString)")
+print("Storage Version String? Answer: \(String(cString:StorageVersionString))")
 
-// print("InstanceIDScopeFirebaseMessaging? Answer: \(InstanceIDScopeFirebaseMessaging)")
+//print("InstanceIDScopeFirebaseMessaging? Answer: \(InstanceIDScopeFirebaseMessaging)")
