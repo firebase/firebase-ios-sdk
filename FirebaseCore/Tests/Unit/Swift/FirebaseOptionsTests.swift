54c3950f335b0a51b338db65ad5a541379a53e0c
Nicholas Cooke <ncooke3@gatech.edu>
2020-06-02 18:12:25 -0400

    Added LLC to Copyright comments and testable import tag

--- FirebaseCore/Tests/Unit/Swift/FirebaseOptionsTests.swift
+++ FirebaseCore/Tests/Unit/Swift/FirebaseOptionsTests.swift
@@ -1,4 +1,4 @@
-// Copyright 2020 Google
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import XCTest
-import FirebaseCore
+@testable import FirebaseCore
 
 class FirebaseOptionsTests: XCTestCase {
   func testDefaultOptions() throws {
