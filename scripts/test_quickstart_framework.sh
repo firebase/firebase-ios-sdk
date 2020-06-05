c47f315b3faf845db7196c0bdc7af6d877c5e1d4
granluo <ollkorrect999@gmail.com>
2020-06-05 16:21:50 -0700

    test video recording

---
+++ scripts/test_quickstart_framework.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+xcodebuild test -project ConfigExample.xcodeproj -scheme ConfigExample -destination 'platform=iOS Simulator,name=iPhone 11 Pro' "OTHER_LDFLAGS=\$(OTHER_LDFLAGS) -ObjC" "FRAMEWORK_SEARCH_PATHS= \$(PROJECT_DIR)/Firebase" HEADER_SEARCH_PATHS='$(PROJECT_DIR)/Firebase' &
+pid=$!
+sleep 4 
+if ps -p $pid > /dev/null
+then
+	xcrun simctl io booted recordVideo abc.mov &
+  FOO_PID=$!
+  echo "foo pid is $FOO_PID"
+  while ps -p $pid; do sleep 1; done ; kill -2 "$FOO_PID"
+fi
+
