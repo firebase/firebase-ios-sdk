5d4015ea2802d097fd2004997b73da0eff94a920
Paul Beusterien <paulbeusterien@google.com>
2020-06-06 18:01:04 -0700

    style

--- FirebaseCore/Tests/Unit/Swift/FirebaseCore-unit-Bridging-Header.h
+++ FirebaseCore/Tests/Unit/Swift/FirebaseCore-unit-Bridging-Header.h
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "FirebaseCore/Tests/Unit/Swift/SwiftTestingUtilities/ExceptionCatcher.h"
 #import "FirebaseCore/Sources/Private/FIRAppInternal.h"
+#import "FirebaseCore/Tests/Unit/Swift/SwiftTestingUtilities/ExceptionCatcher.h"
