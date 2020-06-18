f1e1bb071e1902bc86b5be32a24f16446d73d129
Paul Beusterien <paulbeusterien@google.com>
2020-06-18 16:03:37 -0700

    Fix check module check

--- scripts/check_no_module_imports.sh
+++ scripts/check_no_module_imports.sh
@@ -31,6 +31,7 @@ git grep "${options[@]}" \
   -- ':(exclude,glob)**/Example/**' ':(exclude,glob)**/Sample/**' \
      ':(exclude)FirebaseCore/Sources/Private/FirebaseCoreInternal.h' \
      ':(exclude)FirebaseInstallations/Source/Library/Private/FirebaseInstallationsInternal.h' \
+     ':(exclude,glob)FirebaseStorage/**' \
      ':(exclude)GoogleUtilities/NSData+zlib/Private/GULNSDataInternal.h' \
      ':(exclude)GoogleUtilities/Logger/Private/GULLogger.h' \
      ':(exclude)HeadersImports.md' && exit_with_error
