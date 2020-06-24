945bd0ae34464be664a1a5054c8ada36f1b82e9b
Maksym Malyhin <mmaksym@google.com>
2020-06-24 18:53:49 -0400

    Fix analyzer issue

--- GoogleDataTransport/GDTCORLibrary/GDTCOREvent.m
+++ GoogleDataTransport/GDTCORLibrary/GDTCOREvent.m
@@ -45,9 +45,11 @@
     _target = target;
     _qosTier = GDTCOREventQosDefault;
     _expirationDate = [NSDate dateWithTimeIntervalSinceNow:604800];  // 7 days.
+    
+    GDTCORLogDebug(@"Event %@ created. ID:%@ mappingID: %@ target:%ld", self, _eventID, mappingID,
+    (long)target);
   }
-  GDTCORLogDebug(@"Event %@ created. ID:%@ mappingID: %@ target:%ld", self, _eventID, mappingID,
-                 (long)target);
+
   return self;
 }
 
