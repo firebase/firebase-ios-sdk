4c4911bf97330f60e186a1681157158a02ab8a77
granluo <ollkorrect999@gmail.com>
2020-07-01 14:31:41 -0700

    Update path

--- scripts/setup_quickstart_framework.sh
+++ scripts/setup_quickstart_framework.sh
@@ -40,4 +40,4 @@ do
   fi
 done
 
-"${GITHUB_REPOSITORY}"/scripts/add_framework_script.rb --sdk "${SAMPLE}" --target "${TARGET}" --framework_path Firebase/
+../../scripts/add_framework_script.rb --sdk "${SAMPLE}" --target "${TARGET}" --framework_path Firebase/
