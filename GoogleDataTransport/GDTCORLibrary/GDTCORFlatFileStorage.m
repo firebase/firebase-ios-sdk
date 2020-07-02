a54e7408858beb0b7efc9d37d0a3c80fbc587e26
Maksym Malyhin <mmaksym@google.com>
2020-07-02 10:36:42 -0400

    formatting

--- GoogleDataTransport/GDTCORLibrary/GDTCORFlatFileStorage.m
+++ GoogleDataTransport/GDTCORLibrary/GDTCORFlatFileStorage.m
@@ -232,7 +232,7 @@ NSString *const kGDTCORBatchComponentsExpirationKey = @"GDTCORBatchComponentsExp
 
     NSFileManager *fileManager = [NSFileManager defaultManager];
 
-    void(^removeBatchDir)(NSString *batchDirPath)  = ^(NSString *batchDirPath) {
+    void (^removeBatchDir)(NSString *batchDirPath) = ^(NSString *batchDirPath) {
       NSError *error;
       if ([fileManager removeItemAtPath:batchDirPath error:&error]) {
         GDTCORLogDebug(@"Batch removed at path: %@", batchDirPath);
@@ -251,9 +251,12 @@ NSString *const kGDTCORBatchComponentsExpirationKey = @"GDTCORBatchComponentsExp
         NSString *destinationPath = [[GDTCORFlatFileStorage eventDataStoragePath]
             stringByAppendingPathComponent:target.stringValue];
 
-        // `- [NSFileManager moveItemAtPath:toPath:error:] method fails if an item by the destination path already exists (which usually is the case for the current method). Move the events one by one instead.
+        // `- [NSFileManager moveItemAtPath:toPath:error:] method fails if an item by the
+        // destination path already exists (which usually is the case for the current method). Move
+        // the events one by one instead.
         if ([self moveContentsOfDirectoryAtPath:batchDirPath to:destinationPath error:&error]) {
-          GDTCORLogDebug(@"Batched events at path: %@ moved back to the storage: %@", batchDirPath, destinationPath);
+          GDTCORLogDebug(@"Batched events at path: %@ moved back to the storage: %@", batchDirPath,
+                         destinationPath);
         } else {
           GDTCORLogDebug(@"Error encountered whilst moving events back: %@", error);
         }
