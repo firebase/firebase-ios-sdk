498ccf0292f06df3b5a8d890a7f28411dfaf707d
Paul Beusterien <paulbeusterien@google.com>
2020-07-06 10:54:34 -0700

    Repo-relative path

--- FirebaseABTesting/Sources/ABTExperimentPayload.m
+++ FirebaseABTesting/Sources/ABTExperimentPayload.m
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "ABTExperimentPayload.h"
+#import "FirebaseABTesting/Sources/Private/ABTExperimentPayload.h"
 
 static NSString *const kExperimentPayloadKeyExperimentID = @"experimentId";
 static NSString *const kExperimentPayloadKeyVariantID = @"variantId";
