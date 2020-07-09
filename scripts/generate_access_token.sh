4169f7884d263f29931b09ce6606787d9e8803e1
Nick Cooke <ncooke3@gatech.edu>
2020-07-09 12:47:32 -0400

    Updated README and script

--- scripts/generate_access_token.sh
+++ scripts/generate_access_token.sh
@@ -1,4 +1,4 @@
-  #!/bin/bash
+#!/bin/bash
 
 # Copyright 2020 Google LLC
 #
@@ -14,12 +14,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This script generates access tokens that are needed to make admin API
+# calls to the Firebase Console. The script takes a single argument
+# `output` which represents the location for where the outputted access
+# token will be stored. This script uses Google's Swift Auth Client Library.
+#
+# Visit the repo: https://github.com/googleapis/google-auth-library-swift
+#
+# Generated tokens are `JSON` in the form:
+# {
+#     "token_type":"Bearer",
+#     "expires_in":3599,
+#     "access_token":"1234567890ABCDEFG"
+# }
+
 output="$1"
+
+# The access token is generated using a downloaded Service Account from a Firebase Project.
+# This can be downloaded from Firebase console under 'Project Settings'.
+# Store the downloaded .json file in `$HOME/.credentials/` and point the env var to it.
+export GOOGLE_APPLICATION_CREDENTIALS="$HOME/.credentials/ServiceAccount.json"
 
 git clone https://github.com/googleapis/google-auth-library-swift.git
 cd google-auth-library-swift
-git checkout --quiet 7b1c9cd4ffd8cb784bcd8b7fd599794b69a810cf # This commit will work.
+git checkout --quiet 7b1c9cd4ffd8cb784bcd8b7fd599794b69a810cf # Working main branch as of 7/9/20.
 make -f Makefile
-export GOOGLE_APPLICATION_CREDENTIALS="$HOME/.credentials/ServiceAccount.json"
 swift run TokenSource > $output
-echo Access token generated!
