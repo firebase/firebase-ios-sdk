bd4a49fc20db0c2f58349d31eb90157d2ea60c1c
Chen Liang <chliang@google.com>
2020-07-21 17:38:52 -0700

    revert swift format file

--- FirebaseABTesting/Tests/Integration/ABTQA/AppDelegate.swift
+++ FirebaseABTesting/Tests/Integration/ABTQA/AppDelegate.swift
@@ -20,9 +20,8 @@ import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [
-                     UIApplication.LaunchOptionsKey: Any
-                   ]?) -> Bool {
+                   didFinishLaunchingWithOptions
+                   launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     FirebaseApp.configure()
     return true
