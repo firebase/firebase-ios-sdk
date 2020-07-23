6bee3d1b49a83bb37a291bd2f27a284f73c66d54
Jeremy Durham <jeremydurham@google.com>
2020-07-23 19:03:09 -0400

    Fix import

--- FirebaseAppDistribution/Sources/Public/FirebaseAppDistribution.h
+++ FirebaseAppDistribution/Sources/Public/FirebaseAppDistribution.h
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "FirebaseAppDistribution/Sources/Public/FIRAppDistribution.h"
-#import "FirebaseAppDistribution/Sources/Public/FIRAppDistributionRelease.h"
+#import "FIRAppDistribution.h"
+#import "FIRAppDistributionRelease.h"
