175d02c9d6d25e4666575d17f7711518559e4a13
granluo <ollkorrect999@gmail.com>
2020-07-27 11:10:01 -0700

    Add licence

--- scripts/add_podspecs.swift
+++ scripts/add_podspecs.swift
@@ -1,4 +1,21 @@
 #!/usr/bin/swift
+
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import Foundation
 
 let _DEPENDENCY_LABEL_IN_SPEC = "dependency"
@@ -107,6 +124,7 @@ func shell(_ command: String) -> Int32 {
 
   let data = pipe.fileHandleForReading.readDataToEndOfFile()
   let output = String(data: data, encoding: .utf8)!
+  print(output)
 
   return task.terminationStatus
 }
