6abd2b29c87569ab14f2b8f7797a8f465ef4aabc
Denver Coneybeare <dconeybe@google.com>
2020-08-04 09:46:38 -0400

    Cherry-pick 0a6618705389543191a8c44278d4c6791c84602a
    Add lint checking for Objective-C++ sources (#1048)
    
    * lint.sh now lints Objective-C++ too
    
    * cpplint checks system-style includes that should be user-style
    
    This prevents it from recognizing project sources as if they were C
    system headers and then complaining that they're in the wrong place.
    
    * cpplint checks #imports and #includes
    
    * cpplint checks that C++ system headers aren't #imported
    
    * cpplint checks for C system headers that could be C++ system headers
    
    * cpplint checks that Objective-C sources include their headers

--- scripts/cpplint.py
+++ scripts/cpplint.py
@@ -717,6 +717,11 @@ C_STANDARD_HEADER_FOLDERS = frozenset([
     "xen",
   ])
 
+_C_SYSTEM_DIRECTORIES = frozenset([
+    'libkern',
+    'sys',
+])
+
 # Type names
 _TYPES = re.compile(
     r'^(?:'
@@ -937,7 +942,7 @@ def GetHeaderExtensions():
 # This is set by --extensions flag
 def GetAllExtensions():
   return GetHeaderExtensions().union(_valid_extensions or set(
-    ['c', 'cc', 'cpp', 'cxx', 'c++', 'cu']))
+    ['c', 'cc', 'cpp', 'cxx', 'c++', 'cu', 'm', 'mm']))
 
 def ProcessExtensionsOption(val):
   global _valid_extensions
@@ -4928,7 +4933,7 @@ def CheckStyle(filename, clean_lines, linenum, file_extension, nesting_state,
     CheckSectionSpacing(filename, clean_lines, classinfo, linenum, error)
 
 
-_RE_PATTERN_INCLUDE = re.compile(r'^\s*#\s*include\s*([<"])([^>"]*)[>"].*$')
+_RE_PATTERN_INCLUDE = re.compile(r'^\s*#\s*(?:include|import)\s*([<"])([^>"]*)[>"].*$')
 # Matches the first component of a filename delimited by -s and _s. That is:
 #  _RE_FIRST_COMPONENT.match('foo').group(0) == 'foo'
 #  _RE_FIRST_COMPONENT.match('foo.cc').group(0) == 'foo'
@@ -4964,6 +4969,11 @@ def _DropCommonSuffixes(filename):
     if (filename.endswith(suffix) and len(filename) > len(suffix) and
         filename[-len(suffix) - 1] in ('-', '_')):
       return filename[:-len(suffix) - 1]
+
+  for suffix in ['Tests.h', 'Test.m', 'Test.mm', 'Tests.m', 'Tests.mm']:
+    if (filename.endswith(suffix) and len(filename) > len(suffix)):
+      return filename[:-len(suffix)]
+
   return os.path.splitext(filename)[0]
 
 
@@ -5058,6 +5068,30 @@ def CheckIncludeLine(filename, clean_lines, linenum, include_state, error):
   """
   fileinfo = FileInfo(filename)
   line = clean_lines.lines[linenum]
+
+  # system-style includes should not be used for project includes
+  match = Match(r'#include\s*<(([^/>]+)/[^>]+)', line)
+  if match:
+    if match.group(2) not in _C_SYSTEM_DIRECTORIES:
+      error(filename, linenum, 'build/include', 4,
+            '<%s> should be #include "%s" or #import <%s>' %
+            (match.group(1), match.group(1), match.group(1)))
+
+  # C++ system files should not be #imported
+  match = Match(r'#import\s*<([^/>.]+)>', line)
+  if match:
+    error(filename, linenum, 'build/include', 4,
+          'C++ header <%s> was #imported. Should be #include <%s>' %
+          (match.group(1), match.group(1)))
+
+  # Prefer C++ wrappers for C headers
+  match = Match(r'#include\s*<(([^>]+).h)>', line)
+  if match:
+    wrapper = 'c' + match.group(2)
+    if wrapper in _CPP_HEADERS:
+        error(filename, linenum, 'build/include', 4,
+              'Prefer C++ header <%s> for C system header %s' %
+              (wrapper, match.group(1)))
 
   # "include" should use the new style "foo/bar.h" instead of just "bar.h"
   # Only do this check if the included header follows google naming
