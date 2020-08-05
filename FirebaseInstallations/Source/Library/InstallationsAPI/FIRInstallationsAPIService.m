2e3a33ef9b6fa57e14c66632dea3f34c429389ec
Maksym Malyhin <mmaksym@google.com>
2020-08-05 16:34:34 -0400

    FirebaseInstallations: API requests backoff (#6207)
    * FIRInstallationsBackoffController introduced
    
    * FIRCurrentDateProvider introduced
    
    * FIRInstallationsBackoffControllerTests
    
    * style
    
    * FIRInstallationsBackoffController implementation
    
    * WIP: tests
    
    * Response to backoff event mapping
    
    * FIRInstallationsIDController: integration with FIRInstallationsBackoffController
    
    * Remove not existing error code.
    
    * FIRInstallationsIDController: integration with FIRInstallationsBackoffController
    
    * FIRInstallationsIDController: depend on FIRInstallationsBackoffControllerProtocol
    
    * FIRInstallationsIDControllerTests: supplement tests with backoff validation
    
    * TODOs
    
    * TODO
    
    * API docs
    
    * Header import fixed

--- FirebaseInstallations/Source/Library/InstallationsAPI/FIRInstallationsAPIService.m
+++ FirebaseInstallations/Source/Library/InstallationsAPI/FIRInstallationsAPIService.m
@@ -26,6 +26,7 @@
 
 #import "FirebaseCore/Sources/Private/FirebaseCoreInternal.h"
 #import "FirebaseInstallations/Source/Library/Errors/FIRInstallationsErrorUtil.h"
+#import "FirebaseInstallations/Source/Library/Errors/FIRInstallationsHTTPError.h"
 #import "FirebaseInstallations/Source/Library/FIRInstallationsLogger.h"
 #import "FirebaseInstallations/Source/Library/InstallationsAPI/FIRInstallationsItem+RegisterInstallationAPI.h"
 
@@ -333,7 +334,8 @@ NS_ASSUME_NONNULL_END
   return [FBLPromise attempts:1
       delay:1
       condition:^BOOL(NSInteger remainingAttempts, NSError *_Nonnull error) {
-        return [FIRInstallationsErrorUtil isAPIError:error withHTTPCode:500];
+        return [FIRInstallationsErrorUtil isAPIError:error
+                                        withHTTPCode:FIRInstallationsHTTPCodesServerInternalError];
       }
       retry:^id _Nullable {
         return [self URLRequestPromise:request];
