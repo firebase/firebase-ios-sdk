02f025d01ac0a5d50ab65e863973b07d1da0ae46
granluo <ollkorrect999@gmail.com>
2020-08-07 14:54:48 -0700

    Refactor code.

--- scripts/add_podspecs.swift
+++ scripts/add_podspecs.swift
@@ -21,6 +21,10 @@ import Foundation
 let _DEPENDENCY_LABEL_IN_SPEC = "dependency"
 let _SKIP_LINES_WITH_WORDS = ["unit_tests", "test_spec"]
 let _DEPENDENCY_LINE_SEPARATORS = [" ", ",", "/"] as CharacterSet
+let _POD_SOURCES = ["https://github.com/firebase/SpecsStaging.git", "https://cdn.cocoapods.org/"]
+let _FLAGS = ["--skip-tests"]
+let _FIREBASE_FLAGS = _FLAGS + ["--skip-import-validation", "--use-json"]
+let _FIREBASEFIRESTORE_FLAGS = _FLAGS + ["--allow-warnings"]
 
 class SpecFiles {
   private var specFilesDict: [String: URL]
@@ -83,16 +87,16 @@ func searchDeps(of pod: String, from podSpecFilesObj: SpecFiles) -> [String] {
     fatalError("Could not read \(pod) podspec from \(podSpecURL.path).")
   }
   for line in fileContents.components(separatedBy: .newlines) {
-    if _SKIP_LINES_WITH_WORDS.contains(where: line.contains) {
-      continue
-    }
     if line.contains(_DEPENDENCY_LABEL_IN_SPEC) {
+      if _SKIP_LINES_WITH_WORDS.contains(where: line.contains) {
+        continue
+      }
       let newLine = line.trimmingCharacters(in: .whitespacesAndNewlines)
       let tokens = newLine.components(separatedBy: _DEPENDENCY_LINE_SEPARATORS)
       if let depPrefix = tokens.first {
         if depPrefix.hasSuffix(_DEPENDENCY_LABEL_IN_SPEC) {
           let podNameRaw = String(tokens[1]).replacingOccurrences(of: "'", with: "")
-          deps.append(podNameRaw)
+          if podNameRaw != pod { deps.append(podNameRaw) }
         }
       }
     }
@@ -134,6 +138,30 @@ func shell(_ command: String) -> Int32 {
   return task.terminationStatus
 }
 
+func push_podspec(_ pod: String, from sdk_repo: String, sources: [String],
+                  flags: [String]) -> Int32? {
+  let pod_path = sdk_repo + "/" + pod + ".podspec"
+  let sources_arg = sources.joined(separator: ",")
+  let flags_arg = flags.joined(separator: " ")
+
+  let exit_code =
+    shell("pod repo push ${SPEC_REPO} \(pod_path) --sources=\(sources_arg) \(flags_arg)")
+  shell("pod repo update")
+
+  return exit_code
+}
+
+func erase_remote_branch(){
+  shell("git clone --quiet https://${BOT_TOKEN}@github.com/firebase/SpecsStaging.git")
+  shell("cd SpecsStaging; git rm -r *; git commit -m 'Empty repo'; git push")
+  do {
+    try fileManager.removeItem(at: URL(fileURLWithPath: "\(cur_dir)/SpecsStaging"))
+    print("Specsstaging dir is removed.")
+  } catch {
+    print("error occurred. \(error)")
+  }
+}
+
 let arg_cnts: Int = Int(CommandLine.argc)
 let cur_dir = FileManager().currentDirectoryPath
 var sdk_repo = cur_dir
@@ -158,55 +186,50 @@ do {
     let podName = podspecURL.deletingPathExtension().lastPathComponent
     podSpecFiles[podName] = podspecURL
   }
-  var tmpSet: Set<String> = []
-  print("Detect podspecs: \(podSpecFiles.keys)")
-  let specFile = SpecFiles(podSpecFiles)
-  generateOrderOfInstallation(
-    pods: Array(podSpecFiles.keys),
-    podSpecDict: specFile,
-    parentDeps: &tmpSet
-  )
-  print(specFile.depInstallOrder.joined(separator: "\n"))
-
-  do {
-    if fileManager.fileExists(atPath: "\(cur_dir)/SpecsStaging") {
-      print("remove specsstaging dir.")
-      try fileManager.removeItem(at: URL(fileURLWithPath: "\(cur_dir)/SpecsStaging"))
-    }
-    shell("git clone --quiet https://${BOT_TOKEN}@github.com/firebase/SpecsStaging.git")
-    shell("cd SpecsStaging; git rm -r *; git commit -m 'Empty repo'; git push")
-    try fileManager.removeItem(at: URL(fileURLWithPath: "\(cur_dir)/SpecsStaging"))
-    print("Specsstaging dir is removed.")
-  } catch {
-    print("error occurred.")
-  }
-  var exitCode: Int32?
-  for pod in specFile.depInstallOrder {
-    print("----------\(pod)-----------")
-    if pod == "Firebase" {
-      exitCode =
-        shell(
-          "find \(sdk_repo) -name \(pod).podspec -print -exec pod repo push ${SPEC_REPO} {} --sources=https://github.com/firebase/SpecsStaging.git,https://cdn.cocoapods.org/ --skip-import-validation --skip-tests --use-json \\; -exec pod repo update \\;"
-        )
-    } else if pod == "FirebaseFirestore" {
-      exitCode =
-        shell(
-          "find \(sdk_repo) -name \(pod).podspec -print -exec pod repo push ${SPEC_REPO} {} --sources=https://github.com/firebase/SpecsStaging.git,https://cdn.cocoapods.org/ --allow-warnings --skip-tests \\; -exec pod repo update \\;"
-        )
-
-    } else {
-      exitCode =
-        shell(
-          "find \(sdk_repo) -name \(pod).podspec -print -exec pod repo push ${SPEC_REPO} {} --sources=https://github.com/firebase/SpecsStaging.git,https://cdn.cocoapods.org/ --skip-tests \\; -exec pod repo update \\;"
-        )
-    }
-
-    if let code = exitCode {
-      print("------------exit code : \(code) \(pod)-----------------")
-    } else {
-      print(" Does not have a valid exitCode.")
-    }
-  }
 } catch {
   print("Error while enumerating files \(documentsURL.path): \(error.localizedDescription)")
+}
+
+var tmpSet: Set<String> = []
+print("Detect podspecs: \(podSpecFiles.keys)")
+let specFile = SpecFiles(podSpecFiles)
+generateOrderOfInstallation(
+  pods: Array(podSpecFiles.keys),
+  podSpecDict: specFile,
+  parentDeps: &tmpSet
+)
+print(specFile.depInstallOrder.joined(separator: "\n"))
+
+do {
+  if fileManager.fileExists(atPath: "\(cur_dir)/SpecsStaging") {
+    print("remove specsstaging dir.")
+    try fileManager.removeItem(at: URL(fileURLWithPath: "\(cur_dir)/SpecsStaging"))
+  } else {
+    erase_remote_branch()
+  }
+} catch {
+  print("error occurred. \(error)")
+}
+
+var exitCode: Int32?
+for pod in specFile.depInstallOrder {
+  print("----------\(pod)-----------")
+  switch pod {
+  case "Firebase":
+    exitCode = push_podspec(pod, from: sdk_repo, sources: _POD_SOURCES, flags: _FIREBASE_FLAGS)
+  case "FirebaseFirestore":
+    exitCode = push_podspec(
+      pod,
+      from: sdk_repo,
+      sources: _POD_SOURCES,
+      flags: _FIREBASEFIRESTORE_FLAGS
+    )
+  default:
+    exitCode = push_podspec(pod, from: sdk_repo, sources: _POD_SOURCES, flags: _FLAGS)
+  }
+  if let code = exitCode {
+    print("------------exit code : \(code) \(pod)-----------------")
+  } else {
+    print(" Does not have a valid exitCode.")
+  }
 }
