5149c0dd9dfbb49d39503ef12e7e18803f1daea0
granluo <ollkorrect999@gmail.com>
2020-08-27 02:31:56 -0700

    Refactor release testing setup code.

--- scripts/release_testing_setup.sh
+++ scripts/release_testing_setup.sh
@@ -12,16 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -x
+
 if [ -f "${HOME}/.cocoapods/repos" ]; then
   find "${HOME}/.cocoapods/repos" -type d -maxdepth 1 -exec sh -c 'pod repo remove $(basename {})' \;
 fi
 git config --global user.email "google-oss-bot@example.com"
 git config --global user.name "google-oss-bot"
 mkdir -p /tmp/test/firebase-ios-sdk
-git clone -b "${podspec_repo_branch}" https://github.com/firebase/firebase-ios-sdk.git /tmp/test/firebase-ios-sdk
-cd /tmp/test/firebase-ios-sdk
+git clone -b "${podspec_repo_branch}" https://github.com/firebase/firebase-ios-sdk.git "${local_sdk_repo_dir}"
+cd  "${local_sdk_repo_dir}"
 git tag -a "test" -m "release testing"
-sed  -i "" "s/\s*:git.*/:git => '\/tmp\/test\/firebase-ios-sdk',/; s/\s*:tag.*/:tag => 'test'/" *.podspec
+# Update source and tag, e.g.  ":git => 'https://github.com/firebase/firebase-ios-sdk.git'" to 
+# ":git => /tmp/test/firebase-ios-sdk"
+# ${local_sdk_repo_dir//\//\\\\\/} is to update the dir so it could be recognized by sed.
+# e.g. /tmp/test/firebase-ios-sdk to \/tmp\/test\/firebase-ios-sdk
+sed  -i "" "s/\s*:git.*/:git => '${local_sdk_repo_dir//\//\\\\\/}',/; s/\s*:tag.*/:tag => 'test'/" *.podspec
 cd "${GITHUB_WORKSPACE}/ZipBuilder"
 swift build
-./.build/debug/firebase-pod-updater --git-root "/tmp/test/firebase-ios-sdk" --releasing-pods "${GITHUB_WORKSPACE}/scripts/create_spec_repo/firebase_sdk.textproto"
+# Update Pod versions.
+./.build/debug/firebase-pod-updater --git-root "${local_sdk_repo_dir}" --releasing-pods "${GITHUB_WORKSPACE}/scripts/create_spec_repo/firebase_sdk.textproto"
