bc56eacf1445500a1798c1433693704917c14d53
Maksym Malyhin <mmaksym@google.com>
2020-09-02 16:18:27 -0400

    Reduce FIRMessagingInterop to required minimum

--- FirebaseMessaging/Sources/Interop/FIRMessagingInterop.h
+++ FirebaseMessaging/Sources/Interop/FIRMessagingInterop.h
@@ -36,66 +36,6 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @property(nonatomic, readonly, nullable) NSString *FCMToken NS_SWIFT_NAME(fcmToken);
 
-/**
- * Asynchronously gets the default FCM registration token.
- *
- * A network connection is required for the method to succeed, and data is sent to the Firebase
- * backend to validate the token. To stop this, see `Messaging.isAutoInitEnabled`,
- * `Messaging.delete(completion:)` and `Installations.delete(completion:)`.
- *
- * @param completion The completion handler to handle the token request.
- */
-
-- (void)tokenWithCompletion:(void (^)(NSString *__nullable token,
-                                      NSError *__nullable error))completion;
-
-/**
- * Asynchronously deletes the default FCM registration token.
- *
- * This does not delete all tokens for non-default sender IDs, See `Messaging.delete(completion:)`
- * for deleting all of them. To prevent token auto generation, see `Messaging.isAutoInitEnabled`.
- *
- * @param completion The completion handler to handle the token deletion.
- */
-
-- (void)deleteTokenWithCompletion:(void (^)(NSError *__nullable error))completion;
-
-/**
- *  Retrieves an FCM registration token for a particular Sender ID. This can be used to allow
- *  multiple senders to send notifications to the same device. By providing a different Sender
- *  ID than your default when fetching a token, you can create a new FCM token which you can
- *  give to a different sender. Both tokens will deliver notifications to your device, and you
- *  can revoke a token when you need to.
- *
- *  This registration token is not cached by FIRMessaging. FIRMessaging should have an APNs
- *  token set before calling this to ensure that notifications can be delivered via APNs using
- *  this FCM token. You may re-retrieve the FCM token once you have the APNs token set, to
- *  associate it with the FCM token. The default FCM token is automatically associated with
- *  the APNs token, if the APNs token data is available.
- *
- *  This creates a Firebase Installations ID, if one does not exist, and sends information
- *  about the application and the device to the Firebase backend.
- *
- *  @param senderID The Sender ID for a particular Firebase project.
- *  @param completion The completion handler to handle the token request.
- */
-- (void)retrieveFCMTokenForSenderID:(NSString *)senderID
-                         completion:(void (^)(NSString *_Nullable FCMToken,
-                                              NSError *_Nullable error))completion
-    NS_SWIFT_NAME(retrieveFCMToken(forSenderID:completion:));
-
-/**
- * Invalidates an FCM token for a particular Sender ID. That Sender ID cannot no longer send
- * notifications to that FCM token. This does not delete the Firebase Installations ID that may have
- * been created when  generating the token. See `Installations.delete(completion:)`.
- *
- * @param senderID The senderID for a particular Firebase project.
- * @param completion The completion handler to handle the token deletion.
- */
-- (void)deleteFCMTokenForSenderID:(NSString *)senderID
-                       completion:(void (^)(NSError *_Nullable error))completion
-    NS_SWIFT_NAME(deleteFCMToken(forSenderID:completion:));
-
 @end
 
 NS_ASSUME_NONNULL_END
