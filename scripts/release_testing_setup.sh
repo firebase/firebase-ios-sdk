d1b1b00697072c5a6817f9023d07099fd834efec
Gran Luo <ollkorrect999@gmail.com>
2020-09-10 10:33:22 -0700

    Update typo and silent git clone.

--- scripts/release_testing_setup.sh
+++ scripts/release_testing_setup.sh
@@ -21,10 +21,10 @@ tag_version="nightly-test-${nightly_test_version}"
 git config --global user.email "google-oss-bot@example.com"
 git config --global user.name "google-oss-bot"
 mkdir -p /tmp/test/firebase-ios-sdk
-git clone -b "${podspec_repo_branch}" https://"${BOT_TOKEN}"@github.com/firebase/firebase-ios-sdk.git "${local_sdk_repo_dir}"
+git clone -b -q "${podspec_repo_branch}" https://"${BOT_TOKEN}"@github.com/firebase/firebase-ios-sdk.git "${local_sdk_repo_dir}"
 cd  "${local_sdk_repo_dir}"
 git tag -f -a "${tag_version}" -m "release testing"
-git push origin ${tag_version}"
+git push origin "${tag_version}"
 # Update source and tag, e.g.  ":tag => 'CocoaPods-' + s.version.to_s" to
 # ":tag => test"
 sed  -i "" "s/\s*:tag.*/:tag => '${tag_version}'/" *.podspec
