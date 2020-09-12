8c1a45306697366a628e20f728df1ba1f54f02d4
Gran Luo <ollkorrect999@gmail.com>
2020-09-12 15:42:58 -0700

    Remove nightly tag before update.

--- scripts/release_testing_setup.sh
+++ scripts/release_testing_setup.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -x
+set -xe
 
 if [ -f "${HOME}/.cocoapods/repos" ]; then
   find "${HOME}/.cocoapods/repos" -type d -maxdepth 1 -exec sh -c 'pod repo remove $(basename {})' \;
@@ -23,8 +23,16 @@ git config --global user.name "google-oss-bot"
 mkdir -p /tmp/test/firebase-ios-sdk
 git clone -q -b "${podspec_repo_branch}" https://"${BOT_TOKEN}"@github.com/firebase/firebase-ios-sdk.git "${local_sdk_repo_dir}"
 cd  "${local_sdk_repo_dir}"
+
+# Update a tag.
+set +e
+# If tag_version is new to the remote, remote cannot delete an unexisted tag, 
+# so error is allowed here.
+git push origin --delete "${tag_version}" 
+set -e
 git tag -f -a "${tag_version}" -m "release testing"
 git push origin "${tag_version}"
+
 # Update source and tag, e.g.  ":tag => 'CocoaPods-' + s.version.to_s" to
 # ":tag => test"
 sed  -i "" "s/\s*:tag.*/:tag => '${tag_version}'/" *.podspec
