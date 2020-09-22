7f464bbe22fb108248302cc52588d3e1df217e64
Gran Luo <ollkorrect999@gmail.com>
2020-09-21 18:47:42 -0700

    Test workflows.

--- scripts/create_spec_repo/Sources/SpecRepoBuilder/main.swift
+++ scripts/create_spec_repo/Sources/SpecRepoBuilder/main.swift
@@ -344,6 +344,7 @@ struct SpecRepoBuilder: ParsableCommand {
         exitCode = 1
         failedPods.append(pod)
       }
+      break
     }
     if exitCode != 0 {
       Self.exit(withError: SpecRepoBuilderError.failedToPush(pods: failedPods))
