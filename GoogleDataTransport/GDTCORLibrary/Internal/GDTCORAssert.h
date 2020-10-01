3f3064ce48a3b01fc3de69915aa78d88c2e0165e
Maksym Malyhin <mmaksym@google.com>
2020-10-01 18:13:12 -0400

    Fix import

--- GoogleDataTransport/GDTCORLibrary/Internal/GDTCORAssert.h
+++ GoogleDataTransport/GDTCORLibrary/Internal/GDTCORAssert.h
@@ -16,7 +16,7 @@
 
 #import <Foundation/Foundation.h>
 
-#import "GDTCORConsoleLogger.h"
+#import "GoogleDataTransport/GDTCORLibrary/Public/GoogleDataTransport/GDTCORConsoleLogger.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
