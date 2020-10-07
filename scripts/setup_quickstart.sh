69739dd6aee98d7b5c5b31d432c62a666a531a76
Maksym Malyhin <mmaksym@google.com>
2020-10-07 14:11:57 -0400

    Use debug QS repo branch

--- scripts/setup_quickstart.sh
+++ scripts/setup_quickstart.sh
@@ -38,7 +38,7 @@ if check_secrets || [[ ${SAMPLE} == "installations" ]]; then
   # development pod install. This is needed for the `pod install` command.
   export FIREBASE_POD_REPO_FOR_DEV_POD=`pwd`
 
-  git clone https://github.com/firebase/quickstart-ios.git
+  git clone https://github.com/firebase/quickstart-ios.git --branch mm/debug
   $scripts_dir/localize_podfile.swift quickstart-ios/"$SAMPLE"/Podfile "$RELEASE_TESTING"
   if [ "$RELEASE_TESTING" == "nightly_release_testing" ]; then
     set +x
