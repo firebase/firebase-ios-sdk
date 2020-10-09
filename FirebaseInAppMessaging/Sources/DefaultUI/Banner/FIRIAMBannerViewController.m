0c7f773199f718efa0a41e5cde63ba2f39d4b1b5
Jim Hays <jim.hays@joinroot.com>
2020-10-09 09:18:50 -0400

    Change instances of 'dimiss' to 'dismiss'

--- FirebaseInAppMessaging/Sources/DefaultUI/Banner/FIRIAMBannerViewController.m
+++ FirebaseInAppMessaging/Sources/DefaultUI/Banner/FIRIAMBannerViewController.m
@@ -52,7 +52,7 @@ static const NSTimeInterval kBannerViewAnimationDuration = 0.3;  // in seconds
 
 // Banner view will auto dismiss after this amount of time of showing if user does not take
 // any other actions. It's in seconds.
-static const NSTimeInterval kBannerAutoDimissTime = 12;
+static const NSTimeInterval kBannerAutoDismissTime = 12;
 
 // If the window width is larger than this threshold, we cap banner view width
 // by it: showing a non full-width banner when it happens.
@@ -289,7 +289,7 @@ static const CGFloat kSwipeUpThreshold = -10.0f;
 }
 
 - (void)setupAutoDismissTimer {
-  NSTimeInterval remaining = kBannerAutoDimissTime - super.aggregateImpressionTimeInSeconds;
+  NSTimeInterval remaining = kBannerAutoDismissTime - super.aggregateImpressionTimeInSeconds;
 
   FIRLogDebug(kFIRLoggerInAppMessagingDisplay, @"I-FID300004",
               @"Remaining banner auto dismiss time is %lf", remaining);
