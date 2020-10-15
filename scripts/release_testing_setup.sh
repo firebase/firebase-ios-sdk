acc8af959f56bba2a916b2aaf2431c44dc3b8fe9
granluo <ollkorrect999@gmail.com>
2020-10-14 17:00:49 -0700

    remove else.

--- scripts/release_testing_setup.sh
+++ scripts/release_testing_setup.sh
@@ -33,7 +33,6 @@ test_version=$(git tag -l --sort=-version:refname CocoaPods-*[0-9] | head -n 1 |
 release_branch=$(git branch -r -l "origin/release-${test_version}")
 if [ -z $release_branch ];then
   echo "release-${test_version} branch does not exist in the sdk repo."
-else
   # Get substring before the last ".", e.g. "release-7.0.0" -> "release-7.0"
   test_version=${test_version%.*}
   echo "search for release-${test_version} branch."
