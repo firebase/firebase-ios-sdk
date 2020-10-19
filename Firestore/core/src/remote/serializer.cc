firebase/firebase-ios-sdk

commit 21921e98513b948c40dc19b516ca05f08c001d3e

Add != and notIn support (#6418)

Firestore/core/src/remote/serializer.cc
@@ -1102,20 +1102,28 @@ google_firestore_v1_StructuredQuery_Filter Serializer::EncodeSingularFilter(
     const FieldFilter& filter) const {
   google_firestore_v1_StructuredQuery_Filter result{};
 
-  if (filter.op() == Filter::Operator::Equal) {
-    if (filter.value().is_null() || filter.value().is_nan()) {
-      result.which_filter_type =
-          google_firestore_v1_StructuredQuery_Filter_unary_filter_tag;
-      result.unary_filter.which_operand_type =
-          google_firestore_v1_StructuredQuery_UnaryFilter_field_tag;
-      result.unary_filter.field.field_path = EncodeFieldPath(filter.field());
+  if (filter.op() == Filter::Operator::Equal ||
+      filter.op() == Filter::Operator::NotEqual) {
+    result.which_filter_type =
+        google_firestore_v1_StructuredQuery_Filter_unary_filter_tag;
+    result.unary_filter.which_operand_type =
+        google_firestore_v1_StructuredQuery_UnaryFilter_field_tag;
+    result.unary_filter.field.field_path = EncodeFieldPath(filter.field());
 
+    if (filter.value().is_nan()) {
       auto op =
-          filter.value().is_null()
-              ? google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NULL
-              : google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NAN;
+          filter.op() == Filter::Operator::Equal
+              ? google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NAN
+              : google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NOT_NAN;  // NOLINT
       result.unary_filter.op = op;
+      return result;
 
+    } else if (filter.value().is_null()) {
+      auto op =
+          filter.op() == Filter::Operator::Equal
+              ? google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NULL
+              : google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NOT_NULL;  // NOLINT
+      result.unary_filter.op = op;
       return result;
     }
   }
@@ -1159,6 +1167,14 @@ Filter Serializer::DecodeUnaryFilter(
 
     case google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NAN:
       return FieldFilter::Create(std::move(field), Filter::Operator::Equal,
+                                 FieldValue::Nan());
+
+    case google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NOT_NULL:
+      return FieldFilter::Create(std::move(field), Filter::Operator::NotEqual,
+                                 FieldValue::Null());
+
+    case google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NOT_NAN:
+      return FieldFilter::Create(std::move(field), Filter::Operator::NotEqual,
                                  FieldValue::Nan());
 
     default:
@@ -1226,6 +1242,9 @@ Serializer::EncodeFieldFilterOperator(Filter::Operator op) const {
     case Filter::Operator::Equal:
       return google_firestore_v1_StructuredQuery_FieldFilter_Operator_EQUAL;
 
+    case Filter::Operator::NotEqual:
+      return google_firestore_v1_StructuredQuery_FieldFilter_Operator_NOT_EQUAL;
+
     case Filter::Operator::ArrayContains:
       return google_firestore_v1_StructuredQuery_FieldFilter_Operator_ARRAY_CONTAINS;  // NOLINT
 
@@ -1234,6 +1253,9 @@ Serializer::EncodeFieldFilterOperator(Filter::Operator op) const {
 
     case Filter::Operator::ArrayContainsAny:
       return google_firestore_v1_StructuredQuery_FieldFilter_Operator_ARRAY_CONTAINS_ANY;  // NOLINT
+
+    case Filter::Operator::NotIn:
+      return google_firestore_v1_StructuredQuery_FieldFilter_Operator_NOT_IN;  // NOLINT
 
     default:
       HARD_FAIL("Unhandled Filter::Operator: %s", op);
@@ -1259,6 +1281,9 @@ Filter::Operator Serializer::DecodeFieldFilterOperator(
     case google_firestore_v1_StructuredQuery_FieldFilter_Operator_EQUAL:
       return Filter::Operator::Equal;
 
+    case google_firestore_v1_StructuredQuery_FieldFilter_Operator_NOT_EQUAL:
+      return Filter::Operator::NotEqual;
+
     case google_firestore_v1_StructuredQuery_FieldFilter_Operator_ARRAY_CONTAINS:  // NOLINT
       return Filter::Operator::ArrayContains;
 
@@ -1267,6 +1292,9 @@ Filter::Operator Serializer::DecodeFieldFilterOperator(
 
     case google_firestore_v1_StructuredQuery_FieldFilter_Operator_ARRAY_CONTAINS_ANY:  // NOLINT
       return Filter::Operator::ArrayContainsAny;
+
+    case google_firestore_v1_StructuredQuery_FieldFilter_Operator_NOT_IN:  // NOLINT
+      return Filter::Operator::NotIn;
 
     default:
       reader->Fail(StringFormat("Unhandled FieldFilter.op: %s", op));

Firestore/Example/Tests/Integration/API/FIRQueryTests.mm
@@ -382,6 +382,71 @@
                         ]));
 }
 
+- (void)testQueriesCanUseNotEqualFilters {
+  // These documents are ordered by value in "zip" since notEquals filter is an inequality, which
+  // results in documents being sorted by value.
+  NSDictionary *testDocs = @{
+    @"a" : @{@"zip" : @(NAN)},
+    @"b" : @{@"zip" : @91102},
+    @"c" : @{@"zip" : @98101},
+    @"d" : @{@"zip" : @98103},
+    @"e" : @{@"zip" : @[ @98101 ]},
+    @"f" : @{@"zip" : @[ @98101, @98102 ]},
+    @"g" : @{@"zip" : @[ @"98101", @{@"zip" : @98101} ]},
+    @"h" : @{@"zip" : @{@"code" : @500}},
+    @"i" : @{@"zip" : [NSNull null]},
+    @"j" : @{@"code" : @500},
+  };
+  FIRCollectionReference *collection = [self collectionRefWithDocuments:testDocs];
+
+  // Search for zips not matching 98101.
+  FIRQuerySnapshot *snapshot = [self readDocumentSetForRef:[collection queryWhereField:@"zip"
+                                                                          isNotEqualTo:@98101]];
+  XCTAssertEqualObjects(FIRQuerySnapshotGetData(snapshot), (@[
+                          testDocs[@"a"], testDocs[@"b"], testDocs[@"d"], testDocs[@"e"],
+                          testDocs[@"f"], testDocs[@"g"], testDocs[@"h"]
+                        ]));
+
+  // With objects.
+  snapshot = [self readDocumentSetForRef:[collection queryWhereField:@"zip"
+                                                        isNotEqualTo:@{@"code" : @500}]];
+  XCTAssertEqualObjects(FIRQuerySnapshotGetData(snapshot), (@[
+                          testDocs[@"a"], testDocs[@"b"], testDocs[@"c"], testDocs[@"d"],
+                          testDocs[@"e"], testDocs[@"f"], testDocs[@"g"]
+                        ]));
+
+  // With null.
+  snapshot = [self readDocumentSetForRef:[collection queryWhereField:@"zip"
+                                                        isNotEqualTo:@[ [NSNull null] ]]];
+  XCTAssertEqualObjects(FIRQuerySnapshotGetData(snapshot), (@[
+                          testDocs[@"a"], testDocs[@"b"], testDocs[@"c"], testDocs[@"d"],
+                          testDocs[@"e"], testDocs[@"f"], testDocs[@"g"], testDocs[@"h"]
+                        ]));
+
+  // With NAN.
+  snapshot = [self readDocumentSetForRef:[collection queryWhereField:@"zip" isNotEqualTo:@(NAN)]];
+  XCTAssertEqualObjects(FIRQuerySnapshotGetData(snapshot), (@[
+                          testDocs[@"b"], testDocs[@"c"], testDocs[@"d"], testDocs[@"e"],
+                          testDocs[@"f"], testDocs[@"g"], testDocs[@"h"]
+                        ]));
+}
+
+- (void)testQueriesCanUseNotEqualFiltersWithDocIds {
+  NSDictionary *testDocs = @{
+    @"aa" : @{@"key" : @"aa"},
+    @"ab" : @{@"key" : @"ab"},
+    @"ba" : @{@"key" : @"ba"},
+    @"bb" : @{@"key" : @"bb"},
+  };
+  FIRCollectionReference *collection = [self collectionRefWithDocuments:testDocs];
+
+  FIRQuerySnapshot *snapshot =
+      [self readDocumentSetForRef:[collection queryWhereFieldPath:[FIRFieldPath documentID]
+                                                     isNotEqualTo:@"aa"]];
+  XCTAssertEqualObjects(FIRQuerySnapshotGetData(snapshot),
+                        (@[ testDocs[@"ab"], testDocs[@"ba"], testDocs[@"bb"] ]));
+}
+
 - (void)testQueriesCanUseArrayContainsFilters {
   NSDictionary *testDocs = @{
     @"a" : @{@"array" : @[ @42 ]},
@@ -439,6 +504,75 @@
       [self readDocumentSetForRef:[collection queryWhereFieldPath:[FIRFieldPath documentID]
                                                                in:@[ @"aa", @"ab" ]]];
   XCTAssertEqualObjects(FIRQuerySnapshotGetData(snapshot), (@[ testDocs[@"aa"], testDocs[@"ab"] ]));
+}
+
+- (void)testQueriesCanUseNotInFilters {
+  // These documents are ordered by value in "zip" since the NOT_IN filter is an inequality, which
+  // results in documents being sorted by value.
+  NSDictionary *testDocs = @{
+    @"a" : @{@"zip" : @(NAN)},
+    @"b" : @{@"zip" : @91102},
+    @"c" : @{@"zip" : @98101},
+    @"d" : @{@"zip" : @98103},
+    @"e" : @{@"zip" : @[ @98101 ]},
+    @"f" : @{@"zip" : @[ @98101, @98102 ]},
+    @"g" : @{@"zip" : @[ @"98101", @{@"zip" : @98101} ]},
+    @"h" : @{@"zip" : @{@"code" : @500}},
+    @"i" : @{@"zip" : [NSNull null]},
+    @"j" : @{@"code" : @500},
+  };
+  FIRCollectionReference *collection = [self collectionRefWithDocuments:testDocs];
+
+  // Search for zips not matching 98101, 98103, and [98101, 98102].
+  FIRQuerySnapshot *snapshot = [self
+      readDocumentSetForRef:[collection queryWhereField:@"zip"
+                                                  notIn:@[ @98101, @98103, @[ @98101, @98102 ] ]]];
+  XCTAssertEqualObjects(
+      FIRQuerySnapshotGetData(snapshot),
+      (@[ testDocs[@"a"], testDocs[@"b"], testDocs[@"e"], testDocs[@"g"], testDocs[@"h"] ]));
+
+  // With objects.
+  snapshot = [self readDocumentSetForRef:[collection queryWhereField:@"zip"
+                                                               notIn:@[ @{@"code" : @500} ]]];
+  XCTAssertEqualObjects(FIRQuerySnapshotGetData(snapshot), (@[
+                          testDocs[@"a"], testDocs[@"b"], testDocs[@"c"], testDocs[@"d"],
+                          testDocs[@"e"], testDocs[@"f"], testDocs[@"g"]
+                        ]));
+
+  // With null.
+  snapshot = [self readDocumentSetForRef:[collection queryWhereField:@"zip"
+                                                               notIn:@[ [NSNull null] ]]];
+  XCTAssertTrue(snapshot.isEmpty);
+
+  // With NAN.
+  snapshot = [self readDocumentSetForRef:[collection queryWhereField:@"zip" notIn:@[ @(NAN) ]]];
+  XCTAssertEqualObjects(FIRQuerySnapshotGetData(snapshot), (@[
+                          testDocs[@"b"], testDocs[@"c"], testDocs[@"d"], testDocs[@"e"],
+                          testDocs[@"f"], testDocs[@"g"], testDocs[@"h"]
+                        ]));
+
+  // With NAN and a number.
+  snapshot = [self readDocumentSetForRef:[collection queryWhereField:@"zip"
+                                                               notIn:@[ @(NAN), @98101 ]]];
+  XCTAssertEqualObjects(FIRQuerySnapshotGetData(snapshot), (@[
+                          testDocs[@"b"], testDocs[@"d"], testDocs[@"e"], testDocs[@"f"],
+                          testDocs[@"g"], testDocs[@"h"]
+                        ]));
+}
+
+- (void)testQueriesCanUseNotInFiltersWithDocIds {
+  NSDictionary *testDocs = @{
+    @"aa" : @{@"key" : @"aa"},
+    @"ab" : @{@"key" : @"ab"},
+    @"ba" : @{@"key" : @"ba"},
+    @"bb" : @{@"key" : @"bb"},
+  };
+  FIRCollectionReference *collection = [self collectionRefWithDocuments:testDocs];
+
+  FIRQuerySnapshot *snapshot =
+      [self readDocumentSetForRef:[collection queryWhereFieldPath:[FIRFieldPath documentID]
+                                                            notIn:@[ @"aa", @"ab" ]]];
+  XCTAssertEqualObjects(FIRQuerySnapshotGetData(snapshot), (@[ testDocs[@"ba"], testDocs[@"bb"] ]));
 }
 
 - (void)testQueriesCanUseArrayContainsAnyFilters {

Firestore/Example/Tests/Integration/API/FIRValidationTests.mm
@@ -416,19 +416,17 @@ namespace testutil = firebase::firestore::testutil;
 }
 
 - (void)testNonEqualityQueriesOnNullOrNaNFail {
-  FSTAssertThrows([[self collectionRef] queryWhereField:@"a" isGreaterThan:nil],
-                  @"Invalid Query. Null supports only equality comparisons.");
+  NSString *expected = @"Invalid Query. Null supports only 'equalTo' and 'notEqualTo' comparisons.";
+  FSTAssertThrows([[self collectionRef] queryWhereField:@"a" isGreaterThan:nil], expected);
   FSTAssertThrows([[self collectionRef] queryWhereField:@"a" isGreaterThan:[NSNull null]],
-                  @"Invalid Query. Null supports only equality comparisons.");
-  FSTAssertThrows([[self collectionRef] queryWhereField:@"a" arrayContains:nil],
-                  @"Invalid Query. Null supports only equality comparisons.");
+                  expected);
+  FSTAssertThrows([[self collectionRef] queryWhereField:@"a" arrayContains:nil], expected);
   FSTAssertThrows([[self collectionRef] queryWhereField:@"a" arrayContains:[NSNull null]],
-                  @"Invalid Query. Null supports only equality comparisons.");
+                  expected);
 
-  FSTAssertThrows([[self collectionRef] queryWhereField:@"a" isGreaterThan:@(NAN)],
-                  @"Invalid Query. NaN supports only equality comparisons.");
-  FSTAssertThrows([[self collectionRef] queryWhereField:@"a" arrayContains:@(NAN)],
-                  @"Invalid Query. NaN supports only equality comparisons.");
+  expected = @"Invalid Query. NaN supports only 'equalTo' and 'notEqualTo' comparisons.";
+  FSTAssertThrows([[self collectionRef] queryWhereField:@"a" isGreaterThan:@(NAN)], expected);
+  FSTAssertThrows([[self collectionRef] queryWhereField:@"a" arrayContains:@(NAN)], expected);
 }
 
 - (void)testQueryCannotBeCreatedFromDocumentsMissingSortValues {
@@ -596,13 +594,13 @@ namespace testutil = firebase::firestore::testutil;
   FIRQuery *base = [coll queryWhereField:@"x" isGreaterThanOrEqualTo:@32];
 
   FSTAssertThrows([base queryWhereField:@"y" isLessThan:@"cat"],
-                  @"Invalid Query. All where filters with an inequality (lessThan, "
+                  @"Invalid Query. All where filters with an inequality (notEqual, lessThan, "
                    "lessThanOrEqual, greaterThan, or greaterThanOrEqual) must be on the same "
                    "field. But you have inequality filters on 'x' and 'y'");
 
   NSString *reason =
       @"Invalid query. You have a where filter with "
-       "an inequality (lessThan, lessThanOrEqual, greaterThan, or greaterThanOrEqual) "
+       "an inequality (notEqual, lessThan, lessThanOrEqual, greaterThan, or greaterThanOrEqual) "
        "on field 'x' and so you must also use 'x' as your first queryOrderedBy field, "
        "but your first queryOrderedBy is currently on field 'y' instead.";
   FSTAssertThrows([base queryOrderedByField:@"y"], reason);
@@ -611,6 +609,7 @@ namespace testutil = firebase::firestore::testutil;
   FSTAssertThrows([[[coll queryOrderedByField:@"y"] queryOrderedByField:@"x"] queryWhereField:@"x"
                                                                                 isGreaterThan:@32],
                   reason);
+  FSTAssertThrows([[coll queryOrderedByField:@"y"] queryWhereField:@"x" isNotEqualTo:@32], reason);
 
   XCTAssertNoThrow([base queryWhereField:@"x" isLessThanOrEqualTo:@"cat"],
                    @"Same inequality fields work");
@@ -638,6 +637,20 @@ namespace testutil = firebase::firestore::testutil;
                    @"array_contains different than orderBy works.");
 }
 
+- (void)testQueriesWithMultipleNotEqualAndInequalitiesFail {
+  FIRCollectionReference *coll = [self.db collectionWithPath:@"collection"];
+
+  FSTAssertThrows([[coll queryWhereField:@"x" isNotEqualTo:@1] queryWhereField:@"x"
+                                                                  isNotEqualTo:@2],
+                  @"Invalid Query. You cannot use more than one 'notEqual' filter.");
+
+  FSTAssertThrows([[coll queryWhereField:@"x" isNotEqualTo:@1] queryWhereField:@"y"
+                                                                  isNotEqualTo:@2],
+                  @"Invalid Query. All where filters with an inequality (notEqual, lessThan, "
+                   "lessThanOrEqual, greaterThan, or greaterThanOrEqual) must be on "
+                   "the same field. But you have inequality filters on 'x' and 'y'");
+}
+
 - (void)testQueriesWithMultipleArrayFiltersFail {
   FIRCollectionReference *coll = [self.db collectionWithPath:@"collection"];
   FSTAssertThrows([[coll queryWhereField:@"foo" arrayContains:@1] queryWhereField:@"foo"
@@ -655,6 +668,18 @@ namespace testutil = firebase::firestore::testutil;
       @"Invalid Query. You cannot use 'arrayContains' filters with 'arrayContainsAny' filters.");
 }
 
+- (void)testQueriesWithNotEqualAndNotInFiltersFail {
+  FIRCollectionReference *coll = [self.db collectionWithPath:@"collection"];
+
+  FSTAssertThrows([[coll queryWhereField:@"foo" notIn:@[ @1 ]] queryWhereField:@"foo"
+                                                                  isNotEqualTo:@2],
+                  @"Invalid Query. You cannot use 'notEqual' filters with 'notIn' filters.");
+
+  FSTAssertThrows([[coll queryWhereField:@"foo" isNotEqualTo:@2] queryWhereField:@"foo"
+                                                                           notIn:@[ @1 ]],
+                  @"Invalid Query. You cannot use 'notIn' filters with 'notEqual' filters.");
+}
+
 - (void)testQueriesWithMultipleDisjunctiveFiltersFail {
   FIRCollectionReference *coll = [self.db collectionWithPath:@"collection"];
   FSTAssertThrows([[coll queryWhereField:@"foo" in:@[ @1 ]] queryWhereField:@"foo" in:@[ @2 ]],
@@ -664,6 +689,10 @@ namespace testutil = firebase::firestore::testutil;
                                                                          arrayContainsAny:@[ @2 ]],
                   @"Invalid Query. You cannot use more than one 'arrayContainsAny' filter.");
 
+  FSTAssertThrows([[coll queryWhereField:@"foo" notIn:@[ @1 ]] queryWhereField:@"foo"
+                                                                         notIn:@[ @2 ]],
+                  @"Invalid Query. You cannot use more than one 'notIn' filter.");
+
   FSTAssertThrows([[coll queryWhereField:@"foo" arrayContainsAny:@[ @1 ]] queryWhereField:@"foo"
                                                                                        in:@[ @2 ]],
                   @"Invalid Query. You cannot use 'in' filters with 'arrayContainsAny' filters.");
@@ -672,6 +701,20 @@ namespace testutil = firebase::firestore::testutil;
                                                            arrayContainsAny:@[ @2 ]],
                   @"Invalid Query. You cannot use 'arrayContainsAny' filters with 'in' filters.");
 
+  FSTAssertThrows(
+      [[coll queryWhereField:@"foo" arrayContainsAny:@[ @1 ]] queryWhereField:@"foo" notIn:@[ @2 ]],
+      @"Invalid Query. You cannot use 'notIn' filters with 'arrayContainsAny' filters.");
+
+  FSTAssertThrows(
+      [[coll queryWhereField:@"foo" notIn:@[ @1 ]] queryWhereField:@"foo" arrayContainsAny:@[ @2 ]],
+      @"Invalid Query. You cannot use 'arrayContainsAny' filters with 'notIn' filters.");
+
+  FSTAssertThrows([[coll queryWhereField:@"foo" in:@[ @1 ]] queryWhereField:@"foo" notIn:@[ @2 ]],
+                  @"Invalid Query. You cannot use 'notIn' filters with 'in' filters.");
+
+  FSTAssertThrows([[coll queryWhereField:@"foo" notIn:@[ @1 ]] queryWhereField:@"foo" in:@[ @2 ]],
+                  @"Invalid Query. You cannot use 'in' filters with 'notIn' filters.");
+
   // This is redundant with the above tests, but makes sure our validation doesn't get confused.
   FSTAssertThrows([[[coll queryWhereField:@"foo"
                                        in:@[ @1 ]] queryWhereField:@"foo"
@@ -679,11 +722,23 @@ namespace testutil = firebase::firestore::testutil;
                                                                       arrayContainsAny:@[ @2 ]],
                   @"Invalid Query. You cannot use 'arrayContainsAny' filters with 'in' filters.");
 
+  FSTAssertThrows(
+      [[[coll queryWhereField:@"foo"
+                arrayContains:@1] queryWhereField:@"foo" in:@[ @2 ]] queryWhereField:@"foo"
+                                                                    arrayContainsAny:@[ @2 ]],
+      @"Invalid Query. You cannot use 'arrayContainsAny' filters with 'arrayContains' filters.");
+
+  FSTAssertThrows([[[coll queryWhereField:@"foo"
+                                    notIn:@[ @1 ]] queryWhereField:@"foo"
+                                                     arrayContains:@2] queryWhereField:@"foo"
+                                                                      arrayContainsAny:@[ @2 ]],
+                  @"Invalid Query. You cannot use 'arrayContains' filters with 'notIn' filters.");
+
   FSTAssertThrows([[[coll queryWhereField:@"foo"
                             arrayContains:@1] queryWhereField:@"foo"
                                                            in:@[ @2 ]] queryWhereField:@"foo"
-                                                                      arrayContainsAny:@[ @2 ]],
-                  @"Invalid Query. You cannot use 'arrayContainsAny' filters with 'in' filters.");
+                                                                                 notIn:@[ @2 ]],
+                  @"Invalid Query. You cannot use 'notIn' filters with 'arrayContains' filters.");
 }
 
 - (void)testQueriesCanUseInWithArrayContain {
@@ -715,6 +770,9 @@ namespace testutil = firebase::firestore::testutil;
   FSTAssertThrows([coll queryWhereField:@"foo" in:@[]],
                   @"Invalid Query. A non-empty array is required for 'in' filters.");
 
+  FSTAssertThrows([coll queryWhereField:@"foo" notIn:@[]],
+                  @"Invalid Query. A non-empty array is required for 'notIn' filters.");
+
   FSTAssertThrows([coll queryWhereField:@"foo" arrayContainsAny:@[]],
                   @"Invalid Query. A non-empty array is required for 'arrayContainsAny' filters.");
 
@@ -726,6 +784,9 @@ namespace testutil = firebase::firestore::testutil;
   FSTAssertThrows([coll queryWhereField:@"foo" arrayContainsAny:values],
                   @"Invalid Query. 'arrayContainsAny' filters support a maximum of 10 elements"
                    " in the value array.");
+  FSTAssertThrows(
+      [coll queryWhereField:@"foo" notIn:values],
+      @"Invalid Query. 'notIn' filters support a maximum of 10 elements in the value array.");
 
   NSArray *withNullValues = @[ @1, [NSNull null] ];
   FSTAssertThrows([coll queryWhereField:@"foo" in:withNullValues],

Firestore/Source/API/FIRQuery.mm
@@ -215,6 +215,16 @@ int32_t SaturatedLimitValue(NSInteger limit) {
   return [self queryWithFilterOperator:Filter::Operator::Equal path:path.internalValue value:value];
 }
 
+- (FIRQuery *)queryWhereField:(NSString *)field isNotEqualTo:(id)value {
+  return [self queryWithFilterOperator:Filter::Operator::NotEqual field:field value:value];
+}
+
+- (FIRQuery *)queryWhereFieldPath:(FIRFieldPath *)path isNotEqualTo:(id)value {
+  return [self queryWithFilterOperator:Filter::Operator::NotEqual
+                                  path:path.internalValue
+                                 value:value];
+}
+
 - (FIRQuery *)queryWhereField:(NSString *)field isLessThan:(id)value {
   return [self queryWithFilterOperator:Filter::Operator::LessThan field:field value:value];
 }
@@ -285,6 +295,16 @@ int32_t SaturatedLimitValue(NSInteger limit) {
   return [self queryWithFilterOperator:Filter::Operator::In path:path.internalValue value:values];
 }
 
+- (FIRQuery *)queryWhereField:(NSString *)field notIn:(NSArray<id> *)values {
+  return [self queryWithFilterOperator:Filter::Operator::NotIn field:field value:values];
+}
+
+- (FIRQuery *)queryWhereFieldPath:(FIRFieldPath *)path notIn:(NSArray<id> *)values {
+  return [self queryWithFilterOperator:Filter::Operator::NotIn
+                                  path:path.internalValue
+                                 value:values];
+}
+
 - (FIRQuery *)queryFilteredUsingComparisonPredicate:(NSPredicate *)predicate {
   NSComparisonPredicate *comparison = (NSComparisonPredicate *)predicate;
   if (comparison.comparisonPredicateModifier != NSDirectPredicateModifier) {
@@ -307,6 +327,8 @@ int32_t SaturatedLimitValue(NSInteger limit) {
         return [self queryWhereField:path isGreaterThan:value];
       case NSGreaterThanOrEqualToPredicateOperatorType:
         return [self queryWhereField:path isGreaterThanOrEqualTo:value];
+      case NSNotEqualToPredicateOperatorType:
+        return [self queryWhereField:path isNotEqualTo:value];
       default:;  // Fallback below to throw assertion.
     }
   } else if ([comparison.leftExpression expressionType] == NSConstantValueExpressionType &&
@@ -324,6 +346,8 @@ int32_t SaturatedLimitValue(NSInteger limit) {
         return [self queryWhereField:path isLessThan:value];
       case NSGreaterThanOrEqualToPredicateOperatorType:
         return [self queryWhereField:path isLessThanOrEqualTo:value];
+      case NSNotEqualToPredicateOperatorType:
+        return [self queryWhereField:path isNotEqualTo:value];
       default:;  // Fallback below to throw assertion.
     }
   } else {
@@ -483,7 +507,8 @@ int32_t SaturatedLimitValue(NSInteger limit) {
                                  path:(const FieldPath &)fieldPath
                                 value:(id)value {
   FieldValue fieldValue = [self parsedQueryValue:value
-                                     allowArrays:filterOperator == Filter::Operator::In];
+                                     allowArrays:filterOperator == Filter::Operator::In ||
+                                                 filterOperator == Filter::Operator::NotIn];
   auto describer = [value] { return MakeString(NSStringFromClass([value class])); };
   return Wrap(_query.Filter(fieldPath, filterOperator, std::move(fieldValue), describer));
 }

commit aee16f0a809c95a08e4bf83972f20a2d1f5cbbe2

Fix a memory leak introduced in #6418 (#6778)
The root of the issue is that when serializing a singular filter, it is being treated as a unary filter before it is definitively established whether it is a unary filter or a field filter. The leak is caused by always serializing the unary filter's field path field for equality and non-equality filters -- if the filter's value turns out not to be NaN or null, the serialization code switches the filter's type to a field filter without clearing the partially-initialized unary filter. `pb_release` would not free the `unary_filter.field.field_path` member variable because it would consider the object not to be a unary filter.

Also a small refactoring to make the function easier to digest.

Firestore/core/src/remote/serializer.cc
@@ -1102,30 +1102,34 @@ google_firestore_v1_StructuredQuery_Filter Serializer::EncodeSingularFilter(
     const FieldFilter& filter) const {
   google_firestore_v1_StructuredQuery_Filter result{};
 
-  if (filter.op() == Filter::Operator::Equal ||
-      filter.op() == Filter::Operator::NotEqual) {
+  bool is_unary = (filter.op() == Filter::Operator::Equal ||
+                   filter.op() == Filter::Operator::NotEqual) &&
+                  (filter.value().is_nan() || filter.value().is_null());
+  if (is_unary) {
     result.which_filter_type =
         google_firestore_v1_StructuredQuery_Filter_unary_filter_tag;
     result.unary_filter.which_operand_type =
         google_firestore_v1_StructuredQuery_UnaryFilter_field_tag;
     result.unary_filter.field.field_path = EncodeFieldPath(filter.field());
 
+    bool is_equality = filter.op() == Filter::Operator::Equal;
     if (filter.value().is_nan()) {
-      auto op =
-          filter.op() == Filter::Operator::Equal
+      result.unary_filter.op =
+          is_equality
               ? google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NAN
               : google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NOT_NAN;  // NOLINT
-      result.unary_filter.op = op;
-      return result;
 
     } else if (filter.value().is_null()) {
-      auto op =
-          filter.op() == Filter::Operator::Equal
+      result.unary_filter.op =
+          is_equality
               ? google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NULL
               : google_firestore_v1_StructuredQuery_UnaryFilter_Operator_IS_NOT_NULL;  // NOLINT
-      result.unary_filter.op = op;
-      return result;
+
+    } else {
+      HARD_FAIL("Expected a unary filter");
     }
+
+    return result;
   }
 
   result.which_filter_type =