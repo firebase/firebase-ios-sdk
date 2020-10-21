firebase/firebase-ios-sdk

commit a39b515f3ab0c0f8e0d9bedddc4442f436cfcc22

Update podspecs to min iOS 10 (#6517)

FirebaseDynamicLinks/Sources/FIRDynamicLinks.m
@@ -531,17 +531,7 @@ static const NSInteger FIRErrorCodeDurableDeepLinkFailed = -119;
 }
 
 - (void)passRetrievedDynamicLinkToApplication:(NSURL *)url {
-  id<UIApplicationDelegate> applicationDelegate = [UIApplication sharedApplication].delegate;
-  if (applicationDelegate &&
-      [applicationDelegate respondsToSelector:@selector(application:openURL:options:)]) {
-    // pass url directly to application delegate to avoid hop into
-    // iOS handling of the universal links
-    if (@available(iOS 9.0, *)) {
-      [applicationDelegate application:[UIApplication sharedApplication] openURL:url options:@{}];
-      return;
-    }
-  }
-  [[UIApplication sharedApplication] openURL:url];
+  [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
 }
 
 - (void)handlePendingDynamicLinkRetrievalFailureWithErrorCode:(NSInteger)errorCode

CocoapodsIntegrationTest/TestEnvironments/Cocoapods1_6_1_frameworks/Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'CocoapodsIntegrationTest' do
   use_frameworks!

CocoapodsIntegrationTest/TestEnvironments/Cocoapods1_6_1_staticLibs/Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'CocoapodsIntegrationTest' do
   pod 'FirebaseABTesting', :path => '../'

CocoapodsIntegrationTest/TestEnvironments/Cocoapods1_7_0_frameworks/Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'CocoapodsIntegrationTest' do
   use_frameworks!

CocoapodsIntegrationTest/TestEnvironments/Cocoapods1_7_0_multiprojects_frameworks/Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'CocoapodsIntegrationTest' do
   # Comment the next line if you don't want to use dynamic frameworks

CocoapodsIntegrationTest/TestEnvironments/Cocoapods1_7_0_multiprojects_staticLibs/Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'CocoapodsIntegrationTest' do
   pod 'FirebaseABTesting', :path => '../'

CocoapodsIntegrationTest/TestEnvironments/Cocoapods1_7_0_staticLibs/Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'CocoapodsIntegrationTest' do
   pod 'FirebaseABTesting', :path => '../'

CoreOnly/Tests/FirebasePodTest/Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '10.0'
 
 source 'https://github.com/firebase/SpecsStaging.git'
 source 'https://cdn.cocoapods.org/'

commit 881ba5d597ba3f4dc3f3f4f873fdb69c7007e692

Fixed broken callback to AppDelegate (#6800)
Fixed broken callback to AppDelegate after retrieving a dynamic link during fresh app start.
The AppDelegate call was broken in PR : #6517

Reverting to the old implementation of dynamic link passing to App delegate with changes to remove iOS 9 checks.
Using new "openURL" method instead of the deprecated one.
Clean up in the sample app pod file.

FirebaseDynamicLinks/Sources/FIRDynamicLinks.m
@@ -509,6 +509,15 @@ static const NSInteger FIRErrorCodeDurableDeepLinkFailed = -119;
 }
 
 - (void)passRetrievedDynamicLinkToApplication:(NSURL *)url {
+  id<UIApplicationDelegate> applicationDelegate = [UIApplication sharedApplication].delegate;
+  if (applicationDelegate &&
+      [applicationDelegate respondsToSelector:@selector(application:openURL:options:)]) {
+    // pass url directly to application delegate to avoid hop into
+    // iOS handling of the universal links
+    [applicationDelegate application:[UIApplication sharedApplication] openURL:url options:@{}];
+    return;
+  }
+
   [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
 }
 

FirebaseDynamicLinks/Tests/Sample/Podfile
@@ -1,14 +1,10 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
-
 target 'FDLBuilderTestAppObjC' do
-  # Comment the next line if you don't want to use dynamic frameworks
+  platform :ios, '10.0'
   use_frameworks!
-  pod 'FirebaseAnalytics'
-  pod 'FirebaseCore', :path => '../../../'
-  pod 'FirebaseInstallations', :path => '../../../'
-  pod 'FirebaseInstanceID', :path => '../../../'
-  pod 'FirebaseDynamicLinks', :path => '../../../'
-  pod 'GoogleUtilities', :path => '../../../'
 
+  pod 'FirebaseCore', :path => '../../../'
+  pod 'FirebaseCoreDiagnostics', :path => '../../../'
+  pod 'GoogleDataTransport', :path => '../../../'
+  pod 'GoogleUtilities', :path => '../../../'
+  pod 'FirebaseDynamicLinks', :path => '../../../'
 end