60226e4f1ad73120b01c2f6efd9435abb9ad0352
Sam Stern <samstern@google.com>
2020-10-21 09:50:30 -0400

    Add custom domain support to callable functions (#6787)

--- Functions/Example/Tests/FIRFunctionsTests.m
+++ Functions/Example/Tests/FIRFunctionsTests.m
@@ -17,13 +17,28 @@
 #import "Functions/FirebaseFunctions/FIRFunctions+Internal.h"
 #import "Functions/FirebaseFunctions/Public/FirebaseFunctions/FIRFunctions.h"
 
-@interface FIRFunctionsTests : XCTestCase
+@interface FIRFunctionsTests : XCTestCase {
+  FIRFunctions *_functions;
+  FIRFunctions *_functionsCustomDomain;
+}
 @end
 
 @implementation FIRFunctionsTests
 
 - (void)setUp {
   [super setUp];
+
+  _functions = [[FIRFunctions alloc] initWithProjectID:@"my-project"
+                                                region:@"my-region"
+                                          customDomain:nil
+                                                  auth:nil
+                                             messaging:nil];
+
+  _functionsCustomDomain = [[FIRFunctions alloc] initWithProjectID:@"my-project"
+                                                            region:@"my-region"
+                                                      customDomain:@"https://mydomain.com"
+                                                              auth:nil
+                                                         messaging:nil];
 }
 
 - (void)tearDown {
@@ -31,12 +46,25 @@
 }
 
 - (void)testURLWithName {
-  FIRFunctions *functions = [[FIRFunctions alloc] initWithProjectID:@"my-project"
-                                                             region:@"my-region"
-                                                               auth:nil
-                                                          messaging:nil];
-  NSString *url = [functions URLWithName:@"my-endpoint"];
+  NSString *url = [_functions URLWithName:@"my-endpoint"];
   XCTAssertEqualObjects(@"https://my-region-my-project.cloudfunctions.net/my-endpoint", url);
+}
+
+- (void)testRegionWithEmulator {
+  [_functions useFunctionsEmulatorOrigin:@"http://localhost:5005"];
+  NSString *url = [_functions URLWithName:@"my-endpoint"];
+  XCTAssertEqualObjects(@"http://localhost:5005/my-project/my-region/my-endpoint", url);
+}
+
+- (void)testCustomDomain {
+  NSString *url = [_functionsCustomDomain URLWithName:@"my-endpoint"];
+  XCTAssertEqualObjects(@"https://mydomain.com/my-endpoint", url);
+}
+
+- (void)testCustomDomainWithEmulator {
+  [_functionsCustomDomain useFunctionsEmulatorOrigin:@"http://localhost:5005"];
+  NSString *url = [_functionsCustomDomain URLWithName:@"my-endpoint"];
+  XCTAssertEqualObjects(@"http://localhost:5005/my-project/my-region/my-endpoint", url);
 }
 
 @end
