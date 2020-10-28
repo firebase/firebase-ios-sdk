bdad564cd7cb9d595335ddd0c5f5721ac7d9a746
Tejas Deshpande <tdeshpande@google.com>
2020-10-28 15:16:51 -0400

    Revert deletion of STR_HELPER

--- Crashlytics/Crashlytics/Helpers/FIRCLSDefines.h
+++ Crashlytics/Crashlytics/Helpers/FIRCLSDefines.h
@@ -17,6 +17,8 @@
 #include <TargetConditionals.h>
 
 // macro trickiness
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
 #define CONCAT_EXPANDED(a, b) a##b
 #define CONCAT(a, b) CONCAT_EXPANDED(a, b)
 
