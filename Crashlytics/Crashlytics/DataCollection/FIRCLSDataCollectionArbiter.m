c784d74641271bde40d89ba290a4caf358ba592a
Tejas Deshpande <tdeshpande@google.com>
2020-10-29 11:22:33 -0400

    Change the Crashlytics version constant to read from FIRCore instead of from a build flag. (#6712)

--- Crashlytics/Crashlytics/DataCollection/FIRCLSDataCollectionArbiter.m
+++ Crashlytics/Crashlytics/DataCollection/FIRCLSDataCollectionArbiter.m
@@ -101,7 +101,7 @@ typedef NS_ENUM(NSInteger, FIRCLSDataCollectionSetting) {
       [firebaseCrashlyticsCollectionEnabled isKindOfClass:[NSNumber class]]) {
     return [firebaseCrashlyticsCollectionEnabled boolValue];
   }
-#ifdef CRASHLYTICS_INTERNAL
+#ifdef CRASHLYTICS_1P
   return NO;
 #else
   return [app isDataCollectionDefaultEnabled];
