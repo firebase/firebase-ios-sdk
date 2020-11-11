9d317e5657127b9fc3de8caa6e6cf3b9fb7fa49b
lorenzofiamingo <l99fiamingo@gmail.com>
2020-11-11 21:39:14 +0100

    Formatted with scripts/check.sh

--- Firestore/Swift/Source/Combine/CollectionReference+Combine.swift
+++ Firestore/Swift/Source/Combine/CollectionReference+Combine.swift
@@ -1,36 +1,50 @@
-//
-//  CollectionReference+Combine.swift
-//  Forvice
-//
-//  Created by Lorenzo Fiamingo on 16/09/20.
-//
+/*
+ * Copyright 2019 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #if canImport(Combine) && swift(>=5.0)
 
-import Combine
-import FirebaseFirestore
+  import Combine
+  import FirebaseFirestore
 
-extension CollectionReference {
-    
-    public func addDocumentPublisher(data: [String: Any]) -> AnyPublisher<DocumentReference, Error> {
-        var reference: DocumentReference!
-        return Future { reference = self.addDocument(data: data, completion: $0) }
-            .map { reference }
-            .eraseToAnyPublisher()
-    }
-    
-    public func addDocumentPublisher<T: Encodable>(from value: T, encoder: Firestore.Encoder = Firestore.Encoder()) -> AnyPublisher<DocumentReference, Error> {
-        var reference: DocumentReference!
-        return Future { promise in
-            do {
-                try reference = self.addDocument(from: value, encoder: encoder, completion: promise)
-            } catch {
-                promise(.failure(error))
-            }
-        }
+  extension CollectionReference {
+    public func addDocumentPublisher(data: [String: Any])
+      -> AnyPublisher<DocumentReference, Error> {
+      var reference: DocumentReference!
+      return Future { reference = self.addDocument(data: data, completion: $0) }
         .map { reference }
         .eraseToAnyPublisher()
     }
-}
+
+    public func addDocumentPublisher<T: Encodable>(from value: T,
+                                                   encoder: Firestore.Encoder = Firestore
+                                                     .Encoder()) -> AnyPublisher<
+      DocumentReference,
+      Error
+    > {
+      var reference: DocumentReference!
+      return Future { promise in
+        do {
+          try reference = self.addDocument(from: value, encoder: encoder, completion: promise)
+        } catch {
+          promise(.failure(error))
+        }
+      }
+      .map { reference }
+      .eraseToAnyPublisher()
+    }
+  }
 
 #endif
