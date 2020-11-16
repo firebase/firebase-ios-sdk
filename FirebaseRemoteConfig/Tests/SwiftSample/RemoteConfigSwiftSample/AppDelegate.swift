f0ed8bc7c79c5b495fdd8a27b206700761eb676d
Karen Zeng <karenzeng@google.com>
2020-11-16 14:31:12 -0800

    Add Firebase and move files around

--- FirebaseRemoteConfig/Tests/SwiftSample/RemoteConfigSwiftSample/AppDelegate.swift
+++ FirebaseRemoteConfig/Tests/SwiftSample/RemoteConfigSwiftSample/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    FirebaseApp.configure()
     return true
   }
 
