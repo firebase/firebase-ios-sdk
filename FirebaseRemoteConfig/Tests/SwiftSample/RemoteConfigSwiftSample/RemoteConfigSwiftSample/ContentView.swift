85e018ecd02b9819eb5355920145c6911b9c1277
Karen Zeng <karenzeng@google.com>
2020-11-16 13:45:10 -0800

    Create new app

---
+++ FirebaseRemoteConfig/Tests/SwiftSample/RemoteConfigSwiftSample/RemoteConfigSwiftSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RemoteConfigSwiftSample
+//
+//  Created by Karen Zeng on 11/16/20.
+//  Copyright © 2020 Firebase. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
