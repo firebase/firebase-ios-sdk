f19c2883e1b5019af6727bbdef16266a6ba00fb9
Manjana Chandrasekharan <manjana@google.com>
2020-11-17 12:03:11 -0800

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -592,37 +592,6 @@ let package = Package(
         .headerSearchPath("../../"),
       ],
       linkerSettings: [
-        .linkedFramework("SystemConfiguration", .when(platforms: .some([.iOS, .macOS, .tvOS]))),
-      ]
-    ),
-    .testTarget(
-      name: "MessagingUnit",
-      dependencies: ["FirebaseMessaging", "OCMock"],
-      path: "FirebaseMessaging/Tests/UnitTests",
-      exclude: [
-        "FIRMessagingContextManagerServiceTest.m", // TODO: Adapt its NSBundle usage to SPM.
-      ],
-      cSettings: [
-        .headerSearchPath("../../.."),
-      ]
-    ),
-
-    .target(
-      name: "FirebaseMessaging",
-      dependencies: [
-        "FirebaseCore",
-        "FirebaseInstanceID",
-        "GoogleUtilities_AppDelegateSwizzler",
-        "GoogleUtilities_Environment",
-        "GoogleUtilities_Reachability",
-        "GoogleUtilities_UserDefaults",
-      ],
-      path: "FirebaseMessaging/Sources",
-      publicHeadersPath: "Public",
-      cSettings: [
-        .headerSearchPath("../../"),
-      ],
-      linkerSettings: [
         .linkedFramework("SystemConfiguration", .when(platforms: [.iOS, .macOS, .tvOS])),
       ]
     ),
