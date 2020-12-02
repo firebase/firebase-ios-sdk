781c8b842e61c993361b162546e809b73ddb5873
Jeremy Jiang <64182239+jeremyjiang-dev@users.noreply.github.com>
2020-12-02 09:08:02 -0500

    Expose upload URLs so that perf can stop instrumenting these URLs. (#7064)
    * Expose upload URLs.
    
    * Remove code duplication.
    
    * Fix style.
    
    * Make url dictionary local static and dispatch once.
    
    * Fix style.
    
    * Add a local dictionary and return value from it.
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md

--- GoogleDataTransport/GDTCCTLibrary/Private/GDTCCTUploader.h
+++ GoogleDataTransport/GDTCCTLibrary/Private/GDTCCTUploader.h
@@ -28,6 +28,10 @@ extern NSNotificationName const GDTCCTUploadCompleteNotification;
 /** Class capable of uploading events to the CCT backend. */
 @interface GDTCCTUploader : NSObject <GDTCORUploader>
 
+/** Returns the server URL for target.*/
+// TODO: create a separate public header (e.g. GDTCCTUploadURL.h) to make it a public API.
++ (NSDictionary<NSNumber *, NSURL *> *)uploadURLs;
+
 /** The queue on which all CCT uploading will occur. */
 @property(nonatomic, readonly) dispatch_queue_t uploaderQueue;
 
@@ -45,7 +49,7 @@ extern NSNotificationName const GDTCCTUploadCompleteNotification;
 
 #if !NDEBUG
 /** An upload URL used across all targets. For testing only. */
-@property(nullable, nonatomic) NSURL *testServerURL;
+@property(class, nullable, nonatomic) NSURL *testServerURL;
 
 #endif  // !NDEBUG
 
