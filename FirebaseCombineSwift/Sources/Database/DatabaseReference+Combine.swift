e2bcd302dc480dd22ed1398506e394ef853a3753
Morten Bek Ditlevsen <morten@ka-ching.dk>
2020-12-04 14:40:10 +0100

    Added publishers

--- FirebaseCombineSwift/Sources/Database/DatabaseReference+Combine.swift
+++ FirebaseCombineSwift/Sources/Database/DatabaseReference+Combine.swift
@@ -20,7 +20,7 @@
 
   @available(swift 5.0)
   @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
-  extension DatabaseReference {
+  public extension DatabaseReference {
     // MARK: - Setting values on a `DatabaseReference`
 
     /// Encodes an `Encodable` value and, on success, writes the encoded value to the database.
@@ -28,7 +28,7 @@
     ///   - value: An instance of `Encodable` to be encoded to a document.
     ///   - encoder: An encoder instance to use to run the encoding.
     /// - Returns: A Future that emits the result of encoding/writing the value.
-    public func setValue<T: Encodable>(from encodable: T, encoder: Database.Encoder = Database.Encoder.defaultEncoder()) -> Future<Void, Error> {
+    func setValue<T: Encodable>(from encodable: T, encoder: Database.Encoder = Database.Encoder.defaultEncoder()) -> Future<Void, Error> {
       Future<Void, Error> { promise in
         do {
           try self.setValue(from: encodable, encoder: encoder, completion: promise)
@@ -42,6 +42,60 @@
           promise(.failure(error))
         }
       }
+    }
+
+    func observeSingleEvent<T: Decodable>(of eventType: DataEventType,
+                                                 as type: T.Type,
+                                                 decoder: Database.Decoder = Database.Decoder
+                                                   .defaultDecoder()) -> Future<T, Error> {
+      Future<T, Error> { promise in
+        self.observeSingleEvent(of: eventType) { snapshot in
+          do {
+            let t = try snapshot.data(as: T.self, decoder: decoder)
+            promise(.success(t))
+          }
+          catch {
+            promise(.failure(error))
+          }
+        }
+      }
+    }
+
+    // NOTE: Should this perhaps be named XXXPublisher(...)?
+    // If so, what should it be? 'modelPublisher', 'decodedPublisher'?
+    // Should this be built upon the snapshotPublisher, or is it an ok
+    // performance improvement to create this 'directly'?
+    func observe<T: Decodable>(_ eventType: DataEventType,
+                                      as type: T.Type,
+                                      decoder: Database.Decoder = Database.Decoder
+                                        .defaultDecoder()) -> AnyPublisher<T, Error> {
+      let subject = PassthroughSubject<T, Error>()
+      let handle = observe(eventType) { snapshot in
+        do {
+          let t = try snapshot.data(as: T.self, decoder: decoder)
+          subject.send(t)
+        }
+        catch {
+          subject.send(completion: .failure(error))
+        }
+      }
+      return subject
+        .handleEvents(receiveCancel: {
+          self.removeObserver(withHandle: handle)
+        })
+        .eraseToAnyPublisher()
+    }
+
+    func snapshotPublisher(_ eventType: DataEventType) -> AnyPublisher<DataSnapshot, Never> {
+      let subject = PassthroughSubject<DataSnapshot, Never>()
+      let handle = observe(eventType) { snapshot in
+          subject.send(snapshot)
+      }
+      return subject
+        .handleEvents(receiveCancel: {
+          self.removeObserver(withHandle: handle)
+        })
+        .eraseToAnyPublisher()
     }
   }
 
