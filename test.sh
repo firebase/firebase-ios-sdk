16ed06f9eb5bdeb7510ab0e4dc485f2621f713d1
Gran Luo <ollkorrect999@gmail.com>
2020-12-07 09:19:14 -0800

    Update conditions.

---
+++ test.sh
@@ -0,0 +1,22 @@
+    database_files=( "FirebaseDatabase" \
+                                             ".github/workflows" \
+                                             #"Example/Database/" \
+"Interop/Auth/Public/.*\.h")
+          echo "=============== list changed files ==============="
+          cat < <(git diff --name-only HEAD^ HEAD)
+          echo "========== check paths of changed files ========== ${database_files[@]}"
+          git diff --name-only HEAD^ HEAD > files.txt
+          echo "::set-output name=database_run_job::false"
+          while IFS= read -r file
+          do
+            echo $file
+            for path in "${database_files[@]}"
+            do
+              if [[ "${file}" =~ "$path"* ]]; then 
+                echo "This file is  under the directory "
+                echo "::set-output name=database_run_job::true"
+                break 
+              fi
+            done
+          done < files.txt
+
