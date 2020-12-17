b41a1273368e8af2af26d1dfcb135fc2307b94b3
christibbs <43829046+christibbs@users.noreply.github.com>
2020-12-17 18:53:19 -0500

    Support in-app messaging on tvOS apps (#7160)
    * Unlock tvOS for in-app messaging. Podspec needs refactoring
    
    * Remove trailing whitespace
    
    * Remove trailing whitespace
    
    * Fix whitespace
    
    * Add tvOS tests to GHA
    
    * Add TARGET_OS_TV to endifs. Get rid of another pre iOS10 URL open check
    
    * Fix yml for GHA
    
    * Don't build default UI sample app on tvOS
    
    * Better logging for nil message display component in tvOS
    
    * Silence archiving deprecation warnings
    
    * Lower tvOS support target

--- FirebaseInAppMessaging/Sources/Runtime/FIRInAppMessaging+Bootstrap.m
+++ FirebaseInAppMessaging/Sources/Runtime/FIRInAppMessaging+Bootstrap.m
@@ -15,7 +15,7 @@
  */
 
 #import <TargetConditionals.h>
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_TV
 
 #import "FirebaseInAppMessaging/Sources/Private/Runtime/FIRInAppMessaging+Bootstrap.h"
 
@@ -136,4 +136,4 @@ static NSString *_fiamServerHostName = @"firebaseinappmessaging.googleapis.com";
 
 @end
 
-#endif  // TARGET_OS_IOS
+#endif  // TARGET_OS_IOS || TARGET_OS_TV
