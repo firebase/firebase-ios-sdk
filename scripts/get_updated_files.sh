9c6084af21276c3bd08356cda809514d954a963c
Gran Luo <ollkorrect999@gmail.com>
2021-01-05 03:34:35 -0800

    update script.:

--- scripts/get_updated_files.sh
+++ scripts/get_updated_files.sh
@@ -1,3 +1,17 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 DATABASE_PATHS=("FirebaseDatabase.*" \
   ".github/workflows/database\\.yml" \
   "Example/Database/" \
@@ -35,3 +49,11 @@ do
   done
 done < files.txt
 
+echo "=============== Updated jobs to be triggered ================="
+cat ./run_sdk_jobs.txt
+echo "=============== Update variables ================="
+while IFS= read -r run_sdk
+do
+  echo "${run_sdk}"
+  echo "::set-output name=${run_sdk}_run_job::true"
+done < ./run_sdk_jobs.txt
