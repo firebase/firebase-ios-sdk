8a599653977fe18ccc2b97ff330c0cba81c4e097
Jan Wyszynski <jm.wyszynski@gmail.com>
2021-01-12 19:10:44 -0800

    Add assert descriptions to successor unit tests

--- FirebaseDatabase/Tests/Unit/FNextPushIdTest.m
+++ FirebaseDatabase/Tests/Unit/FNextPushIdTest.m
@@ -25,7 +25,7 @@ static NSInteger MAX_KEY_LEN = 786;
 - (void)testSuccessorSpecialValues {
   NSString *maxIntegerKeySuccessor =
       [FNextPushId successor:[NSString stringWithFormat:@"%d", INTEGER_32_MAX]];
-  XCTAssertEqualObjects(maxIntegerKeySuccessor, MIN_PUSH_CHAR, @"");
+  XCTAssertEqualObjects(maxIntegerKeySuccessor, MIN_PUSH_CHAR, @"successor(INTEGER_32_MAX) == MIN_PUSH_CHAR");
   NSString *maxKey = [@"" stringByPaddingToLength:MAX_KEY_LEN
                                        withString:MAX_PUSH_CHAR
                                   startingAtIndex:0];
@@ -36,17 +36,17 @@ static NSInteger MAX_KEY_LEN = 786;
 - (void)testSuccessorBasic {
   NSString *actual = [FNextPushId successor:@"abc"];
   NSString *expected = [NSString stringWithFormat:@"abc%@", MIN_PUSH_CHAR];
-  XCTAssertEqualObjects(expected, actual, @"");
+  XCTAssertEqualObjects(expected, actual, @"successor(abc) == abc + MIN_PUSH_CHAR");
 
   actual = [FNextPushId successor:[@"abc" stringByPaddingToLength:MAX_KEY_LEN
                                                        withString:MAX_PUSH_CHAR
                                                   startingAtIndex:0]];
   expected = @"abd";
-  XCTAssertEqualObjects(expected, actual, @"");
+  XCTAssertEqualObjects(expected, actual, @"successor(abc + MAX_PUSH_CHAR repeated MAX_KEY_LEN - 3 times) == abd");
 
   actual = [FNextPushId successor:[NSString stringWithFormat:@"abc%@", MIN_PUSH_CHAR]];
   expected = [NSString stringWithFormat:@"abc%@%@", MIN_PUSH_CHAR, MIN_PUSH_CHAR];
-  XCTAssertEqualObjects(expected, actual, @"");
+  XCTAssertEqualObjects(expected, actual, @"successor(abc + MIN_PUSH_CHAR) == abc + MIN_PUSH_CHAR + MIN_PUSH_CHAR");
 }
 
 @end
