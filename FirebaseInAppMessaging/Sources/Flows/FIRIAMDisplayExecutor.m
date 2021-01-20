52fa1752d03044ecfb3bb2edc953663a41bd65ed
Chris Tibbs <eggyeggbean@google.com>
2021-01-20 16:33:56 -0500

    Keep nullability with card initializers, put nil message return back if imageData is nil

--- FirebaseInAppMessaging/Sources/Flows/FIRIAMDisplayExecutor.m
+++ FirebaseInAppMessaging/Sources/Flows/FIRIAMDisplayExecutor.m
@@ -423,7 +423,7 @@
 
 - (FIRInAppMessagingCardDisplay *)
     cardDisplayMessageWithMessageDefinition:(FIRIAMMessageDefinition *)definition
-                          portraitImageData:(nonnull FIRInAppMessagingImageData *)portraitImageData
+                          portraitImageData:(FIRInAppMessagingImageData *)portraitImageData
                          landscapeImageData:
                              (nullable FIRInAppMessagingImageData *)landscapeImageData
                                 triggerType:(FIRInAppMessagingDisplayTriggerType)triggerType {
@@ -568,11 +568,15 @@
 
 - (FIRInAppMessagingDisplayMessage *)
     displayMessageWithMessageDefinition:(FIRIAMMessageDefinition *)definition
-                              imageData:(nullable FIRInAppMessagingImageData *)imageData
+                              imageData:(FIRInAppMessagingImageData *)imageData
                      landscapeImageData:(nullable FIRInAppMessagingImageData *)landscapeImageData
                             triggerType:(FIRInAppMessagingDisplayTriggerType)triggerType {
   switch (definition.renderData.renderingEffectSettings.viewMode) {
     case FIRIAMRenderAsCardView:
+      if (imageData == nil) {
+        // Image data should never nil for a valid card message.
+        return nil;
+      }
       return [self cardDisplayMessageWithMessageDefinition:definition
                                          portraitImageData:imageData
                                         landscapeImageData:landscapeImageData
