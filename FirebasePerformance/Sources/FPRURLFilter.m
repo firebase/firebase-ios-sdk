7dc683b2df4d1c37ac94a8ed967b2b84cf90c25c
Paul Beusterien <paulbeusterien@google.com>
2021-02-02 14:46:59 -0800

    Fix build warnings introduced with Xcode 12.5

--- FirebasePerformance/Sources/FPRURLFilter.m
+++ FirebasePerformance/Sources/FPRURLFilter.m
@@ -39,7 +39,7 @@ typedef NS_ENUM(NSInteger, FPRURLAllowlistStatus) {
  *
  *  @return the set of denied URL strings.
  */
-NSSet<NSString *> *GetSystemDenyListURLStrings() {
+NSSet<NSString *> *GetSystemDenyListURLStrings(void) {
   // The denylist of URLs for uploading events to avoid cyclic generation of those network events.
   static NSSet *denylist = nil;
   static dispatch_once_t onceToken;
