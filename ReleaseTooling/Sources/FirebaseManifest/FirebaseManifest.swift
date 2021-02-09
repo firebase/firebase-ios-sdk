37f356d0fce392159f62a48d483cbe03129d9a30
Paul Beusterien <paulbeusterien@google.com>
2021-02-09 14:00:26 -0800

    fix model

--- ReleaseTooling/Sources/FirebaseManifest/FirebaseManifest.swift
+++ ReleaseTooling/Sources/FirebaseManifest/FirebaseManifest.swift
@@ -44,7 +44,7 @@ public let shared = Manifest(
     Pod("FirebasePerformance", platforms: ["ios", "tvos"], zip: true),
     Pod("FirebaseStorage", zip: true),
     Pod("FirebaseStorageSwift", isBeta: true),
-    Pod("FirebaseMLDownloader", isBeta: true, zip: true),
+    Pod("FirebaseMLModelDownloader", isBeta: true, zip: true),
     Pod("Firebase", allowWarnings: true, zip: true),
   ]
 )
