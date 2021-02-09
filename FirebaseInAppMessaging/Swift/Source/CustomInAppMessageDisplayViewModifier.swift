b910ab2ed495888950aa851d30eca1d82ebfe9ee
Chris Tibbs <eggyeggbean@google.com>
2021-02-09 17:08:28 -0500

    Add viewmodifier for displaying custom in-app messages w/ SwiftUI

---
+++ FirebaseInAppMessaging/Swift/Source/CustomInAppMessageDisplayViewModifier.swift
@@ -0,0 +1,63 @@
+import FirebaseInAppMessaging
+import SwiftUI
+
+// Handle delegate for FIAM actions.
+struct CustomInAppMessageDisplayViewModifier<DisplayMessage: View>: ViewModifier {
+  var closure: (InAppMessagingDisplayMessage, InAppMessagingDisplayDelegate) -> DisplayMessage
+
+  @ObservedObject var delegateBridge: DelegateBridge = DelegateBridge()
+
+  init(closure: @escaping (InAppMessagingDisplayMessage, InAppMessagingDisplayDelegate)
+    -> DisplayMessage) {
+    self.closure = closure
+  }
+
+  func body(content: Content) -> some View {
+    let inAppMessage = delegateBridge.inAppMessage
+    return content
+      .overlay(inAppMessage == nil ? AnyView(EmptyView()) :
+        AnyView(closure(inAppMessage!.0, inAppMessage!.1)))
+  }
+}
+
+class DelegateBridge: NSObject, InAppMessagingDisplay, InAppMessagingDisplayDelegate,
+  ObservableObject {
+  @Published var inAppMessageData: (InAppMessagingDisplayMessage,
+                                    InAppMessagingDisplayDelegate)? = nil
+
+  override init() {
+    super.init()
+    InAppMessaging.inAppMessaging().messageDisplayComponent = self
+    InAppMessaging.inAppMessaging().delegate = self
+  }
+
+  func displayMessage(_ messageForDisplay: InAppMessagingDisplayMessage,
+                      displayDelegate: InAppMessagingDisplayDelegate) {
+    DispatchQueue.main.async {
+      self.inAppMessageData = (messageForDisplay, displayDelegate)
+    }
+  }
+
+  func messageClicked(_ inAppMessage: InAppMessagingDisplayMessage,
+                      with action: InAppMessagingAction) {
+    DispatchQueue.main.async {
+      self.inAppMessageData = nil
+    }
+  }
+
+  func messageDismissed(_ inAppMessage: InAppMessagingDisplayMessage,
+                        dismissType: FIRInAppMessagingDismissType) {
+    DispatchQueue.main.async {
+      self.inAppMessageData = nil
+    }
+  }
+}
+
+// View modifier that takes a closure for handling in-app message display.
+public extension View {
+  func onDisplayInAppMessage<T: View>(closure: @escaping (InAppMessagingDisplayMessage,
+                                                          InAppMessagingDisplayDelegate) -> T)
+    -> some View {
+    modifier(CustomInAppMessageDisplayViewModifier(closure: closure))
+  }
+}
