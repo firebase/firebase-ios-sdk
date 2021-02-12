461cf946e886686e3322553aa57cf06360eb5a54
Gran Luo <ollkorrect999@gmail.com>
2021-02-12 08:57:03 -0800

    Setup Firestore code coverage tests.

--- scripts/code_coverage_report/pod_test_code_coverage_report.sh
+++ scripts/code_coverage_report/pod_test_code_coverage_report.sh
@@ -27,10 +27,8 @@ if [ $SDK == "FirebasePerformance" ]; then
   scripts/setup_bundler.sh
   scripts/third_party/travis/retry.sh scripts/build.sh Performance ${platform} unit
 elif [ $SDK == "FirebaseFirestore" ]; then
-  scripts/setup_bundler.sh
-  scripts/third_party/travis/retry.sh scripts/pod_lib_lint.rb FirebaseFirestore.podspec \
-    --platforms=ios \
-    --allow-warnings
+  scripts/install_prereqs.sh Firestore ${platform} xcodebuild
+  scripts/third_party/travis/retry.sh scripts/build.sh Firestore ${platform} xcodebuild
 else
   # Run unit tests of pods and put xcresult bundles into output_path, which
   # should be a targeted dir of actions/upload-artifact in workflows.
