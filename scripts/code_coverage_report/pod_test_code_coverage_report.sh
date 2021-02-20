53c38044b51ff1c5d0d99950f60a546fe5dfe979
Gran Luo <ollkorrect999@gmail.com>
2021-02-19 16:32:25 -0800

    Test database.

--- scripts/code_coverage_report/pod_test_code_coverage_report.sh
+++ scripts/code_coverage_report/pod_test_code_coverage_report.sh
@@ -23,18 +23,17 @@ rm -r /Users/runner/Library/Developer/Xcode/DerivedData/*
 fi
 
 # Setup for pod unit tests
-if [ $SDK == "FirebasePerformance" ]; then
-  scripts/setup_bundler.sh
-  scripts/third_party/travis/retry.sh scripts/build.sh Performance ${platform} unit
-elif [ $SDK == "FirebaseFirestore" ]; then
+if [ $SDK == "FirebaseFirestore" ]; then
   scripts/install_prereqs.sh Firestore ${platform} xcodebuild
   scripts/third_party/travis/retry.sh scripts/build.sh Firestore ${platform} xcodebuild
 else
+  scripts/setup_bundler.sh
+  scripts/third_party/travis/retry.sh scripts/build.sh "${SDK}" ${platform} unit
   # Run unit tests of pods and put xcresult bundles into output_path, which
   # should be a targeted dir of actions/upload-artifact in workflows.
   # In code coverage workflow, files under output_path will be uploaded to
   # Github Actions.
-  scripts/third_party/travis/retry.sh scripts/pod_lib_lint.rb "${SDK}".podspec --platforms="$(tr '[:upper:]' '[:lower:]'<<<${platform})" --test-specs=unit
+  #scripts/third_party/travis/retry.sh scripts/pod_lib_lint.rb "${SDK}".podspec --platforms="$(tr '[:upper:]' '[:lower:]'<<<${platform})" --test-specs=unit
 fi
 
 find /Users/runner/Library/Developer/Xcode/DerivedData -type d -regex ".*/.*\.xcresult" -execdir cp -R '{}' "${output_path}" \;
