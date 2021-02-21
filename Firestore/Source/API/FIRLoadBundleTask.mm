826fd8acf0d02d808c8db39a4f77d902548850b7
Wu-Hui <wu.hui.github@gmail.com>
2021-02-21 15:06:52 -0500

    Alright, API load bundle task added.

--- Firestore/Source/API/FIRLoadBundleTask.mm
+++ Firestore/Source/API/FIRLoadBundleTask.mm
@@ -14,4 +14,102 @@
  * limitations under the License.
  */
 
-#import "FIRLoadBundleTask+Internal.h"
+#import "FIRLoadBundleTask.h"
+
+#include <memory>
+
+#include "Firestore/core/src/api/load_bundle_task.h"
+#include "Firestore/core/src/util/hard_assert.h"
+#include "Firestore/core/src/util/string_apple.h"
+
+namespace api = firebase::firestore::api;
+namespace util = firebase::firestore::util;
+
+NS_ASSUME_NONNULL_BEGIN
+
+namespace {
+
+/**
+ * Converts a public FIRLoadBundleTaskState into its internal equivalent.
+ */
+api::LoadBundleTaskState InternalLoadBundleTaskState(FIRLoadBundleTaskState state) {
+  switch (state) {
+    case FIRLoadBundleTaskStateInProgress:
+      return api::LoadBundleTaskState::InProgress;
+    case FIRLoadBundleTaskStateSuccess:
+      return api::LoadBundleTaskState::Success;
+    case FIRLoadBundleTaskStateError:
+      return api::LoadBundleTaskState::Error;
+    default:
+      HARD_FAIL("Unexpected load bundle task state : %s", state);
+  }
+}
+
+}  // namespace
+
+@implementation FIRLoadBundleTaskProgress {
+}
+
+- (instancetype)initWithInternal:(api::LoadBundleTaskProgress)progress {
+  if (self = [super init]) {
+    _bytesLoaded = progress.bytes_loaded();
+    _documentsLoaded = progress.documents_loaded();
+    _totalBytes = progress.total_bytes();
+    _totalDocuments = progress.total_documents();
+
+    switch (progress.state()) {
+      case api::LoadBundleTaskState::InProgress:
+        _state = FIRLoadBundleTaskStateInProgress;
+        break;
+      case api::LoadBundleTaskState::Success:
+        _state = FIRLoadBundleTaskStateSuccess;
+        break;
+      case api::LoadBundleTaskState::Error:
+        _state = FIRLoadBundleTaskStateError;
+        break;
+    }
+  }
+  return self;
+}
+
+@end
+
+@implementation FIRLoadBundleTask {
+  std::unique_ptr<api::LoadBundleTask> _task;
+}
+
+- (instancetype)initWithTask:(std::unique_ptr<api::LoadBundleTask>)task {
+  if (self = [super init]) {
+    _task = std::move(task);
+  }
+  return self;
+}
+
+- (FIRLoadBundleHandle)observeState:(FIRLoadBundleTaskState)state
+                            handler:(void (^)(FIRLoadBundleTaskProgress *progress))handler {
+  if (!handler) {
+    return @"";
+  }
+
+  api::ProgressObserver observer = [handler](api::LoadBundleTaskProgress internal_progress) {
+    handler([[FIRLoadBundleTaskProgress alloc] initWithInternal:internal_progress]);
+  };
+  return util::MakeNSString(
+      _task->ObserveState(InternalLoadBundleTaskState(state), std::move(observer)));
+}
+
+- (void)removeObserverWithHandle:(FIRLoadBundleHandle)handle {
+  _task->RemoveObserver(util::MakeString(handle));
+}
+
+- (void)removeAllObserversForState:(FIRLoadBundleTaskState)state {
+  _task->RemoveObservers(InternalLoadBundleTaskState(state));
+}
+
+- (void)removeAllObservers {
+  _task->RemoveAllObservers();
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
