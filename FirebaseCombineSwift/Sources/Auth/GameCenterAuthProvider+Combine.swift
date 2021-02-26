ce1034a1926c127289c91542036a5256f0dd9cbd
Tamim ziad <tamim.ziad@gmail.com>
2021-02-26 18:22:21 +0100

    Mark GameCenterAuthProvider as unavailable on WatchOS (#7599)
    Thanks for the fix, @ziadtamim.

--- FirebaseCombineSwift/Sources/Auth/GameCenterAuthProvider+Combine.swift
+++ FirebaseCombineSwift/Sources/Auth/GameCenterAuthProvider+Combine.swift
@@ -18,7 +18,8 @@
   import FirebaseAuth
 
   @available(swift 5.0)
-  @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
+  @available(watchOS, unavailable)
+  @available(macOS 10.15, iOS 13, tvOS 13, *)
   extension GameCenterAuthProvider {
     /// Creates a `AuthCredential` for a Game Center sign in.
     ///
