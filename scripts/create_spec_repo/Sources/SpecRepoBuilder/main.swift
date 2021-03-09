27fc3c6a34e2619147d0433ae66d84ed5ea11d42
Gran Luo <ollkorrect999@gmail.com>
2021-03-09 13:30:07 -0800

    Test MLModelDownloader in prerelease.

--- scripts/create_spec_repo/Sources/SpecRepoBuilder/main.swift
+++ scripts/create_spec_repo/Sources/SpecRepoBuilder/main.swift
@@ -30,7 +30,7 @@ extension Constants {
 
 // flags for 'pod push'
 extension Constants {
-  static let flags = ["--skip-tests", "--allow-warnings", "--verbose"]
+  static let flags = ["--skip-tests", "--allow-warnings"]
   static let umbrellaPodFlags = Constants.flags + ["--skip-import-validation", "--use-json"]
 }
 
