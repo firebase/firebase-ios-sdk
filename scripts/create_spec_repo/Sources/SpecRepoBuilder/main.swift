bea82c61ff02594ccc9fc4d5bfba139d8c829165
Gran Luo <ollkorrect999@gmail.com>
2021-03-11 18:08:53 -0800

    Test MLModelDownloader in prerelease.

--- scripts/create_spec_repo/Sources/SpecRepoBuilder/main.swift
+++ scripts/create_spec_repo/Sources/SpecRepoBuilder/main.swift
@@ -251,7 +251,7 @@ struct SpecRepoBuilder: ParsableCommand {
 
     print("Outcome is \(outcome)")
 
-    shell.run("cat \(podPath)", displaylog: true)
+    shell.run("cat \(podPath)", displayLog: true)
     return outcome
   }
 
