78a87773c730263744c93e16d7ba980a3aed02a4
Ziad <hey@byziad.com>
2021-03-30 23:54:58 +0200

    Add FIRPhoneAuthProviderFake

---
+++ FirebaseTestingSupport/Auth/Sources/FIRPhoneAuthProviderFake.m
@@ -0,0 +1,30 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "FirebaseTestingSupport/Auth/Sources/Public/FirebaseAuthTestingSupport/FIRPhoneAuthProviderFake.h"
+
+@implementation FIRPhoneAuthProviderFake
+
+- (instancetype)init {
+  // The object is partially initialized. Make sure the methods used during testing are overridden.
+  return self;
+}
+
+- (void)verifyPhoneNumber:(NSString *)phoneNumber UIDelegate:(id<FIRAuthUIDelegate>)UIDelegate completion:(FIRVerificationResultCallback)completion {
+    if (self.verifyPhoneNumberHandler) {
+      self.verifyPhoneNumberHandler(completion);
+    }
+}
+
+@end
---
+++ FirebaseTestingSupport/Auth/Tests/PhoneAuthProviderFakeTests.swift
@@ -0,0 +1,45 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+import XCTest
+@testable import FirebaseAuthTestingSupport
+
+class PhoneAuthProviderFakeTests: XCTestCase {
+    func testPhoneAuthProviderFakeConstructor() throws {
+      let fakePhoneAuthProvider = PhoneAuthProviderFake()
+      XCTAssertNotNil(fakePhoneAuthProvider)
+      XCTAssertTrue(fakePhoneAuthProvider.isKind(of: PhoneAuthProvider.self))
+    }
+    
+    func testVerifyPhoneNumberHandler() {
+      let fakePhoneAuthProvider = PhoneAuthProviderFake()
+
+      let handlerExpectation = expectation(description: "Handler called")
+        fakePhoneAuthProvider.verifyPhoneNumberHandler = { completion in
+            handlerExpectation.fulfill()
+
+            completion(nil, nil)
+        }
+
+      let completionExpectation = expectation(description: "Completion called")
+        fakePhoneAuthProvider.verifyPhoneNumber("", uiDelegate: nil) { verficationID, error in
+            completionExpectation.fulfill()
+            XCTAssertNil(verficationID)
+            XCTAssertNil(error)
+        }
+
+      wait(for: [handlerExpectation, completionExpectation], timeout: 0.5)
+    }
+}
