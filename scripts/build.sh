b163ec505831ec6c8122763a10b5475ade5cf491
Paul Beusterien <paulbeusterien@google.com>
2021-04-05 11:41:33 -0700

    Firebase pod support for watchOS for RC and RTDB (#7841)

--- scripts/build.sh
+++ scripts/build.sh
@@ -425,6 +425,14 @@ case "$product-$platform-$method" in
       build
     ;;
 
+  WatchOSSample-*-*)
+    RunXcodebuild \
+      -workspace 'Example/watchOSSample/SampleWatchApp.xcworkspace' \
+      -scheme "SampleWatchAppWatchKitApp" \
+      "${xcb_flags[@]}" \
+      build
+    ;;
+
   Database-*-unit)
     pod_gen FirebaseDatabase.podspec --platforms="${gen_platform}"
     RunXcodebuild \
