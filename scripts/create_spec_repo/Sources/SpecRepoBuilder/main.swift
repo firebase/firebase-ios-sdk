6c1831ac395e101afb5db8d109eea04ce4553c16
granluo <granluo@google.com>
2021-04-20 20:56:31 -0700

    Format update.

--- scripts/create_spec_repo/Sources/SpecRepoBuilder/main.swift
+++ scripts/create_spec_repo/Sources/SpecRepoBuilder/main.swift
@@ -319,10 +319,10 @@ struct SpecRepoBuilder: ParsableCommand {
       for podspecURL in podspecURLs {
         let podName = podspecURL.deletingPathExtension().lastPathComponent
         if Constants.exclusivePods.contains(podName) {
-            continue
-        } else if includePods.isEmpty || includePods.contains(podName){
+          continue
+        } else if includePods.isEmpty || includePods.contains(podName) {
           podSpecFiles[podName] = podspecURL
-        } 
+        }
       }
     } catch {
       print(
