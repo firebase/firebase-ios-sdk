49062706cc1eabcb21a7e59a5ffb3276cac43d45
Sebastian Schmidt <mrschmidt@google.com>
2021-04-27 10:33:11 -0600

    Update FIRStorageIntegrationTests.m

--- FirebaseStorage/Tests/Integration/FIRStorageIntegrationTests.m
+++ FirebaseStorage/Tests/Integration/FIRStorageIntegrationTests.m
@@ -471,7 +471,7 @@ NSString *const kTestPassword = KPASSWORD;
   FIRStorageReference *ref = [self.storage referenceWithPath:@"ios/public/1mb"];
 
   // Download URL format is
-  // "https://firebasestorage.googleapis.com/v0/b/{bucket}/o/{path}?alt=media&token={token}"
+  // "https://firebasestorage.googleapis.com:443/v0/b/{bucket}/o/{path}?alt=media&token={token}"
   NSString *downloadURLPattern =
       @"^https:\\/\\/firebasestorage.googleapis.com:443\\/v0\\/b\\/[^\\/]*\\/o\\/"
       @"ios%2Fpublic%2F1mb\\?alt=media&token=[a-z0-9-]*$";
