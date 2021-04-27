5a6d1a089588ef3c3a2041929f327d5ab0f3537a
Paul Beusterien <paulbeusterien@google.com>
2021-04-27 14:24:19 -0700

    style

--- ReleaseTooling/Sources/ZipBuilder/FrameworkBuilder.swift
+++ ReleaseTooling/Sources/ZipBuilder/FrameworkBuilder.swift
@@ -809,9 +809,9 @@ struct FrameworkBuilder {
     // TODO: remove this when we either remove iOS 10 support or have a cleaner way to support
     // iOS 11+ only pods.
     if let arm64Path = builtSlices[.iOSDevice],
-       let armv7Path = builtSlices[.iOSSimulator],
-       arm64Path.lastPathComponent == "FirebaseAppCheck" {
-        return [.arm64: arm64Path, .armv7: armv7Path]
+      let armv7Path = builtSlices[.iOSSimulator],
+      arm64Path.lastPathComponent == "FirebaseAppCheck" {
+      return [.arm64: arm64Path, .armv7: armv7Path]
     }
 
     let fileManager = FileManager.default
