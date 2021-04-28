31307cdf7e2e7f0cf931ccf30f54fb409cad037d
Paul Beusterien <paulbeusterien@google.com>
2021-04-27 21:56:45 -0700

    Skip armv7 for iOS11+ AppCheck (#7983)

--- ReleaseTooling/Sources/ZipBuilder/FrameworkBuilder.swift
+++ ReleaseTooling/Sources/ZipBuilder/FrameworkBuilder.swift
@@ -184,7 +184,18 @@ struct FrameworkBuilder {
     let distributionFlag = setCarthage ? "-DFIREBASE_BUILD_CARTHAGE" :
       "-DFIREBASE_BUILD_ZIP_FILE"
     let cFlags = "OTHER_CFLAGS=$(value) \(distributionFlag)"
-    let archs = targetPlatform.archs.map { $0.rawValue }.joined(separator: " ")
+
+    var archs = targetPlatform.archs.map { $0.rawValue }.joined(separator: " ")
+    // The 32 bit archs do not build for iOS 11.
+    // TODO: Make a more robust solution if we need to support more of a mix between iOS 11 and
+    // under.
+    if framework == "FirebaseAppCheck" {
+      if targetPlatform == .iOSDevice {
+        archs = "arm64"
+      } else if targetPlatform == .iOSSimulator {
+        archs = "x86_64 arm64"
+      }
+    }
 
     var args = ["build",
                 "-configuration", "release",
@@ -813,6 +824,12 @@ struct FrameworkBuilder {
       if platform == .iOSSimulator {
         // Exclude the arm64 slice for simulator since Carthage can't package as an XCFramework.
         archs.removeAll(where: { $0 == .arm64 })
+      }
+
+      // lipo doesn't work if only one architecture.
+      if archs.count == 1 {
+        slices[archs.first!] = binary
+        continue
       }
 
       // Loop through the architectures and strip out each by using `lipo`.
