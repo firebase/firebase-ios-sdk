f4a8c9b2feed3cfc8d9c33d62589d75573762414
Chen Liang <chliang@google.com>
2021-05-20 16:36:56 -0700

    fix a bug that sdk did not capture correct state when app is open from closed

--- FirebaseMessaging/Apps/Shared/AppDelegate.swift
+++ FirebaseMessaging/Apps/Shared/AppDelegate.swift
@@ -21,7 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                    didFinishLaunchingWithOptions launchOptions: [UIApplication
                      .LaunchOptionsKey: Any]?) -> Bool {
     FirebaseApp.configure()
-
     let center = UNUserNotificationCenter.current()
     center.delegate = self
 
@@ -42,14 +41,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     completionHandler([.alert, .sound])
   }
 
-  // MARK: UISceneSession Lifecycle
-
-  func application(_ application: UIApplication,
-                   configurationForConnecting connectingSceneSession: UISceneSession,
-                   options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return UISceneConfiguration(name: "Default Configuration",
-                                sessionRole: connectingSceneSession.role)
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              didReceive response: UNNotificationResponse,
+                              withCompletionHandler completionHandler: @escaping () -> Void) {
+    completionHandler()
   }
 }
