d7e8b1a58ab9300e3bf04497408823996a21d13c
Gran <granluo@google.com>
2021-05-28 16:19:03 -0700

    Update get_diff_lines.sh

--- scripts/code_coverage_report/get_diff_lines.sh
+++ scripts/code_coverage_report/get_diff_lines.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-/*
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-# This will generate a JSON file of changed files and their newly added lines.
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# USAGE: git diff -U0 [base_commit] HEAD | get_diff_lines.sh [output_file_name]
+#
+# This will generate a JSON file, named ${output_file_name}, of changed
+# files and their newly added lines.
 oIFS=$IFS
 output_file=$1
 
 json_output="["
 # Concatenate files and line indices into a JSON file.
-concatenate(){
+concatenate() {
   local path=$1
   shift
   IFS=","
