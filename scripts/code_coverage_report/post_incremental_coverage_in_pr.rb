76f5f4ee3babc32d362f1f5b3b56931872b7f826
granluo <granluo@google.com>
2021-06-03 09:27:44 -0700

    Update style.

--- scripts/code_coverage_report/post_incremental_coverage_in_pr.rb
+++ scripts/code_coverage_report/post_incremental_coverage_in_pr.rb
@@ -1,3 +1,22 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# USAGE: git diff -U0 [base_commit] HEAD | get_diff_lines.sh
+#
+# This will generate a JSON output of changed files and their newly added
+# lines.
+
 require 'octokit'
 require 'json'
 
@@ -16,13 +35,13 @@ def clean_coverage_comments(client)
   comment_page = 0
   loop do
     comment_page += 1
-    cur_page_comment = client.pull_request_comments(REPO, PULL_REQUEST, { :per_page =>100, :page => comment_page }) 
+    cur_page_comment = client.pull_request_comments(REPO, PULL_REQUEST, { :per_page =>100, :page => comment_page })
     if cur_page_comment.length == 0
       break
     end
     for cmt in cur_page_comment do
       # Remove comments when the comment body meets the REMOVE_PATTERN.
-      if cmt.body =~ REMOVE_PATTERN 
+      if cmt.body =~ REMOVE_PATTERN
         client.delete_pull_request_comment(REPO,cmt.id)
       end
     end
@@ -35,7 +54,7 @@ def add_coverage_comments(client, uncovered_files)
     start_line = -1
     coverage_line.each_with_index do |line, idx|
       # Init start_line to the first uncovered line of a file.
-      if start_line == -1 
+      if start_line == -1
         start_line = line
       end
       if idx < coverage_line.length() && line + 1 == coverage_line[idx+1]
