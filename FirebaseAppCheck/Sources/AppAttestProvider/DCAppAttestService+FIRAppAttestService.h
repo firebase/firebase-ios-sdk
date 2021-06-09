8894e82a38ba68736e70fed48dc2b2fd9ca31b9a
Maksym Malyhin <mmaksym@google.com>
2021-06-09 11:44:47 -0400

    Add #import <TargetConditionals.h> where needed.

--- FirebaseAppCheck/Sources/AppAttestProvider/DCAppAttestService+FIRAppAttestService.h
+++ FirebaseAppCheck/Sources/AppAttestProvider/DCAppAttestService+FIRAppAttestService.h
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+#import <TargetConditionals.h>
+
 // Currently DCAppAttestService is available on iOS only.
 #if TARGET_OS_IOS && !TARGET_OS_MACCATALYST  // Catalyst should be possible with Xcode 12.5+
 
