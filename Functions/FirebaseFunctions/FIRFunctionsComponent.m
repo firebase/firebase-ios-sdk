25d62fa7e54330f7835dec4749d7d73f043b2282
Morgan Chen <morganchen12@gmail.com>
2021-06-15 15:41:38 -0700

    Fix bad import

--- Functions/FirebaseFunctions/FIRFunctionsComponent.m
+++ Functions/FirebaseFunctions/FIRFunctionsComponent.m
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "FIRFunctionsComponent.h"
+#import "Functions/FirebaseFunctions/FIRFunctionsComponent.h"
 
 #import "Functions/FirebaseFunctions/FIRFunctions_Private.h"
 
