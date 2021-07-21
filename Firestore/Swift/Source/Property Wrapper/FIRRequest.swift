79f57b6a029c2b61e786583c73affb89409c0755
florian schweizer <florian.schweizer@tum.de>
2021-07-21 07:04:14 +0200

    Treat CollectionReference as Query

--- Firestore/Swift/Source/Property Wrapper/FIRRequest.swift
+++ Firestore/Swift/Source/Property Wrapper/FIRRequest.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 public enum FIRPredicate {
     case isEqualTo(_ lhs: String, _ rhs: Any)
-
+    
     case isIn(_ lhs: String, rhs: [Any])
     case isNotIn(_ lhs: String, _ rhs: [Any])
-
+    
     case arrayContains(_ lhs: String, _ rhs: Any)
     case arrayContainsAny(_ lhs: String, _ rhs: [Any])
-
+    
     case isLessThan(_ lhs: String, _ rhs: Any)
     case isGreaterThan(_ lhs: String, _ rhs: Any)
-
+    
     case isLessThanOrEqualTo(_ lhs: String, _ rhs: Any)
     case isGreaterThanOrEqualTo(_ lhs: String, _ rhs: Any)
 }
@@ -34,91 +34,42 @@ public class FirebaseStore<T: Decodable>: ObservableObject {
     }
     
     private func load(from collection: String, withPredicates predicates: [FIRPredicate]) {
-        let reference = store.collection(collection)
-
-        if predicates.isEmpty {
-            reference
-                .getDocuments { snapshot, error in
-                    guard error == nil, let snapshot = snapshot else {
-                        return
-                    }
-
-                    self.items = snapshot.documents.compactMap { document in
-                        try? document.data(as: T.self)
-                    }
+        let query: Query = store.collection(collection)
+        
+        for predicate in predicates {
+            switch predicate {
+                case .isEqualTo(let lhs, let rhs):
+                    query = query.whereField(lhs, isEqualTo: rhs)
+                case .isIn(let lhs, let rhs):
+                    query = query.whereField(lhs, in: rhs)
+                case .isNotIn(let lhs, let rhs):
+                    query = query.whereField(lhs, notIn: rhs)
+                case .arrayContains(let lhs, let rhs):
+                    query = query.whereField(lhs, arrayContains: rhs)
+                case .arrayContainsAny(let lhs, let rhs):
+                    query = query.whereField(lhs, arrayContainsAny: rhs)
+                case .isLessThan(let lhs, let rhs):
+                    query = query.whereField(lhs, isLessThan: rhs)
+                case .isGreaterThan(let lhs, let rhs):
+                    query = query.whereField(lhs, isGreaterThan: rhs)
+                case .isLessThanOrEqualTo(let lhs, let rhs):
+                    query = query.whereField(lhs, isLessThanOrEqualTo: rhs)
+                case .isGreaterThanOrEqualTo(let lhs, let rhs):
+                    query = query.whereField(lhs, isGreaterThanOrEqualTo: rhs)
+            }
+        }
+        
+        query
+            .getDocuments { snapshot, error in
+                guard error == nil, let snapshot = snapshot else {
+                    self.items = []
+                    return
                 }
-        } else {
-            var query: Query? = nil
-            for predicate in predicates {
-                switch predicate {
-                    case .isEqualTo(let lhs, let rhs):
-                        if let tempQuery = query {
-                            query = tempQuery.whereField(lhs, isEqualTo: rhs)
-                        } else {
-                            query = reference.whereField(lhs, isEqualTo: rhs)
-                        }
-                    case .isIn(let lhs, let rhs):
-                        if let tempQuery = query {
-                            query = tempQuery.whereField(lhs, in: rhs)
-                        } else {
-                            query = reference.whereField(lhs, in: rhs)
-                        }
-                    case .isNotIn(let lhs, let rhs):
-                        if let tempQuery = query {
-                            query = tempQuery.whereField(lhs, notIn: rhs)
-                        } else {
-                            query = reference.whereField(lhs, notIn: rhs)
-                        }
-                    case .arrayContains(let lhs, let rhs):
-                        if let tempQuery = query {
-                            query = tempQuery.whereField(lhs, arrayContains: rhs)
-                        } else {
-                            query = reference.whereField(lhs, arrayContains: rhs)
-                        }
-                    case .arrayContainsAny(let lhs, let rhs):
-                        if let tempQuery = query {
-                            query = tempQuery.whereField(lhs, arrayContainsAny: rhs)
-                        } else {
-                            query = reference.whereField(lhs, arrayContainsAny: rhs)
-                        }
-                    case .isLessThan(let lhs, let rhs):
-                        if let tempQuery = query {
-                            query = tempQuery.whereField(lhs, isLessThan: rhs)
-                        } else {
-                            query = reference.whereField(lhs, isLessThan: rhs)
-                        }
-                    case .isGreaterThan(let lhs, let rhs):
-                        if let tempQuery = query {
-                            query = tempQuery.whereField(lhs, isGreaterThan: rhs)
-                        } else {
-                            query = reference.whereField(lhs, isGreaterThan: rhs)
-                        }
-                    case .isLessThanOrEqualTo(let lhs, let rhs):
-                        if let tempQuery = query {
-                            query = tempQuery.whereField(lhs, isLessThanOrEqualTo: rhs)
-                        } else {
-                            query = reference.whereField(lhs, isLessThanOrEqualTo: rhs)
-                        }
-                    case .isGreaterThanOrEqualTo(let lhs, let rhs):
-                        if let tempQuery = query {
-                            query = tempQuery.whereField(lhs, isGreaterThanOrEqualTo: rhs)
-                        } else {
-                            query = reference.whereField(lhs, isGreaterThanOrEqualTo: rhs)
-                        }
+                
+                self.items = snapshot.documents.compactMap { document in
+                    try? document.data(as: T.self)
                 }
             }
-
-            query?
-                .getDocuments { snapshot, error in
-                    guard error == nil, let snapshot = snapshot else {
-                        return
-                    }
-
-                    self.items = snapshot.documents.compactMap { document in
-                        try? document.data(as: T.self)
-                    }
-                }
-        }
     }
 }
 
