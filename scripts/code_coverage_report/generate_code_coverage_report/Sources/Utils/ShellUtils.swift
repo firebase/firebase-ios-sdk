26035d12c7aa560483d61ed0de0b7091a4137866
granluo <granluo@google.com>
2021-07-20 17:14:54 -0700

    Pod binary size measure.

--- scripts/code_coverage_report/generate_code_coverage_report/Sources/Utils/ShellUtils.swift
+++ scripts/code_coverage_report/generate_code_coverage_report/Sources/Utils/ShellUtils.swift
@@ -16,13 +16,21 @@
 
 import Foundation
 
+public enum ProcessCom {
+    case pipe
+    case stdout
+}
+
 public enum Shell {
   @discardableResult
   public static func run(_ command: String, displayCommand: Bool = true,
-                         displayFailureResult: Bool = true) -> Int32 {
+                         displayFailureResult: Bool = true, 
+                         stdout: ProcessCom = .pipe) -> Int32 {
     let task = Process()
     let pipe = Pipe()
-    task.standardOutput = pipe
+    if stdout == ProcessCom.pipe{
+      task.standardOutput = pipe
+    }
     task.launchPath = "/bin/zsh"
     task.arguments = ["-c", command]
     task.launch()
@@ -30,11 +38,13 @@ public enum Shell {
       print("[CoverageReportParser] Command:\(command)\n")
     }
     task.waitUntilExit()
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    let log = String(data: data, encoding: .utf8)!
-    if displayFailureResult, task.terminationStatus != 0 {
-      print("-----Exit code: \(task.terminationStatus)")
-      print("-----Log:\n \(log)")
+    if stdout == ProcessCom.pipe{
+      let data = pipe.fileHandleForReading.readDataToEndOfFile()
+      let log = String(data: data, encoding: .utf8)!
+      if displayFailureResult, task.terminationStatus != 0 {
+        print("-----Exit code: \(task.terminationStatus)")
+        print("-----Log:\n \(log)")
+      }
     }
     return task.terminationStatus
   }
