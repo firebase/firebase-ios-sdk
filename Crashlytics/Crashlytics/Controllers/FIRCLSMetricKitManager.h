c38606a97fc176ccae830ad3d549a4c50a9354ec
elenadoty <elenadoty@google.com>
2021-07-28 13:55:37 -0400

    include additional availability check

--- Crashlytics/Crashlytics/Controllers/FIRCLSMetricKitManager.h
+++ Crashlytics/Crashlytics/Controllers/FIRCLSMetricKitManager.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import <Foundation/Foundation.h>
 #include <AvailabilityMacros.h>
+#import <Foundation/Foundation.h>
 
 #define CLS_METRICKIT_SUPPORTED (__has_include(<MetricKit/MetricKit.h>) && TARGET_OS_IOS && defined(IOS_VERSION_15_0))
 
