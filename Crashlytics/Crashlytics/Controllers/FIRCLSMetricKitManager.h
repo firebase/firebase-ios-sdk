032ae3883cce1a8c97c747e30f4a7b5f8f7ec3de
elenadoty <elenadoty@google.com>
2021-07-28 13:55:10 -0400

    include additional availability check

--- Crashlytics/Crashlytics/Controllers/FIRCLSMetricKitManager.h
+++ Crashlytics/Crashlytics/Controllers/FIRCLSMetricKitManager.h
@@ -13,8 +13,9 @@
 // limitations under the License.
 
 #import <Foundation/Foundation.h>
+#include <AvailabilityMacros.h>
 
-#define CLS_METRICKIT_SUPPORTED (__has_include(<MetricKit/MetricKit.h>) && TARGET_OS_IOS)
+#define CLS_METRICKIT_SUPPORTED (__has_include(<MetricKit/MetricKit.h>) && TARGET_OS_IOS && defined(IOS_VERSION_15_0))
 
 #if CLS_METRICKIT_SUPPORTED
 #import <MetricKit/MetricKit.h>
@@ -30,7 +31,6 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-API_AVAILABLE(ios(15.0))
 @interface FIRCLSMetricKitManager : NSObject <MXMetricManagerSubscriber>
 
 - (instancetype)initWithManagerData:(FIRCLSManagerData *)managerData
