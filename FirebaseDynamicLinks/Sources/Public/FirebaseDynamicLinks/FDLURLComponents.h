247d78d6ef802d17bc9b157db4a8c7591673c77f
Morgan Chen <morganchen12@gmail.com>
2021-07-30 11:53:33 -0700

    Move Dynamic Links to API_UNAVAILABLE

--- FirebaseDynamicLinks/Sources/Public/FirebaseDynamicLinks/FDLURLComponents.h
+++ FirebaseDynamicLinks/Sources/Public/FirebaseDynamicLinks/FDLURLComponents.h
@@ -16,16 +16,6 @@
 
 #import <Foundation/Foundation.h>
 
-// NS_SWIFT_NAME can only translate factory methods before the iOS 9.3 SDK.
-// Wrap it in our own macro if it's a non-compatible SDK.
-#ifndef FIR_SWIFT_NAME
-#ifdef __IPHONE_9_3
-#define FIR_SWIFT_NAME(X) NS_SWIFT_NAME(X)
-#else
-#define FIR_SWIFT_NAME(X)  // Intentionally blank.
-#endif                     // #ifdef __IPHONE_9_3
-#endif                     // #ifndef FIR_SWIFT_NAME
-
 NS_ASSUME_NONNULL_BEGIN
 
 /**
@@ -40,7 +30,7 @@ typedef NS_ENUM(NSInteger, FIRShortDynamicLinkPathLength) {
   FIRShortDynamicLinkPathLengthShort,
   /** Short link with an extra long path for great difficulty in guessing. */
   FIRShortDynamicLinkPathLengthUnguessable,
-} FIR_SWIFT_NAME(ShortDynamicLinkPathLength);
+} NS_SWIFT_NAME(ShortDynamicLinkPathLength);
 
 /**
  * @abstract The definition of the completion block used by URL shortener.
@@ -53,13 +43,13 @@ typedef NS_ENUM(NSInteger, FIRShortDynamicLinkPathLength) {
 typedef void (^FIRDynamicLinkShortenerCompletion)(NSURL *_Nullable shortURL,
                                                   NSArray<NSString *> *_Nullable warnings,
                                                   NSError *_Nullable error)
-    FIR_SWIFT_NAME(DynamicLinkShortenerCompletion);
+    NS_SWIFT_NAME(DynamicLinkShortenerCompletion);
 
 /**
  * @class FIRDynamicLinkGoogleAnalyticsParameters
  * @abstract The Dynamic Link analytics parameters.
  */
-FIR_SWIFT_NAME(DynamicLinkGoogleAnalyticsParameters)
+NS_SWIFT_NAME(DynamicLinkGoogleAnalyticsParameters)
 @interface FIRDynamicLinkGoogleAnalyticsParameters : NSObject
 
 /**
@@ -138,7 +128,7 @@ FIR_SWIFT_NAME(DynamicLinkGoogleAnalyticsParameters)
  * @class FIRDynamicLinkIOSParameters
  * @abstract The Dynamic Link iOS parameters.
  */
-FIR_SWIFT_NAME(DynamicLinkIOSParameters)
+NS_SWIFT_NAME(DynamicLinkIOSParameters)
 @interface FIRDynamicLinkIOSParameters : NSObject
 
 /**
@@ -214,7 +204,7 @@ FIR_SWIFT_NAME(DynamicLinkIOSParameters)
  * @class FIRDynamicLinkItunesConnectAnalyticsParameters
  * @abstract The Dynamic Link iTunes Connect parameters.
  */
-FIR_SWIFT_NAME(DynamicLinkItunesConnectAnalyticsParameters)
+NS_SWIFT_NAME(DynamicLinkItunesConnectAnalyticsParameters)
 @interface FIRDynamicLinkItunesConnectAnalyticsParameters : NSObject
 
 /**
@@ -255,7 +245,7 @@ FIR_SWIFT_NAME(DynamicLinkItunesConnectAnalyticsParameters)
  * @class FIRDynamicLinkAndroidParameters
  * @abstract The Dynamic Link Android parameters.
  */
-FIR_SWIFT_NAME(DynamicLinkAndroidParameters)
+NS_SWIFT_NAME(DynamicLinkAndroidParameters)
 @interface FIRDynamicLinkAndroidParameters : NSObject
 
 /**
@@ -303,7 +293,7 @@ FIR_SWIFT_NAME(DynamicLinkAndroidParameters)
  * @class FIRDynamicLinkSocialMetaTagParameters
  * @abstract The Dynamic Link Social Meta Tag parameters.
  */
-FIR_SWIFT_NAME(DynamicLinkSocialMetaTagParameters)
+NS_SWIFT_NAME(DynamicLinkSocialMetaTagParameters)
 @interface FIRDynamicLinkSocialMetaTagParameters : NSObject
 
 /**
@@ -344,7 +334,7 @@ FIR_SWIFT_NAME(DynamicLinkSocialMetaTagParameters)
  * @class FIRDynamicLinkNavigationInfoParameters
  * @abstract Options class for defining navigation behavior of the Dynamic Link.
  */
-FIR_SWIFT_NAME(DynamicLinkNavigationInfoParameters)
+NS_SWIFT_NAME(DynamicLinkNavigationInfoParameters)
 @interface FIRDynamicLinkNavigationInfoParameters : NSObject
 
 /**
@@ -379,7 +369,7 @@ FIR_SWIFT_NAME(DynamicLinkNavigationInfoParameters)
  * @abstract Options class for defining other platform(s) parameters of the Dynamic Link.
  *     Other here means not covered by specific parameters (not iOS and not Android).
  */
-FIR_SWIFT_NAME(DynamicLinkOtherPlatformParameters)
+NS_SWIFT_NAME(DynamicLinkOtherPlatformParameters)
 @interface FIRDynamicLinkOtherPlatformParameters : NSObject
 
 /**
@@ -411,7 +401,7 @@ FIR_SWIFT_NAME(DynamicLinkOtherPlatformParameters)
  * @class FIRDynamicLinkComponentsOptions
  * @abstract Options class for defining how Dynamic Link URLs are generated.
  */
-FIR_SWIFT_NAME(DynamicLinkComponentsOptions)
+NS_SWIFT_NAME(DynamicLinkComponentsOptions)
 @interface FIRDynamicLinkComponentsOptions : NSObject
 
 /**
@@ -444,7 +434,7 @@ FIR_SWIFT_NAME(DynamicLinkComponentsOptions)
  *     Dynamic Link URLs. Short URLs will have a domain and a randomized path; long URLs will have a
  *     domain and a query that contains all of the Dynamic Link parameters.
  */
-FIR_SWIFT_NAME(DynamicLinkComponents)
+NS_SWIFT_NAME(DynamicLinkComponents)
 @interface FIRDynamicLinkComponents : NSObject
 
 /**
