e98a2ba83f5f38cd8b3c55872d20235d86854db2
Jesus Crespo <jesuscrespo@google.com>
2021-08-05 10:37:36 -0500

    Update FunctionsAPITests.swift with watchos and Task

--- Functions/Tests/Unit/Swift/FunctionsAPITests.swift
+++ Functions/Tests/Unit/Swift/FunctionsAPITests.swift
@@ -61,9 +61,9 @@ final class FunctionsAPITests {
     }
 
     #if swift(>=5.5)
-      if #available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, *) {
+      if #available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, *) {
         // async/await is a Swift 5.5+ feature available on iOS 15+
-        async {
+        Task {
           do {
             let result = try await callableRef.call(data)
             _ = result.data
@@ -83,9 +83,9 @@ final class FunctionsAPITests {
     }
 
     #if swift(>=5.5)
-      if #available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, *) {
+      if #available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, *) {
         // async/await is a Swift 5.5+ feature available on iOS 15+
-        async {
+        Task {
           do {
             let result = try await callableRef.call()
             _ = result.data
