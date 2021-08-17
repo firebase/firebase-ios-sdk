07c35f03c2235b01d38f4071fa91394730147b38
granluo <granluo@google.com>
2021-08-17 02:19:07 -0700

    Create Binary Size Report Generation

--- scripts/health_metrics/generate_code_coverage_report/Package.swift
+++ scripts/health_metrics/generate_code_coverage_report/Package.swift
@@ -34,6 +34,10 @@ let package = Package(
       name: "IncrementalCoverageReportGenerator",
       targets: ["IncrementalCoverageReportGenerator"]
     ),
+    .executable(
+      name: "BinarySizeReportGenerator",
+      targets: ["BinarySizeReportGenerator"]
+    ),
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "0.2.0"),
@@ -54,6 +58,13 @@ let package = Package(
     ),
     .target(
       name: "IncrementalCoverageReportGenerator",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        "Utils",
+      ]
+    ),
+    .target(
+      name: "BinarySizeReportGenerator",
       dependencies: [
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
         "Utils",
