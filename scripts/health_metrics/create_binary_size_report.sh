6fcdb96d8933abdca1be25010fee68bbf66b5076
granluo <granluo@google.com>
2021-08-19 01:24:29 -0700

    Test binary size.

--- scripts/health_metrics/create_binary_size_report.sh
+++ scripts/health_metrics/create_binary_size_report.sh
@@ -34,5 +34,6 @@ BINARY_SIZE_SDK+=('FirebaseStorage')
 if [ -n "$BINARY_SIZE_SDK" ]; then
   cd scripts/health_metrics/generate_code_coverage_report/
   git clone https://github.com/google/cocoapods-size
-  swift run BinarySizeReportGenerator --binary-size-tool-dir cocoapods-size/  --sdk-repo-dir "${GITHUB_WORKSPACE}" --sdk ${BINARY_SIZE_SDK[@]} --presubmit "firebase/firebase-ios-sdk" --head-commit "${GITHUB_SHA}" --token $(gcloud auth print-identity-token) --log-link "https://github.com/firebase/firebase-ios-sdk/actions/runs/${GITHUB_RUN_ID}" --base-commit "$base_commit"
+  swift build
+  .build/debug/BinarySizeReportGenerator --binary-size-tool-dir cocoapods-size/  --sdk-repo-dir "${GITHUB_WORKSPACE}" --sdk ${BINARY_SIZE_SDK[@]} --presubmit "firebase/firebase-ios-sdk" --head-commit "${GITHUB_SHA}" --token $(gcloud auth print-identity-token) --log-link "https://github.com/firebase/firebase-ios-sdk/actions/runs/${GITHUB_RUN_ID}" --base-commit "$base_commit"
 fi
