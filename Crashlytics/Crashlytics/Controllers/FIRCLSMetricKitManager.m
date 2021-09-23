6a3e4d277969e84502644550ed58351378c2ebcc
Elena Doty <elenadoty@google.com>
2021-09-23 10:15:29 -0700

    update comment

--- Crashlytics/Crashlytics/Controllers/FIRCLSMetricKitManager.m
+++ Crashlytics/Crashlytics/Controllers/FIRCLSMetricKitManager.m
@@ -133,8 +133,9 @@
     return false;
   }
 
-  // TODO: Time stamp information is only available in begin and end time periods. Hopefully this
-  // is updated with iOS 15.
+  // Time stamp information from MetricKit is only available in begin and end time periods. For now,
+  // we rely on the current timestamp to set the event timestamp, since this is likely more accurate
+  // that the 24 hour block we'd otherwise have.
   NSTimeInterval beginSecondsSince1970 = [[NSDate date] timeIntervalSince1970];
   NSTimeInterval endSecondsSince1970 = [diagnosticPayload.timeStampEnd timeIntervalSince1970];
 
