6693dcd0f30ddb21c7fafe7ede35173e67fe6f29
Rosalyn Tan <rosalyntan@google.com>
2021-09-24 15:33:56 -0700

    Add underlyingError to FIRAuthInternalErrorCodeUnexpectedErrorResponse errors.

--- FirebaseAuth/Sources/Utilities/FIRAuthErrorUtils.h
+++ FirebaseAuth/Sources/Utilities/FIRAuthErrorUtils.h
@@ -90,6 +90,18 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (NSError *)unexpectedErrorResponseWithDeserializedResponse:(id)deserializedResponse;
 
+/** @fn unexpectedErrorResponseWithDeserializedResponse:underlyingError:
+    @brief Constructs an @c NSError with the @c FIRAuthInternalErrorCodeUnexpectedErrorResponse
+        code, and populated @c FIRAuthErrorUserInfoDeserializedResponseKey and
+        @c NSUnderlyingErrorKey keys in the @c NSError.userInfo dictionary.
+    @param deserializedResponse The value of the @c FIRAuthErrorUserInfoDeserializedResponseKey key.
+    @param underlyingError The value of the @c NSUnderlyingErrorKey key.
+    @remarks This error is used when a network request results in an error, and the body data was
+        deserializable as JSON, but couldn't be decoded as an error.
+ */
++ (NSError *)unexpectedErrorResponseWithDeserializedResponse:(id)deserializedResponse
+                                             underlyingError:(NSError *)underlyingError;
+
 /** @fn malformedJWTErrorWithToken:underlyingError:
     @brief Constructs an @c NSError with the code set to @c FIRAuthErrorCodeMalformedJWT and
         populates the userInfo dictionary with an error message, the bad token, and an underlying
