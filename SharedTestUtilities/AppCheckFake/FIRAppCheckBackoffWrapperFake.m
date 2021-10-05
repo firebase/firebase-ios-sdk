129c227f374441a759763eee9bd5a4b9b82c3e46
Maksym Malyhin <mmaksym@google.com>
2021-10-05 16:54:05 -0400

    WIP FIRAppCheckBackoffWrapperFake

---
+++ SharedTestUtilities/AppCheckFake/FIRAppCheckBackoffWrapperFake.m
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#import "SharedTestUtilities/AppCheckFake/FIRAppCheckBackoffWrapperFake.h"
+
+#if __has_include(<FBLPromises/FBLPromises.h>)
+#import <FBLPromises/FBLPromises.h>
+#else
+#import "FBLPromises.h"
+#endif
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation FIRAppCheckBackoffWrapperFake
+
+- (FBLPromise *)backoff:(FIRAppCheckBackoffOperationProvider)operationProvider errorHandler:(FIRAppCheckBackoffErrorHandler)errorHandler {
+  [self.backoffExpectation fulfill];
+
+  if (self.isNextOperationAllowed) {
+    return operationProvider();
+  } else {
+    FBLPromise *rejectedPromise = [FBLPromise pendingPromise];
+    NSError *error = [NSError errorWithDomain:@"FIRAppCheckBackoffWrapperFake.backoff" code:-1 userInfo:nil];
+    [rejectedPromise reject:error];
+    return rejectedPromise;
+  }
+}
+
+- (FIRAppCheckBackoffErrorHandler)defaultErrorHandler {
+  <#code#>
+}
+
+- (void)resetBackoff {
+  <#code#>
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
