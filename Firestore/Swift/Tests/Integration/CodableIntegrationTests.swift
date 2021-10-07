95acfb22402ba133837988c296dccff14a8d77cb
Paul Beusterien <paulbeusterien@google.com>
2021-10-07 07:28:49 -0700

    Fix inadvertent line delete

--- Firestore/Swift/Tests/Integration/CodableIntegrationTests.swift
+++ Firestore/Swift/Tests/Integration/CodableIntegrationTests.swift
@@ -100,66 +100,66 @@ class CodableIntegrationTests: FSTIntegrationTestCase {
     }
   }
 
-    func testServerTimestamp() throws {
-      struct Model: Codable, Equatable {
-        var name: String
-        @ServerTimestamp var ts: Timestamp? = nil
-      }
-      let model = Model(name: "name")
-      let docToWrite = documentRef()
+  func testServerTimestamp() throws {
+    struct Model: Codable, Equatable {
+      var name: String
+      @ServerTimestamp var ts: Timestamp? = nil
+    }
+    let model = Model(name: "name")
+    let docToWrite = documentRef()
 
-      for flavor in allFlavors {
-        try setData(from: model, forDocument: docToWrite, withFlavor: flavor)
+    for flavor in allFlavors {
+      try setData(from: model, forDocument: docToWrite, withFlavor: flavor)
 
-        let decoded = try readDocument(forRef: docToWrite).data(as: Model.self)
+      let decoded = try readDocument(forRef: docToWrite).data(as: Model.self)
 
-        XCTAssertNotNil(decoded?.ts, "Failed with flavor \(flavor)")
-        if let ts = decoded?.ts {
-          XCTAssertGreaterThan(ts.seconds, 1_500_000_000, "Failed with flavor \(flavor)")
-        } else {
-          XCTFail("Expect server timestamp is set, but getting .pending")
-        }
+      XCTAssertNotNil(decoded?.ts, "Failed with flavor \(flavor)")
+      if let ts = decoded?.ts {
+        XCTAssertGreaterThan(ts.seconds, 1_500_000_000, "Failed with flavor \(flavor)")
+      } else {
+        XCTFail("Expect server timestamp is set, but getting .pending")
       }
     }
+  }
 
-    func testServerTimestampBehavior() throws {
-      struct Model: Codable, Equatable {
-        var name: String
-        @ServerTimestamp var ts: Timestamp? = nil
-      }
-
-      disableNetwork()
-      let docToWrite = documentRef()
-      let now = Int64(Date().timeIntervalSince1970)
-      let pastTimestamp = Timestamp(seconds: 807_940_800, nanoseconds: 0)
-
-      // Write a document with a current value to enable testing with .previous.
-      let originalModel = Model(name: "name", ts: pastTimestamp)
-      let completion1 = completionForExpectation(withName: "setData")
-      try docToWrite.setData(from: originalModel, completion: completion1)
-
-      // Overwrite with a nil server timestamp so that ServerTimestampBehavior is testable.
-      let newModel = Model(name: "name")
-      let completion2 = completionForExpectation(withName: "setData")
-      try docToWrite.setData(from: newModel, completion: completion2)
-
-      let snapshot = readDocument(forRef: docToWrite)
-      var decoded = try snapshot.data(as: Model.self, with: .none)
-      XCTAssertNil(decoded?.ts)
-
-      decoded = try snapshot.data(as: Model.self, with: .estimate)
-      XCTAssertNotNil(decoded?.ts)
-      XCTAssertNotNil(decoded?.ts?.seconds)
-      XCTAssertGreaterThanOrEqual(decoded!.ts!.seconds, now)
-
-      decoded = try snapshot.data(as: Model.self, with: .previous)
-      XCTAssertNotNil(decoded?.ts)
-      XCTAssertNotNil(decoded?.ts?.seconds)
-      XCTAssertEqual(decoded!.ts!.seconds, pastTimestamp.seconds)
-
-      enableNetwork()
-      awaitExpectations()
+  func testServerTimestampBehavior() throws {
+    struct Model: Codable, Equatable {
+      var name: String
+      @ServerTimestamp var ts: Timestamp? = nil
     }
+
+    disableNetwork()
+    let docToWrite = documentRef()
+    let now = Int64(Date().timeIntervalSince1970)
+    let pastTimestamp = Timestamp(seconds: 807_940_800, nanoseconds: 0)
+
+    // Write a document with a current value to enable testing with .previous.
+    let originalModel = Model(name: "name", ts: pastTimestamp)
+    let completion1 = completionForExpectation(withName: "setData")
+    try docToWrite.setData(from: originalModel, completion: completion1)
+
+    // Overwrite with a nil server timestamp so that ServerTimestampBehavior is testable.
+    let newModel = Model(name: "name")
+    let completion2 = completionForExpectation(withName: "setData")
+    try docToWrite.setData(from: newModel, completion: completion2)
+
+    let snapshot = readDocument(forRef: docToWrite)
+    var decoded = try snapshot.data(as: Model.self, with: .none)
+    XCTAssertNil(decoded?.ts)
+
+    decoded = try snapshot.data(as: Model.self, with: .estimate)
+    XCTAssertNotNil(decoded?.ts)
+    XCTAssertNotNil(decoded?.ts?.seconds)
+    XCTAssertGreaterThanOrEqual(decoded!.ts!.seconds, now)
+
+    decoded = try snapshot.data(as: Model.self, with: .previous)
+    XCTAssertNotNil(decoded?.ts)
+    XCTAssertNotNil(decoded?.ts?.seconds)
+    XCTAssertEqual(decoded!.ts!.seconds, pastTimestamp.seconds)
+
+    enableNetwork()
+    awaitExpectations()
+  }
 
   func testFieldValue() throws {
     struct Model: Encodable {
@@ -185,99 +185,100 @@ class CodableIntegrationTests: FSTIntegrationTestCase {
     }
   }
 
-    func testExplicitNull() throws {
-      struct Model: Encodable {
-        var name: String
-        @ExplicitNull var explicitNull: String?
-        var optional: String?
-      }
-      let model = Model(
-        name: "name",
-        explicitNull: nil,
-        optional: nil
-      )
+  func testExplicitNull() throws {
+    struct Model: Encodable {
+      var name: String
+      @ExplicitNull var explicitNull: String?
+      var optional: String?
+    }
+    let model = Model(
+      name: "name",
+      explicitNull: nil,
+      optional: nil
+    )
 
-      let docToWrite = documentRef()
+    let docToWrite = documentRef()
 
-      for flavor in allFlavors {
-        try setData(from: model, forDocument: docToWrite, withFlavor: flavor)
+    for flavor in allFlavors {
+      try setData(from: model, forDocument: docToWrite, withFlavor: flavor)
 
-        let data = readDocument(forRef: docToWrite).data()
-
-        XCTAssertTrue(data!.keys.contains("explicitNull"), "Failed with flavor \(flavor)")
-        XCTAssertEqual(data!["explicitNull"] as! NSNull, NSNull(), "Failed with flavor \(flavor)")
-        XCTAssertFalse(data!.keys.contains("optional"), "Failed with flavor \(flavor)")
-      }
-
-    func testSelfDocumentID() throws {
-      struct Model: Codable, Equatable {
-        var name: String
-        @DocumentID var docId: DocumentReference?
-      }
-
-      let docToWrite = documentRef()
-      let model = Model(
-        name: "name",
-        docId: nil
-      )
-
-      try setData(from: model, forDocument: docToWrite, withFlavor: .docRef)
       let data = readDocument(forRef: docToWrite).data()
 
-      // "docId" is ignored during encoding
-      XCTAssertEqual(data! as! [String: String], ["name": "name"])
+      XCTAssertTrue(data!.keys.contains("explicitNull"), "Failed with flavor \(flavor)")
+      XCTAssertEqual(data!["explicitNull"] as! NSNull, NSNull(), "Failed with flavor \(flavor)")
+      XCTAssertFalse(data!.keys.contains("optional"), "Failed with flavor \(flavor)")
+    }
+  }
 
-      // Decoded result has "docId" auto-populated.
-      let decoded = try readDocument(forRef: docToWrite).data(as: Model.self)
-      XCTAssertEqual(decoded!, Model(name: "name", docId: docToWrite))
+  func testSelfDocumentID() throws {
+    struct Model: Codable, Equatable {
+      var name: String
+      @DocumentID var docId: DocumentReference?
     }
 
-    func testSelfDocumentIDWithCustomCodable() throws {
-      struct Model: Codable, Equatable {
-        var name: String
-        @DocumentID var docId: DocumentReference?
+    let docToWrite = documentRef()
+    let model = Model(
+      name: "name",
+      docId: nil
+    )
 
-        enum CodingKeys: String, CodingKey {
-          case name
-          case docId
-        }
+    try setData(from: model, forDocument: docToWrite, withFlavor: .docRef)
+    let data = readDocument(forRef: docToWrite).data()
 
-        public init(name: String, docId: DocumentReference?) {
-          self.name = name
-          self.docId = docId
-        }
+    // "docId" is ignored during encoding
+    XCTAssertEqual(data! as! [String: String], ["name": "name"])
 
-        public init(from decoder: Decoder) throws {
-          let container = try decoder.container(keyedBy: CodingKeys.self)
-          name = try container.decode(String.self, forKey: .name)
-          docId = try container.decode(DocumentID<DocumentReference>.self, forKey: .docId)
-            .wrappedValue
-        }
+    // Decoded result has "docId" auto-populated.
+    let decoded = try readDocument(forRef: docToWrite).data(as: Model.self)
+    XCTAssertEqual(decoded!, Model(name: "name", docId: docToWrite))
+  }
 
-        public func encode(to encoder: Encoder) throws {
-          var container = encoder.container(keyedBy: CodingKeys.self)
-          try container.encode(name, forKey: .name)
-          // DocumentId should not be encoded when writing to Firestore; it's auto-populated when
-          // reading.
-        }
+  func testSelfDocumentIDWithCustomCodable() throws {
+    struct Model: Codable, Equatable {
+      var name: String
+      @DocumentID var docId: DocumentReference?
+
+      enum CodingKeys: String, CodingKey {
+        case name
+        case docId
       }
 
-      let docToWrite = documentRef()
-      let model = Model(
-        name: "name",
-        docId: nil
-      )
+      public init(name: String, docId: DocumentReference?) {
+        self.name = name
+        self.docId = docId
+      }
 
-      try setData(from: model, forDocument: docToWrite, withFlavor: .docRef)
-      let data = readDocument(forRef: docToWrite).data()
+      public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        docId = try container.decode(DocumentID<DocumentReference>.self, forKey: .docId)
+          .wrappedValue
+      }
 
-      // "docId" is ignored during encoding
-      XCTAssertEqual(data! as! [String: String], ["name": "name"])
-
-      // Decoded result has "docId" auto-populated.
-      let decoded = try readDocument(forRef: docToWrite).data(as: Model.self)
-      XCTAssertEqual(decoded!, Model(name: "name", docId: docToWrite))
+      public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        // DocumentId should not be encoded when writing to Firestore; it's auto-populated when
+        // reading.
+      }
     }
+
+    let docToWrite = documentRef()
+    let model = Model(
+      name: "name",
+      docId: nil
+    )
+
+    try setData(from: model, forDocument: docToWrite, withFlavor: .docRef)
+    let data = readDocument(forRef: docToWrite).data()
+
+    // "docId" is ignored during encoding
+    XCTAssertEqual(data! as! [String: String], ["name": "name"])
+
+    // Decoded result has "docId" auto-populated.
+    let decoded = try readDocument(forRef: docToWrite).data(as: Model.self)
+    XCTAssertEqual(decoded!, Model(name: "name", docId: docToWrite))
+  }
 
   func testSetThenMerge() throws {
     struct Model: Codable, Equatable {
