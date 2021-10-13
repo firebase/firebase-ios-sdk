1295c735136b0b42f8c5ff1e078939b9752b4e76
Nick Cooke <nickcooke@google.com>
2021-10-13 12:57:50 -0400

    Rename HeartbeatLogger -> HeartbeatController

--- HeartbeatLogging/Tests/HeartbeatControllerTests.swift
+++ HeartbeatLogging/Tests/HeartbeatControllerTests.swift
@@ -15,7 +15,7 @@
 import XCTest
 @testable import HeartbeatLogging
 
-class HeartbeatLoggerTests: XCTestCase {
+class HeartbeatControllerTests: XCTestCase {
   override func setUpWithError() throws {
     // Put setup code here. This method is called before the
     // invocation of each test method in the class.
