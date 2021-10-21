6aa2492365360af0ad66b6597f5db34720cb7229
Paul Beusterien <paulbeusterien@google.com>
2021-10-21 07:49:08 -0700

    Remove FIRApp warning for macOS and tvOS (#8841)

--- FirebaseCore/Sources/FIRApp.m
+++ FirebaseCore/Sources/FIRApp.m
@@ -135,12 +135,6 @@ static FIRApp *sDefaultApp;
                        kPlistURL];
   }
   [FIRApp configureWithOptions:options];
-#if TARGET_OS_OSX || TARGET_OS_TV
-  FIRLogNotice(kFIRLoggerCore, @"I-COR000028",
-               @"tvOS and macOS SDK support is not part of the official Firebase product. "
-               @"Instead they are community supported. Details at "
-               @"https://github.com/firebase/firebase-ios-sdk/blob/master/README.md.");
-#endif
 }
 
 + (void)configureWithOptions:(FIROptions *)options {
