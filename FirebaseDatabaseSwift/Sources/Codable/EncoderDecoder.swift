7fe5306ac68a9f7379f405b0d7016212c66ee650
Morten Bek Ditlevsen <morten@ka-ching.dk>
2021-10-23 13:22:52 +0200

    Moved Database.Encoder/Decoder to a new target and rebranded as StructureEncoder/Decoder

---
+++ FirebaseDatabaseSwift/Sources/Codable/EncoderDecoder.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Morten Bek Ditlevsen on 23/10/2021.
+//
+
+import FirebaseDatabase
+
+extension Database {
+  public typealias Encoder = StructureEncoder
+  public typealias Decoder = StructureDecoder
+}
