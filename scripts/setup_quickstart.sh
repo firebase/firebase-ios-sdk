b2a9daf0a68810fe366b7ea9c21b19aadaf61efe
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2021-10-25 19:53:50 -0400

    update qs sha

--- scripts/setup_quickstart.sh
+++ scripts/setup_quickstart.sh
@@ -49,7 +49,7 @@ if check_secrets || [[ ${SAMPLE} == "installations" ]]; then
 
   git clone https://github.com/firebase/quickstart-ios.git
   cd quickstart-ios
-  git checkout da411426fc181d7480ca3434b42ab1267427bb63
+  git checkout 9156ff989bc7e59a21476decabd74890a50e8324
   cd ../
   $scripts_dir/localize_podfile.swift "$WORKSPACE_DIR"/Podfile "$RELEASE_TESTING"
   if [ "$RELEASE_TESTING" == "nightly_release_testing" ]; then
