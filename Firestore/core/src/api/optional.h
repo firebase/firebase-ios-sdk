25a85f8757f891f752ab00a0e63c1615c84119a5
Wu-Hui <wu.hui.github@gmail.com>
2021-11-01 17:10:35 -0400

    Fix lint

--- Firestore/core/src/api/optional.h
+++ Firestore/core/src/api/optional.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ namespace api {
 
 struct nullopt_t {
   struct init {};
-  nullopt_t(init) {
+  explicit nullopt_t(init) {
   }
 };
 
