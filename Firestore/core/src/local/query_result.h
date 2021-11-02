ee37c68a5c08a0e967e4df05be34e116f070629b
Denver Coneybeare <dconeybe@google.com>
2021-11-02 13:53:06 -0400

    Add some missing includes to local_write_result.h and query_result.h

--- Firestore/core/src/local/query_result.h
+++ Firestore/core/src/local/query_result.h
@@ -20,6 +20,7 @@
 #include <utility>
 #include <vector>
 
+#include "Firestore/core/src/model/document.h"
 #include "Firestore/core/src/model/document_key_set.h"
 #include "Firestore/core/src/model/model_fwd.h"
 
