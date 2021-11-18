16f58199aae3d6bdaf32307ad4c65882378f97d2
Paul Beusterien <paulbeusterien@google.com>
2021-11-18 07:23:45 -0800

    Update Functions public API docs for Installations

--- FirebaseFunctions/Sources/Public/FirebaseFunctions/FIRHTTPSCallable.h
+++ FirebaseFunctions/Sources/Public/FirebaseFunctions/FIRHTTPSCallable.h
@@ -47,12 +47,12 @@ NS_SWIFT_NAME(HTTPSCallable)
  * Executes this Callable HTTPS trigger asynchronously without any parameters.
  *
  * The request to the Cloud Functions backend made by this method automatically includes a
- * Firebase Instance ID token to identify the app instance. If a user is logged in with Firebase
+ * Firebase Installations token to identify the app instance. If a user is logged in with Firebase
  * Auth, an auth ID token for the user is also automatically included.
  *
- * Firebase Instance ID sends data to the Firebase backend periodically to collect information
- * regarding the app instance. To stop this, see `[FIRInstanceID deleteIDWithHandler:]`. It
- * resumes with a new Instance ID the next time you call this method.
+ * Firebase Installations sends data to the Firebase backend periodically to collect information
+ * regarding the app instance. To stop this, see `Installations.delete()`. It
+ * resumes with a new Installations ID the next time you call this method.
  *
  * @param completion The block to call when the HTTPS request has completed.
  */
