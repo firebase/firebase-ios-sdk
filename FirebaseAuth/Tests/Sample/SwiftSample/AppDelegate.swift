85c0ea4491f0577ca849b376c74a67c3ca638571
Rosalyn Tan <rosalyntan@google.com>
2021-11-18 16:51:16 -0800

    Add copyright header and run swiftformat.

--- FirebaseAuth/Tests/Sample/SwiftSample/AppDelegate.swift
+++ FirebaseAuth/Tests/Sample/SwiftSample/AppDelegate.swift
@@ -19,21 +19,22 @@ import FBSDKCoreKit
 import UIKit
 
 @objc protocol OpenURLDelegate: AnyObject {
-  func handleOpenURL(_ url: URL, sourceApplication:String) -> Bool
+  func handleOpenURL(_ url: URL, sourceApplication: String) -> Bool
 }
 
 @main
 public class AppDelegate: UIResponder, UIApplicationDelegate {
-
-  static weak var gOpenURLDelegate: OpenURLDelegate?
+  weak static var gOpenURLDelegate: OpenURLDelegate?
   public var window: UIWindow?
   var sampleAppMainViewController: MainViewController?
 
-  @objc class func setOpenURLDelegate(_ openURLDelegate: OpenURLDelegate?) -> Void {
+  @objc class func setOpenURLDelegate(_ openURLDelegate: OpenURLDelegate?) {
     gOpenURLDelegate = openURLDelegate
   }
 
-  public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+  public func application(_ application: UIApplication,
+                          didFinishLaunchingWithOptions launchOptions: [UIApplication
+                            .LaunchOptionsKey: Any]?) -> Bool {
     GTMSessionFetcher.setLoggingEnabled(true)
     FirebaseConfiguration.shared.setLoggerLevel(FirebaseLoggerLevel.info)
 
@@ -48,43 +49,48 @@ public class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // Load and present the UI.
     let window = UIWindow(frame: UIScreen.main.bounds)
-    sampleAppMainViewController = MainViewController(nibName: NSStringFromClass(MainViewController.self),                                                  bundle:nil)
+    sampleAppMainViewController = MainViewController(
+      nibName: NSStringFromClass(MainViewController.self),
+      bundle: nil
+    )
     sampleAppMainViewController?.navigationItem.title = "Firebase Auth"
-    window.rootViewController = UINavigationController(rootViewController: sampleAppMainViewController!)
+    window
+      .rootViewController = UINavigationController(rootViewController: sampleAppMainViewController!)
     self.window = window
     self.window?.makeKeyAndVisible()
 
     return true
   }
 
-  public func application(
-      _ app: UIApplication,
-      open url: URL,
-      options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-  ) -> Bool {
-      ApplicationDelegate.shared.application(
-          app,
-          open: url,
-          sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-          annotation: options[UIApplication.OpenURLOptionsKey.annotation]
-      )
+  public func application(_ app: UIApplication,
+                          open url: URL,
+                          options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+    ApplicationDelegate.shared.application(
+      app,
+      open: url,
+      sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+      annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+    )
 
-    if (Self.gOpenURLDelegate!.handleOpenURL(url, sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String)) {
+    if Self.gOpenURLDelegate!.handleOpenURL(
+      url,
+      sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String
+    ) {
       return true
     }
-    if (sampleAppMainViewController!.handleIncomingLink(with: url)) {
+    if sampleAppMainViewController!.handleIncomingLink(with: url) {
       return true
     }
     return false
   }
 
   public func application(_ application: UIApplication,
-                   continue userActivity: NSUserActivity,
-                   restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-    if (userActivity.webpageURL != nil) {
+                          continue userActivity: NSUserActivity,
+                          restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void)
+    -> Bool {
+    if userActivity.webpageURL != nil {
       return sampleAppMainViewController!.handleIncomingLink(with: userActivity.webpageURL!)
     }
     return false
   }
 }
-
