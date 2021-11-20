7354b50d6951e682b9d367ec43a53b2221d5cb6e
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2021-11-20 12:37:51 -0500

    Refactor library and add WIP controller tests (#8952)
    * Refactor library and add WIP controller tests
    
    * Document HeartbeatInfo's append(_:) implementation
    
    * Add some docs and tweak naming
    
    * Fix HeartbeatInfo's append(_:) implementation
    
    * Storage test WIP
    
    * Finish test
    
    * Refactor controller flush() implementation
    
    * Add comment to controller flush() implementation
    
    * WIP Storage tests
    
    * Review
    
    * Review 2
    
    * Review 3
    
    * Review 4

--- HeartbeatLogging/Sources/HeartbeatController.swift
+++ HeartbeatLogging/Sources/HeartbeatController.swift
@@ -18,40 +18,83 @@ import Foundation
 public final class HeartbeatController {
   /// The thread-safe storage object to log and flush heartbeats from.
   private let storage: HeartbeatStorageProtocol
+  // TODO: Document.
+  private let limit: Int = 30 // TODO: Decide on default value.
+  /// Current date provider. It is used instead of `Date.init` for testability.
+  private let dateProvider: () -> Date
+  // TODO: Verify that this standardization aligns with backend.
+  /// Used for standardizing dates for calendar-day comparision.
+  static let dateStandardizer = Calendar(identifier: .gregorian).startOfDay(for:)
 
   /// Public initializer.
   ///
   /// - Parameter id: The `id` to associate this logger's internal storage with.
   public convenience init(id: String) {
-    let storage = StorageFactory.makeStorage(id: id)
-    self.init(storage: HeartbeatStorage(id: id, storage: storage))
+    // TODO: Sanitize id.
+    let storage = HeartbeatStorage.getInstance(id: id)
+    self.init(storage: storage)
   }
 
   /// Designated initializer.
   ///
   /// - Parameters:
   ///   - storage: The logger's internal storage object.
-  init(storage: HeartbeatStorageProtocol) {
+  ///   - dateProvider: TODO: Document.
+  init(storage: HeartbeatStorageProtocol,
+       dateProvider: @escaping () -> Date = Date.init) {
     self.storage = storage
+    self.dateProvider = { Self.dateStandardizer(dateProvider()) }
   }
 
   /// Asynchronously log a new heartbeat, if needed.
   ///
   /// - Note: This API is thread-safe.
   ///
-  /// - Parameter info: A `String` identifier to associate a new heartbeat with.
-  public func log(_ info: String) {
-    let newHeartbeat = Heartbeat(info: info)
-    storage.offer(newHeartbeat)
+  /// - Parameter agent: A `String` identifier to associate a new heartbeat with.
+  public func log(_ agent: String) {
+    let date = dateProvider()
+    let capacity = limit
+
+    storage.readAndWriteAsync { heartbeatInfo in
+      var heartbeatInfo = heartbeatInfo ?? HeartbeatInfo(capacity: capacity)
+
+      let timePeriods = heartbeatInfo.cache.filter { timePeriod, lastDate in
+        date.timeIntervalSince(lastDate) >= timePeriod.timeInterval
+      }
+      .map { timePeriod, _ in timePeriod }
+
+      if !timePeriods.isEmpty {
+        let heartbeat = Heartbeat(agent: agent, date: date, timePeriods: timePeriods)
+        heartbeatInfo.append(heartbeat)
+      }
+
+      return heartbeatInfo
+    }
   }
 
   /// Synchronously flushes heartbeats from storage.
   ///
   /// - Note: This API is thread-safe.
   ///
-  /// - Returns: The flushed heartbeats in the form of `HeartbeatInfo`.
+  /// - Returns: The flushed heartbeats in the form of `HeartbeatsPayload`.
   @discardableResult
-  public func flush() -> HeartbeatInfo? {
-    storage.flush()
+  public func flush() -> HeartbeatsPayload {
+    let capacity = limit
+
+    let resetTransform: (HeartbeatInfo?) -> HeartbeatInfo? = { heartbeatInfo in
+      guard let oldHeartbeatInfo = heartbeatInfo else {
+        return nil // Storage was empty.
+      }
+
+      // The new value that's stored will use the old's cache.
+      return HeartbeatInfo(capacity: capacity, cache: oldHeartbeatInfo.cache)
+    }
+
+    // Synchronously gets and returns the stored heartbeats and resets storage
+    // using the given transform. If the operation threw an error, assume no
+    // heartbeats were retrieved/reset.
+    let heartbeatInfo = try? storage.getAndReset(using: resetTransform)
+
+    return HeartbeatsPayload.makePayload(heartbeatInfo: heartbeatInfo)
   }
 }
