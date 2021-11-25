077759e13893c22d00d1ccfa829c95a4dd18b08c
Paul Beusterien <paulbeusterien@google.com>
2021-11-25 07:05:39 -0800

    Update required SPM abseil dep

--- Package.swift
+++ Package.swift
@@ -169,7 +169,7 @@ let package = Package(
     .package(
       name: "abseil",
       url: "https://github.com/firebase/abseil-cpp-SwiftPM.git",
-      "0.20200225.3" ..< "0.20200226.0"
+      "0.20200225.4" ..< "0.20200226.0"
     ),
     .package(
       name: "gRPC",
