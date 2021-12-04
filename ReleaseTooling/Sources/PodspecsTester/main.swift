8f46a8de98878a86f412c7fdfbd9866c17d625e6
Gran <granluo@google.com>
2021-12-03 17:35:35 -0800

    Set up specs testing (#9035)

---
+++ ReleaseTooling/Sources/PodspecsTester/main.swift
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Foundation
+
+import ArgumentParser
+import Utils
+
+struct PodspecsTester: ParsableCommand {
+  /// The root of the Firebase git repo.
+  @Option(help: "The root of the firebase-ios-sdk checked out git repo.",
+          transform: URL.init(fileURLWithPath:))
+  var gitRoot: URL
+
+  mutating func validate() throws {
+    guard FileManager.default.fileExists(atPath: gitRoot.path) else {
+      throw ValidationError("git-root does not exist: \(gitRoot.path)")
+    }
+  }
+
+  func run() throws {
+    let startDate = Date()
+    print("Started at: \(startDate.dateTimeString())")
+    InitializeSpecTesting.setupRepo(sdkRepoURL: gitRoot)
+    let finishDate = Date()
+    print("Finished at: \(finishDate.dateTimeString()). " +
+      "Duration: \(startDate.formattedDurationSince(finishDate))")
+  }
+}
+
+// Start the parsing and run the tool.
+PodspecsTester.main()
