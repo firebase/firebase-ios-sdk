672a34eec4e84337b0ee3389649a63a4a3f1436e
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2021-12-09 16:36:37 -0500

    [Heartbeat Logging] - Integration tests, documentation, refactoring (#9015)
    * Add public API integration test
    
    * Add documentation
    
    * Add more docs and some refactoring
    
    * Fix typo that broke CI
    
    * Minor edits
    
    * Finish docs for HeartbeatInfo
    
    * Fix docs in HeartbeatController
    
    * Fix docs in HeartbeatController 2
    
    * Remove needless static property
    
    * Remove unneeded test
    
    * Doc updates
    
    * More integration tests
    
    * Adding integration tests and bug fixes
    
    * Add time zone unit tests
    
    * Add comments to integration test
    
    * Comments and spacing
    
    * Add integration tests that verify storage
    
    * Fix tvOS CI
    
    * Review
    
    * Review 2
    
    * Review 2 followup
    
    * Review 3
    
    * Review 4
    
    * Simplify storage API
    
    * Post-review doc updates
    
    * Style
    
    * Improved unit tests for `HeartbeatStorage`
    
    * Address some TODOs
    
    * Final test suite passthrough
    
    * Review 5
    
    * Review 6
    
    * Review 7
    
    * Review 8
    
    * Rename  to
    
    * Rename HeartbeatInfo to HeartbeatsBundle (2)
    
    * Review 9
    
    * Review 10
    
    * Review 11

--- HeartbeatLogging/Sources/HeartbeatsBundle.swift
+++ HeartbeatLogging/Sources/HeartbeatsBundle.swift
@@ -14,65 +14,75 @@
 
 import Foundation
 
-// TODO: Document.
-
-/// A type that can be represented as a `HeartbeatsPayload`.
+/// A type that can be converted to a `HeartbeatsPayload`.
 protocol HeartbeatsPayloadConvertible {
   func makeHeartbeatsPayload() -> HeartbeatsPayload
 }
 
-/// <#Description#>
-struct HeartbeatInfo: Codable, HeartbeatsPayloadConvertible {
-  /// The maximum number of heartbeats.
+/// A codable collection of heartbeats that has a fixed capacity and optimizations for storing heartbeats of
+/// multiple time periods.
+struct HeartbeatsBundle: Codable, HeartbeatsPayloadConvertible {
+  /// The maximum number of heartbeats that can be stored in the buffer.
   let capacity: Int
-  /// <#Description#>
-  private(set) var cache: [TimePeriod: Date]
-  /// <#Description#>
+  /// A cache used for keeping track of the last heartbeat date recorded for a given time period.
+  ///
+  /// The cache contains the last added date for each time period. The reason only the date is cached is
+  /// because it's the only piece of information that should be used by clients to determine whether or not
+  /// to append a new heartbeat.
+  private(set) var lastAddedHeartbeatDates: [TimePeriod: Date]
+  /// A ring buffer of heartbeats.
   private var buffer: RingBuffer<Heartbeat>
 
+  /// A default cache provider that provides a dictionary of all time periods mapping to a default date.
   static var cacheProvider: () -> [TimePeriod: Date] {
-    let timePeriodsAndDates = TimePeriod.periods.map { ($0, Date.distantPast) }
+    let timePeriodsAndDates = TimePeriod.allCases.map { ($0, Date.distantPast) }
     return { Dictionary(uniqueKeysWithValues: timePeriodsAndDates) }
   }
 
-  /// <#Description#>
+  /// Designated initializer.
   /// - Parameters:
-  ///   - capacity: <#capacity description#>
-  ///   - cacheProvider: <#cacheProvider description#>
+  ///   - capacity: The heartbeat capacity of the inititialized collection.
+  ///   - cache: A cache of time periods mapping to dates. Defaults to using static `cacheProvider`.
   init(capacity: Int,
        cache: [TimePeriod: Date] = cacheProvider()) {
     buffer = RingBuffer(capacity: capacity)
     self.capacity = capacity
-    self.cache = cache
+    lastAddedHeartbeatDates = cache
   }
 
-  /// <#Description#>
-  /// - Parameter heartbeat: <#heartbeat description#>
+  /// Appends a heartbeat to this collection.
+  /// - Parameter heartbeat: The heartbeat to append.
   mutating func append(_ heartbeat: Heartbeat) {
+    guard capacity > 0 else {
+      return // Do not append if capacity is non-positive.
+    }
+
     // 1. Push the heartbeat to the back of the buffer.
     if let overwrittenHeartbeat = buffer.push(heartbeat) {
       // If a heartbeat was overwritten, update the cache to ensure it's date
       // is removed (if it was stored).
-      cache = cache.mapValues { date in
+      lastAddedHeartbeatDates = lastAddedHeartbeatDates.mapValues { date in
         overwrittenHeartbeat.date == date ? .distantPast : date
       }
     }
 
     // 2. Update cache with the new heartbeat's date.
     heartbeat.timePeriods.forEach {
-      cache[$0] = heartbeat.date
+      lastAddedHeartbeatDates[$0] = heartbeat.date
     }
   }
 
+  /// Makes and returns a `HeartbeatsPayload` from this heartbeats bundle.
+  /// - Returns: A heartbeats payload.
   func makeHeartbeatsPayload() -> HeartbeatsPayload {
     let agentAndDates = buffer.map { heartbeat in
       (heartbeat.agent, [heartbeat.date])
     }
 
-    let heartbeats = [String: [Date]](agentAndDates, uniquingKeysWith: +)
+    let userAgentPayloads = [String: [Date]](agentAndDates, uniquingKeysWith: +)
       .map(HeartbeatsPayload.UserAgentPayload.init)
       .sorted { $0.agent < $1.agent } // Sort payloads by user agent.
 
-    return HeartbeatsPayload(heartbeats: heartbeats)
+    return HeartbeatsPayload(userAgentPayloads: userAgentPayloads)
   }
 }
