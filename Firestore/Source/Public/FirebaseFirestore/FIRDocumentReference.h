1616902c6ccc75133da0f8b96079ab9ab84db09f
Sebastian Schmidt <mrschmidt@google.com>
2021-12-21 11:12:03 -0700

    Improve documentation for merge behavior

--- Firestore/Source/Public/FirebaseFirestore/FIRDocumentReference.h
+++ Firestore/Source/Public/FirebaseFirestore/FIRDocumentReference.h
@@ -90,7 +90,9 @@ NS_SWIFT_NAME(DocumentReference)
  *
  * @param documentData An `NSDictionary` that contains the fields and data to write to the
  * document.
- * @param merge Whether to merge the provided data into any existing document.
+ * @param merge Whether to merge the provided data into any existing document. If enabled,
+ * all omitted fields remain untouched. If your input sets any field to an empty map, any nested
+ * field at this location is overwritten.
  */
 - (void)setData:(NSDictionary<NSString *, id> *)documentData merge:(BOOL)merge;
 
@@ -106,8 +108,9 @@ NS_SWIFT_NAME(DocumentReference)
  * @param documentData An `NSDictionary` containing the fields that make up the document
  * to be written.
  * @param mergeFields An `NSArray` that contains a list of `NSString` or `FIRFieldPath` elements
- *     specifying which fields to merge. Fields can contain dots to reference nested fields within
- *     the document.
+ * specifying which fields to merge. Fields can contain dots to reference nested fields within
+ * the document. If your input sets any field to an empty map, any nested field at this location is
+ * overwritten.
  */
 - (void)setData:(NSDictionary<NSString *, id> *)documentData mergeFields:(NSArray<id> *)mergeFields;
 
