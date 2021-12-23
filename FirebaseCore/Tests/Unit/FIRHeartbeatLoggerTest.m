618903a1eda6d2d295a893fae5e20c789165f2b7
Nick Cooke <nickcooke@google.com>
2021-12-23 18:07:40 -0500

    Testing to see if file is tested

--- FirebaseCore/Tests/Unit/FIRHeartbeatLoggerTest.m
+++ FirebaseCore/Tests/Unit/FIRHeartbeatLoggerTest.m
@@ -60,6 +60,7 @@
 
 - (void)testDoNotLogMoreThanOnceToday {
   // Given
+  XCTFail(@"Intentionally failing test case.");
   FIRHeartbeatLogger *heartbeatLogger = self.heartbeatLogger;
   NSString *expectedDate = [[self class] formattedStringForDate:[NSDate date]];
   // When
