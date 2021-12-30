8dbca416aad435c2142c4fdddcd5c2e4196d5d37
Nick Cooke <nickcooke@google.com>
2021-12-30 16:02:14 -0500

    Compress v2 heartbeat payloads

--- HeartbeatLogging/Sources/HeartbeatsPayload.swift
+++ HeartbeatLogging/Sources/HeartbeatsPayload.swift
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import Foundation
+import GoogleUtilities_NSData
 
 /// A type that provides a string representation for use in an HTTP header.
 public protocol HTTPHeaderRepresentable {
@@ -85,14 +86,13 @@ extension HeartbeatsPayload: HTTPHeaderRepresentable {
     let encoder = JSONEncoder()
     encoder.dateEncodingStrategy = .formatted(Self.dateFormatter)
 
-    guard let data = try? encoder.encode(self) else {
-      // Return empty string if encoding failed.
-      return ""
+    do {
+      let data = try encoder.encode(self)
+      let gzippedData = try data.zipped()
+      return gzippedData.base64URLEncodedString()
+    } catch {
+      return "" // Return empty string if processing failed.
     }
-
-    // TODO: Compress the payload before base64 encoding.
-    // TODO: Confirm in e2e test if base64 URL safe string is needed.
-    return data.base64EncodedString()
   }
 }
 
@@ -116,3 +116,62 @@ extension HeartbeatsPayload {
 
 extension HeartbeatsPayload: Equatable {}
 extension HeartbeatsPayload.UserAgentPayload: Equatable {}
+
+// MARK: - Data
+
+public extension Data {
+  /// Returns a Base-64 URL-safe encoded string.
+  ///
+  /// - parameter options: The options to use for the encoding. Default value is `[]`.
+  /// - returns: The Base-64 URL-safe encoded string.
+  func base64URLEncodedString(options: Data.Base64EncodingOptions = []) -> String {
+    base64EncodedString()
+      .replacingOccurrences(of: "/", with: "_")
+      .replacingOccurrences(of: "+", with: "-")
+      .replacingOccurrences(of: "=", with: "")
+  }
+
+  /// Initialize a `Data` from a Base-64 URL encoded String using the given options.
+  ///
+  /// Returns nil when the input is not recognized as valid Base-64.
+  /// - parameter base64URLString: The string to parse.
+  /// - parameter options: Encoding options. Default value is `[]`.
+  init?(base64URLEncoded base64URLString: String, options: Data.Base64DecodingOptions = []) {
+    var base64Encoded = base64URLString
+      .replacingOccurrences(of: "_", with: "/")
+      .replacingOccurrences(of: "-", with: "+")
+
+    // Pad the string with "=" signs until the string's length is a multiple of 4.
+    while !base64Encoded.count.isMultiple(of: 4) {
+      base64Encoded.append("=")
+    }
+
+    self.init(base64Encoded: base64Encoded, options: options)
+  }
+
+  /// Returns the compressed data.
+  /// - Returns: The compressed data.
+  /// - Throws: An error if compression failed.
+  func zipped() throws -> Data {
+    if #available(iOS 13.0, macOS 10.15, macCatalyst 13.0, tvOS 13.0, watchOS 6.0, *) {
+      // Use standard library's zlib implementation.
+      return try (self as NSData).compressed(using: .zlib) as Data
+    } else {
+      // Fall back to Google Utilities gzip implementation.
+      return try NSData.gul_data(byGzippingData: self)
+    }
+  }
+
+  /// Returns the uncompressed data.
+  /// - Returns: The decompressed data.
+  /// - Throws: An error if decompression failed.
+  func unzipped() throws -> Data {
+    if #available(iOS 13.0, macOS 10.15, macCatalyst 13.0, tvOS 13.0, watchOS 6.0, *) {
+      // Use standard library's zlib implementation.
+      return try (self as NSData).decompressed(using: .zlib) as Data
+    } else {
+      // Fall back to Google Utilities gzip implementation.
+      return try NSData.gul_data(byInflatingGzippedData: self)
+    }
+  }
+}
