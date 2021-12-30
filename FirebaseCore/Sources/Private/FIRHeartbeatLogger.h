82fe6678f166be8f50877b0ac64433ca42ac990d
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2021-12-30 14:19:42 -0500

    [Heartbeat Logging] Add ObjC interop API and Firebase Core integration (#9117)
    * Add FIRHeartbeatLogger and unit tests
    
    * Add XCTest module
    
    * Fix warnings
    
    * Exclude files from module import rule
    
    * Add swift_version to FirebaseStorage.podspec
    
    * Update FirebaseStorage.podspec
    
    * Update FirebaseCore.podspec
    
    * Debug storage GHA
    
    * Create separate testing utils target
    
    * Revert "Debug storage GHA"
    
    This reverts commit 962567e1807e8fcbf2c57de19520a066d92fd287.
    
    * WIP Reorganizing for cocoapods
    
    * Copy some spec config from OCMock
    
    * Fix build issue
    
    * Fix tests
    
    * Add env var to only use testing spec for pod lib lint
    
    * Add new heartbeat-unit test spec
    
    * Bump Core dependency to 8.11
    
    * Test
    
    * Revert "Test"
    
    This reverts commit 23cddd0b17a1c0434dd9755fc0ae77a5d2371dc5.
    
    * Refactor podspec
    
    * Revert "Refactor podspec"
    
    This reverts commit 71d87fb5d0448afb1a3ff834775b0ba425019e29.
    
    * Testing to see if file is tested
    
    * Revert "Testing to see if file is tested"
    
    This reverts commit 618903a1eda6d2d295a893fae5e20c789165f2b7.
    
    * Revert "Revert "Refactor podspec""
    
    This reverts commit ac42b40b06a1267e005f7648e281d38739aadf18.
    
    * catalyst testing needs env var too
    
    * Remove outdated comment
    
    * Review
    
    * Style
    
    * Review 2
    
    * Fix tvOS CI

---
+++ FirebaseCore/Sources/Private/FIRHeartbeatLogger.h
@@ -0,0 +1,58 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import <Foundation/Foundation.h>
+
+// TODO: Remove in future PR when `FIRHeartbeatInfo` symbol is removed.
+#import "FirebaseCore/Sources/Private/FIRHeartbeatInfo.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class FIRHeartbeatsPayload;
+
+/// Returns a string header value from a given heartbeats payload.
+/// @param heartbeatsPayload The heartbeats payload.
+NSString *FIRHeaderValueFromHeartbeatsPayload(FIRHeartbeatsPayload *heartbeatsPayload);
+
+/// A thread safe, synchronized object that logs and flushes platform logging info.
+@interface FIRHeartbeatLogger : NSObject
+
+// TODO: Decide how clients get an instance.
+
+- (instancetype)initWithAppID:(NSString *)appID;
+
+/// Asynchronously logs a new heartbeat corresponding to the Firebase User Agent, if needed.
+///
+/// @note This API is thread-safe.
+- (void)log;
+
+/// Flushes heartbeats from storage into a structured payload of heartbeats.
+///
+/// This API is for clients using platform logging V2.
+///
+/// @note This API is thread-safe.
+/// @return A payload of heartbeats.
+- (FIRHeartbeatsPayload *)flushHeartbeatsIntoPayload;
+
+/// Gets the corresponding heartbeat code for the current heartbeat storage state.
+///
+/// This API is for clients using platform logging V1.
+///
+/// @note This API is thread-safe.
+/// @return Heartbeat code indicating whether or not there is an unsent global heartbeat.
+- (FIRHeartbeatInfoCode)heartbeatCode;
+
+@end
+
+NS_ASSUME_NONNULL_END
