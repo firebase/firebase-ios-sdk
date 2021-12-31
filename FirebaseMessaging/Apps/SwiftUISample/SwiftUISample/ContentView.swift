122a11893c800f593529eb76f6e045a9dd62fd80
Charlotte Liang <chliang@google.com>
2021-12-30 19:32:19 -0800

    add a SwiftUI Sample app

---
+++ FirebaseMessaging/Apps/SwiftUISample/SwiftUISample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISample
+//
+//  Created by Charlotte Liang on 12/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
