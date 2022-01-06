d9d0f67bcf66fd029e08e344cee1266064fa131b
Narlei Moreira <narlei.guitar@gmail.com>
2022-01-06 08:27:29 -0300

    Change APIKey documentation

--- FirebaseCore/Sources/Public/FirebaseCore/FIROptions.h
+++ FirebaseCore/Sources/Public/FirebaseCore/FIROptions.h
@@ -32,7 +32,7 @@ NS_SWIFT_NAME(FirebaseOptions)
 
 /**
  * An iOS API key used for authenticating requests from your app, e.g.
- * The key contains about 39 alphanumeric digits, used to identify your app to Google servers.
+ * The key must begin with "A" and contain exactly 39 alphanumeric characters, used to identify your app to Google servers.
  */
 @property(nonatomic, copy, nullable) NSString *APIKey NS_SWIFT_NAME(apiKey);
 
