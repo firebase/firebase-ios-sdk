43743948638e127fe7459c6a89dcb92085c1e2a9
Elena Doty <elenadoty@google.com>
2022-01-11 13:42:03 -0500

    update variable name for clarity

--- Crashlytics/Crashlytics/Controllers/FIRCLSMetricKitManager.m
+++ Crashlytics/Crashlytics/Controllers/FIRCLSMetricKitManager.m
@@ -153,13 +153,13 @@
           : nil;
   NSString *currentReportID =
       [_managerData.executionIDModel.executionID stringByAppendingString:@"/"];
-  BOOL fatal = ([diagnosticPayload.crashDiagnostics count] > 0) && (newestUnsentReportID != nil) &&
+  BOOL crashlyticsFatalReported = ([diagnosticPayload.crashDiagnostics count] > 0) && (newestUnsentReportID != nil) &&
                ([self.fileManager
                    fileExistsAtPath:[activePath stringByAppendingString:newestUnsentReportID]]);
 
   // Set the MetricKit fatal path appropriately depending on whether we also captured a Crashlytics
   // fatal event and whether the diagnostic report came from a fatal or nonfatal event.
-  if (fatal) {
+  if (crashlyticsFatalReported) {
     metricKitFatalReportFile = [[activePath stringByAppendingString:newestUnsentReportID]
         stringByAppendingString:FIRCLSMetricKitFatalReportFile];
   } else {
