cf9ec8c046cad6922f26f7c2df6022e3c0bd2d5d
granluo <granluo@google.com>
2022-01-18 16:05:08 -0800

    Update release setup to released tags.

--- scripts/release_testing_setup.sh
+++ scripts/release_testing_setup.sh
@@ -23,7 +23,6 @@ if [ -f "${HOME}/.cocoapods/repos" ]; then
 fi
 
 mkdir -p "${local_sdk_repo_dir}"
-echo "git clone ${podspec_repo_branch} from github.com/firebase/firebase-ios-sdk.git to ${local_sdk_repo_dir}"
 set +x
 # Using token here to update tags later.
 git clone -q https://"${BOT_TOKEN}"@github.com/firebase/firebase-ios-sdk.git "${local_sdk_repo_dir}"
@@ -31,28 +30,17 @@ set -x
 
 cd  "${local_sdk_repo_dir}"
 # The chunk below is to determine the latest version by searching
-# Get the latest origin/release-X.Y.Z branch in the remote repo for release branch testing.
-release_branch=$(git branch -l -r --sort=-version:refname origin/release-* | head -n 1 )
-# Get the latest released tag Cocoapods-X.Y.Z for prerelease testing, beta version will be excluded.
+# Get the latest released tag Cocoapods-X.Y.Z for release and prerelease testing, beta version will be excluded.
 test_version=$(git tag -l --sort=-version:refname CocoaPods-*[0-9] | head -n 1)
-
-if [ -z $podspec_repo_branch ];then
-  # Get release branch, origin/release-X.Y.Z.
-  podspec_repo_branch=$(echo $release_branch | sed -n 's/\s*//p')
-fi
 
 git config --global user.email "google-oss-bot@example.com"
 git config --global user.name "google-oss-bot"
-git checkout "${podspec_repo_branch}"
 if [ "$TESTINGMODE" = "release_testing" ]; then
-  # Latest release branch with prefix CocoaPods, e.g. CocoaPods-7.11
-  latest_branch_version=$(git branch -l -r --sort=-v:refname origin/release-* | head -n 1 | sed -n 's/^[ \t]*origin\/release/CocoaPods/p' )
-  # Latest tag of release branch on the repo, e.g. Cocoapods-7.9.0
-  latest_release_branch_tag=$(git tag -l --sort=-version:refname "$latest_branch_version"*[0-9] | head -n 1)
-  echo "Podspecs tags of Nightly release testing will be updated to ${latest_release_branch_tag}."
+  git checkout "${test_version}"
+  echo "Podspecs tags of Nightly release testing will be updated to ${test_version}."
   # Update source and tag, e.g.  ":tag => 'CocoaPods-' + s.version.to_s" to
   # ":tag => 'CocoaPods-7.9.0'"
-  sed  -i "" "s/\s*:tag.*/:tag => '${latest_release_branch_tag}'/" *.podspec
+  sed  -i "" "s/\s*:tag.*/:tag => '${test_version}'/" *.podspec
 elif [ "$TESTINGMODE" = "prerelease_testing" ]; then
   tag_version="${test_version}.nightly"
   echo "A new tag, ${tag_version},for prerelease testing will be created."
