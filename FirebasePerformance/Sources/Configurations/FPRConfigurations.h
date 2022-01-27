4fc58d0cb7bc83d43332a95f327f0d83d0d5cf68
Leo Zhan <zhanl@google.com>
2022-01-26 19:59:41 -0800

    comment

--- FirebasePerformance/Sources/Configurations/FPRConfigurations.h
+++ FirebasePerformance/Sources/Configurations/FPRConfigurations.h
@@ -62,6 +62,16 @@ FOUNDATION_EXTERN FPRConfigName kFPRConfigInstrumentationEnabled;
 
 #pragma mark - Configuration fetcher methods.
 
+/**
+ * Controls how the SDK detects and drops prewarmed app start traces:
+ * 0 = drop all app start traces;
+ * 1 = drop only if ActivePrewarm returns true;
+ * 2 = drop only if double dispatch returns true;
+ * 3 = drop if either ActivePrewarm or double dispatch returns true;
+ * Other numbers = do not drop any app start traces.
+ *
+ * @return an integer representing the detection mode of prewarm app start traces.
+ */
 - (uint32_t)prewarmDetectionMode;
 
 /**
