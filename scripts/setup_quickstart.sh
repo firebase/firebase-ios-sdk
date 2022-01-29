bc7bc3c48766af75523d3a43ac14e6f84064418b
A-25 <anuryadov@gmail.com>
2022-01-29 23:49:50 +0300

    Revert "Testing quickstart version without Firebase pod"
    This reverts commit 9b377a842431c808e01d9652e9f9236d84758ab4.

--- scripts/setup_quickstart.sh
+++ scripts/setup_quickstart.sh
@@ -47,8 +47,7 @@ if check_secrets || [[ ${SAMPLE} == "installations" ]]; then
   # development pod install. This is needed for the `pod install` command.
   export FIREBASE_POD_REPO_FOR_DEV_POD=`pwd`
 
-  git clone https://github.com/a-25/quickstart-ios.git
-  git checkout remove-firebase-pod
+  git clone https://github.com/firebase/quickstart-ios.git
   $scripts_dir/localize_podfile.swift "$WORKSPACE_DIR"/Podfile "$RELEASE_TESTING"
   if [ "$RELEASE_TESTING" == "nightly_release_testing" ]; then
     set +x
