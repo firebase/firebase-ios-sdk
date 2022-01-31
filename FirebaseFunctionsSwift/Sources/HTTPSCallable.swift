935fb10f59acef7fc8dd7b2f52c646d47b9cae5e
Paul Beusterien <paulbeusterien@google.com>
2022-01-31 09:10:27 -0800

    Functions Swift - podspec, formatting, copyrights, CI, public API fixes (#9272)

--- FirebaseFunctionsSwift/Sources/HTTPSCallable.swift
+++ FirebaseFunctionsSwift/Sources/HTTPSCallable.swift
@@ -1,9 +1,16 @@
-// FULLY PORTED
-//  File.swift
-//  
+// Copyright 2022 Google LLC
 //
-//  Created by Ryan Wilson on 2022-01-25.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import Foundation
 
@@ -11,7 +18,7 @@ import Foundation
  * A `HTTPSCallableResult` contains the result of calling a `HTTPSCallable`.
  */
 @objc(FIRHTTPSCallableResult)
-public class HTTPSCallableResult : NSObject {
+public class HTTPSCallableResult: NSObject {
   /**
    * The data that was returned from the Callable HTTPS trigger.
    *
@@ -26,12 +33,11 @@ public class HTTPSCallableResult : NSObject {
   }
 }
 
-
 /**
  * A `HTTPSCallable` is reference to a particular Callable HTTPS trigger in Cloud Functions.
  */
 @objc(FIRHTTPSCallable)
-public class HTTPSCallable : NSObject {
+public class HTTPSCallable: NSObject {
   // MARK: - Private Properties
 
   // The functions client to use for making calls.
@@ -46,7 +52,6 @@ public class HTTPSCallable : NSObject {
    * The timeout to use when calling the function. Defaults to 70 seconds.
    */
   public var timeoutInterval: TimeInterval = 70
-
 
   internal init(functions: Functions, name: String) {
     self.functions = functions
@@ -74,46 +79,47 @@ public class HTTPSCallable : NSObject {
    * @param data Parameters to pass to the trigger.
    * @param completion The block to call when the HTTPS request has completed.
    */
-  public func call(_ data: Any? = nil, completion: @escaping (HTTPSCallableResult?, Error?) -> Void) {
+  public func call(_ data: Any? = nil,
+                   completion: @escaping (HTTPSCallableResult?, Error?) -> Void) {
     functions.callFunction(name: name,
                            withObject: data,
                            timeout: timeoutInterval) { result in
       switch result {
-      case .success(let callableResult):
+      case let .success(callableResult):
         completion(callableResult, nil)
-      case .failure(let error):
+      case let .failure(error):
         completion(nil, error)
       }
     }
   }
 
-#if compiler(>=5.5) && canImport(_Concurrency)
-  /**
-   * Executes this Callable HTTPS trigger asynchronously.
-   *
-   * The request to the Cloud Functions backend made by this method automatically includes a
-   * FCM token to identify the app instance. If a user is logged in with Firebase
-   * Auth, an auth ID token for the user is also automatically included.
-   *
-   * Firebase Cloud Messaging sends data to the Firebase backend periodically to collect information
-   * regarding the app instance. To stop this, see `Messaging.deleteData()`. It
-   * resumes with a new FCM Token the next time you call this method.
-   *
-   * @param data Parameters to pass to the trigger.
-   * @returns The result of the call.
-   */
-  @available(iOS 15, tvOS 15, macOS 12, watchOS 8, *)
-  public func call(_ data: Any? = nil) async throws -> HTTPSCallableResult {
-    return try await withCheckedThrowingContinuation({ continuation in
+  #if compiler(>=5.5) && canImport(_Concurrency)
+    /**
+     * Executes this Callable HTTPS trigger asynchronously.
+     *
+     * The request to the Cloud Functions backend made by this method automatically includes a
+     * FCM token to identify the app instance. If a user is logged in with Firebase
+     * Auth, an auth ID token for the user is also automatically included.
+     *
+     * Firebase Cloud Messaging sends data to the Firebase backend periodically to collect information
+     * regarding the app instance. To stop this, see `Messaging.deleteData()`. It
+     * resumes with a new FCM Token the next time you call this method.
+     *
+     * @param data Parameters to pass to the trigger.
+     * @returns The result of the call.
+     */
+    @available(iOS 15, tvOS 15, macOS 12, watchOS 8, *)
+    public func call(_ data: Any? = nil) async throws -> HTTPSCallableResult {
+      return try await withCheckedThrowingContinuation { continuation in
         // TODO(bonus): Use task to handle and cancellation.
-      self.call(data) { callableResult, error in
-        if let callableResult = callableResult {
-          continuation.resume(returning: callableResult)
-        } else {
-          continuation.resume(throwing: error!)
+        self.call(data) { callableResult, error in
+          if let callableResult = callableResult {
+            continuation.resume(returning: callableResult)
+          } else {
+            continuation.resume(throwing: error!)
+          }
         }
       }
-    })
-  }
-  #endif  // compiler(>=5.5) && canImport(_Concurrency)
+    }
+  #endif // compiler(>=5.5) && canImport(_Concurrency)
 }
