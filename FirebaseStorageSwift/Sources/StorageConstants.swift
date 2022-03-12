ccbc191ee67e2b964a1b3c1f0d792b66a97e6b8e
Paul Beusterien <paulbeusterien@google.com>
2022-03-12 14:59:58 -0800

    Simplify error code code and skip quickstart for now

--- FirebaseStorageSwift/Sources/StorageConstants.swift
+++ FirebaseStorageSwift/Sources/StorageConstants.swift
@@ -23,78 +23,19 @@ import FirebaseStorageObjC
   case pause
   case success
   case failure
-
-  public typealias RawValue = Int
-
-  public var rawValue: RawValue {
-    switch self {
-    case .unknown:
-      return FIRIMPLStorageTaskStatus.unknown.rawValue
-    case .resume:
-      return FIRIMPLStorageTaskStatus.resume.rawValue
-    case .progress:
-      return FIRIMPLStorageTaskStatus.progress.rawValue
-    case .pause:
-      return FIRIMPLStorageTaskStatus.pause.rawValue
-    case .success:
-      return FIRIMPLStorageTaskStatus.success.rawValue
-    case .failure:
-      return FIRIMPLStorageTaskStatus.failure.rawValue
-    }
-  }
 }
 
 @objc(FIRStorageErrorCode) public enum StorageErrorCode: Int {
-  case unknown
-  case objectNotFound
-  case bucketNotFound
-  case projectNotFound
-  case quotaExceeded
-  case unauthenticated
+  case unknown = -13000
+  case objectNotFound = -13010
+  case bucketNotFound = -13011
+  case projectNotFound = -13012
+  case quotaExceeded = -13013
+  case unauthenticated = -13020
   case unauthorized = -13021
-  case retryLimitExceeded
-  case nonMatchingChecksum
+  case retryLimitExceeded = -13030
+  case nonMatchingChecksum = -13031
   case downloadSizeExceeded = -13032
-  case cancelled
-  case invalidArgument
-
-  public typealias RawValue = Int
-
-  public var rawValue: RawValue {
-    switch self {
-    case .unknown:
-      return FIRIMPLStorageErrorCode.unknown.rawValue
-    case .objectNotFound:
-      return FIRIMPLStorageErrorCode.objectNotFound.rawValue
-    case .bucketNotFound:
-      return FIRIMPLStorageErrorCode.bucketNotFound.rawValue
-    case .projectNotFound:
-      return FIRIMPLStorageErrorCode.projectNotFound.rawValue
-    case .quotaExceeded:
-      return FIRIMPLStorageErrorCode.quotaExceeded.rawValue
-    case .unauthenticated:
-      return FIRIMPLStorageErrorCode.unauthenticated.rawValue
-    case .unauthorized:
-      return FIRIMPLStorageErrorCode.unauthorized.rawValue
-    case .retryLimitExceeded:
-      return FIRIMPLStorageErrorCode.retryLimitExceeded.rawValue
-    case .nonMatchingChecksum:
-      return FIRIMPLStorageErrorCode.nonMatchingChecksum.rawValue
-    case .downloadSizeExceeded:
-      return FIRIMPLStorageErrorCode.downloadSizeExceeded.rawValue
-    case .cancelled:
-      return FIRIMPLStorageErrorCode.cancelled.rawValue
-    case .invalidArgument:
-      return FIRIMPLStorageErrorCode.invalidArgument.rawValue
-    }
-  }
-
-  public init?(rawValue: RawValue) {
-    switch rawValue {
-    case FIRIMPLStorageErrorCode.unknown.rawValue:
-      self = .unknown
-    default:
-      self = .unauthorized
-    }
-  }
+  case cancelled = -13040
+  case invalidArgument = -13050
 }
