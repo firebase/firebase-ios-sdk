42474d8c778d7ba233a67bef9ead71750eec188a
Morgan Chen <morganchen12@gmail.com>
2022-03-28 16:07:19 -0700

    give up and use manual wrapper

--- Firestore/Swift/Tests/Integration/AsyncAwaitIntegrationTests.swift
+++ Firestore/Swift/Tests/Integration/AsyncAwaitIntegrationTests.swift
@@ -65,5 +65,16 @@ let emptyBundle = """
         .loadBundle(InputStream(data: bundle.data(using: String.Encoding.utf8)!))
       XCTAssertEqual(LoadBundleTaskState.success, bundleProgress.state)
     }
+
+    func testRunTransactionDoesNotCrashOnNilSuccess() async throws {
+      let document = collectionRef().document()
+      try await document.setData(["": ""]) // ensure document exists
+      let value = try await db.runTransaction { transact, error in
+        transact.setData(["test": "test"], forDocument: document)
+        return nil // should not crash
+      }
+
+      XCTAssertNil(value, "value should be nil on success")
+    }
   }
 #endif
