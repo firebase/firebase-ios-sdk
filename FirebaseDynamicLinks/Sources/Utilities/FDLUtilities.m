6b48b4810b34fa6f805a9d1e862698d5576916d0
Eldhose Mathokkil Babu <eldhosembabu@google.com>
2022-03-28 13:20:26 -0700

    Refactoring custom domain validation logic with fixes for validating whitelisted domain with paths.

--- FirebaseDynamicLinks/Sources/Utilities/FDLUtilities.m
+++ FirebaseDynamicLinks/Sources/Utilities/FDLUtilities.m
@@ -201,38 +201,34 @@ NSString *FIRDLDeviceTimezone() {
   return timeZoneName;
 }
 
-BOOL FIRDLIsURLForAllowedCustomDomain(NSURL *_Nullable URL) {
-  BOOL customDomainMatchFound = false;
+BOOL FIRDLIsURLForAllowedCustomDomain(NSURL *URL) {
+    if(URL){
   for (NSURL *allowedCustomDomain in FIRDLCustomDomains) {
     // At least one custom domain host name should match at a minimum.
-    if ([allowedCustomDomain.host isEqualToString:URL.host]) {
-      NSString *urlStr = URL.absoluteString;
-      NSString *domainURIPrefixStr = allowedCustomDomain.absoluteString;
-
-      // Next, do a string compare to check if entire domainURIPrefix matches as well.
-      if (([urlStr rangeOfString:domainURIPrefixStr
-                         options:NSCaseInsensitiveSearch | NSAnchoredSearch]
-               .location) == 0) {
-        NSString *urlWithoutDomainURIPrefix = [urlStr substringFromIndex:domainURIPrefixStr.length];
-
-        // For a valid custom domain DL Suffix:
+    if([URL.absoluteString hasPrefix:allowedCustomDomain.absoluteString]) {
+        NSString *urlWithoutDomainURIPrefix =[URL.absoluteString substringFromIndex:allowedCustomDomain.absoluteString.length];
+      
+        //The urlWithoutDomainURIPrefix should be starting with '/' or '?' otherwise it means the allowed domain is not
+        // exactly matching the incoming URL domain prefix.
+        // For a valid custom domain DL Suffix the urlWithoutDomainURIPrefix should have:
         // 1. At least one path exists OR
         // 2. Should have a link query param with an http/https link
+        //      2.1 The link param can be anywhere in the url but should not be in a fragment (#)
+
         BOOL matchesRegularExpression =
             ([urlWithoutDomainURIPrefix
                  rangeOfString:
-                     @"^((\\/[A-Za-z0-9]+)|((\\?|\\/\\?)(link=https?.*)|(.*\\&link=https?.*))$)"
+                     @"^((\\/[A-Za-z0-9]+)|((\\?|\\/\\?)((link=https?.*)|([^#]*\\&link=https?.*)))$)"
                        options:NSRegularExpressionSearch]
                  .location != NSNotFound);
 
         if (matchesRegularExpression) {
-          customDomainMatchFound = true;
-          break;
+            return true;
         }
       }
     }
-  }
-  return customDomainMatchFound;
+    }
+  return false;
 }
 
 /* We are validating following domains in proper format.
