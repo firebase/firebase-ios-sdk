34e36e6e1265210687d44548024ce889faeb3d64
Morgan Chen <morganchen12@gmail.com>
2022-03-29 11:56:50 -0700

    test

--- Firestore/Swift/Tests/Integration/AsyncAwaitIntegrationTests.swift
+++ Firestore/Swift/Tests/Integration/AsyncAwaitIntegrationTests.swift
@@ -67,14 +67,11 @@ let emptyBundle = """
     }
 
     func testRunTransactionDoesNotCrashOnNilSuccess() async throws {
-      let document = collectionRef().document()
-      try await document.setData(["": ""]) // ensure document exists
       let value = try await db.runTransaction { transact, error in
-        transact.setData(["test": "test"], forDocument: document)
         return nil // should not crash
       }
 
-//      XCTAssertNil(value, "value should be nil on success")
+      XCTAssertNil(value, "value should be nil on success")
     }
   }
 #endif
