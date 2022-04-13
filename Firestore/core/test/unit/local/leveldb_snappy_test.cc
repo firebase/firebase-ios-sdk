73a558cd7d3693bcb9c031bc37f9bb78535c01c2
Denver Coneybeare <dconeybe@google.com>
2022-04-13 10:32:33 -0400

    leveldb_snappy_test.cc: REVERT ME: temporarily allow the test to fail on iOS to verify the fix to xcresult_logs.py in https://github.com/firebase/firebase-ios-sdk/pull/9645

--- Firestore/core/test/unit/local/leveldb_snappy_test.cc
+++ Firestore/core/test/unit/local/leveldb_snappy_test.cc
@@ -43,12 +43,6 @@ Path CreateLevelDbDatabaseThatUsesSnappyCompression();
 // This test ensures that we don't accidentally regress having added in Snappy
 // compression support (https://github.com/firebase/firebase-ios-sdk/pull/9596).
 TEST(LevelDbSnappy, LevelDbHasSnappySupportCompiledIn) {
-// Only run this test in cmake builds, since Snappy support is not included
-// when pulling in LevelDb via CocoaPods or Swift Package Manager.
-#if FIRESTORE_TESTS_CMAKE_BUILD
-  GTEST_SKIP() << "Snappy support is only present in cmake builds";
-#endif
-
   Path leveldb_path = CreateLevelDbDatabaseThatUsesSnappyCompression();
   if (HasFatalFailure()) return;
 
