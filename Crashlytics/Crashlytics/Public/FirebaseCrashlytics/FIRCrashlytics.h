6799942263fa8c2a5f6043c64f060090bfa526fd
Elena Doty <elenadoty@google.com>
2022-04-29 11:57:02 -0400

    update NS_SWIFT_NAME parameter

--- Crashlytics/Crashlytics/Public/FirebaseCrashlytics/FIRCrashlytics.h
+++ Crashlytics/Crashlytics/Public/FirebaseCrashlytics/FIRCrashlytics.h
@@ -72,7 +72,7 @@ NS_SWIFT_NAME(Crashlytics)
  * @param args Arguments to substitute into format
  */
 - (void)logWithFormat:(NSString *)format
-            arguments:(va_list)args NS_SWIFT_NAME(log(format:arguments:));
+            arguments:(va_list)args NS_SWIFT_NAME(log(withFormat:arguments:));
 
 /**
  * Sets a custom key and value to be associated with subsequent fatal and non-fatal reports.
