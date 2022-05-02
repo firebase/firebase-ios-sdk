c0d5c4acd492889b0a96d3a7311a1ea86ab78d1e
Gran Luo <granluo@google.com>
2022-05-01 19:49:22 -0700

    Update branch name.

--- scripts/create_pull_request.rb
+++ scripts/create_pull_request.rb
@@ -53,15 +53,14 @@ COMMIT_COMMENT=@options[:commit_comment]
 
 puts @options
 def generate_pr_for_target_changes(repo_root:, target_path:)
-  system("cd #{repo_root}")
-  system("git checkout -b #{BASE_BRANCH}\n #{PR_BODY}\n")
+  system("cd #{REPO_ROOT}\ngit checkout -b #{BASE_BRANCH}\n git add #{REPO_ROOT}/#{TARGET_PATH}\n")
   if `git diff #{TARGET_PATH}`==""
     puts "The file has no changes."
     return
   end
-  system("git commit -m \"#{COMMIT_COMMENT}\"\n git push -u origin target_diff_detected")
+  system("git commit -m \"#{COMMIT_COMMENT}\"\n git push -u origin #{BASE_BRANCH}t ")
   client = Octokit::Client.new(access_token: ACCESS_TOKEN)
-  client.create_pull_request("firebase/firebase-ios-sdk", "master", "target_diff_detected", PR_TITLE, PR_BODY)
+  client.create_pull_request("firebase/firebase-ios-sdk", "master", BASE_BRANCH, PR_TITLE, PR_BODY)
 end
 
 
