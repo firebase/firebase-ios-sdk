5969ce4463d73e5f00d6d1d3a44ce5acae0127cc
Quan Pham <qdpham@google.com>
2022-05-18 15:37:28 -0700

    Format file

--- FirebaseRemoteConfig/Sources/RCNConfigRealtime.m
+++ FirebaseRemoteConfig/Sources/RCNConfigRealtime.m
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-#import <Foundation/Foundation.h>
 #import "FirebaseRemoteConfig/Sources/RCNConfigRealtime.h"
+#import <Foundation/Foundation.h>
 #import "FirebaseRemoteConfig/Sources/RCNConfigConstants.h"
 
 @implementation FIRConfigUpdateListenerRegistration {
