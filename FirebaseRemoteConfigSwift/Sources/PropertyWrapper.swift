64e47b7843d6e0971e4ddb371f10a02324055a29
Fumito Ito <fumito.ito@nex.nikkei.com>
2022-05-25 01:03:27 +0900

    initialize with simplest features

--- FirebaseRemoteConfigSwift/Sources/PropertyWrapper.swift
+++ FirebaseRemoteConfigSwift/Sources/PropertyWrapper.swift
@@ -1,8 +1,40 @@
 //
-//  File.swift
+//  PropertyWrapper.swift
 //  
 //
 //  Created by Fumito Ito on 2022/05/25.
 //
 
-import Foundation
+import FirebaseRemoteConfig
+
+@available(iOS 14.0, macOS 11.0, macCatalyst 14.0, tvOS 14.0, watchOS 7.0, *)
+@propertyWrapper
+public struct RemoteConfigProperty<T: Decodable> {
+    private let key: String
+    private let remoteConfig: RemoteConfig
+
+    public var wrappedValue: T? {
+        get {
+            try? self.remoteConfig[self.key].decoded(asType: T.self)
+        }
+        @available(*, unavailable, message: "RemoteConfig property wrapper does not support setting property.")
+        set {
+            fatalError("RemoteConfig property wrapper does not support setting property.")
+        }
+    }
+
+    public init(
+        forKey key: String
+    ) {
+        self.key = key
+        self.remoteConfig = RemoteConfig.remoteConfig()
+    }
+
+    public init(
+        forKey key: String,
+        remoteConfig: RemoteConfig
+    ) {
+        self.key = key
+        self.remoteConfig = remoteConfig
+    }
+}
