9ac926b0c1650b01fd7fe7d8a32499d338ad41d0
Denver Coneybeare <dconeybe@google.com>
2022-05-25 01:06:59 -0400

    firestore.h: Add a TODO to remove the default value for max_attempts

--- Firestore/core/src/api/firestore.h
+++ Firestore/core/src/api/firestore.h
@@ -93,6 +93,8 @@ class Firestore : public std::enable_shared_from_this<Firestore> {
   WriteBatch GetBatch();
   core::Query GetCollectionGroup(std::string collection_id);
 
+  // TODO: Remove the default value of `max_attempts` once the CPP SDK has
+  // been updated to specify an explicit value.
   void RunTransaction(core::TransactionUpdateCallback update_callback,
                       core::TransactionResultCallback result_callback,
                       int max_attempts = kDefaultTransactionMaxAttempts);
