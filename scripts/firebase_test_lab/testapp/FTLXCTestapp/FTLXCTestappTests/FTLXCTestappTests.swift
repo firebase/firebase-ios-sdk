76eddcdcdc35ef971d41e24e5b2792171cba3d3d
Gran Luo <granluo@google.com>
2022-07-11 12:45:43 -0700

    Add notices.

--- scripts/firebase_test_lab/testapp/FTLXCTestapp/FTLXCTestappTests/FTLXCTestappTests.swift
+++ scripts/firebase_test_lab/testapp/FTLXCTestapp/FTLXCTestappTests/FTLXCTestappTests.swift
@@ -1,27 +1,33 @@
-//
-//  FTLXCTestappTests.swift
-//  FTLXCTestappTests
-//
-//  Created by Gran Luo on 7/6/22.
-//
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import XCTest
 @testable import FTLXCTestapp
 
 class FTLXCTestappTests: XCTestCase {
-  override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-  }
+  override func setUpWithError() throws {}
 
-  override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-  }
+  override func tearDownWithError() throws {}
 
   func testExample() throws {
     XCTAssert(true)
   }
 
   func testFailedExample() throws {
+    // Set the test to fail intentionally to test failed test cases on FTL.
     XCTAssert(false)
   }
 
--- scripts/firebase_test_lab/testapp/FTLXCTestapp/FTLXCTestappUITests/FTLXCTestappUITests.swift
+++ scripts/firebase_test_lab/testapp/FTLXCTestapp/FTLXCTestappUITests/FTLXCTestappUITests.swift
@@ -1,50 +1,40 @@
-//
-//  FTLXCTestappUITests.swift
-//  FTLXCTestappUITests
-//
-//  Created by Gran Luo on 7/6/22.
-//
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import XCTest
 
 class FTLXCTestappUITests: XCTestCase {
   override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-
-    // In UI tests it is usually best to stop immediately when a failure occurs.
     continueAfterFailure = false
-
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
   }
 
-  override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-  }
+  override func tearDownWithError() throws {}
 
   func testExample() throws {
-    // UI tests must launch the application that they test.
     let app = XCUIApplication()
     app.launch()
 
     XCTAssert(app.staticTexts["Hello, world!"].exists)
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
   }
 
   func testFailedExample() throws {
-    // UI tests must launch the application that they test.
     let app = XCUIApplication()
     app.launch()
 
+    // Set the test to fail intentionally to test failed test cases on FTL.
     XCTAssert(app.staticTexts["No Exsited"].exists)
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-  }
-
-  func testLaunchPerformance() throws {
-    if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-      // This measures how long it takes to launch your application.
-      measure(metrics: [XCTApplicationLaunchMetric()]) {
-        XCUIApplication().launch()
-      }
-    }
   }
 }
