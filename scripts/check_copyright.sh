dcc04701dde9c2fcba7ec107052403d7e4000d09
Chuan Ren <chuanr@google.com>
2022-07-25 10:47:15 -0700

    Update check_copyright.sh

--- scripts/check_copyright.sh
+++ scripts/check_copyright.sh
@@ -26,7 +26,7 @@ list=$(git grep "${options[@]}" -- \
     CMakeLists.txt '**/CMakeLists.txt' \
     ':(exclude)**/third_party/**')
 
-# Allow copyrights before 2020 without LLC.
+# Allow copyrights before 2030 without LLC.
 result=$(grep -L 'Copyright 20[0-2][0-9].*Google' $list)
 
 if [[ $result ]]; then
