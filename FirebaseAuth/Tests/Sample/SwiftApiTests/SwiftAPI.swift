b92009177a3e896ccbf01d7cf080bcc68dcea58a
Chuan Ren <chuanr@google.com>
2022-07-27 16:08:42 -0700

    Update SwiftAPI.swift

--- FirebaseAuth/Tests/Sample/SwiftApiTests/SwiftAPI.swift
+++ FirebaseAuth/Tests/Sample/SwiftApiTests/SwiftAPI.swift
@@ -212,6 +212,16 @@ class AuthAPI_hOnlyTests: XCTestCase {
     _ = AuthErrorCode.keychainError
     _ = AuthErrorCode.internalError
     _ = AuthErrorCode.malformedJWT
+    _ = AuthErrorCode.invalidRecaptchaScore
+    _ = AuthErrorCode.missingRecaptchaToken
+    _ = AuthErrorCode.invalidRecaptchaToken
+    _ = AuthErrorCode.invalidRecaptchaAction
+    _ = AuthErrorCode.invalidRecaptchaEnforcementState
+    _ = AuthErrorCode.recaptchaNotEnabled
+    _ = AuthErrorCode.missingClientType
+    _ = AuthErrorCode.missingRecaptchaVersion
+    _ = AuthErrorCode.invalidReqType
+    _ = AuthErrorCode.invalidRecaptchaVersion
   }
 
   func FIRAuthUIDelegate_h() {
