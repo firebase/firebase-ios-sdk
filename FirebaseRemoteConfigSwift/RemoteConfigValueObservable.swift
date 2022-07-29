c7e805cc01647bb4473f25f120533a5a81c2cfa0
Charlotte Liang <chliang@google.com>
2022-07-28 17:51:27 -0700

    add config property wrapper

---
+++ FirebaseRemoteConfigSwift/RemoteConfigValueObservable.swift
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import SwiftUI
+import FirebaseRemoteConfig
+import FirebaseRemoteConfigSwift
+
+@available(iOS 14.0, macOS 11.0, macCatalyst 14.0, tvOS 14.0, watchOS 7.0, *)
+internal class RemoteConfigValueObservable<T: Decodable>: ObservableObject {
+  @Published var configValue: T
+  private let key: String
+  private let remoteConfig: RemoteConfig
+  private var registration : FIRConfigUpdateListenerRegistration?
+
+  init(key: String, remoteConfig: RemoteConfig) {
+    self.key = key
+    self.remoteConfig = remoteConfig
+    self.configValue = try! remoteConfig.configValue(forKey: key).decoded(asType: T.self)
+    self.registration = remoteConfig.add(onConfigUpdateListener: { error in
+          guard error == nil else {
+            return
+          }
+          self.remoteConfig.activate() { changed, error in
+            guard error == nil else {
+              return
+            }
+            self.remoteConfig.activate{ (changed, error) in
+              DispatchQueue.main.async {
+                self.configValue = try! remoteConfig.configValue(forKey: self.key).decoded(asType: T.self)
+              }
+            }
+          }
+        })
+
+  }
+
+  deinit {
+    self.registration?.remove()
+  }
+}
+
