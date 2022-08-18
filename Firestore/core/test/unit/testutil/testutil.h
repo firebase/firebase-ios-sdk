88ac246110e19a8285bde6fca35837b1f04f7f22
cherylEnkidu <96084918+cherylEnkidu@users.noreply.github.com>
2022-08-18 10:51:08 -0400

    Add setIndexConfiguration for client side indexing (#10090)
    * Add setIndexConfiguration for client side indexing
    
    * Address feedback
    
    * Templates in headers
    
    Co-authored-by: Wu-Hui <wu.hui.github@gmail.com>

--- Firestore/core/test/unit/testutil/testutil.h
+++ Firestore/core/test/unit/testutil/testutil.h
@@ -43,6 +43,14 @@ class Timestamp;
 namespace firestore {
 class GeoPoint;
 
+namespace remote {
+class RemoteEvent;
+}  // namespace remote
+
+namespace model {
+class MutableDocument;
+}  // namespace model
+
 namespace nanopb {
 class ByteString;
 }  // namespace nanopb
@@ -362,6 +370,25 @@ core::Query Query(absl::string_view path);
 
 core::Query CollectionGroupQuery(absl::string_view collection_id);
 
+remote::RemoteEvent AddedRemoteEvent(
+    const std::vector<model::MutableDocument>& docs,
+    const std::vector<model::TargetId>& added_to_targets);
+
+remote::RemoteEvent AddedRemoteEvent(
+    const model::MutableDocument& doc,
+    const std::vector<model::TargetId>& added_to_targets);
+
+remote::RemoteEvent UpdateRemoteEvent(
+    const model::MutableDocument& doc,
+    const std::vector<model::TargetId>& updated_in_targets,
+    const std::vector<model::TargetId>& removed_from_targets);
+
+remote::RemoteEvent UpdateRemoteEventWithLimboTargets(
+    const model::MutableDocument& doc,
+    const std::vector<model::TargetId>& updated_in_targets,
+    const std::vector<model::TargetId>& removed_from_targets,
+    const std::vector<model::TargetId>& limbo_targets);
+
 model::SetMutation SetMutation(
     absl::string_view path,
     nanopb::Message<google_firestore_v1_Value> values,
@@ -394,6 +421,14 @@ model::PatchMutation PatchMutationHelper(
     std::vector<std::pair<std::string, model::TransformOperation>> transforms,
     model::Precondition precondition,
     const absl::optional<std::vector<model::FieldPath>>& update_mask);
+
+/**
+ * Creates a pair of field name, TransformOperation that represents a
+ * server-generated timestamp, suitable for passing to TransformMutation,
+ * above.
+ */
+std::pair<std::string, model::TransformOperation> ServerTimestamp(
+    std::string field);
 
 /**
  * Creates a pair of field name, TransformOperation that represents a numeric
