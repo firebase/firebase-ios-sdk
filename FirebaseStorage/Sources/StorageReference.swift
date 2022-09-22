c5f5fa38da908f72b83ce6108db531adf3b32465
Paul Beusterien <paulbeusterien@google.com>
2022-09-22 11:42:43 -0700

    Docs update for Extensions and putFile (#10248)

--- FirebaseStorage/Sources/StorageReference.swift
+++ FirebaseStorage/Sources/StorageReference.swift
@@ -174,6 +174,7 @@ import Foundation
 
   /**
    * Asynchronously uploads a file to the currently specified `StorageReference`.
+   * `putData` should be used instead of `putFile` in Extensions.
    * - Parameters:
    *   - fileURL: A URL representing the system file path of the object to be uploaded.
    *   - metadata: `StorageMetadata` containing additional information (MIME type, etc.)
@@ -188,6 +189,7 @@ import Foundation
   /**
    * Asynchronously uploads a file to the currently specified `StorageReference`,
    * without additional metadata.
+   * `putData` should be used instead of `putFile` in Extensions.
    * @param fileURL A URL representing the system file path of the object to be uploaded.
    * @return An instance of StorageUploadTask, which can be used to monitor or manage the upload.
    */
@@ -197,6 +199,7 @@ import Foundation
 
   /**
    * Asynchronously uploads a file to the currently specified `StorageReference`.
+   * `putData` should be used instead of `putFile` in Extensions.
    * - Parameters:
    *   - fileURL: A URL representing the system file path of the object to be uploaded.
    *   - metadata: `StorageMetadata` containing additional information (MIME type, etc.)
