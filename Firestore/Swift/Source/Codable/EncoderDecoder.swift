bec09bf8376025069a1b0dbd76cfd5a55c1ecc51
Morgan Chen <morganchen12@gmail.com>
2022-09-27 11:54:12 -0700

    Address PR feedback for Firestore Codable

--- Firestore/Swift/Source/Codable/EncoderDecoder.swift
+++ Firestore/Swift/Source/Codable/EncoderDecoder.swift
@@ -100,7 +100,7 @@ public extension Firestore {
     public func decode<T: Decodable>(_ t: T.Type, from data: Any,
                                      in reference: DocumentReference?) throws -> T {
       if let reference = reference {
-        userInfo[documentRefUserInfoKey] = reference
+        userInfo[CodingUserInfoKey.documentRefUserInfoKey] = reference
       }
       return try decode(T.self, from: data)
     }
