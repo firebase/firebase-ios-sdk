797f7f438f73f84cafa4561781102d44b4570d73
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2022-09-30 15:24:44 -0400

    [skip ci] Update FirebaseStorage/Sources/Storage.swift

--- FirebaseStorage/Sources/Storage.swift
+++ FirebaseStorage/Sources/Storage.swift
@@ -24,7 +24,7 @@ import FirebaseAuthInterop
 #endif
 
 // Avoids exposing internal FirebaseCore APIs to Swift users.
-import FirebaseCoreExtension
+@_implementationOnly  import FirebaseCoreExtension
 
 /**
  * Firebase Storage is a service that supports uploading and downloading binary objects,
