9ea83c816cbfcee5f392351b69f804a4413a6332
chuanr <chuanr@google.com>
2022-10-04 08:26:16 -0700

    Persist FIRPhoneMultiFactorInfo (instead of FIRMultiFactorInfo) so that full phone number is always cached (#10296)

--- FirebaseAuth/Sources/MultiFactor/Phone/FIRPhoneMultiFactorInfo.m
+++ FirebaseAuth/Sources/MultiFactor/Phone/FIRPhoneMultiFactorInfo.m
@@ -23,6 +23,8 @@
 #import "FirebaseAuth/Sources/Backend/RPC/Proto/FIRAuthProtoMFAEnrollment.h"
 #import "FirebaseAuth/Sources/MultiFactor/FIRMultiFactorInfo+Internal.h"
 
+static NSString *kPhoneNumberCodingKey = @"phoneNumber";
+
 extern NSString *const FIRPhoneMultiFactorID;
 
 @implementation FIRPhoneMultiFactorInfo
@@ -34,6 +36,25 @@ extern NSString *const FIRPhoneMultiFactorID;
     _phoneNumber = proto.phoneInfo;
   }
   return self;
+}
+
+#pragma mark - NSSecureCoding
+
++ (BOOL)supportsSecureCoding {
+  return YES;
+}
+
+- (nullable instancetype)initWithCoder:(NSCoder *)aDecoder {
+  self = [super initWithCoder:aDecoder];
+  if (self) {
+    _phoneNumber = [aDecoder decodeObjectOfClass:[NSString class] forKey:kPhoneNumberCodingKey];
+  }
+  return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+  [super encodeWithCoder:aCoder];
+  [aCoder encodeObject:_phoneNumber forKey:kPhoneNumberCodingKey];
 }
 
 @end
