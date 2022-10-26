db58a03e8be0b9745adca2d932e454bda1673356
cherylEnkidu <cheryllin@google.com>
2022-10-26 08:00:13 -0400

    Performing IN expansion

--- Firestore/core/src/local/leveldb_index_manager.cc
+++ Firestore/core/src/local/leveldb_index_manager.cc
@@ -536,21 +536,14 @@ IndexManager::IndexType LevelDbIndexManager::GetIndexType(
 
 absl::optional<std::vector<model::DocumentKey>>
 LevelDbIndexManager::GetDocumentsMatchingTarget(const core::Target& target) {
-  std::unordered_map<core::Target, model::FieldIndex> indexes;
+  std::vector<DocumentKey> result;
+  std::unordered_set<std::string> existing_keys;
   for (const auto& sub_target : GetSubTargets(target)) {
     auto index_opt = GetFieldIndex(sub_target);
     if (!index_opt.has_value()) {
       return absl::nullopt;
     }
-
-    indexes.insert({sub_target, index_opt.value()});
-  }
-
-  std::vector<DocumentKey> result;
-  std::unordered_set<std::string> existing_keys;
-  for (const auto& entry : indexes) {
-    const Target& sub_target = entry.first;
-    const FieldIndex& index = entry.second;
+    const FieldIndex& index = index_opt.value();
 
     LOG_DEBUG("Using index %s to execute target %s", index.collection_group(),
               sub_target.CanonicalId());
