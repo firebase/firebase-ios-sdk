c9c0f0f3b898e39b2645842ee1a2cf0a45f12a68
samedson <samedson@google.com>
2022-10-28 11:26:17 -0400

    Stuff

--- FirebaseSessions/Tests/TestApp/UITestsiOS/UITestsiOS.swift
+++ FirebaseSessions/Tests/TestApp/UITestsiOS/UITestsiOS.swift
@@ -16,19 +16,6 @@
 import XCTest
 
 final class UITestsiOS: XCTestCase {
-  override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-
-    // In UI tests it is usually best to stop immediately when a failure occurs.
-    continueAfterFailure = false
-
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-  }
-
-  override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-  }
-
   func test_sessionGenerated_onColdStart() throws {
     // UI tests must launch the application that they test.
     let app = XCUIApplication()
