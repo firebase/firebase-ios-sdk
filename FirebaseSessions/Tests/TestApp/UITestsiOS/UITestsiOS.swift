9876601e149e35792ded06e03b2a781ec8201816
Leo Zhan <zhanl@google.com>
2022-11-01 09:43:17 -0700

    [Sessions] Add Test App and Integration tests for the Sessions SDK (#10396)

---
+++ FirebaseSessions/Tests/TestApp/UITestsiOS/UITestsiOS.swift
@@ -0,0 +1,28 @@
+//
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import XCTest
+
+final class UITestsiOS: XCTestCase {
+  func test_sessionGenerated_onColdStart() throws {
+    // UI tests must launch the application that they test.
+    let app = XCUIApplication()
+    app.launch()
+    XCUIDevice.shared.press(.home)
+    app.activate()
+
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+}
