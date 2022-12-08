c2c9b07974263ba8a0b937d6143ea1e5efa9c682
Tejas Deshpande <tdeshpande@google.com>
2022-12-08 16:19:44 -0500

    Fix public status of start feedback method

--- FirebaseAppDistributionInternal/Sources/InAppFeedback.swift
+++ FirebaseAppDistributionInternal/Sources/InAppFeedback.swift
@@ -3,7 +3,7 @@ import UIKit
 
 @objc(FIRFADInAppFeedback) open class InAppFeedback: NSObject {
   
-  @objc(feedbackViewController) static func feedbackViewController() -> UIViewController {
+  @objc(feedbackViewController) static public func feedbackViewController() -> UIViewController {
     let resourceBundle = Bundle(identifier: "main")
     let storyboard = UIStoryboard(name: "FIRAppDistributionInternalStoryboard", bundle: resourceBundle)
     let vc = storyboard.instantiateViewController(withIdentifier: "fir-ad-iaf")
