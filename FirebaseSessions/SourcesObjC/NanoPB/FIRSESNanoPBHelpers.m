d91550c4e74c87dca7998c21936131c9dcc1efff
samedson <samedson@google.com>
2022-12-14 12:17:59 -0500

    Fix check_imports

--- FirebaseSessions/SourcesObjC/NanoPB/FIRSESNanoPBHelpers.m
+++ FirebaseSessions/SourcesObjC/NanoPB/FIRSESNanoPBHelpers.m
@@ -17,17 +17,8 @@
 
 #import <GoogleUtilities/GULNetworkInfo.h>
 
-#if SWIFT_PACKAGE
-#import "FIRSESNanoPBHelpers.h"
-#else
 #import "FirebaseSessions/SourcesObjC/NanoPB/FIRSESNanoPBHelpers.h"
-#endif  // SWIFT_PACKAGE
-
-#if SWIFT_PACKAGE
-#import "SourcesObjC/Protogen/nanopb/sessions.nanopb.h"
-#else
 #import "FirebaseSessions/SourcesObjC/Protogen/nanopb/sessions.nanopb.h"
-#endif  // SWIFT_PACKAGE
 
 #import <nanopb/pb.h>
 #import <nanopb/pb_decode.h>
