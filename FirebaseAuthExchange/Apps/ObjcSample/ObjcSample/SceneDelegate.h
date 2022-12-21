54d8fcae17e164a50f9a3d1f4001d86a846c76ea
Rosalyn Tan <rosalyntan@google.com>
2022-12-21 16:39:27 -0500

    Ran scripts/style.sh.

--- FirebaseAuthExchange/Apps/ObjcSample/ObjcSample/SceneDelegate.h
+++ FirebaseAuthExchange/Apps/ObjcSample/ObjcSample/SceneDelegate.h
@@ -16,7 +16,6 @@
 
 @interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
 
-@property (strong, nonatomic) UIWindow * window;
+@property(strong, nonatomic) UIWindow* window;
 
 @end
-
--- FirebaseAuthExchange/Apps/ObjcSample/ObjcSample/ViewController.m
+++ FirebaseAuthExchange/Apps/ObjcSample/ObjcSample/ViewController.m
@@ -25,5 +25,4 @@
   // Do any additional setup after loading the view.
 }
 
-
 @end
