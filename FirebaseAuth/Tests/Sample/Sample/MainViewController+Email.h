ddf8a9de61e28d8952e87e61eb5de3e4832a7155
Paul Beusterien <paulbeusterien@google.com>
2022-12-29 17:31:49 -0800

    checkpoint

--- FirebaseAuth/Tests/Sample/Sample/MainViewController+Email.h
+++ FirebaseAuth/Tests/Sample/Sample/MainViewController+Email.h
@@ -18,8 +18,7 @@
 
 #import "MainViewController.h"
 
-#import <FirebaseAuth/FIRAuth.h>
-#import <FirebaseAuth/FIRAuthCredential.h>
+@import FirebaseAuth;
 #import "StaticContentTableViewManager.h"
 
 NS_ASSUME_NONNULL_BEGIN
