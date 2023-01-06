78d0df7a5cdeac2f6265c41a316070d3647dda5f
Paul Beusterien <paulbeusterien@google.com>
2023-01-06 14:20:59 -0800

    AuthProviderStrings enum FR#9236 and tests

---
+++ FirebaseAuth/Tests/Unit/AuthProviderIDTests.swift
@@ -0,0 +1,31 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+import XCTest
+import FirebaseAuth
+
+final class AuthProviderIDTests: XCTestCase {
+
+  // Verify that AuthProviderID enum values match the class values published for Objective C compatibility.
+  func testAuthProviderIDEnumRawValue() {
+    XCTAssertEqual(AuthProviderString.apple.rawValue, "apple.com")
+    XCTAssertEqual(AuthProviderString.email.rawValue, EmailAuthProvider.id)
+    XCTAssertEqual(AuthProviderString.facebook.rawValue, FacebookAuthProvider.id)
+    XCTAssertEqual(AuthProviderString.gameCenter.rawValue, GameCenterAuthProvider.id)
+    XCTAssertEqual(AuthProviderString.gitHub.rawValue, GitHubAuthProvider.id)
+    XCTAssertEqual(AuthProviderString.google.rawValue, GoogleAuthProvider.id)
+    XCTAssertEqual(AuthProviderString.phone.rawValue, PhoneAuthProvider.id)
+  }
+}
