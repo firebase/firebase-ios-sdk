d765e695a9966d0328965832bd627a2eb53500d2
Paul Beusterien <paulbeusterien@google.com>
2023-01-05 20:56:43 -0800

    GameCenter Provider minus some error codes

--- FirebaseAuth/Sources/Auth/FIRAuth.m
+++ FirebaseAuth/Sources/Auth/FIRAuth.m
@@ -35,7 +35,6 @@
 #import "FirebaseAuth/Sources/Auth/FIRAuthOperationType.h"
 //#import "FirebaseAuth/Sources/AuthProvider/Email/FIREmailPasswordAuthCredential.h"
 #import "FirebaseAuth/Sources/AuthProvider/FIRAuthCredential_Internal.h"
-#import "FirebaseAuth/Sources/AuthProvider/GameCenter/FIRGameCenterAuthCredential.h"
 #import "FirebaseAuth/Sources/Backend/FIRAuthBackend.h"
 #import "FirebaseAuth/Sources/Public/FirebaseAuth/FIRAuthRequestConfiguration.h"
 #import "FirebaseAuth/Sources/Backend/RPC/FIRCreateAuthURIRequest.h"
@@ -818,7 +817,7 @@ static NSMutableDictionary *gKeychainServiceNameForAppName;
                                                FIRAdditionalUserInfo *additionalUserInfo =
                                                    [[FIRAdditionalUserInfo alloc]
                                                        initWithProviderID:
-                                                           FIRGameCenterAuthProviderID
+                                                           FIRGameCenterAuthProvider.id
                                                                   profile:nil
                                                                  username:nil
                                                                 isNewUser:response.isNewUser];
