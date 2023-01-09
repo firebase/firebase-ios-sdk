4c9fa70d2b92046a85bf14383d44b993c85708a3
chuanr <chuanr@google.com>
2023-01-09 14:40:36 -0800

    Expose only needed error codes (#10635)

--- FirebaseAuth/Tests/Sample/SwiftApiTests/SwiftAPI.swift
+++ FirebaseAuth/Tests/Sample/SwiftApiTests/SwiftAPI.swift
@@ -215,10 +215,6 @@ class AuthAPI_hOnlyTests: XCTestCase {
     _ = AuthErrorCode.invalidRecaptchaAction
     _ = AuthErrorCode.invalidRecaptchaEnforcementState
     _ = AuthErrorCode.recaptchaNotEnabled
-    _ = AuthErrorCode.missingClientType
-    _ = AuthErrorCode.missingRecaptchaVersion
-    _ = AuthErrorCode.invalidReqType
-    _ = AuthErrorCode.invalidRecaptchaVersion
     _ = AuthErrorCode.missingClientIdentifier
     _ = AuthErrorCode.keychainError
     _ = AuthErrorCode.internalError
