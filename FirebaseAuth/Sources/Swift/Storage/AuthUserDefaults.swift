304a7eab1e094648ce79806d5082521ae7c95061
Paul Beusterien <paulbeusterien@google.com>
2023-01-24 16:09:32 -0500

    Fix build warnings and style (#10735)

--- FirebaseAuth/Sources/Swift/Storage/AuthUserDefaults.swift
+++ FirebaseAuth/Sources/Swift/Storage/AuthUserDefaults.swift
@@ -21,100 +21,100 @@ private let kPersistentDomainNamePrefix = "com.google.Firebase.Auth."
 // exposed to Objective-C due to convention and meaning of nil values in Objective-C.
 // This wrapper allows us to always return a value, thus allowing us to expose Objective-C api.
 @objc(FIRDataWrapper) public class DataWrapper: NSObject {
-    @objc public let data: Data?
-    @objc public init(data: Data?) {
-        self.data = data
-    }
+  @objc public let data: Data?
+  @objc public init(data: Data?) {
+    self.data = data
+  }
 }
 
 /**
     @brief The protocol for permanant data storage.
  */
 @objc(FIRAuthStorage) public protocol AuthStorage: NSObjectProtocol {
+  // NOTE: Removing this from protocol since it's not being used anywhere.
+  /** @fn initWithService:
+      @brief Initialize a @c FIRAuthStorage instance.
+      @param service The name of the storage service to use.
+      @return An initialized @c FIRAuthStorage instance for the specified service.
+   */
+  // - (id<FIRAuthStorage>)initWithService:(NSString *)service;
 
-// NOTE: Removing this from protocol since it's not being used anywhere.
-/** @fn initWithService:
-    @brief Initialize a @c FIRAuthStorage instance.
-    @param service The name of the storage service to use.
-    @return An initialized @c FIRAuthStorage instance for the specified service.
- */
-//- (id<FIRAuthStorage>)initWithService:(NSString *)service;
+  /** @fn dataForKey:error:
+      @brief Gets the data for @c key in the storage. The key is set for the attribute
+          @c kSecAttrAccount of a generic password query.
+      @param key The key to use.
+      @param error The address to store any error that occurs during the process, if not NULL.
+          If the operation was successful, its content is set to @c nil .
+      @return The data stored in the storage for @c key, if any.
+   */
+  @objc func data(forKey key: String) throws -> DataWrapper
 
-/** @fn dataForKey:error:
-    @brief Gets the data for @c key in the storage. The key is set for the attribute
-        @c kSecAttrAccount of a generic password query.
-    @param key The key to use.
-    @param error The address to store any error that occurs during the process, if not NULL.
-        If the operation was successful, its content is set to @c nil .
-    @return The data stored in the storage for @c key, if any.
- */
-@objc func data(forKey key: String) throws -> DataWrapper
+  /** @fn setData:forKey:error:
+      @brief Sets the data for @c key in the storage. The key is set for the attribute
+          @c kSecAttrAccount of a generic password query.
+      @param data The data to store.
+      @param key The key to use.
+      @param error The address to store any error that occurs during the process, if not NULL.
+      @return Whether the operation succeeded or not.
+   */
+  @objc func setData(_ data: Data, forKey key: String) throws
 
-/** @fn setData:forKey:error:
-    @brief Sets the data for @c key in the storage. The key is set for the attribute
-        @c kSecAttrAccount of a generic password query.
-    @param data The data to store.
-    @param key The key to use.
-    @param error The address to store any error that occurs during the process, if not NULL.
-    @return Whether the operation succeeded or not.
- */
-@objc func setData(_ data: Data, forKey key: String) throws
-
-/** @fn removeDataForKey:error:
-    @brief Removes the data for @c key in the storage. The key is set for the attribute
-        @c kSecAttrAccount of a generic password query.
-    @param key The key to use.
-    @param error The address to store any error that occurs during the process, if not NULL.
-    @return Whether the operation succeeded or not.
- */
-@objc func removeData(forKey key: String) throws
+  /** @fn removeDataForKey:error:
+      @brief Removes the data for @c key in the storage. The key is set for the attribute
+          @c kSecAttrAccount of a generic password query.
+      @param key The key to use.
+      @param error The address to store any error that occurs during the process, if not NULL.
+      @return Whether the operation succeeded or not.
+   */
+  @objc func removeData(forKey key: String) throws
 }
 
 /** @class FIRAuthUserDefaults
     @brief The utility class to storage data in NSUserDefaults.
  */
-@objc(FIRAuthUserDefaults) public class AuthUserDefaults : NSObject, AuthStorage {
-    /** @var _persistentDomainName
-        @brief The name of the persistent domain in user defaults.
-     */
-    private let persistentDomainName: String
+@objc(FIRAuthUserDefaults) public class AuthUserDefaults: NSObject, AuthStorage {
+  /** @var _persistentDomainName
+      @brief The name of the persistent domain in user defaults.
+   */
+  private let persistentDomainName: String
 
-    /** @var _storage
-        @brief The backing NSUserDefaults storage for this instance.
-     */
-    private let storage: UserDefaults
+  /** @var _storage
+      @brief The backing NSUserDefaults storage for this instance.
+   */
+  private let storage: UserDefaults
 
-    @objc public required init(service: String) {
-        self.persistentDomainName = kPersistentDomainNamePrefix + service
-        self.storage = UserDefaults()
+  @objc public required init(service: String) {
+    persistentDomainName = kPersistentDomainNamePrefix + service
+    storage = UserDefaults()
+  }
+
+  @objc public func data(forKey key: String) throws -> DataWrapper {
+    guard let allData = storage.persistentDomain(forName: persistentDomainName)
+    else { return DataWrapper(data: nil) }
+    if let data = allData[key] as? Data {
+      return DataWrapper(data: data)
     }
 
-    @objc public func data(forKey key: String) throws -> DataWrapper {
-        guard let allData = storage.persistentDomain(forName: persistentDomainName) else { return DataWrapper(data: nil) }
-        if let data = allData[key] as? Data {
-            return DataWrapper(data: data)
-        }
+    return DataWrapper(data: nil)
+  }
 
-        return DataWrapper(data: nil)
-    }
+  @objc public func setData(_ data: Data, forKey key: String) throws {
+    var allData = storage.persistentDomain(forName: persistentDomainName) ?? [:]
+    allData[key] = data
+    storage.setPersistentDomain(allData, forName: persistentDomainName)
+  }
 
-    @objc public func setData(_ data: Data, forKey key: String) throws {
-        var allData = storage.persistentDomain(forName: persistentDomainName) ?? [:]
-        allData[key] = data
-        storage.setPersistentDomain(allData, forName: persistentDomainName)
-    }
+  @objc public func removeData(forKey key: String) throws {
+    guard var allData = storage.persistentDomain(forName: persistentDomainName) else { return }
+    allData.removeValue(forKey: key)
+    storage.setPersistentDomain(allData, forName: persistentDomainName)
+  }
 
-    @objc public func removeData(forKey key: String) throws {
-        guard var allData = storage.persistentDomain(forName: persistentDomainName) else { return }
-        allData.removeValue(forKey: key)
-        storage.setPersistentDomain(allData, forName: persistentDomainName)
-    }
-
-    /** @fn clear
-        @brief Clears all data from the storage.
-        @remarks This method is only supposed to be called from tests.
-     */
-    @objc public func clear() {
-        storage.setPersistentDomain([:], forName: persistentDomainName)
-    }
+  /** @fn clear
+      @brief Clears all data from the storage.
+      @remarks This method is only supposed to be called from tests.
+   */
+  @objc public func clear() {
+    storage.setPersistentDomain([:], forName: persistentDomainName)
+  }
 }
