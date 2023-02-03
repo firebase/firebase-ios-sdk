1762d8f87c67ec32aebcc00594b4bdda5551a8f5
Nick Cooke <nickcooke@google.com>
2023-02-03 14:16:31 -0500

    Generalize script more and remove private module map

--- scripts/build_private_module_map.rb
+++ scripts/build_private_module_map.rb
@@ -14,46 +14,67 @@
 
 require 'cocoapods'
 
+def usage()
+    script = File.basename($0)
+    STDERR.puts <<~EOF
+    USAGE: #{script} <podspec> [options]
+
+    Generates a private module map for the given podspec. The generated module
+    map contains the private headers listed in the podspec's
+    'private_header_files' attribute.
+
+    This generated module map will resemble the following template:
+
+        framework module $(SPEC_MODULE_NAME)_Private {
+          header "PrivateHeader_1.h"
+          header "PrivateHeader_2.h"
+          header "PrivateHeader_3.h"
+          // And so on for each of the spec's private headers...
+        }
+
+    OPTIONS:
+      --dry-run: Prints the generated private module map to STDOUT without
+                 writing it to the filesystem.
+    EOF
+  end
+
 def main(args)
-    # TODO(ncooke3): Generalize this to take a module name as an argument.
-    if args.length != 1
-        puts "Usage: build_private_module_map.rb <module_name>"
+    if args.length < 1 || args.length > 2
+        usage()
         exit 1
     end
 
     STDOUT.sync = true
 
-    module_name = args[0]
-    spec = Pod::Spec.from_file("#{module_name}.podspec")
+    spec = Pod::Spec.from_file(args[0])
 
-    # Build of the private module map in the following template:
-    #
-    #       framework module FirebaseAuth_Private {
-    #         header "PrivateHeader_1.h"
-    #         header "PrivateHeader_2.h"
-    #         header "PrivateHeader_3.h"
-    #       }
-    #
-    private_module_map_contents = "framework module #{module_name}_Private {\n"
+    private_module_map_contents = "framework module #{spec.module_name}_Private {\n"
 
-    Dir[
-        'FirebaseAuth/Sources/**/*.h',
-        'FirebaseCore/Extension/*.h',
-        'FirebaseAuth/Interop/*.h'
-    ]
-        .reject{ |f| f['FirebaseAuth/Sources/Public/'] }
-        .each do |filepath|
-            filename = File.basename(filepath)
-            private_module_map_contents << "  header \"#{filename}\"\n"
+    private_hdrs = spec.attributes_hash['private_header_files']
+        # Expand all path globs to get the complete list of private headers.
+        .flat_map { |hdr_path_glob| Dir[hdr_path_glob] }
+        # Add each private header to the private module map's contents.
+        .each do |hdr_path|
+            # Note: Only the header file's name is needed in the module map.
+            # This is because the module map evaluates its headers relative to
+            # the private headers directory (Foo.framework/PrivateHeaders)
+            # within the CocoaPods-generated framework.
+            hdr_name = File.basename(hdr_path)
+            private_module_map_contents << "  header \"#{hdr_name}\"\n"
         end
 
     private_module_map_contents << "}\n"
 
-    # Overwrite the private module map with the generated contents.
-    File.write(
-        'FirebaseAuth/Sources/FirebaseAuth.private.modulemap', 
-        private_module_map_contents
-    )
+    if args.length == 2 && args[1] == '--dry-run'
+        STDOUT.puts private_module_map_contents
+    else
+        # Overwrite the private module map with the generated contents.
+        File.write(
+            # TODO(ncooke3): See if you can throw it in the public headers directory?
+            "#{spec.module_name}/Sources/#{spec.module_name}.private.modulemap",
+            private_module_map_contents
+        )
+    end
     
 end
 
