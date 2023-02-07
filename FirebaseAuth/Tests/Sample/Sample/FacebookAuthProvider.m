2662a13514599b6de19b1537cc2dbbdba01c227c
chuanr <chuanr@google.com>
2023-02-06 18:11:46 -0800

    Update FB sdk to v14

--- FirebaseAuth/Tests/Sample/Sample/FacebookAuthProvider.m
+++ FirebaseAuth/Tests/Sample/Sample/FacebookAuthProvider.m
@@ -48,7 +48,7 @@ static NSString *const kFacebookAppID = KFACEBOOK_APP_ID;
   [self signOut];
 
   [ApplicationDelegate setOpenURLDelegate:self];
-  [FBSDKSettings setAppID:kFacebookAppID];
+  [FBSDKSettings.sharedSettings setAppID:kFacebookAppID];
   [_loginManager logInWithPermissions:@[ @"email" ]
                    fromViewController:viewController
                               handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
