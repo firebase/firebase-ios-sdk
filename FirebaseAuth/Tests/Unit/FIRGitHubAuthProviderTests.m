47ced0e6c07fb447ad11ff1edbe49032e8eca371
Paul Beusterien <paulbeusterien@google.com>
2023-02-11 09:02:21 -0800

    Post rebase updates for #10805 and #10441

--- FirebaseAuth/Tests/Unit/FIRGitHubAuthProviderTests.m
+++ FirebaseAuth/Tests/Unit/FIRGitHubAuthProviderTests.m
@@ -46,7 +46,7 @@ static NSString *const kFirebaseAppID = @"appID";
  */
 - (void)testCredentialWithToken {
   FIRAuthRequestConfiguration *requestConfiguration =
-      [[FIRAuthRequestConfiguration alloc] initWithAPIKey:kAPIKey appID:kFirebaseAppID];
+      [[FIRAuthRequestConfiguration alloc] initWithAPIKey:kAPIKey appID:kFirebaseAppID auth:nil];
   FIRAuthCredential *credential = [FIRGitHubAuthProvider credentialWithToken:kGitHubToken];
   FIRVerifyAssertionRequest *request =
       [[FIRVerifyAssertionRequest alloc] initWithProviderID:FIRGitHubAuthProvider.id
