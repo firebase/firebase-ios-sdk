149709fb1c3f743bd084e2aba91a56b1b9ab1c31
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2023-02-15 16:48:48 -0500

    [auth-swift] Depend on FirebaseCoreExtension rather than making it a part of Auth's source (#10824)

--- FirebaseAuth/Sources/Auth/FIRAuth_Internal.h
+++ FirebaseAuth/Sources/Auth/FIRAuth_Internal.h
@@ -17,7 +17,7 @@
 #import <Foundation/Foundation.h>
 #import "FirebaseAuth/Interop/FIRAuthInterop.h"
 #import "FirebaseAuth/Sources/Public/FirebaseAuth/FIRAuth.h"
-#import "FirebaseCore/Extension/FIRLogger.h"
+@import FirebaseCoreExtension;
 
 @class FIRAuthRequestConfiguration;
 @class FIRAuthURLPresenter;
