7c531be70884a718b656cd8f9baee2a6082304a0
Tejas Deshpande <tdeshpande@google.com>
2023-02-22 16:44:28 -0500

    Fix spm issues

--- FirebaseAppDistribution/Sources/FIRAppDistribution.m
+++ FirebaseAppDistribution/Sources/FIRAppDistribution.m
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #import <GoogleUtilities/GULAppDelegateSwizzler.h>
 #import <GoogleUtilities/GULUserDefaults.h>
--- FirebaseAppDistribution/Sources/Public/FirebaseAppDistribution/FIRAppDistribution.h
+++ FirebaseAppDistribution/Sources/Public/FirebaseAppDistribution/FIRAppDistribution.h
@@ -15,6 +15,7 @@
 @class FIRAppDistributionRelease;
 @class UIApplication;
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
