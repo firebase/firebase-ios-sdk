ca3216b04661a38b56337bd963230e545347e968
Denver Coneybeare <dconeybe@google.com>
2023-02-22 11:14:57 -0500

    md5_openssl.cc: avoid compiling when openssl/md5.h is not available

--- Firestore/core/src/util/md5_openssl.cc
+++ Firestore/core/src/util/md5_openssl.cc
@@ -16,6 +16,10 @@
 
 #include "Firestore/core/src/util/md5.h"
 
+#include "Firestore/core/src/util/config.h"
+
+#if HAVE_OPENSSL_MD5_H
+
 #include "Firestore/core/src/util/hard_assert.h"
 
 #include "openssl/md5.h"
@@ -51,3 +55,5 @@ std::array<unsigned char, 16> CalculateMd5Digest(absl::string_view s) {
 }  // namespace util
 }  // namespace firestore
 }  // namespace firebase
+
+#endif  // HAVE_OPENSSL_MD5_H
