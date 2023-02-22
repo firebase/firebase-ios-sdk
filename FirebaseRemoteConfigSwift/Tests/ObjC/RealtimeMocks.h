c9314a38e3d54a2f8a28594742047d0e25a62359
Quan Pham <qdpham@google.com>
2023-02-22 11:45:50 -0800

    Add missing quote

--- FirebaseRemoteConfigSwift/Tests/ObjC/RealtimeMocks.h
+++ FirebaseRemoteConfigSwift/Tests/ObjC/RealtimeMocks.h
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 #import <Foundation/Foundation.h>
-#import "FirebaseRemoteConfig/Sources/RCNConfigRealtime.h
+#import "FirebaseRemoteConfig/Sources/RCNConfigRealtime.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
