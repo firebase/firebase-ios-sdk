c086dad7e8e82f9ca53b7e18efc517d5ef83ef97
Quan Pham <qdpham@google.com>
2023-02-22 12:39:30 -0800

    Update import

--- FirebaseRemoteConfig/Sources/Private/RCNConfigSettings.h
+++ FirebaseRemoteConfig/Sources/Private/RCNConfigSettings.h
@@ -16,7 +16,7 @@
 
 #import <Foundation/Foundation.h>
 
-#import <FirebaseRemoteConfig/FIRRemoteConfig.h>
+#import <FirebaseRemoteConfig/Sources/Public/FirebaseRemoteConfig/FIRRemoteConfig.h>
 
 @class RCNConfigDBManager;
 
