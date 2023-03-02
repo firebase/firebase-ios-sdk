cf4b78a8d1f77bb67ba3c76deace0bea494cb199
milaGGL <107142260+milaGGL@users.noreply.github.com>
2023-03-02 12:27:19 -0800

    make expected_count optional in proto

--- Firestore/core/src/remote/serializer.cc
+++ Firestore/core/src/remote/serializer.cc
@@ -637,6 +637,7 @@ google_firestore_v1_Target Serializer::EncodeTarget(
         nanopb::CopyBytesArray(target_data.resume_token().get());
 
     if (target_data.expected_count().has_value()) {
+      result.has_expected_count = true;
       int32_t expected_count = target_data.expected_count().value();
       result.expected_count.value = expected_count;
     }
