96a36e4b354aa442b98834e109f859a96686155b
Tejas Deshpande <tdeshpande@google.com>
2023-03-02 15:43:13 -0500

    Fix typo

--- FirebaseAppDistributionInternal/Tests/Unit/Bridging-Header.h
+++ FirebaseAppDistributionInternal/Tests/Unit/Bridging-Header.h
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-git status#import "FirebaseInstallations/Source/Library/FIRInstallationsAuthTokenResultInternal.h"
+#import "FirebaseInstallations/Source/Library/FIRInstallationsAuthTokenResultInternal.h"
