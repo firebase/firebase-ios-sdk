2ead71d5d62ef40f46853ee891f755e2f65f8264
Brianna Zamora <briannadoubt@icloud.com>
2023-03-06 09:39:10 -0800

    feature: Add `FavouriteFruitsAnimationView`
    This view demostrates how to use the ``FirestoreQuery`` property wrapper, using the `animation` parameter to make sure list items are animated when being added or removed.

---
+++ Example/FirestoreSample/FirestoreSample/Views/FavouriteFruitsAnimationView.swift
@@ -0,0 +1,69 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import SwiftUI
+import FirebaseFirestoreSwift
+
+/// This view demostrates how to use the ``FirestoreQuery`` property wrapper,
+/// using the `animation` parameter to make sure list items are animated when
+/// being added or removed.
+struct FavouriteFruitsAnimationView: View {
+  @FirestoreQuery(
+    collectionPath: "fruits",
+    predicates: [
+      .where("isFavourite", isEqualTo: true),
+    ],
+    animation: .default
+  ) fileprivate var fruitResults: Result<[Fruit], Error>
+
+  @State var showOnlyFavourites = true
+
+  var body: some View {
+    if case let .success(fruits) = fruitResults {
+      List(fruits) { fruit in
+        Text(fruit.name)
+      }
+      .navigationTitle("Fruits")
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          Button(action: toggleFilter) {
+            Image(systemName: showOnlyFavourites
+              ? "line.3.horizontal.decrease.circle.fill"
+              : "line.3.horizontal.decrease.circle")
+          }
+        }
+      }
+    } else if case let .failure(error) = fruitResults {
+      // Handle error
+      Text("Couldn't map data: \(error.localizedDescription)")
+    }
+  }
+
+  func toggleFilter() {
+    showOnlyFavourites.toggle()
+    if showOnlyFavourites {
+      $fruitResults.predicates = [
+        .whereField("isFavourite", isEqualTo: true),
+      ]
+    } else {
+      $fruitResults.predicates = []
+    }
+  }
+}
+
+struct FavouriteFruitsView_Previews: PreviewProvider {
+  static var previews: some View {
+    FavouriteFruitsView()
+  }
+}
