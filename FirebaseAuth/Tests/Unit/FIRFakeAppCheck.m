efd2407a36a34ff49950cff46c2272da46699805
Yvonne Cheng <yvonnecheng@google.com>
2023-03-15 15:27:28 -0700

    correct copyright info

--- FirebaseAuth/Tests/Unit/FIRFakeAppCheck.m
+++ FirebaseAuth/Tests/Unit/FIRFakeAppCheck.m
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Google
+ * Copyright 2023 Googleb LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
