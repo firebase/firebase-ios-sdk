b2383a9d3ca37b6a9e99c5fd9b75abdcf5a497c8
cherylEnkidu <96084918+cherylEnkidu@users.noreply.github.com>
2023-03-14 22:48:51 -0400

    Reorganize OR query test (#10902)
    * Reorganize OR query test
    
    * Address comments
    
    * Change message

--- Firestore/Swift/Tests/Integration/QueryIntegrationTests.swift
+++ Firestore/Swift/Tests/Integration/QueryIntegrationTests.swift
@@ -58,16 +58,8 @@ class QueryIntegrationTests: FSTIntegrationTestCase {
     checkOnlineAndOfflineQuery(collRef.whereFilter(filter1),
                                matchesResult: ["doc1", "doc2", "doc4", "doc5"])
 
-    // with one inequality: a>2 || b==1.
-    let filter2 = Filter.orFilter(
-      [Filter.whereField("a", isGreaterThan: 2),
-       Filter.whereField("b", isEqualTo: 1)]
-    )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter2),
-                               matchesResult: ["doc5", "doc2", "doc3"])
-
     // (a==1 && b==0) || (a==3 && b==2)
-    let filter3 = Filter.orFilter(
+    let filter2 = Filter.orFilter(
       [Filter.andFilter(
         [Filter.whereField("a", isEqualTo: 1),
          Filter.whereField("b", isEqualTo: 0)]
@@ -77,22 +69,22 @@ class QueryIntegrationTests: FSTIntegrationTestCase {
          Filter.whereField("b", isEqualTo: 2)]
       )]
     )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter3),
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter2),
                                matchesResult: ["doc1", "doc3"])
 
     // a==1 && (b==0 || b==3).
-    let filter4 = Filter.andFilter(
+    let filter3 = Filter.andFilter(
       [Filter.whereField("a", isEqualTo: 1),
        Filter.orFilter(
          [Filter.whereField("b", isEqualTo: 0),
           Filter.whereField("b", isEqualTo: 3)]
        )]
     )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter4),
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter3),
                                matchesResult: ["doc1", "doc4"])
 
     // (a==2 || b==2) && (a==3 || b==3)
-    let filter5 = Filter.andFilter(
+    let filter4 = Filter.andFilter(
       [Filter.orFilter(
         [Filter.whereField("a", isEqualTo: 2),
          Filter.whereField("b", isEqualTo: 2)]
@@ -102,56 +94,79 @@ class QueryIntegrationTests: FSTIntegrationTestCase {
          Filter.whereField("b", isEqualTo: 3)]
       )]
     )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter5),
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter4),
                                matchesResult: ["doc3"])
 
+    // Test with limits without orderBy (the __name__ ordering is the tie breaker).
+    let filter5 = Filter.orFilter(
+      [Filter.whereField("a", isEqualTo: 2),
+       Filter.whereField("b", isEqualTo: 1)]
+    )
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter5).limit(to: 1),
+                               matchesResult: ["doc2"])
+  }
+
+  func testOrQueriesWithCompositeIndexes() throws {
+    // TODO(orquery): Enable this test against production when possible.
+    try XCTSkipIf(!FSTIntegrationTestCase.isRunningAgainstEmulator(),
+                  "Skip this test if running against production because it results in" +
+                    "a 'missing index' error. The Firestore Emulator, however, does serve these queries.")
+
+    let collRef = collectionRef(
+      withDocuments: ["doc1": ["a": 1, "b": 0],
+                      "doc2": ["a": 2, "b": 1],
+                      "doc3": ["a": 3, "b": 2],
+                      "doc4": ["a": 1, "b": 3],
+                      "doc5": ["a": 1, "b": 1]]
+    )
+
+    // with one inequality: a>2 || b==1.
+    let filter1 = Filter.orFilter(
+      [Filter.whereField("a", isGreaterThan: 2),
+       Filter.whereField("b", isEqualTo: 1)]
+    )
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter1),
+                               matchesResult: ["doc5", "doc2", "doc3"])
+
     // Test with limits (implicit order by ASC): (a==1) || (b > 0) LIMIT 2
-    let filter6 = Filter.orFilter(
+    let filter2 = Filter.orFilter(
       [Filter.whereField("a", isEqualTo: 1),
        Filter.whereField("b", isGreaterThan: 0)]
     )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter6).limit(to: 2),
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter2).limit(to: 2),
                                matchesResult: ["doc1", "doc2"])
 
     // Test with limits (explicit order by): (a==1) || (b > 0) LIMIT_TO_LAST 2
     // Note: The public query API does not allow implicit ordering when limitToLast is used.
-    let filter7 = Filter.orFilter(
+    let filter3 = Filter.orFilter(
       [Filter.whereField("a", isEqualTo: 1),
        Filter.whereField("b", isGreaterThan: 0)]
     )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter7)
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter3)
       .limit(toLast: 2)
       .order(by: "b"),
       matchesResult: ["doc3", "doc4"])
 
     // Test with limits (explicit order by ASC): (a==2) || (b == 1) ORDER BY a LIMIT 1
-    let filter8 = Filter.orFilter(
+    let filter4 = Filter.orFilter(
       [Filter.whereField("a", isEqualTo: 2),
        Filter.whereField("b", isEqualTo: 1)]
     )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter8).limit(to: 1)
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter4).limit(to: 1)
       .order(by: "a"),
       matchesResult: ["doc5"])
 
     // Test with limits (explicit order by DESC): (a==2) || (b == 1) ORDER BY a LIMIT_TO_LAST 1
-    let filter9 = Filter.orFilter(
+    let filter5 = Filter.orFilter(
       [Filter.whereField("a", isEqualTo: 2),
        Filter.whereField("b", isEqualTo: 1)]
     )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter9).limit(toLast: 1)
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter5).limit(toLast: 1)
       .order(by: "a"),
       matchesResult: ["doc2"])
-
-    // Test with limits without orderBy (the __name__ ordering is the tie breaker).
-    let filter10 = Filter.orFilter(
-      [Filter.whereField("a", isEqualTo: 2),
-       Filter.whereField("b", isEqualTo: 1)]
-    )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter10).limit(to: 1),
-                               matchesResult: ["doc2"])
   }
 
-  func testOrQueriesWithInAndNotIn() throws {
+  func testOrQueriesWithIn() throws {
     let collRef = collectionRef(
       withDocuments: ["doc1": ["a": 1, "b": 0],
                       "doc2": ["b": 1],
@@ -162,20 +177,36 @@ class QueryIntegrationTests: FSTIntegrationTestCase {
     )
 
     // a==2 || b in [2,3]
-    let filter1 = Filter.orFilter(
+    let filter = Filter.orFilter(
       [Filter.whereField("a", isEqualTo: 2),
        Filter.whereField("b", in: [2, 3])]
     )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter1),
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter),
                                matchesResult: ["doc3", "doc4", "doc6"])
+  }
+
+  func testOrQueriesWithNotIn() throws {
+    // TODO(orquery): Enable this test against production when possible.
+    try XCTSkipIf(!FSTIntegrationTestCase.isRunningAgainstEmulator(),
+                  "Skip this test if running against production because it results in " +
+                    "a 'missing index' error. The Firestore Emulator, however, does serve these queries")
+
+    let collRef = collectionRef(
+      withDocuments: ["doc1": ["a": 1, "b": 0],
+                      "doc2": ["b": 1],
+                      "doc3": ["a": 3, "b": 2],
+                      "doc4": ["a": 1, "b": 3],
+                      "doc5": ["a": 1],
+                      "doc6": ["a": 2]]
+    )
 
     // a==2 || b not-in [2,3]
     // Has implicit orderBy b.
-    let filter2 = Filter.orFilter(
+    let filter = Filter.orFilter(
       [Filter.whereField("a", isEqualTo: 2),
        Filter.whereField("b", notIn: [2, 3])]
     )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter2),
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter),
                                matchesResult: ["doc1", "doc2"])
   }
 
@@ -207,6 +238,10 @@ class QueryIntegrationTests: FSTIntegrationTestCase {
   }
 
   func testMultipleInOps() throws {
+    // TODO(orquery): Enable this test against production when possible.
+    try XCTSkipIf(!FSTIntegrationTestCase.isRunningAgainstEmulator(),
+                  "Skip this test if running against production because it's not yet supported.")
+
     let collRef = collectionRef(
       withDocuments: ["doc1": ["a": 1, "b": 0],
                       "doc2": ["b": 1],
@@ -224,69 +259,21 @@ class QueryIntegrationTests: FSTIntegrationTestCase {
     checkOnlineAndOfflineQuery(collRef.whereFilter(filter1).order(by: "a"),
                                matchesResult: ["doc1", "doc6", "doc3"])
 
-    // Two IN operations on different fields with conjunction.
-    let filter2 = Filter.andFilter(
-      [Filter.whereField("a", in: [2, 3]),
-       Filter.whereField("b", in: [0, 2])]
-    )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter2).order(by: "a"),
-                               matchesResult: ["doc3"])
-
-    // Two IN operations on the same field.
-    // a IN [1,2,3] && a IN [0,1,4] should result in "a==1".
-    let filter3 = Filter.andFilter(
-      [Filter.whereField("a", in: [1, 2, 3]),
-       Filter.whereField("a", in: [0, 1, 4])]
-    )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter3),
-                               matchesResult: ["doc1", "doc4", "doc5"])
-
-    // a IN [2,3] && a IN [0,1,4] is never true and so the result should be an empty set.
-    let filter4 = Filter.andFilter(
-      [Filter.whereField("a", in: [2, 3]),
-       Filter.whereField("a", in: [0, 1, 4])]
-    )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter4),
-                               matchesResult: [])
-
+    // Two IN operations on same fields with disjunction.
     // a IN [0,3] || a IN [0,2] should union them (similar to: a IN [0,2,3]).
-    let filter5 = Filter.orFilter(
+    let filter2 = Filter.orFilter(
       [Filter.whereField("a", in: [0, 3]),
        Filter.whereField("a", in: [0, 2])]
     )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter5),
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter2),
                                matchesResult: ["doc3", "doc6"])
-
-    // Nested composite filter on the same field.
-    let filter6 = Filter.andFilter(
-      [Filter.whereField("a", in: [1, 3]),
-       Filter.orFilter(
-         [Filter.whereField("a", in: [0, 2]),
-          Filter.andFilter(
-            [Filter.whereField("b", isGreaterOrEqualTo: 1),
-             Filter.whereField("a", in: [1, 3])]
-          )]
-       )]
-    )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter6),
-                               matchesResult: ["doc3", "doc4"])
-
-    // Nested composite filter on different fields.
-    let filter7 = Filter.andFilter(
-      [Filter.whereField("b", in: [0, 3]),
-       Filter.orFilter(
-         [Filter.whereField("b", in: [1]),
-          Filter.andFilter(
-            [Filter.whereField("b", in: [2, 3]),
-             Filter.whereField("a", in: [1, 3])]
-          )]
-       )]
-    )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter7),
-                               matchesResult: ["doc4"])
   }
 
-  func testUseInWithArrayContainsAny() throws {
+  func testUsingInWithArrayContainsAny() throws {
+    // TODO(orquery): Enable this test against production when possible.
+    try XCTSkipIf(!FSTIntegrationTestCase.isRunningAgainstEmulator(),
+                  "Skip this test if running against production because it's not yet supported.")
+
     let collRef = collectionRef(
       withDocuments: ["doc1": ["a": 1, "b": [0]],
                       "doc2": ["b": [1]],
@@ -303,32 +290,15 @@ class QueryIntegrationTests: FSTIntegrationTestCase {
     checkOnlineAndOfflineQuery(collRef.whereFilter(filter1),
                                matchesResult: ["doc1", "doc3", "doc4", "doc6"])
 
-    let filter2 = Filter.andFilter(
-      [Filter.whereField("a", in: [2, 3]),
-       Filter.whereField("b", arrayContainsAny: [0, 7])]
-    )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter2),
-                               matchesResult: ["doc3"])
-
-    let filter3 = Filter.orFilter(
+    let filter2 = Filter.orFilter(
       [Filter.andFilter(
         [Filter.whereField("a", in: [2, 3]),
          Filter.whereField("c", isEqualTo: 10)]
       ),
       Filter.whereField("b", arrayContainsAny: [0, 7])]
     )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter3),
+    checkOnlineAndOfflineQuery(collRef.whereFilter(filter2),
                                matchesResult: ["doc1", "doc3", "doc4"])
-
-    let filter4 = Filter.andFilter(
-      [Filter.whereField("a", in: [2, 3]),
-       Filter.orFilter(
-         [Filter.whereField("b", arrayContainsAny: [0, 7]),
-          Filter.whereField("c", isEqualTo: 20)]
-       )]
-    )
-    checkOnlineAndOfflineQuery(collRef.whereFilter(filter4),
-                               matchesResult: ["doc3", "doc6"])
   }
 
   func testUseInWithArrayContains() throws {
@@ -377,6 +347,10 @@ class QueryIntegrationTests: FSTIntegrationTestCase {
   }
 
   func testOrderByEquality() throws {
+    // TODO(orquery): Enable this test against production when possible.
+    try XCTSkipIf(!FSTIntegrationTestCase.isRunningAgainstEmulator(),
+                  "Skip this test if running against production because order-by-equality is not supported yet.")
+
     let collRef = collectionRef(
       withDocuments: ["doc1": ["a": 1, "b": [0]],
                       "doc2": ["b": [1]],
