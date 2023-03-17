b4f031d762573ffe20aac3459e43a97b205229f1
Tejas Deshpande <tdeshpande@google.com>
2023-03-17 10:52:25 -0400

    Initial scroll view nesting + programmatic constraints

--- FirebaseAppDistributionInternal/Sources/FeedbackViewController.swift
+++ FirebaseAppDistributionInternal/Sources/FeedbackViewController.swift
@@ -27,15 +27,23 @@ class FeedbackViewController: UIViewController {
   @IBOutlet var additionalFormTextLabel: UILabel!
   @IBOutlet var feedbackTextView: UITextView!
   @IBOutlet var navigationBar: UINavigationBar!
-
+  @IBOutlet weak var scrollView: UIScrollView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    feedbackTextView.isScrollEnabled = false
+    setScrollViewConstraints()
+    setAdditionalFormTextConstraints()
+    setFeedbackInputConstraints()
+    setScreenshotImageConstrains()
 
     let additionalFormText = additionalFormText
     if additionalFormText != nil {
       additionalFormTextLabel.text = additionalFormText
     }
+    // TODO: Consider default text.
 
     let image = image
     if image != nil {
@@ -103,4 +111,56 @@ class FeedbackViewController: UIViewController {
     // TODO(tundeagboola) show success toast
     dismiss(animated: true)
   }
+  
+  // MARK: - UI Constraints
+  
+  func setScrollViewConstraints() {
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.alwaysBounceVertical = true
+    let bottomConstraint = NSLayoutConstraint(item: scrollView!, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: 0)
+    let leftConstraint = NSLayoutConstraint(item: scrollView!, attribute: .left, relatedBy: .equal, toItem: navigationBar, attribute: .left, multiplier: 1, constant: 0)
+    let rightConstraint = NSLayoutConstraint(item: scrollView!, attribute: .right, relatedBy: .equal, toItem: navigationBar, attribute: .right, multiplier: 1, constant: 0)
+    self.view.addConstraints([bottomConstraint, leftConstraint, rightConstraint])
+  }
+  
+  func setAdditionalFormTextConstraints() {
+    additionalFormTextLabel.translatesAutoresizingMaskIntoConstraints = false
+    additionalFormTextLabel.numberOfLines = 0
+    
+    let topConstraint = NSLayoutConstraint(item: additionalFormTextLabel!, attribute: .top, relatedBy: .equal, toItem: scrollView, attribute: .top, multiplier: 1, constant: 0)
+    let bottomConstraint = NSLayoutConstraint(item: additionalFormTextLabel!, attribute: .bottom, relatedBy: .greaterThanOrEqual, toItem: scrollView, attribute: .top, multiplier: 1, constant: 80)
+    let leftConstraint = NSLayoutConstraint(item: additionalFormTextLabel!, attribute: .left, relatedBy: .equal, toItem: scrollView, attribute: .left, multiplier: 1, constant: 0)
+    let rightConstraint = NSLayoutConstraint(item: additionalFormTextLabel!, attribute: .right, relatedBy: .equal, toItem: scrollView, attribute: .right, multiplier: 1, constant: 0)
+    scrollView.addConstraints([topConstraint, bottomConstraint, leftConstraint, rightConstraint])
+    let widthConstraint = NSLayoutConstraint(item: additionalFormTextLabel!, attribute: .width, relatedBy: .equal, toItem: navigationBar, attribute: .width, multiplier: 1, constant: 0)
+    self.view.addConstraints([topConstraint, bottomConstraint, leftConstraint, rightConstraint, widthConstraint])
+    
+    
+    additionalFormTextLabel.layoutMargins = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
+    // TODO: Better color
+    additionalFormTextLabel.backgroundColor = .lightGray
+  }
+  
+  func setFeedbackInputConstraints() {
+    feedbackTextView.translatesAutoresizingMaskIntoConstraints = false
+    let topConstraint = NSLayoutConstraint(item: feedbackTextView!, attribute: .top, relatedBy: .equal, toItem: additionalFormTextLabel, attribute: .bottom, multiplier: 1, constant: 0)
+    let bottomConstraint = NSLayoutConstraint(item: feedbackTextView!, attribute: .bottom, relatedBy: .greaterThanOrEqual, toItem: additionalFormTextLabel, attribute: .top, multiplier: 1, constant: 160)
+    let leftConstraint = NSLayoutConstraint(item: feedbackTextView!, attribute: .left, relatedBy: .equal, toItem: scrollView, attribute: .left, multiplier: 1, constant: 0)
+    let rightConstraint = NSLayoutConstraint(item: feedbackTextView!, attribute: .right, relatedBy: .equal, toItem: scrollView, attribute: .right, multiplier: 1, constant: 0)
+    let widthConstraint = NSLayoutConstraint(item: feedbackTextView!, attribute: .width, relatedBy: .equal, toItem: navigationBar, attribute: .width, multiplier: 1, constant: 0)
+    self.view.addConstraints([topConstraint, bottomConstraint, leftConstraint, rightConstraint, widthConstraint])
+    
+    feedbackTextView.layoutMargins = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
+  }
+  
+  func setScreenshotImageConstrains() {
+    screenshotUIImageView.translatesAutoresizingMaskIntoConstraints = false
+    let topConstraint = NSLayoutConstraint(item: screenshotUIImageView!, attribute: .top, relatedBy: .equal, toItem: feedbackTextView, attribute: .bottom, multiplier: 1, constant: 0)
+    let bottomConstraint = NSLayoutConstraint(item: screenshotUIImageView!, attribute: .bottom, relatedBy: .greaterThanOrEqual, toItem: feedbackTextView, attribute: .top, multiplier: 1, constant: 40)
+    let leftConstraint = NSLayoutConstraint(item: screenshotUIImageView!, attribute: .left, relatedBy: .equal, toItem: scrollView, attribute: .left, multiplier: 1, constant: 0)
+    let rightConstraint = NSLayoutConstraint(item: screenshotUIImageView!, attribute: .right, relatedBy: .equal, toItem: scrollView, attribute: .right, multiplier: 1, constant: 0)
+    let widthConstraint = NSLayoutConstraint(item: screenshotUIImageView!, attribute: .width, relatedBy: .equal, toItem: feedbackTextView, attribute: .width, multiplier: 1, constant: 0)
+    scrollView.addConstraints([topConstraint, bottomConstraint, leftConstraint, rightConstraint, widthConstraint])
+  }
+  
 }
