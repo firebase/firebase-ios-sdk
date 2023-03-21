1b06e1f463dee54ea058a4b05ea2cfe63c1b24ea
Tejas Deshpande <tdeshpande@google.com>
2023-03-21 11:55:58 -0400

    try different insets

--- FirebaseAppDistributionInternal/Sources/Internal/AdditionalFormTextLabel.swift
+++ FirebaseAppDistributionInternal/Sources/Internal/AdditionalFormTextLabel.swift
@@ -8,18 +8,10 @@
 import UIKit
 
 class AdditionalFormTextLabel: UILabel {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-  var topInset = 5.0
-  var bottomInset = 10.0
-  var leftInset = 5.0
-  var rightInset = 5.0
+  var topInset = 10.0
+  var bottomInset = 15.0
+  var leftInset = 10.0
+  var rightInset = 10.0
   
   override func drawText(in rect: CGRect) {
     let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
