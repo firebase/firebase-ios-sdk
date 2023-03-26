48c10c1c11697d76898ff7433c91fbcf02e2c56d
Paul Beusterien <paulbeusterien@google.com>
2023-03-26 08:23:34 -0700

    style

--- FirebaseAuth/Sources/Swift/Utilities/AuthWebView.swift
+++ FirebaseAuth/Sources/Swift/Utilities/AuthWebView.swift
@@ -12,51 +12,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #if os(iOS)
 
-import UIKit
-import WebKit
+  import UIKit
+  import WebKit
 
-@objc(FIRAuthWebView) public class AuthWebView: UIView {
-
+  @objc(FIRAuthWebView) public class AuthWebView: UIView {
     public lazy var webView: WKWebView = createWebView()
     public lazy var spinner: UIActivityIndicatorView = createSpinner()
 
     override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.backgroundColor = .white
-        self.initializeSubviews()
+      super.init(frame: frame)
+      backgroundColor = .white
+      initializeSubviews()
     }
 
+    @available(*, unavailable)
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+      fatalError("init(coder:) has not been implemented")
     }
 
     private func initializeSubviews() {
-        let webView = self.createWebView()
-        let spinner = self.createSpinner()
+      let webView = createWebView()
+      let spinner = createSpinner()
 
-        // The order of the following controls z-order.
-        self.addSubview(webView)
-        self.addSubview(spinner)
+      // The order of the following controls z-order.
+      addSubview(webView)
+      addSubview(spinner)
 
-        self.layoutSubviews()
-        self.webView = webView
-        self.spinner = spinner
+      layoutSubviews()
+      self.webView = webView
+      self.spinner = spinner
     }
 
-  // TODO: Should not be public
+    // TODO: Should not be public
 
-  public override func layoutSubviews() {
-        super.layoutSubviews()
-        let height = self.bounds.size.height
-        let width = self.bounds.size.width
-        self.webView.frame = CGRect(x: 0, y: 0, width: width, height: height)
-        self.spinner.center = self.webView.center
+    override public func layoutSubviews() {
+      super.layoutSubviews()
+      let height = bounds.size.height
+      let width = bounds.size.width
+      webView.frame = CGRect(x: 0, y: 0, width: width, height: height)
+      spinner.center = webView.center
     }
 
-  private func createWebView() -> WKWebView {
+    private func createWebView() -> WKWebView {
       let webView = WKWebView(frame: .zero)
       // Trickery to make the web view not do weird things (like showing a black background when
       // the prompt in the navigation bar animates changes.)
@@ -68,24 +67,24 @@ import WebKit
       webView.scrollView.alwaysBounceVertical = false
       webView.scrollView.alwaysBounceHorizontal = false
       return webView
-  }
+    }
 
-  private func createSpinner() -> UIActivityIndicatorView {
+    private func createSpinner() -> UIActivityIndicatorView {
       var spinnerStyle: UIActivityIndicatorView.Style = .gray
       #if targetEnvironment(macCatalyst)
-      if #available(iOS 13.0, *) {
+        if #available(iOS 13.0, *) {
           spinnerStyle = .medium
-      } else {
+        } else {
           // iOS 13 deprecation
-  //            #pragma clang diagnostic push
-  //            #pragma clang diagnostic ignored "-Wdeprecated-declarations"
+          //            #pragma clang diagnostic push
+          //            #pragma clang diagnostic ignored "-Wdeprecated-declarations"
           spinnerStyle = .gray
-  //            #pragma clang diagnostic pop
-      }
+          //            #pragma clang diagnostic pop
+        }
       #endif
       let spinner = UIActivityIndicatorView(style: spinnerStyle)
       return spinner
+    }
   }
-}
 
 #endif
