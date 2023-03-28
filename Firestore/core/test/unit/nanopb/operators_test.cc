fd93e96866e89e2ae1421128445c8406d3f52a22
Denver Coneybeare <dconeybe@google.com>
2023-03-28 11:55:12 -0400

    operators_test.cc: add tests for google_firestore_v1_BloomFilter and more refactoring

--- Firestore/core/test/unit/nanopb/operators_test.cc
+++ Firestore/core/test/unit/nanopb/operators_test.cc
@@ -25,18 +25,41 @@ namespace firebase {
 namespace firestore {
 namespace {
 
-using nanopb::MakeBytesArray;
-using nanopb::Message;
+/**
+ * Base class for test fixtures that test the operator overloads of proto types.
+ *
+ * @tparam RawProtoT The raw proto struct whose operator(s) are being tested,
+ * such as `google_firestore_v1_BitSequence`.
+ */
+template <typename RawProtoT>
+class BaseOperatorsTest : public ::testing::Test {
+ protected:
+  static void TestOperatorEquals(bool expected_result,
+                                 nanopb::Message<RawProtoT> message1,
+                                 nanopb::Message<RawProtoT> message2) {
+    const RawProtoT& proto1 = *message1;
+    const RawProtoT& proto2 = *message2;
+    EXPECT_EQ(proto1 == proto2, expected_result);
+    EXPECT_NE(proto1 != proto2, expected_result);
+  }
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 // Tests for operator==() for google_firestore_v1_BitSequence
 ////////////////////////////////////////////////////////////////////////////////
 
-class NanopbOperatorsTest_BitSequence : public ::testing::Test {
+class NanopbOperatorsTest_BitSequence
+    : public BaseOperatorsTest<google_firestore_v1_BitSequence> {
  public:
+  static constexpr int32_t kSamplePadding = 567;
+  static constexpr int32_t kDifferentSamplePadding = 765;
+  static const absl::optional<std::vector<uint8_t>> kNullBitmap;
+  static const std::vector<uint8_t> kSampleBitmap;
+  static const std::vector<uint8_t> kDifferentSampleBitmap;
+
   // Stores the `padding` and `bitmap` of a `google_firestore_v1_BitSequence`
   // proto, and enables creating `Message` objects from them.
-  class ProtoFieldValues {
+  class ProtoFieldValues final {
    public:
     ProtoFieldValues(int32_t padding,
                      const absl::optional<std::vector<uint8_t>>& bitmap)
@@ -48,13 +71,31 @@ class NanopbOperatorsTest_BitSequence : public ::testing::Test {
                            absl::optional<std::vector<uint8_t>>(bitmap)) {
     }
 
-    Message<google_firestore_v1_BitSequence> CreateProto() const {
-      Message<google_firestore_v1_BitSequence> proto;
-      proto->padding = padding_;
-      if (!bitmap_.has_value()) {
-        proto->bitmap = nullptr;
-      } else {
-        proto->bitmap = MakeBytesArray(bitmap_.value());
+    ProtoFieldValues(const absl::optional<std::vector<uint8_t>>& bitmap)
+        : ProtoFieldValues(kSamplePadding, bitmap) {
+    }
+
+    ProtoFieldValues(const std::vector<uint8_t>& bitmap)
+        : ProtoFieldValues(kSamplePadding, bitmap) {
+    }
+
+    ProtoFieldValues(int32_t padding)
+        : ProtoFieldValues(padding, kSampleBitmap) {
+    }
+
+    nanopb::Message<google_firestore_v1_BitSequence> CreateMessage() const {
+      return nanopb::Message<google_firestore_v1_BitSequence>(CreateProto());
+    }
+
+    // The caller assumes ownership of the returned object, and is, therefore,
+    // responsible for freeing any dynamically-allocated memory that it
+    // references, such as memory allocated by `nanopb::MakeBytesArray()`.
+    google_firestore_v1_BitSequence CreateProto() const {
+      google_firestore_v1_BitSequence proto =
+          google_firestore_v1_BitSequence_init_zero;
+      proto.padding = padding_;
+      if (bitmap_.has_value()) {
+        proto.bitmap = nanopb::MakeBytesArray(bitmap_.value());
       }
       return proto;
     }
@@ -66,50 +107,183 @@ class NanopbOperatorsTest_BitSequence : public ::testing::Test {
 
  protected:
   static void TestOperatorEquals(bool expected_result,
-                                 ProtoFieldValues lhs,
-                                 ProtoFieldValues rhs) {
-    const Message<google_firestore_v1_BitSequence> message1 = lhs.CreateProto();
-    const Message<google_firestore_v1_BitSequence> message2 = rhs.CreateProto();
-    const google_firestore_v1_BitSequence& proto1 = *message1;
-    const google_firestore_v1_BitSequence& proto2 = *message2;
-    EXPECT_EQ(proto1 == proto2, expected_result);
-    EXPECT_NE(proto1 != proto2, expected_result);
+                                 const ProtoFieldValues& values1,
+                                 const ProtoFieldValues& values2) {
+    return BaseOperatorsTest::TestOperatorEquals(
+        expected_result, values1.CreateMessage(), values2.CreateMessage());
   }
 };
 
+const absl::optional<std::vector<uint8_t>>
+    NanopbOperatorsTest_BitSequence::kNullBitmap(absl::nullopt);
+const std::vector<uint8_t> NanopbOperatorsTest_BitSequence::kSampleBitmap(
+    {100, 101, 102, 103});
+const std::vector<uint8_t>
+    NanopbOperatorsTest_BitSequence::kDifferentSampleBitmap({200, 201, 202,
+                                                             203});
+
 TEST_F(NanopbOperatorsTest_BitSequence,
        EqualsShouldReturnTrueIfBothMessagesHaveSamePaddingAndNullBitmap) {
-  TestOperatorEquals(true, {789, absl::nullopt}, {789, absl::nullopt});
+  TestOperatorEquals(true, {kSamplePadding, kNullBitmap},
+                     {kSamplePadding, kNullBitmap});
 }
 
 TEST_F(NanopbOperatorsTest_BitSequence,
        EqualsShouldReturnTrueIfBothMessagesHaveSamePaddingAndBitmap) {
-  TestOperatorEquals(true, {789, {1, 2, 3, 4}}, {789, {1, 2, 3, 4}});
+  TestOperatorEquals(true, {kSamplePadding, kSampleBitmap},
+                     {kSamplePadding, kSampleBitmap});
 }
 
 TEST_F(NanopbOperatorsTest_BitSequence,
-       EqualsShouldReturnFalseIfDifferentPadding) {
-  TestOperatorEquals(false, {789, absl::nullopt}, {987, absl::nullopt});
+       EqualsShouldReturnFalseIfMessageHaveDifferentPadding) {
+  TestOperatorEquals(false, {kSamplePadding}, {kDifferentSamplePadding});
+}
+
+TEST_F(
+    NanopbOperatorsTest_BitSequence,
+    EqualsShouldReturnFalseIfMessage1HasNonNullBitmapButMessage2HasNullBitmap) {
+  TestOperatorEquals(false, {kSampleBitmap}, {kNullBitmap});
+}
+
+TEST_F(
+    NanopbOperatorsTest_BitSequence,
+    EqualsShouldReturnFalseIfMessage1HasNullBitmapButMessage2HasNonNullBitmap) {
+  TestOperatorEquals(false, {kNullBitmap}, {kSampleBitmap});
 }
 
 TEST_F(NanopbOperatorsTest_BitSequence,
-       EqualsShouldReturnFalseIfDifferentBitmapLength) {
-  TestOperatorEquals(false, {789, {1, 2, 3, 4, 5}}, {789, {1, 2, 3}});
+       EqualsShouldReturnFalseMessagesHaveSameSizeBitmapsButDifferentValues) {
+  TestOperatorEquals(false, {{1, 2, 3, 4}}, {{4, 3, 2, 1}});
 }
 
 TEST_F(NanopbOperatorsTest_BitSequence,
-       EqualsShouldReturnFalseIfSameBitmapLengthButDifferentValues) {
-  TestOperatorEquals(false, {789, {1, 2, 3, 4}}, {789, {4, 3, 2, 1}});
+       EqualsShouldReturnFalseMessagesHaveDifferentSizeBitmaps) {
+  TestOperatorEquals(false, {{1, 2, 3}}, {{1, 2, 3, 4}});
 }
 
-TEST_F(NanopbOperatorsTest_BitSequence,
-       EqualsShouldReturnFalseIfArg1HasNullBitmapAndArg2HasNonNullBitmap) {
-  TestOperatorEquals(false, {789, absl::nullopt}, {789, {4, 3, 2, 1}});
+////////////////////////////////////////////////////////////////////////////////
+// Tests for operator==() for google_firestore_v1_BloomFilter
+////////////////////////////////////////////////////////////////////////////////
+
+class NanopbOperatorsTest_BloomFilter
+    : public BaseOperatorsTest<google_firestore_v1_BloomFilter> {
+ public:
+  static constexpr int32_t kSampleHashCount = 17;
+  static constexpr int32_t kDifferentSampleHashCount = 71;
+  static const absl::optional<NanopbOperatorsTest_BitSequence::ProtoFieldValues>
+      kNoBits;
+  static const NanopbOperatorsTest_BitSequence::ProtoFieldValues kSampleBits;
+  static const NanopbOperatorsTest_BitSequence::ProtoFieldValues
+      kDifferentSampleBits;
+
+  // Stores the `hash_count` and `bits` of a `google_firestore_v1_BloomFilter`
+  // proto, and enables creating `Message` objects from them.
+  class ProtoFieldValues final {
+   public:
+    ProtoFieldValues(
+        int32_t hash_count,
+        const absl::optional<NanopbOperatorsTest_BitSequence::ProtoFieldValues>&
+            bits)
+        : hash_count_(hash_count), bits_(bits) {
+    }
+
+    ProtoFieldValues(
+        int32_t hash_count,
+        const NanopbOperatorsTest_BitSequence::ProtoFieldValues& bits)
+        : ProtoFieldValues(
+              hash_count,
+              absl::optional<NanopbOperatorsTest_BitSequence::ProtoFieldValues>(
+                  bits)) {
+    }
+
+    ProtoFieldValues(
+        const absl::optional<NanopbOperatorsTest_BitSequence::ProtoFieldValues>&
+            bits)
+        : ProtoFieldValues(kSampleHashCount, bits) {
+    }
+
+    ProtoFieldValues(
+        const NanopbOperatorsTest_BitSequence::ProtoFieldValues& bits)
+        : ProtoFieldValues(kSampleHashCount, bits) {
+    }
+
+    ProtoFieldValues(int32_t hash_count)
+        : ProtoFieldValues(hash_count, kSampleBits) {
+    }
+
+    nanopb::Message<google_firestore_v1_BloomFilter> CreateMessage() const {
+      return nanopb::Message<google_firestore_v1_BloomFilter>(CreateProto());
+    }
+
+    // The caller assumes ownership of the returned object, and is, therefore,
+    // responsible for freeing any dynamically-allocated memory that it
+    // references, such as memory allocated by `nanopb::MakeBytesArray()`.
+    google_firestore_v1_BloomFilter CreateProto() const {
+      google_firestore_v1_BloomFilter proto =
+          google_firestore_v1_BloomFilter_init_zero;
+      proto.hash_count = hash_count_;
+      if (bits_.has_value()) {
+        proto.bits = bits_->CreateProto();
+        proto.has_bits = true;
+      }
+      return proto;
+    }
+
+   private:
+    int32_t hash_count_;
+    absl::optional<NanopbOperatorsTest_BitSequence::ProtoFieldValues> bits_;
+  };
+
+ protected:
+  static void TestOperatorEquals(bool expected_result,
+                                 ProtoFieldValues values1,
+                                 ProtoFieldValues values2) {
+    return BaseOperatorsTest::TestOperatorEquals(
+        expected_result, values1.CreateMessage(), values2.CreateMessage());
+  }
+};
+
+const absl::optional<NanopbOperatorsTest_BitSequence::ProtoFieldValues>
+    NanopbOperatorsTest_BloomFilter::kNoBits(absl::nullopt);
+const NanopbOperatorsTest_BitSequence::ProtoFieldValues
+    NanopbOperatorsTest_BloomFilter::kSampleBits(
+        NanopbOperatorsTest_BitSequence::kSamplePadding,
+        NanopbOperatorsTest_BitSequence::kSampleBitmap);
+const NanopbOperatorsTest_BitSequence::ProtoFieldValues
+    NanopbOperatorsTest_BloomFilter::kDifferentSampleBits(
+        NanopbOperatorsTest_BitSequence::kDifferentSamplePadding,
+        NanopbOperatorsTest_BitSequence::kDifferentSampleBitmap);
+
+TEST_F(NanopbOperatorsTest_BloomFilter,
+       EqualsShouldReturnTrueIfBothMessagesHaveSameHashCountAndNoBits) {
+  TestOperatorEquals(true, {kSampleHashCount, kNoBits},
+                     {kSampleHashCount, kNoBits});
 }
 
-TEST_F(NanopbOperatorsTest_BitSequence,
-       EqualsShouldReturnFalseIfArg1HasNonNullBitmapAndArg2HasNullBitmap) {
-  TestOperatorEquals(false, {789, {4, 3, 2, 1}}, {789, absl::nullopt});
+TEST_F(NanopbOperatorsTest_BloomFilter,
+       EqualsShouldReturnTrueIfBothMessagesHaveSameHashCountAndBits) {
+  TestOperatorEquals(true, {kSampleHashCount, kSampleBits},
+                     {kSampleHashCount, kSampleBits});
+}
+
+TEST_F(NanopbOperatorsTest_BloomFilter,
+       EqualsShouldReturnFalseIfMessageHaveDifferentHashCount) {
+  TestOperatorEquals(false, {kSampleHashCount}, {kDifferentSampleHashCount});
+}
+
+TEST_F(NanopbOperatorsTest_BloomFilter,
+       EqualsShouldReturnFalseIfMessage1HasBitsButMessage2DoesNotHaveBits) {
+  TestOperatorEquals(false, {kSampleBits}, {kNoBits});
+}
+
+TEST_F(NanopbOperatorsTest_BloomFilter,
+       EqualsShouldReturnFalseIfMessage1DoesNotHavBitsButMessage2HasBits) {
+  TestOperatorEquals(false, {kNoBits}, {kSampleBits});
+}
+
+TEST_F(NanopbOperatorsTest_BloomFilter,
+       EqualsShouldReturnFalseIfMessagesHaveDifferentBits) {
+  TestOperatorEquals(false, {kSampleBits}, {kDifferentSampleBits});
 }
 
 }  //  namespace
