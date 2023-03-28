ca7e73c19089df24f2e35f28228b440625751dd6
Denver Coneybeare <dconeybe@google.com>
2023-03-28 10:38:09 -0400

    operators_test.cc: refactor for improved modularity and readability by adding `NanopbOperatorsTest_BitSequence` fixture class and a `TestOperatorEquals()` method, which makes each TEST_F() function a single line.

--- Firestore/core/test/unit/nanopb/operators_test.cc
+++ Firestore/core/test/unit/nanopb/operators_test.cc
@@ -18,6 +18,7 @@
 
 #include "Firestore/core/src/nanopb/message.h"
 #include "Firestore/core/src/nanopb/nanopb_util.h"
+#include "absl/types/optional.h"
 #include "gtest/gtest.h"
 
 namespace firebase {
@@ -27,122 +28,89 @@ namespace {
 using nanopb::MakeBytesArray;
 using nanopb::Message;
 
-TEST(
-    OperatorsTest,
-    // NOLINTNEXTLINE(whitespace/line_length)
-    BitSequenceOperatorEqualsShouldReturnTrueIfBothArgsHaveSamePaddingAndNullBitmap) {
-  Message<google_firestore_v1_BitSequence> bit_sequence1;
-  Message<google_firestore_v1_BitSequence> bit_sequence2;
-  bit_sequence1->padding = 789;
-  bit_sequence2->padding = 789;
-  bit_sequence1->bitmap = nullptr;
-  bit_sequence2->bitmap = nullptr;
+////////////////////////////////////////////////////////////////////////////////
+// Tests for operator==() for google_firestore_v1_BitSequence
+////////////////////////////////////////////////////////////////////////////////
 
-  google_firestore_v1_BitSequence proto1 = *bit_sequence1;
-  google_firestore_v1_BitSequence proto2 = *bit_sequence2;
-  EXPECT_TRUE(proto1 == proto2);
-  EXPECT_FALSE(proto1 != proto2);
+class NanopbOperatorsTest_BitSequence : public ::testing::Test {
+ public:
+  // Stores the `padding` and `bitmap` of a `google_firestore_v1_BitSequence`
+  // proto, and enables creating `Message` objects from them.
+  class ProtoFieldValues {
+   public:
+    ProtoFieldValues(int32_t padding,
+                     const absl::optional<std::vector<uint8_t>>& bitmap)
+        : padding_(padding), bitmap_(bitmap) {
+    }
+
+    ProtoFieldValues(int32_t padding, const std::vector<uint8_t>& bitmap)
+        : ProtoFieldValues(padding,
+                           absl::optional<std::vector<uint8_t>>(bitmap)) {
+    }
+
+    Message<google_firestore_v1_BitSequence> CreateProto() const {
+      Message<google_firestore_v1_BitSequence> proto;
+      proto->padding = padding_;
+      if (!bitmap_.has_value()) {
+        proto->bitmap = nullptr;
+      } else {
+        proto->bitmap = MakeBytesArray(bitmap_.value());
+      }
+      return proto;
+    }
+
+   private:
+    int32_t padding_;
+    absl::optional<std::vector<uint8_t>> bitmap_;
+  };
+
+ protected:
+  static void TestOperatorEquals(bool expected_result,
+                                 ProtoFieldValues lhs,
+                                 ProtoFieldValues rhs) {
+    const Message<google_firestore_v1_BitSequence> message1 = lhs.CreateProto();
+    const Message<google_firestore_v1_BitSequence> message2 = rhs.CreateProto();
+    const google_firestore_v1_BitSequence& proto1 = *message1;
+    const google_firestore_v1_BitSequence& proto2 = *message2;
+    EXPECT_EQ(proto1 == proto2, expected_result);
+    EXPECT_NE(proto1 != proto2, expected_result);
+  }
+};
+
+TEST_F(NanopbOperatorsTest_BitSequence,
+       EqualsShouldReturnTrueIfBothMessagesHaveSamePaddingAndNullBitmap) {
+  TestOperatorEquals(true, {789, absl::nullopt}, {789, absl::nullopt});
 }
 
-TEST(
-    OperatorsTest,
-    // NOLINTNEXTLINE(whitespace/line_length)
-    BitSequenceOperatorEqualsShouldReturnTrueIfBothArgsHaveSamePaddingAndBitmap) {
-  Message<google_firestore_v1_BitSequence> bit_sequence1;
-  Message<google_firestore_v1_BitSequence> bit_sequence2;
-  bit_sequence1->padding = 789;
-  bit_sequence2->padding = 789;
-  bit_sequence1->bitmap = MakeBytesArray(std::vector<uint8_t>{1, 2, 3, 4});
-  bit_sequence2->bitmap = MakeBytesArray(std::vector<uint8_t>{1, 2, 3, 4});
-
-  google_firestore_v1_BitSequence proto1 = *bit_sequence1;
-  google_firestore_v1_BitSequence proto2 = *bit_sequence2;
-  EXPECT_TRUE(proto1 == proto2);
-  EXPECT_FALSE(proto1 != proto2);
+TEST_F(NanopbOperatorsTest_BitSequence,
+       EqualsShouldReturnTrueIfBothMessagesHaveSamePaddingAndBitmap) {
+  TestOperatorEquals(true, {789, {1, 2, 3, 4}}, {789, {1, 2, 3, 4}});
 }
 
-TEST(OperatorsTest,
-     BitSequenceOperatorEqualsShouldReturnFalseIfDifferentPadding) {
-  Message<google_firestore_v1_BitSequence> bit_sequence1;
-  Message<google_firestore_v1_BitSequence> bit_sequence2;
-  bit_sequence1->padding = 789;
-  bit_sequence2->padding = 987;
-  bit_sequence1->bitmap = nullptr;
-  bit_sequence2->bitmap = nullptr;
-
-  google_firestore_v1_BitSequence proto1 = *bit_sequence1;
-  google_firestore_v1_BitSequence proto2 = *bit_sequence2;
-  EXPECT_FALSE(proto1 == proto2);
-  EXPECT_TRUE(proto1 != proto2);
+TEST_F(NanopbOperatorsTest_BitSequence,
+       EqualsShouldReturnFalseIfDifferentPadding) {
+  TestOperatorEquals(false, {789, absl::nullopt}, {987, absl::nullopt});
 }
 
-TEST(OperatorsTest,
-     BitSequenceOperatorEqualsShouldReturnFalseIfDifferentBitmapLength) {
-  Message<google_firestore_v1_BitSequence> bit_sequence1;
-  Message<google_firestore_v1_BitSequence> bit_sequence2;
-  bit_sequence1->padding = 789;
-  bit_sequence2->padding = 789;
-  bit_sequence1->bitmap = MakeBytesArray(std::vector<uint8_t>{1, 2, 3, 4, 5});
-  bit_sequence2->bitmap = MakeBytesArray(std::vector<uint8_t>{1, 2, 3});
-
-  google_firestore_v1_BitSequence proto1 = *bit_sequence1;
-  google_firestore_v1_BitSequence proto2 = *bit_sequence2;
-  EXPECT_FALSE(proto1 == proto2);
-  EXPECT_TRUE(proto1 != proto2);
+TEST_F(NanopbOperatorsTest_BitSequence,
+       EqualsShouldReturnFalseIfDifferentBitmapLength) {
+  TestOperatorEquals(false, {789, {1, 2, 3, 4, 5}}, {789, {1, 2, 3}});
 }
 
-TEST(
-    OperatorsTest,
-    // NOLINTNEXTLINE(whitespace/line_length)
-    BitSequenceOperatorEqualsShouldReturnFalseIfSameBitmapLengthButDifferentValues) {
-  Message<google_firestore_v1_BitSequence> bit_sequence1;
-  Message<google_firestore_v1_BitSequence> bit_sequence2;
-  bit_sequence1->padding = 789;
-  bit_sequence2->padding = 789;
-  bit_sequence1->bitmap = MakeBytesArray(std::vector<uint8_t>{1, 2, 3});
-  bit_sequence2->bitmap = MakeBytesArray(std::vector<uint8_t>{3, 2, 1});
-
-  google_firestore_v1_BitSequence proto1 = *bit_sequence1;
-  google_firestore_v1_BitSequence proto2 = *bit_sequence2;
-  EXPECT_FALSE(proto1 == proto2);
-  EXPECT_TRUE(proto1 != proto2);
+TEST_F(NanopbOperatorsTest_BitSequence,
+       EqualsShouldReturnFalseIfSameBitmapLengthButDifferentValues) {
+  TestOperatorEquals(false, {789, {1, 2, 3, 4}}, {789, {4, 3, 2, 1}});
 }
 
-TEST(
-    OperatorsTest,
-    // NOLINTNEXTLINE(whitespace/line_length)
-    BitSequenceOperatorEqualsShouldReturnFalseIfArg1HasNullBitmapAndArg2HasNonNullBitmap) {
-  Message<google_firestore_v1_BitSequence> bit_sequence1;
-  Message<google_firestore_v1_BitSequence> bit_sequence2;
-  bit_sequence1->padding = 789;
-  bit_sequence2->padding = 789;
-  bit_sequence1->bitmap = nullptr;
-  bit_sequence2->bitmap = MakeBytesArray(std::vector<uint8_t>{1, 2, 3});
-
-  google_firestore_v1_BitSequence proto1 = *bit_sequence1;
-  google_firestore_v1_BitSequence proto2 = *bit_sequence2;
-  EXPECT_FALSE(proto1 == proto2);
-  EXPECT_TRUE(proto1 != proto2);
+TEST_F(NanopbOperatorsTest_BitSequence,
+       EqualsShouldReturnFalseIfArg1HasNullBitmapAndArg2HasNonNullBitmap) {
+  TestOperatorEquals(false, {789, absl::nullopt}, {789, {4, 3, 2, 1}});
 }
 
-TEST(
-    OperatorsTest,
-    // NOLINTNEXTLINE(whitespace/line_length)
-    BitSequenceOperatorEqualsShouldReturnFalseIfArg1HasNonNullBitmapAndArg2HasNullBitmap) {
-  Message<google_firestore_v1_BitSequence> bit_sequence1;
-  Message<google_firestore_v1_BitSequence> bit_sequence2;
-  bit_sequence1->padding = 789;
-  bit_sequence2->padding = 789;
-  bit_sequence1->bitmap = MakeBytesArray(std::vector<uint8_t>{1, 2, 3});
-  bit_sequence2->bitmap = nullptr;
-
-  google_firestore_v1_BitSequence proto1 = *bit_sequence1;
-  google_firestore_v1_BitSequence proto2 = *bit_sequence2;
-  EXPECT_FALSE(proto1 == proto2);
-  EXPECT_TRUE(proto1 != proto2);
+TEST_F(NanopbOperatorsTest_BitSequence,
+       EqualsShouldReturnFalseIfArg1HasNonNullBitmapAndArg2HasNullBitmap) {
+  TestOperatorEquals(false, {789, {4, 3, 2, 1}}, {789, absl::nullopt});
 }
-
-// TODO(dconeybe): Add tests for google_firestore_v1_BloomFilter
 
 }  //  namespace
 }  //  namespace firestore
