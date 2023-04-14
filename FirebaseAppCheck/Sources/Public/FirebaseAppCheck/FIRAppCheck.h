d9031e40f8160e2824116735f80d7a4c0aa3a9e6
Yvonne Cheng <yvonnecheng@google.com>
2023-04-14 11:20:33 -0700

    suppress style error

--- FirebaseAppCheck/Sources/Public/FirebaseAppCheck/FIRAppCheck.h
+++ FirebaseAppCheck/Sources/Public/FirebaseAppCheck/FIRAppCheck.h
@@ -56,9 +56,9 @@ NS_SWIFT_NAME(AppCheck)
 /// configured.
 ///
 /// If your non-Firebase backend exposes sensitive or expensive endpoints that has low traffic
-/// volume, consider protecting it with <a href=[--URL to public docs--]>Replay Protection</a>. In
-/// this case, use the limitedUseTokenWithCompletion() instead to obtain a limited-use token.
-///
+/// volume, consider protecting it with <a href="https://firebase.google.com/docs/app-check/custom-resource-backend#replay-protection">Replay
+/// Protection</a>. In this case, use the limitedUseTokenWithCompletion() instead to obtain a
+/// limited-use token.
 /// @param forcingRefresh If `YES`,  a new Firebase app check token is requested and the token
 /// cache is ignored. If `NO`, the cached token is used if it exists and has not expired yet. In
 /// most cases, `NO` should be used. `YES` should only be used if the server explicitly returns an
@@ -74,11 +74,12 @@ NS_SWIFT_NAME(AppCheck)
 /// requests to a non-Firebase backend.
 ///
 /// Returns limited-use tokens that are intended for use with your non-Firebase backend endpoints
-/// that are protected with <a href=[--URL to public docs--]>Replay Protection</a>. This method does
-/// not affect the token generation behavior of the tokenForcingRefresh() method.
-- (void)limitedUseTokenWithCompletion:
-    (void (^)(FIRAppCheckToken *_Nullable token, NSError *_Nullable error))handler
-    NS_SWIFT_NAME(getLimitedUseToken(completion:));
+/// that are protected with <a href=" https://firebase.google.com/docs/app-check/custom-resource-backend#replay-protection">Replay
+/// Protection</a>. This method does not affect the token generation behavior of the
+/// tokenForcingRefresh() method.
+- (void)limitedUseTokenWithCompletion:(void (^)(FIRAppCheckToken *_Nullable token,
+                                                NSError *_Nullable error))handler;
+// NS_SWIFT_NAME(getLimitedUseToken(completion:));
 
 /// Sets the `AppCheckProviderFactory` to use to generate
 /// `AppCheckDebugProvider` objects.
