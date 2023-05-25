1991083049278b98801d6ec52f34aabfc5032880
Ryan Wilson <wilsonryan@google.com>
2023-05-25 15:02:47 -0400

    Fixed build

--- FirebaseAuth/Sources/Swift/Backend/RPC/GetProjectConfigResponse.swift
+++ FirebaseAuth/Sources/Swift/Backend/RPC/GetProjectConfigResponse.swift
@@ -34,31 +34,3 @@ public class GetProjectConfigResponse: AuthRPCResponse {
     }
   }
 }
-
-@objc(FIRGetProjectConfigResponse) public class GetProjectConfigResponse: NSObject,
-  AuthRPCResponse {
-  /** @property projectID
-      @brief The unique ID pertaining to the current project.
-   */
-  @objc public var projectID: String?
-
-  /** @property authorizedDomains
-      @brief A list of domains allowlisted for the current project.
-   */
-  @objc public var authorizedDomains: [String]?
-
-  public func setFields(dictionary: [String: AnyHashable]) throws {
-    projectID = dictionary["projectId"] as? String
-    if let authorizedDomains = dictionary["authorizedDomains"] as? String,
-       let data = authorizedDomains.data(using: .utf8) {
-      if let decoded = try? JSONSerialization.jsonObject(
-        with: data,
-        options: [.mutableLeaves]
-      ), let array = decoded as? [String] {
-        self.authorizedDomains = array
-      }
-    } else if let authorizedDomains = dictionary["authorizedDomains"] as? [String] {
-      self.authorizedDomains = authorizedDomains
-    }
-  }
-}
