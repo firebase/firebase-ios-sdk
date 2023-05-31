edc6c65fb17fd15fa02949826808ce8d8d3aedb1
Nick Cooke <nickcooke@google.com>
2023-05-31 14:17:58 -0400

    Review

--- Package.swift
+++ Package.swift
@@ -1346,5 +1346,5 @@ func googleAppMeasurementDependency() -> Package.Dependency {
     return .package(url: appMeasurementURL, branch: "main")
   }
 
-  return .package(url: appMeasurementURL, branch: "main")
+  return .package(url: appMeasurementURL, exact: "10.10.0")
 }
