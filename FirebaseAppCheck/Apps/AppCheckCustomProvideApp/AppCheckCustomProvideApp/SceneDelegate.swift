8c77f4854a06c16d641668f9134ded7650d5d8c9
Paul Beusterien <paulbeusterien@google.com>
2023-07-11 13:51:53 -0700

    Update FirebaseAppCheck/Apps/AppCheckCustomProvideApp/AppCheckCustomProvideApp/SceneDelegate.swift
    Co-authored-by: Nick Cooke <36927374+ncooke3@users.noreply.github.com>

--- FirebaseAppCheck/Apps/AppCheckCustomProvideApp/AppCheckCustomProvideApp/SceneDelegate.swift
+++ FirebaseAppCheck/Apps/AppCheckCustomProvideApp/AppCheckCustomProvideApp/SceneDelegate.swift
@@ -69,7 +69,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func sceneDidEnterBackground(_ scene: UIScene) {
     // Called as the scene transitions from the foreground to the background.
     // Use this method to save data, release shared resources, and store enough scene-specific state
-    // information
-    // to restore the scene back to its current state.
+    // information to restore the scene back to its current state.
   }
 }
