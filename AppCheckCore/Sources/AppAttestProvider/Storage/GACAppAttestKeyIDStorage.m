e6cc60fb4fa95c6f5e6f24f057954f9e028c8881
Andrew Heard <andrewheard@google.com>
2023-07-21 10:17:20 -0400

    Renamed `AppCheck` to `AppCheckCore` (#11581)
    Renamed the App Check (Lite) SDK to App Check Core.

---
+++ AppCheckCore/Sources/AppAttestProvider/Storage/GACAppAttestKeyIDStorage.m
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "AppCheckCore/Sources/AppAttestProvider/Storage/GACAppAttestKeyIDStorage.h"
+
+#if __has_include(<FBLPromises/FBLPromises.h>)
+#import <FBLPromises/FBLPromises.h>
+#else
+#import "FBLPromises.h"
+#endif
+
+#import "AppCheckCore/Sources/Core/Errors/GACAppCheckErrorUtil.h"
+
+/// The `NSUserDefaults` suite name for the storage location of the app attest key ID.
+static NSString *const kKeyIDStorageDefaultsSuiteName = @"com.firebase.GACAppAttestKeyIDStorage";
+
+@interface GACAppAttestKeyIDStorage ()
+
+@property(nonatomic, readonly) NSString *keySuffix;
+
+/// The app attest key ID is stored using `NSUserDefaults` .
+@property(nonatomic, readonly) NSUserDefaults *userDefaults;
+
+@end
+
+@implementation GACAppAttestKeyIDStorage
+
+- (instancetype)initWithKeySuffix:(NSString *)keySuffix {
+  self = [super init];
+  if (self) {
+    _keySuffix = [keySuffix copy];
+    _userDefaults = [[NSUserDefaults alloc] initWithSuiteName:kKeyIDStorageDefaultsSuiteName];
+  }
+  return self;
+}
+
+- (nonnull FBLPromise<NSString *> *)setAppAttestKeyID:(nullable NSString *)keyID {
+  [self storeAppAttestKeyID:keyID];
+  return [FBLPromise resolvedWith:keyID];
+}
+
+- (nonnull FBLPromise<NSString *> *)getAppAttestKeyID {
+  NSString *appAttestKeyID = [self appAttestKeyIDFromStorage];
+  if (appAttestKeyID) {
+    return [FBLPromise resolvedWith:appAttestKeyID];
+  } else {
+    NSError *error = [GACAppCheckErrorUtil appAttestKeyIDNotFound];
+    FBLPromise *rejectedPromise = [FBLPromise pendingPromise];
+    [rejectedPromise reject:error];
+    return rejectedPromise;
+  }
+}
+
+#pragma mark - Helpers
+
+- (void)storeAppAttestKeyID:(nullable NSString *)keyID {
+  if (keyID) {
+    [self.userDefaults setObject:keyID forKey:[self keyIDStorageKey]];
+  } else {
+    [self.userDefaults removeObjectForKey:[self keyIDStorageKey]];
+  }
+}
+
+- (nullable NSString *)appAttestKeyIDFromStorage {
+  NSString *appAttestKeyID = nil;
+  appAttestKeyID = [self.userDefaults objectForKey:[self keyIDStorageKey]];
+  return appAttestKeyID;
+}
+
+- (NSString *)keyIDStorageKey {
+  return [NSString stringWithFormat:@"app_attest_keyID.%@", self.keySuffix];
+}
+
+@end
---
+++ AppCheckCore/Tests/Integration/GACDeviceCheckAPIServiceE2ETests.m
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <TargetConditionals.h>
+
+// Tests that use the Keychain require a host app and Swift Package Manager
+// does not support adding a host app to test targets.
+#if !SWIFT_PACKAGE
+
+// Skip keychain tests on Catalyst and macOS. Tests are skipped because they
+// involve interactions with the keychain that require a provisioning profile.
+// See go/firebase-macos-keychain-popups for more details.
+#if !TARGET_OS_MACCATALYST && !TARGET_OS_OSX
+
+#import <XCTest/XCTest.h>
+
+#import "FBLPromise+Testing.h"
+
+#import <FirebaseCore/FirebaseCore.h>
+#import <FirebaseCoreExtension/FirebaseCoreInternal.h>
+
+#import "AppCheckCore/Sources/Core/APIService/GACAppCheckAPIService.h"
+#import "AppCheckCore/Sources/DeviceCheckProvider/API/GACDeviceCheckAPIService.h"
+#import "AppCheckCore/Sources/Public/AppCheckCore/GACAppCheckToken.h"
+
+// TODO(andrewheard): Remove from generic App Check SDK.
+// FIREBASE_APP_CHECK_ONLY_BEGIN
+static NSString *const kHeartbeatKey = @"X-firebase-client";
+// FIREBASE_APP_CHECK_ONLY_END
+
+@interface GACDeviceCheckAPIServiceE2ETests : XCTestCase
+@property(nonatomic) GACDeviceCheckAPIService *deviceCheckAPIService;
+@property(nonatomic) GACAppCheckAPIService *APIService;
+@property(nonatomic) NSURLSession *URLSession;
+@end
+
+// TODO(ncooke3): Fix these tests up and get them running on CI.
+
+@implementation GACDeviceCheckAPIServiceE2ETests
+
+- (void)setUp {
+  self.URLSession = [NSURLSession
+      sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+  FIROptions *options = [self firebaseTestOptions];
+  FIRHeartbeatLogger *heartbeatLogger =
+      [[FIRHeartbeatLogger alloc] initWithAppID:options.googleAppID];
+  GACAppCheckAPIRequestHook heartbeatLoggerHook = ^(NSMutableURLRequest *request) {
+    [request setValue:FIRHeaderValueFromHeartbeatsPayload(
+                          [heartbeatLogger flushHeartbeatsIntoPayload])
+        forHTTPHeaderField:kHeartbeatKey];
+  };
+
+  self.APIService = [[GACAppCheckAPIService alloc] initWithURLSession:self.URLSession
+                                                              baseURL:nil
+                                                               APIKey:options.APIKey
+                                                         requestHooks:@[ heartbeatLoggerHook ]];
+  self.deviceCheckAPIService = [[GACDeviceCheckAPIService alloc]
+      initWithAPIService:self.APIService
+            resourceName:[GACDeviceCheckAPIServiceE2ETests resourceNameFromOptions:options]];
+}
+
+- (void)tearDown {
+  self.deviceCheckAPIService = nil;
+  self.APIService = nil;
+  self.URLSession = nil;
+}
+
+// TODO: Re-enable the test once secret with "GoogleService-Info.plist" is configured.
+- (void)temporaryDisabled_testAppCheckTokenSuccess {
+  __auto_type appCheckPromise =
+      [self.deviceCheckAPIService appCheckTokenWithDeviceToken:[NSData data]];
+
+  XCTAssert(FBLWaitForPromisesWithTimeout(20));
+
+  XCTAssertNil(appCheckPromise.error);
+  XCTAssertNotNil(appCheckPromise.value);
+
+  XCTAssertNotNil(appCheckPromise.value.token);
+  XCTAssertNotNil(appCheckPromise.value.expirationDate);
+}
+
+#pragma mark - Helpers
+
+- (FIROptions *)firebaseTestOptions {
+  NSString *plistPath =
+      [[NSBundle bundleForClass:[self class]] pathForResource:@"GoogleService-Info"
+                                                       ofType:@"plist"];
+  FIROptions *options = [[FIROptions alloc] initWithContentsOfFile:plistPath];
+  return options;
+}
+
+// TODO(andrewheard): Remove from generic App Check SDK.
+// FIREBASE_APP_CHECK_ONLY_BEGIN
+
++ (NSString *)resourceNameFromOptions:(FIROptions *)options {
+  return [NSString stringWithFormat:@"projects/%@/apps/%@", options.projectID, options.googleAppID];
+}
+
+// FIREBASE_APP_CHECK_ONLY_END
+
+@end
+
+#endif  // !TARGET_OS_MACCATALYST && !TARGET_OS_OSX
+
+#endif  // !SWIFT_PACKAGE
