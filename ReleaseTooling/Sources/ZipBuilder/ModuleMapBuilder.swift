6f1858699a573a510b522a6a1620907d1f775628
Paul Beusterien <paulbeusterien@google.com>
2023-07-27 13:46:43 -0700

    Fix crash building a module map for a product with an iOS-only Interop dependency (#11620)

--- ReleaseTooling/Sources/ZipBuilder/ModuleMapBuilder.swift
+++ ReleaseTooling/Sources/ZipBuilder/ModuleMapBuilder.swift
@@ -128,7 +128,14 @@ struct ModuleMapBuilder {
   /// to make sure we install the right version and from the right location.
   private func generate(framework: FrameworkInfo) {
     let podName = framework.versionedPod.name
-    let deps = CocoaPodUtils.transitiveVersionedPodDependencies(for: podName, in: allPods)
+    let deps = CocoaPodUtils.transitiveVersionedPodDependencies(for: podName, in: allPods).filter {
+      // Don't include Interop pods in the module map calculation since they shouldn't add anything
+      // and it uses the platform-independent version of the dependency list, which causes a crash
+      // for the iOS-only RecaptchaInterop pod when the subsequent code tries to `pod install` it
+      // for macOS. All this module code should go away when we switch to building dynamic
+      // frameworks.
+      !$0.name.hasSuffix("Interop")
+    }
 
     CocoaPodUtils.installPods(allSubspecList(framework: framework) + deps,
                               inDir: projectDir,
