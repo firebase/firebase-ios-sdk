ed21700b5ec4e6e4290181366d44f17693534c04
Andrew Heard <andrewheard@google.com>
2023-08-02 14:51:34 -0400

    Depend on `AppCheckCore` staged in `SpecsDev` (#11641)
    Updated the FirebaseAppCheck pod to depend on AppCheckCore from google/app-check, staged in firebase/SpecsDev.

--- ReleaseTooling/Sources/PodspecsTester/main.swift
+++ ReleaseTooling/Sources/PodspecsTester/main.swift
@@ -62,8 +62,9 @@ struct PodspecsTester: ParsableCommand {
         return "--\(key)"
       }
     }.joined(separator: " ")
+    // TODO(andrewheard): Remove SpecsDev URL from sources before merging into `master`.
     let command =
-      "pod spec lint \(spec) \(arguments) --sources=https://github.com/firebase/SpecsTesting,https://cdn.cocoapods.org/"
+      "pod spec lint \(spec) \(arguments) --sources=https://github.com/firebase/SpecsTesting,https://github.com/firebase/SpecsDev.git,https://cdn.cocoapods.org/"
     print(command)
     let result = Shell.executeCommandFromScript(
       command,
