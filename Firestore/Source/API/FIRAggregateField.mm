63fd2cbffbc21bbebb3598ef4a54203d9971de60
Mark Duckworth <1124037+MarkDuckworth@users.noreply.github.com>
2023-08-03 14:06:54 -0600

    Revert "Clean up imports in FIRAggregateField.mm"
    This reverts commit d1cb0ac29f33883fa16f4e0d0666b3ddbe8b7347.

--- Firestore/Source/API/FIRAggregateField.mm
+++ Firestore/Source/API/FIRAggregateField.mm
@@ -21,6 +21,8 @@
 #import "Firestore/Source/API/FIRAggregateField+Internal.h"
 #import "Firestore/Source/API/FIRFieldPath+Internal.h"
 
+#import "Firestore/core/src/model/aggregate_field.h"
+
 using firebase::firestore::model::AggregateField;
 
 NS_ASSUME_NONNULL_BEGIN
