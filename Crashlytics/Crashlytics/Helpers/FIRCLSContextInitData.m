836643d38beca7fc9d618a2ef4e063ce679b2889
Ken Grigsby <kgrigsby@diamondkinetics.com>
2023-08-23 08:43:52 -0400

    Fix import path

--- Crashlytics/Crashlytics/Helpers/FIRCLSContextInitData.m
+++ Crashlytics/Crashlytics/Helpers/FIRCLSContextInitData.m
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "FIRCLSContextInitData.h"
+#import "Crashlytics/Crashlytics/Helpers/FIRCLSContextInitData.h"
 
 @implementation FIRCLSContextInitData
 @end
