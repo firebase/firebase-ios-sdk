db6caba2c3df14556f1f620d482e9995875bcf46
Nick Cooke <nickcooke@google.com>
2023-09-01 14:18:49 -0400

    Revert "[Config] Merge RemoteConfig and its Swift extension in a non-breaking way (#11720)"
    This reverts commit 05b088d28e67395d98ca05a6d6cd856f917d648f.

--- Package.swift
+++ Package.swift
@@ -892,9 +892,7 @@ let package = Package(
       dependencies: [
         "FirebaseCore",
         "FirebaseInstallations",
-        // TODO(ncooke3): Consider if this should import the merged module
-        // (`FirebaseRemoteConfig`).
-        "FirebaseRemoteConfigInternal",
+        "FirebaseRemoteConfig",
         "FirebaseSessions",
         .product(name: "GoogleDataTransport", package: "GoogleDataTransport"),
         .product(name: "GULEnvironment", package: "GoogleUtilities"),
@@ -958,7 +956,7 @@ let package = Package(
     // MARK: - Firebase Remote Config
 
     .target(
-      name: "FirebaseRemoteConfigInternal",
+      name: "FirebaseRemoteConfig",
       dependencies: [
         "FirebaseCore",
         "FirebaseABTesting",
@@ -973,7 +971,7 @@ let package = Package(
     ),
     .testTarget(
       name: "RemoteConfigUnit",
-      dependencies: ["FirebaseRemoteConfigInternal", .product(name: "OCMock", package: "ocmock")],
+      dependencies: ["FirebaseRemoteConfig", .product(name: "OCMock", package: "ocmock")],
       path: "FirebaseRemoteConfig/Tests/Unit",
       exclude: [
         // Need to be evaluated/ported to RC V2.
@@ -993,23 +991,16 @@ let package = Package(
       ]
     ),
     .target(
-      name: "FirebaseRemoteConfig",
+      name: "FirebaseRemoteConfigSwift",
       dependencies: [
-        "FirebaseRemoteConfigInternal",
+        "FirebaseRemoteConfig",
         "FirebaseSharedSwift",
       ],
       path: "FirebaseRemoteConfigSwift/Sources"
     ),
-    .target(
-      name: "FirebaseRemoteConfigSwift",
-      dependencies: [
-        "FirebaseRemoteConfig",
-      ],
-      path: "FirebaseRemoteConfigSwift/Exporter"
-    ),
     .testTarget(
       name: "RemoteConfigFakeConsole",
-      dependencies: ["FirebaseRemoteConfig",
+      dependencies: ["FirebaseRemoteConfigSwift",
                      "RemoteConfigFakeConsoleObjC"],
       path: "FirebaseRemoteConfigSwift/Tests",
       exclude: [
