66e6fb089f411c24c2217d09551a5de6055eb8b1
Wu-Hui <wu.hui.github@gmail.com>
2023-09-05 10:04:56 -0400

    Fix internal grpccpp api usage

--- Firestore/core/test/unit/nanopb/message_test.cc
+++ Firestore/core/test/unit/nanopb/message_test.cc
@@ -25,7 +25,7 @@
 #include "Firestore/core/src/nanopb/writer.h"
 #include "Firestore/core/src/remote/grpc_nanopb.h"
 #include "Firestore/core/test/unit/testutil/status_testing.h"
-#include "grpcpp/impl/codegen/grpc_library.h"
+#include "grpcpp/impl/grpc_library.h"
 #include "grpcpp/support/byte_buffer.h"
 #include "gtest/gtest.h"
 
@@ -65,7 +65,7 @@ class MessageTest : public testing::Test {
   // Note: gRPC slice will crash upon destruction if gRPC library hasn't been
   // initialized, which is normally done by inheriting from this class (which
   // does initialization in its constructor).
-  grpc::GrpcLibraryCodegen grpc_initializer_;
+  grpc::internal::GrpcLibrary grpc_initializer_;
 };
 
 #if !__clang_analyzer__
