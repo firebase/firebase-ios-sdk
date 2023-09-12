a8a662b7d32104765aea8f77cc002348cefb47de
Nick Cooke <nickcooke@google.com>
2023-09-12 17:36:10 -0400

    Add TODO to update changelog entry if warning is enabled

--- FirebaseInAppMessagingSwift/Sources/FirebaseInAppMessagingSwift.swift
+++ FirebaseInAppMessagingSwift/Sources/FirebaseInAppMessagingSwift.swift
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 // TODO: Enable warning when ready to surface deprecation to customers.
+// TODO: When warning is enabled, ensure it is reflected in release note.
 // #warning("""
 // The FirebaseInAppMessagingSwift module is deprecated and will be removed in
 // the future. All of the public API from FirebaseInAppMessagingSwift can now be
