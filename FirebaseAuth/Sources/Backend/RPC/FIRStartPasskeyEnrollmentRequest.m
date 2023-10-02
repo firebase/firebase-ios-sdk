1dd888a2a4ae9fe7293177b95f63f1bb083a7597
Liubin Jiang <liubinj@google.com>
2023-10-02 15:20:11 -0700

    [Passkey #1 cloned] start passkey enrollment rpc (#11872) 1. implemented rpc call to startPasskeyEnrollment 2. defined startPasskeyEnrollment request and response 3. added unit tests

---
+++ FirebaseAuth/Sources/Backend/RPC/FIRStartPasskeyEnrollmentRequest.m
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "FirebaseAuth/Sources/Backend/RPC/FIRStartPasskeyEnrollmentRequest.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ @var kStartPasskeyEnrollmentEndPoint
+ @brief GCIP endpoint for startPasskeyEnrollment rpc
+ */
+static NSString *const kStartPasskeyEnrollmentEndPoint = @"accounts/passkeyEnrollment:start";
+
+/**
+ @var kTenantIDKey
+ @brief The key for the tenant id value in the request.
+ */
+static NSString *const kTenantIDKey = @"tenantId";
+
+/**
+ @var kIDToken
+ @brief The key for idToken value in the request.
+ */
+static NSString *const kIDToken = @"idToken";
+
+@implementation FIRStartPasskeyEnrollmentRequest
+
+- (nullable instancetype)initWithIDToken:(NSString *)IDToken
+                    requestConfiguration:(FIRAuthRequestConfiguration *)requestConfiguration {
+  self = [super initWithEndpoint:kStartPasskeyEnrollmentEndPoint
+            requestConfiguration:requestConfiguration];
+
+  if (self) {
+    _IDToken = IDToken;
+    self.useIdentityPlatform = YES;
+  }
+
+  return self;
+}
+
+- (nullable id)unencodedHTTPRequestBodyWithError:(NSError *__autoreleasing _Nullable *)error {
+  NSMutableDictionary *postBody = [NSMutableDictionary dictionary];
+  if (_IDToken) {
+    postBody[kIDToken] = _IDToken;
+  }
+  if (self.tenantID) {
+    postBody[kTenantIDKey] = self.tenantID;
+  }
+  return [postBody copy];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
