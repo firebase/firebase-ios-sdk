52e1b1f52a5af90453393c8611dd8b8aa808f9ce
milaGGL <107142260+milaGGL@users.noreply.github.com>
2023-10-04 18:17:41 -0400

    move terraform into Firestore

--- Firestore/Example/Tests/Integration/API/FIRCompositeIndexQueryTests.mm
+++ Firestore/Example/Tests/Integration/API/FIRCompositeIndexQueryTests.mm
@@ -33,11 +33,12 @@ static NSString *const COMPOSITE_INDEX_TEST_COLLECTION = @"composite-index-test-
  * controlled testing environment.
  *
  * Key Features:
- * - Runs tests against the dedicated test collection with predefined composite
- *   indexes.
- * - Automatically associates a test ID with documents for data isolation.
- * - Utilizes TTL policy for automatic test data cleanup.
- * - Constructs Firestore queries with test ID filters.
+ * <ul>
+ *   <li>Runs tests against the dedicated test collection with predefined composite indexes.
+ *   <li>Automatically associates a test ID with documents for data isolation.
+ *   <li>Utilizes TTL policy for automatic test data cleanup.
+ *   <li>Constructs Firestore queries with test ID filters.
+ * </ul>
  */
 @interface FIRCompositeIndexQueryTests : FSTIntegrationTestCase
 // Creates a new unique identifier for each test case to ensure data isolation.
@@ -124,14 +125,16 @@ static NSString *const COMPOSITE_INDEX_TEST_COLLECTION = @"composite-index-test-
 }
 
 // Adds a filter on test id for a query.
-- (FIRQuery *)query:(FIRQuery *)query_ {
+- (FIRQuery *)compositeIndexQuery:(FIRQuery *)query_ {
   return [query_ queryWhereField:TEST_ID_FIELD isEqualTo:self.testId];
 }
 
 // Get a document reference from a document key.
-- (FIRDocumentReference *)getDocRef:(FIRCollectionReference *)coll docId:(NSString *)docId {
-  NSString *hashedDocId = [self toHashedId:docId];
-  return [coll documentWithPath:hashedDocId];
+- (FIRDocumentReference *)getDocRef:(FIRCollectionReference *)collection docId:(NSString *)docId {
+  if (![docId containsString:@"test-id-"]) {
+    docId = [self toHashedId:docId];
+  }
+  return [collection documentWithPath:docId];
 }
 
 // Adds a document to a Firestore collection with test-specific fields.
@@ -147,27 +150,24 @@ static NSString *const COMPOSITE_INDEX_TEST_COLLECTION = @"composite-index-test-
   return [self mergeDocumentRef:document data:updatedData];
 }
 
-// Update a document in Firestore with test-specific fields.
 - (void)updateDoc:(FIRDocumentReference *)document data:(NSDictionary<NSString *, id> *)data {
-  NSDictionary<NSString *, id> *updatedData = [self addTestSpecificFieldsToDoc:data];
-  [self updateDocumentRef:document data:updatedData];
+  [self updateDocumentRef:document data:data];
 }
 
-// Delete a document from Firestore.
 - (void)deleteDoc:(FIRDocumentReference *)document {
   [self deleteDocumentRef:document];
 }
 
 // Retrieve a single document from Firestore with test-specific fields removed.
 // TODO(composite-index-testing) Return sanitized DocumentSnapshot instead of its data.
-- (NSDictionary<NSString *, id> *)getDocSnapshotData:(FIRDocumentReference *)document {
+- (NSDictionary<NSString *, id> *)getSanitizedDocumentData:(FIRDocumentReference *)document {
   FIRDocumentSnapshot *docSnapshot = [self readDocumentForRef:document];
   return [self removeTestSpecificFieldsFromDoc:docSnapshot.data];
 }
 
 // Retrieve multiple documents from Firestore with test-specific fields removed.
 // TODO(composite-index-testing) Return sanitized QuerySnapshot instead of its data.
-- (NSArray<NSDictionary<NSString *, id> *> *)getQuerySnapshotData:(FIRQuery *)query {
+- (NSArray<NSDictionary<NSString *, id> *> *)getSanitizedQueryData:(FIRQuery *)query {
   FIRQuerySnapshot *querySnapshot = [self readDocumentSetForRef:query];
   NSMutableArray<NSDictionary<NSString *, id> *> *result = [NSMutableArray array];
   for (FIRDocumentSnapshot *doc in querySnapshot.documents) {
@@ -201,46 +201,52 @@ static NSString *const COMPOSITE_INDEX_TEST_COLLECTION = @"composite-index-test-
     @"doc5" : @{@"a" : @1, @"b" : @1}
   }];
   // with one inequality: a>2 || b==1.
-  FIRFilter *filter1 = [FIRFilter orFilterWithFilters:@[
-    [FIRFilter filterWhereField:@"a" isGreaterThan:@2], [FIRFilter filterWhereField:@"b"
-                                                                          isEqualTo:@1]
-  ]];
-  [self assertOnlineAndOfflineResultsMatch:[self query:[collRef queryWhereFilter:filter1]]
+  FIRQuery *query1 = [collRef
+      queryWhereFilter:[FIRFilter orFilterWithFilters:@[
+        [FIRFilter filterWhereField:@"a" isGreaterThan:@2], [FIRFilter filterWhereField:@"b"
+                                                                              isEqualTo:@1]
+      ]]];
+  [self assertOnlineAndOfflineResultsMatch:[self compositeIndexQuery:query1]
                               expectedDocs:@[ @"doc5", @"doc2", @"doc3" ]];
 
   // Test with limits (implicit order by ASC): (a==1) || (b > 0) LIMIT 2
-  FIRFilter *filter2 = [FIRFilter orFilterWithFilters:@[
-    [FIRFilter filterWhereField:@"a" isEqualTo:@1], [FIRFilter filterWhereField:@"b"
-                                                                  isGreaterThan:@0]
-  ]];
-  [self assertOnlineAndOfflineResultsMatch:[[self query:[collRef queryWhereFilter:filter2]]
-                                               queryLimitedTo:2]
+  FIRQuery *query2 =
+      [collRef queryWhereFilter:[FIRFilter orFilterWithFilters:@[
+                 [FIRFilter filterWhereField:@"a" isEqualTo:@1], [FIRFilter filterWhereField:@"b"
+                                                                               isGreaterThan:@0]
+               ]]];
+  [self assertOnlineAndOfflineResultsMatch:[[self compositeIndexQuery:query2] queryLimitedTo:2]
                               expectedDocs:@[ @"doc1", @"doc2" ]];
 
   // Test with limits (explicit order by): (a==1) || (b > 0) LIMIT_TO_LAST 2
   // Note: The public query API does not allow implicit ordering when limitToLast is used.
-  FIRFilter *filter3 = [FIRFilter orFilterWithFilters:@[
-    [FIRFilter filterWhereField:@"a" isEqualTo:@1], [FIRFilter filterWhereField:@"b"
-                                                                  isGreaterThan:@0]
-  ]];
-  [self assertOnlineAndOfflineResultsMatch:[[[self query:[collRef queryWhereFilter:filter3]]
-                                               queryLimitedToLast:2] queryOrderedByField:@"b"]
+  FIRQuery *query3 =
+      [collRef queryWhereFilter:[FIRFilter orFilterWithFilters:@[
+                 [FIRFilter filterWhereField:@"a" isEqualTo:@1], [FIRFilter filterWhereField:@"b"
+                                                                               isGreaterThan:@0]
+               ]]];
+  [self assertOnlineAndOfflineResultsMatch:[[[self compositeIndexQuery:query3] queryLimitedToLast:2]
+                                               queryOrderedByField:@"b"]
                               expectedDocs:@[ @"doc3", @"doc4" ]];
 
   // Test with limits (explicit order by ASC): (a==2) || (b == 1) ORDER BY a LIMIT 1
-  FIRFilter *filter4 = [FIRFilter orFilterWithFilters:@[
-    [FIRFilter filterWhereField:@"a" isEqualTo:@2], [FIRFilter filterWhereField:@"b" isEqualTo:@1]
-  ]];
-  [self assertOnlineAndOfflineResultsMatch:[[[self query:[collRef queryWhereFilter:filter4]]
-                                               queryLimitedTo:1] queryOrderedByField:@"a"]
+  FIRQuery *query4 =
+      [collRef queryWhereFilter:[FIRFilter orFilterWithFilters:@[
+                 [FIRFilter filterWhereField:@"a" isEqualTo:@2], [FIRFilter filterWhereField:@"b"
+                                                                                   isEqualTo:@1]
+               ]]];
+  [self assertOnlineAndOfflineResultsMatch:[[[self compositeIndexQuery:query4] queryLimitedTo:1]
+                                               queryOrderedByField:@"a"]
                               expectedDocs:@[ @"doc5" ]];
 
   // Test with limits (explicit order by DESC): (a==2) || (b == 1) ORDER BY a LIMIT_TO_LAST 1
-  FIRFilter *filter5 = [FIRFilter orFilterWithFilters:@[
-    [FIRFilter filterWhereField:@"a" isEqualTo:@2], [FIRFilter filterWhereField:@"b" isEqualTo:@1]
-  ]];
-  [self assertOnlineAndOfflineResultsMatch:[[[self query:[collRef queryWhereFilter:filter5]]
-                                               queryLimitedToLast:1] queryOrderedByField:@"a"]
+  FIRQuery *query5 =
+      [collRef queryWhereFilter:[FIRFilter orFilterWithFilters:@[
+                 [FIRFilter filterWhereField:@"a" isEqualTo:@2], [FIRFilter filterWhereField:@"b"
+                                                                                   isEqualTo:@1]
+               ]]];
+  [self assertOnlineAndOfflineResultsMatch:[[[self compositeIndexQuery:query5] queryLimitedToLast:1]
+                                               queryOrderedByField:@"a"]
                               expectedDocs:@[ @"doc2" ]];
 }
 
