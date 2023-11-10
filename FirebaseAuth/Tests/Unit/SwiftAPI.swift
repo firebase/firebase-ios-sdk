3b1e6fe69078cbd9650a49679341723796f626ab
Paul Beusterien <paulbeusterien@google.com>
2023-11-10 13:25:42 -0800

    [auth-swift] Remove erroneously added public api (#12094)

--- FirebaseAuth/Tests/Unit/SwiftAPI.swift
+++ FirebaseAuth/Tests/Unit/SwiftAPI.swift
@@ -94,7 +94,7 @@ class AuthAPI_hOnlyTests: XCTestCase {
     #endif
   }
 
-  func FIRAuth_h() throws {
+  func FIRAuth_h(credential: AuthCredential) throws {
     let auth = FirebaseAuth.Auth.auth()
     let authApp = FirebaseAuth.Auth.auth(app: FirebaseApp.app()!)
     let user = auth.currentUser!
@@ -155,8 +155,10 @@ class AuthAPI_hOnlyTests: XCTestCase {
       _ = auth.canHandle(URL(fileURLWithPath: "/my/path"))
       auth.setAPNSToken(Data(), type: AuthAPNSTokenType(rawValue: 2)!)
       _ = auth.canHandleNotification([:])
-      auth.initializeRecaptchaConfig { _ in
-      }
+      #if !targetEnvironment(macCatalyst)
+        auth.initializeRecaptchaConfig { _ in
+        }
+      #endif
     #endif
     auth.revokeToken(withAuthorizationCode: "A")
     try auth.useUserAccessGroup("abc")
@@ -180,9 +182,11 @@ class AuthAPI_hOnlyTests: XCTestCase {
     let provider = OAuthProvider(providerID: "abc")
     #if os(iOS)
       let credential = try await provider.credential(with: nil)
-      _ = try await auth.signIn(with: OAuthProvider(providerID: "abc"), uiDelegate: nil)
       _ = try await auth.signIn(with: credential)
-      try await auth.initializeRecaptchaConfig()
+      _ = try await auth.signIn(with: OAuthProvider(providerID: "abc"), uiDelegate: nil)
+      #if !targetEnvironment(macCatalyst)
+        try await auth.initializeRecaptchaConfig()
+      #endif
     #endif
     _ = try await auth.signInAnonymously()
     _ = try await auth.signIn(withCustomToken: "abc")
@@ -655,7 +659,6 @@ class AuthAPI_hOnlyTests: XCTestCase {
     _ = try await user.getIDTokenResult()
     _ = try await user.getIDTokenResult(forcingRefresh: true)
     _ = try await user.getIDToken()
-    _ = try await user.getIDToken(forcingRefresh: false)
     _ = try await user.link(with: credential)
     _ = try await user.unlink(fromProvider: "abc")
     try await user.sendEmailVerification()
