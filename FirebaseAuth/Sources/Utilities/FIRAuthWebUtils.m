82c3504ce32364d2fc1819c59ffaf303801a1d65
renkelvin <chuanr@google.com>
2023-11-14 13:34:06 -0800

    Update FIRAuthWebUtils.m

--- FirebaseAuth/Sources/Utilities/FIRAuthWebUtils.m
+++ FirebaseAuth/Sources/Utilities/FIRAuthWebUtils.m
@@ -104,7 +104,6 @@ NS_ASSUME_NONNULL_BEGIN
                   return;
                 }
                 // Look up an authorized domain ends with one of the supportedAuthDomains.
-                // The sequence of supportedAuthDomains matters. ("firebaseapp.com", "web.app")
                 // The searching ends once the first valid suportedAuthDomain is found.
                 NSString *authDomain;
                 for (NSString *domain in response.authorizedDomains) {
