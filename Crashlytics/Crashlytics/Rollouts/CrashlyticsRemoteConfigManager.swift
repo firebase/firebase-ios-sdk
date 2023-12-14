3934ae6941091f066a38d6727f95cf2a2a15b02f
Themis wang <themisw@google.com>
2023-12-14 13:02:19 -0500

    [Rollouts]Crashlytics interop integration

---
+++ Crashlytics/Crashlytics/Rollouts/CrashlyticsRemoteConfigManager.swift
@@ -0,0 +1,61 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import FirebaseRemoteConfigInterop
+import Foundation
+
+protocol CrashlyticsPersistentLog: NSObject {
+  func updateRolloutsStateToPersistence(rolloutAssignments: [RolloutAssignment])
+}
+
+@objc(FIRCLSRemoteConfigManager)
+public class CrashlyticsRemoteConfigManager: NSObject {
+  public static let maxRolloutAssignment = 128
+  public static let maxParameterValueLength = 256
+
+  var remoteConfig: RemoteConfigInterop
+  public private(set) var rolloutAssignment: [RolloutAssignment] = []
+  weak var persistenceDelegate: CrashlyticsPersistentLog?
+
+  @objc public init(remoteConfig: RemoteConfigInterop) {
+    self.remoteConfig = remoteConfig
+  }
+
+  @objc public func updateRolloutsState(rolloutsState: RolloutsState) {
+    rolloutAssignment = validateRolloutAssignment(assignments: Array(rolloutsState.assignments))
+  }
+}
+
+private extension CrashlyticsRemoteConfigManager {
+  func validateRolloutAssignment(assignments: [RolloutAssignment]) -> [RolloutAssignment] {
+    var validatedAssignments = assignments
+    if assignments.count > CrashlyticsRemoteConfigManager.maxRolloutAssignment {
+      validatedAssignments =
+        Array(assignments[..<CrashlyticsRemoteConfigManager.maxRolloutAssignment])
+    }
+
+    _ = validatedAssignments.map { assignment in
+      if assignment.parameterValue.count > CrashlyticsRemoteConfigManager.maxParameterValueLength {
+        let upperBound = String.Index(
+          utf16Offset: CrashlyticsRemoteConfigManager.maxParameterValueLength,
+          in: assignment.parameterValue
+        )
+        let slicedParameterValue = assignment.parameterValue[..<upperBound]
+        assignment.parameterValue = String(slicedParameterValue)
+      }
+    }
+
+    return validatedAssignments
+  }
+}
