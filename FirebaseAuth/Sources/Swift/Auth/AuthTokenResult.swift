39bb174c3d41ff5969615d28da61f021349d8b41
Paul Beusterien <paulbeusterien@google.com>
2024-01-11 15:20:17 -0800

    Secure Coding for AuthTokenResult

--- FirebaseAuth/Sources/Swift/Auth/AuthTokenResult.swift
+++ FirebaseAuth/Sources/Swift/Auth/AuthTokenResult.swift
@@ -14,56 +14,53 @@
 
 import Foundation
 
+extension AuthTokenResult: NSSecureCoding {}
+
 /** @class FIRAuthTokenResult
     @brief A data class containing the ID token JWT string and other properties associated with the
     token including the decoded payload claims.
  */
 @objc(FIRAuthTokenResult) open class AuthTokenResult: NSObject {
   /** @property token
-      @brief Stores the JWT string of the ID token.
+   @brief Stores the JWT string of the ID token.
    */
   @objc open var token: String
 
   /** @property expirationDate
-      @brief Stores the ID token's expiration date.
+   @brief Stores the ID token's expiration date.
    */
   @objc open var expirationDate: Date
 
   /** @property authDate
-      @brief Stores the ID token's authentication date.
-      @remarks This is the date the user was signed in and NOT the date the token was refreshed.
+   @brief Stores the ID token's authentication date.
+   @remarks This is the date the user was signed in and NOT the date the token was refreshed.
    */
   @objc open var authDate: Date
 
   /** @property issuedAtDate
-      @brief Stores the date that the ID token was issued.
-      @remarks This is the date last refreshed and NOT the last authentication date.
+   @brief Stores the date that the ID token was issued.
+   @remarks This is the date last refreshed and NOT the last authentication date.
    */
   @objc open var issuedAtDate: Date
 
   /** @property signInProvider
-      @brief Stores sign-in provider through which the token was obtained.
-      @remarks This does not necessarily map to provider IDs.
+   @brief Stores sign-in provider through which the token was obtained.
+   @remarks This does not necessarily map to provider IDs.
    */
   @objc open var signInProvider: String
 
   /** @property signInSecondFactor
-      @brief Stores sign-in second factor through which the token was obtained.
+   @brief Stores sign-in second factor through which the token was obtained.
    */
   @objc open var signInSecondFactor: String
 
   /** @property claims
-      @brief Stores the entire payload of claims found on the ID token. This includes the standard
-          reserved claims as well as custom claims set by the developer via the Admin SDK.
+   @brief Stores the entire payload of claims found on the ID token. This includes the standard
+   reserved claims as well as custom claims set by the developer via the Admin SDK.
    */
   @objc open var claims: [String: Any]
 
-  /** @fn tokenResultWithToken:
-       @brief Parse a token string to a structured token.
-       @param token The token string to parse.
-       @return A structured token result.
-   */
-  @objc open class func tokenResult(token: String) -> AuthTokenResult? {
+  private class func getTokenPayloadData(_ token: String) -> Data? {
     let tokenStringArray = token.components(separatedBy: ".")
 
     // The JWT should have three parts, though we only use the second in this method.
@@ -85,56 +82,81 @@ import Foundation
       let length = tokenPayload.count + (4 - tokenPayload.count % 4)
       tokenPayload = tokenPayload.padding(toLength: length, withPad: "=", startingAt: 0)
     }
+    return Data(base64Encoded: tokenPayload, options: [.ignoreUnknownCharacters])
+  }
 
-    guard let decodedTokenPayloadData = Data(
-      base64Encoded: tokenPayload,
-      options: [.ignoreUnknownCharacters]
-    ) else {
-      return nil
-    }
-    guard let tokenPayloadDictionary = try? JSONSerialization.jsonObject(
-      with: decodedTokenPayloadData,
+  private class func getTokenPayloadDictionary(_ payloadData: Data) -> [String: Any]? {
+    return try? JSONSerialization.jsonObject(
+      with: payloadData,
       options: [.mutableContainers, .allowFragments]
-    ) as? [String: Any] else {
-      return nil
-    }
+    ) as? [String: Any]
+  }
+
+  private class func getJWT(_ payloadData: Data) -> JWT? {
     // These are dates since 00:00:00 January 1 1970, as described by the Terminology section in
     // the JWT spec. https://tools.ietf.org/html/rfc7519
     let decoder = JSONDecoder()
     decoder.dateDecodingStrategy = .secondsSince1970
     decoder.keyDecodingStrategy = .convertFromSnakeCase
-    guard let jwt = try? decoder.decode(JWT.self, from: decodedTokenPayloadData) else {
+    guard let jwt = try? decoder.decode(JWT.self, from: payloadData) else {
       return nil
     }
-
-    let tokenResult = AuthTokenResult(token: token,
-                                      expirationDate: jwt.exp,
-                                      authDate: jwt.authTime,
-                                      issuedAtDate: jwt.iat,
-                                      signInProvider: jwt.firebase.signInProvider,
-                                      signInSecondFactor: jwt.firebase.signInSecondFactor,
-                                      claims: tokenPayloadDictionary)
-    return tokenResult
+    return jwt
   }
 
-  init(token: String,
-       expirationDate: Date,
-       authDate: Date,
-       issuedAtDate: Date,
-       signInProvider: String,
-       signInSecondFactor: String?,
-       claims: [String: Any]) {
+  /** @fn tokenResultWithToken:
+       @brief Parse a token string to a structured token.
+       @param token The token string to parse.
+       @return A structured token result.
+   */
+  @objc open class func tokenResult(token: String) -> AuthTokenResult? {
+    guard let payloadData = getTokenPayloadData(token),
+          let claims = getTokenPayloadDictionary(payloadData),
+          let jwt = getJWT(payloadData) else {
+      return nil
+    }
+    return AuthTokenResult(token: token, jwt: jwt, claims: claims)
+  }
+
+  private init(token: String, jwt: JWT, claims: [String: Any]) {
     self.token = token
-    self.expirationDate = expirationDate
-    self.authDate = authDate
-    self.issuedAtDate = issuedAtDate
-    self.signInProvider = signInProvider
-    self.signInSecondFactor = signInSecondFactor ?? ""
+    expirationDate = jwt.exp
+    authDate = jwt.authTime
+    issuedAtDate = jwt.iat
+    signInProvider = jwt.firebase.signInProvider
+    signInSecondFactor = jwt.firebase.signInSecondFactor ?? ""
     self.claims = claims
+  }
+
+  // MARK: Secure Coding
+
+  private static let kTokenKey = "token"
+
+  public static var supportsSecureCoding: Bool {
+    return true
+  }
+
+  public func encode(with coder: NSCoder) {
+    coder.encode(token, forKey: AuthTokenResult.kTokenKey)
+  }
+
+  public required convenience init?(coder: NSCoder) {
+    guard let token = coder.decodeObject(
+      of: [NSString.self],
+      forKey: AuthTokenResult.kTokenKey
+    ) as? String else {
+      return nil
+    }
+    guard let payloadData = AuthTokenResult.getTokenPayloadData(token),
+          let claims = AuthTokenResult.getTokenPayloadDictionary(payloadData),
+          let jwt = AuthTokenResult.getJWT(payloadData) else {
+      return nil
+    }
+    self.init(token: token, jwt: jwt, claims: claims)
   }
 }
 
-struct JWT: Decodable {
+private struct JWT: Decodable {
   struct FirebasePayload: Decodable {
     let signInProvider: String
     let signInSecondFactor: String?
