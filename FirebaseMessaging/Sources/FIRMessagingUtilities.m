ee5413eee9e1948af632780d2980134414e8076a
Jing Liu <leojay@google.com>
2024-01-25 17:00:22 -0800

    Delete unnecessary file system API usages.
    Before storing a new message to the message database, we check whether there is enough free disk space.
    If there is no enough disk space, nothing will be stored.
    This check is not necessary. Because when there is no enough disk space, sqlite will return SQLITE_FULL and abort the transaction.

--- FirebaseMessaging/Sources/FIRMessagingUtilities.m
+++ FirebaseMessaging/Sources/FIRMessagingUtilities.m
@@ -114,28 +114,6 @@ BOOL FIRMessagingIsWatchKitExtension(void) {
 #endif
 }
 
-uint64_t FIRMessagingGetFreeDiskSpaceInMB(void) {
-  NSError *error;
-  NSArray *paths =
-      NSSearchPathForDirectoriesInDomains(FIRMessagingSupportedDirectory(), NSUserDomainMask, YES);
-
-  NSDictionary *attributesMap =
-      [[NSFileManager defaultManager] attributesOfFileSystemForPath:[paths lastObject]
-                                                              error:&error];
-  if (attributesMap) {
-    uint64_t totalSizeInBytes __unused = [attributesMap[NSFileSystemSize] longLongValue];
-    uint64_t freeSizeInBytes = [attributesMap[NSFileSystemFreeSize] longLongValue];
-    FIRMessagingLoggerDebug(
-        kFIRMessagingMessageCodeUtilities001, @"Device has capacity %llu MB with %llu MB free.",
-        totalSizeInBytes / kBytesToMegabytesDivisor, freeSizeInBytes / kBytesToMegabytesDivisor);
-    return ((double)freeSizeInBytes) / kBytesToMegabytesDivisor;
-  } else {
-    FIRMessagingLoggerError(kFIRMessagingMessageCodeUtilities002,
-                            @"Error in retreiving device's free memory %@", error);
-    return 0;
-  }
-}
-
 NSSearchPathDirectory FIRMessagingSupportedDirectory(void) {
 #if TARGET_OS_TV
   return NSCachesDirectory;
