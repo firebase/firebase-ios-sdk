9ccd47cd5e6d8e32dfb4c3fd18cab85aa734d506
Andrew Heard <andrewheard@google.com>
2024-02-22 11:01:44 -0500

    Add sample apps using Vertex AI

---
+++ FirebaseVertexAI/Sample/ChatSample/Views/ErrorDetailsView.swift
@@ -0,0 +1,247 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import FirebaseVertexAI
+import MarkdownUI
+import SwiftUI
+
+extension SafetySetting.HarmCategory: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case .dangerousContent: "Dangerous content"
+    case .harassment: "Harassment"
+    case .hateSpeech: "Hate speech"
+    case .sexuallyExplicit: "Sexually explicit"
+    case .unknown: "Unknown"
+    case .unspecified: "Unspecified"
+    }
+  }
+}
+
+extension SafetyRating.HarmProbability: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case .high: "High"
+    case .low: "Low"
+    case .medium: "Medium"
+    case .negligible: "Negligible"
+    case .unknown: "Unknown"
+    case .unspecified: "Unspecified"
+    }
+  }
+}
+
+private struct SubtitleFormRow: View {
+  var title: String
+  var value: String
+
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text(title)
+        .font(.subheadline)
+      Text(value)
+    }
+  }
+}
+
+private struct SubtitleMarkdownFormRow: View {
+  var title: String
+  var value: String
+
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text(title)
+        .font(.subheadline)
+      Markdown(value)
+    }
+  }
+}
+
+private struct SafetyRatingsSection: View {
+  var ratings: [SafetyRating]
+
+  var body: some View {
+    Section("Safety ratings") {
+      List(ratings, id: \.self) { rating in
+        HStack {
+          Text("\(String(describing: rating.category))")
+            .font(.subheadline)
+          Spacer()
+          Text("\(String(describing: rating.probability))")
+        }
+      }
+    }
+  }
+}
+
+struct ErrorDetailsView: View {
+  var error: Error
+
+  var body: some View {
+    NavigationView {
+      Form {
+        switch error {
+        case let GenerateContentError.internalError(underlying: underlyingError):
+          Section("Error Type") {
+            Text("Internal error")
+          }
+
+          Section("Details") {
+            SubtitleFormRow(title: "Error description",
+                            value: underlyingError.localizedDescription)
+          }
+
+        case let GenerateContentError.promptBlocked(response: generateContentResponse):
+          Section("Error Type") {
+            Text("Your prompt was blocked")
+          }
+
+          Section("Details") {
+            if let reason = generateContentResponse.promptFeedback?.blockReason {
+              SubtitleFormRow(title: "Reason for blocking", value: reason.rawValue)
+            }
+
+            if let text = generateContentResponse.text {
+              SubtitleMarkdownFormRow(title: "Last chunk for the response", value: text)
+            }
+          }
+
+          if let ratings = generateContentResponse.candidates.first?.safetyRatings {
+            SafetyRatingsSection(ratings: ratings)
+          }
+
+        case let GenerateContentError.responseStoppedEarly(
+          reason: finishReason,
+          response: generateContentResponse
+        ):
+
+          Section("Error Type") {
+            Text("Response stopped early")
+          }
+
+          Section("Details") {
+            SubtitleFormRow(title: "Reason for finishing early", value: finishReason.rawValue)
+
+            if let text = generateContentResponse.text {
+              SubtitleMarkdownFormRow(title: "Last chunk for the response", value: text)
+            }
+          }
+
+          if let ratings = generateContentResponse.candidates.first?.safetyRatings {
+            SafetyRatingsSection(ratings: ratings)
+          }
+
+        case GenerateContentError.invalidAPIKey:
+          Section("Error Type") {
+            Text("Invalid API Key")
+          }
+
+          Section("Details") {
+            SubtitleFormRow(title: "Error description", value: error.localizedDescription)
+            SubtitleMarkdownFormRow(
+              title: "Help",
+              value: """
+              Please provide a valid value for `API_KEY` in the `GenerativeAI-Info.plist` file.
+              """
+            )
+          }
+
+        case GenerateContentError.unsupportedUserLocation:
+          Section("Error Type") {
+            Text("Unsupported User Location")
+          }
+
+          Section("Details") {
+            SubtitleFormRow(title: "Error description", value: error.localizedDescription)
+            SubtitleMarkdownFormRow(
+              title: "Help",
+              value: """
+              The API is unsupported in your location (country / territory); please see the list of
+              [available regions](https://ai.google.dev/available_regions#available_regions).
+              """
+            )
+          }
+
+        default:
+          Section("Error Type") {
+            Text("Some other error")
+          }
+
+          Section("Details") {
+            SubtitleFormRow(title: "Error description", value: error.localizedDescription)
+          }
+        }
+      }
+      .navigationTitle("Error details")
+      .navigationBarTitleDisplayMode(.inline)
+    }
+  }
+}
+
+#Preview("Response Stopped Early") {
+  let error = GenerateContentError.responseStoppedEarly(
+    reason: .maxTokens,
+    response: GenerateContentResponse(candidates: [
+      CandidateResponse(content: ModelContent(role: "model", [
+        """
+        A _hypothetical_ model response.
+        Cillum ex aliqua amet aliquip labore amet eiusmod consectetur reprehenderit sit commodo.
+        """,
+      ]),
+      safetyRatings: [
+        SafetyRating(category: .dangerousContent, probability: .high),
+        SafetyRating(category: .harassment, probability: .low),
+        SafetyRating(category: .hateSpeech, probability: .low),
+        SafetyRating(category: .sexuallyExplicit, probability: .low),
+      ],
+      finishReason: FinishReason.maxTokens,
+      citationMetadata: nil),
+    ],
+    promptFeedback: nil)
+  )
+
+  return ErrorDetailsView(error: error)
+}
+
+#Preview("Prompt Blocked") {
+  let error = GenerateContentError.promptBlocked(
+    response: GenerateContentResponse(candidates: [
+      CandidateResponse(content: ModelContent(role: "model", [
+        """
+        A _hypothetical_ model response.
+        Cillum ex aliqua amet aliquip labore amet eiusmod consectetur reprehenderit sit commodo.
+        """,
+      ]),
+      safetyRatings: [
+        SafetyRating(category: .dangerousContent, probability: .high),
+        SafetyRating(category: .harassment, probability: .low),
+        SafetyRating(category: .hateSpeech, probability: .low),
+        SafetyRating(category: .sexuallyExplicit, probability: .low),
+      ],
+      finishReason: FinishReason.other,
+      citationMetadata: nil),
+    ],
+    promptFeedback: nil)
+  )
+
+  return ErrorDetailsView(error: error)
+}
+
+#Preview("Invalid API Key") {
+  ErrorDetailsView(error: GenerateContentError.invalidAPIKey)
+}
+
+#Preview("Unsupported User Location") {
+  ErrorDetailsView(error: GenerateContentError.unsupportedUserLocation)
+}
---
+++ FirebaseVertexAI/Sample/GenerativeAIMultimodalSample/ViewModels/PhotoReasoningViewModel.swift
@@ -0,0 +1,119 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import FirebaseVertexAI
+import Foundation
+import OSLog
+import PhotosUI
+import SwiftUI
+
+@MainActor
+class PhotoReasoningViewModel: ObservableObject {
+  // Maximum value for the larger of the two image dimensions (height and width) in pixels. This is
+  // being used to reduce the image size in bytes.
+  private static let largestImageDimension = 768.0
+
+  private var logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "generative-ai")
+
+  @Published
+  var userInput: String = ""
+
+  @Published
+  var selectedItems = [PhotosPickerItem]()
+
+  @Published
+  var outputText: String? = nil
+
+  @Published
+  var errorMessage: String?
+
+  @Published
+  var inProgress = false
+
+  private var model: GenerativeModel?
+
+  init() {
+    model = VertexAI.generativeModel(modelName: "gemini-1.0-pro-vision", location: "us-central1")
+  }
+
+  func reason() async {
+    defer {
+      inProgress = false
+    }
+    guard let model else {
+      return
+    }
+
+    do {
+      inProgress = true
+      errorMessage = nil
+      outputText = ""
+
+      let prompt = "Look at the image(s), and then answer the following question: \(userInput)"
+
+      var images = [PartsRepresentable]()
+      for item in selectedItems {
+        if let data = try? await item.loadTransferable(type: Data.self) {
+          guard let image = UIImage(data: data) else {
+            logger.error("Failed to parse data as an image, skipping.")
+            continue
+          }
+          if image.size.fits(largestDimension: PhotoReasoningViewModel.largestImageDimension) {
+            images.append(image)
+          } else {
+            guard let resizedImage = image
+              .preparingThumbnail(of: image.size
+                .aspectFit(largestDimension: PhotoReasoningViewModel.largestImageDimension)) else {
+              logger.error("Failed to resize image: \(image)")
+              continue
+            }
+
+            images.append(resizedImage)
+          }
+        }
+      }
+
+      let outputContentStream = model.generateContentStream(prompt, images)
+
+      // stream response
+      for try await outputContent in outputContentStream {
+        guard let line = outputContent.text else {
+          return
+        }
+
+        outputText = (outputText ?? "") + line
+      }
+    } catch {
+      logger.error("\(error.localizedDescription)")
+      errorMessage = error.localizedDescription
+    }
+  }
+}
+
+private extension CGSize {
+  func fits(largestDimension length: CGFloat) -> Bool {
+    return width <= length && height <= length
+  }
+
+  func aspectFit(largestDimension length: CGFloat) -> CGSize {
+    let aspectRatio = width / height
+    if width > height {
+      let width = min(self.width, length)
+      return CGSize(width: width, height: round(width / aspectRatio))
+    } else {
+      let height = min(self.height, length)
+      return CGSize(width: round(height * aspectRatio), height: height)
+    }
+  }
+}
