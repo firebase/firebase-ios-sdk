d8467e4f953c5ada1642d9edf3e930a030d3cde8
Pragati <pragatimodi@google.com>
2024-02-29 13:43:34 -0800

    change ordering

--- FirebaseAuth/Tests/SampleSwift/AuthenticationExample/ViewControllers/AuthViewController.swift
+++ FirebaseAuth/Tests/SampleSwift/AuthenticationExample/ViewControllers/AuthViewController.swift
@@ -11,10 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+@testable import FirebaseAuth
 
 // For Sign in with Facebook
 import FBSDKLoginKit
-@testable import FirebaseAuth
 // [START auth_import]
 import FirebaseCore
 import GameKit
