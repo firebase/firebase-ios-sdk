c23a95774430c341e7a2633e55fb67794a4c9d64
Nick Cooke <nickcooke@google.com>
2024-03-05 14:38:25 -0500

    style

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
   platforms: [.iOS(.v11), .macCatalyst(.v13), .macOS(.v10_13), .tvOS(.v12), .watchOS(.v7)],
   products: [
     .library(
-        name: "FirebaseABTesting",
-        targets: ["FirebaseABTesting"]
+      name: "FirebaseABTesting",
+      targets: ["FirebaseABTesting"]
     ),
     .library(
       name: "FirebaseAnalytics",
