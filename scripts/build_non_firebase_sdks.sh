7105e5f3e4938dc42ac796e46fb3fa5453cb7b74
Nick Cooke <nickcooke@google.com>
2024-04-17 15:29:00 -0400

    wrokaround Xcode 15 resource issues

--- scripts/build_non_firebase_sdks.sh
+++ scripts/build_non_firebase_sdks.sh
@@ -42,3 +42,16 @@ unzip -o "${REPO}"/sdk_zip/Frameworks.zip -d "${HOME}"/ios_frameworks/Firebase/
 
 # Move Frameworks to Firebase dir, so be align with Firebase SDKs.
 mv -n "${HOME}"/ios_frameworks/Firebase/Binaries "${HOME}"/ios_frameworks/Firebase/NonFirebaseSDKs/
+
+for xcframework in "${HOME}"/ios_frameworks/Firebase/NonFirebaseSDKs/**/*.xcframework; do
+    if [ -d "$xcframework/Resources" ]; then
+        for framework_resource in "$xcframework/Resources"/*; do
+            for platform in "ios-arm64" "ios-arm64_x86_64-simulator"; do
+                framework="$xcframework/$platform/$(basename "$xcframework" .xcframework).framework"
+                if [ -d  $framework ]; then
+                    cp -rP $framework_resource $framework
+                fi
+            done
+        done
+    fi 
+done
