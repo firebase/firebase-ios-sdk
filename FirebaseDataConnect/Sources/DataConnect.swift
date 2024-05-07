b1abe6df449b895345f1a203b3201ecb539feb65
Aashish Patil <aashishp@google.com>
2024-05-07 13:59:22 -0700

    Make useEmulator an instance method

--- FirebaseDataConnect/Sources/DataConnect.swift
+++ FirebaseDataConnect/Sources/DataConnect.swift
@@ -20,12 +20,10 @@ import FirebaseCore
 
 @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
 public class DataConnect {
+
   private var connectorConfig: ConnectorConfig
   private var app: FirebaseApp
-  fileprivate var settings: DataConnectSettings
-
-
-  private var emulatorSettings: DataConnectSettings?
+  private var settings: DataConnectSettings
 
   private var grpcClient: GrpcClient
   private var operationsManager: OperationsManager
@@ -33,7 +31,7 @@ public class DataConnect {
 
   // MARK: Static Creation
 
-  public class func dataConnect(connectorConfig: ConnectorConfig, app: FirebaseApp? = FirebaseApp.app(), settings: DataConnectSettings = DataConnectSettings()) -> DataConnect {
+  public class func dataConnect(app: FirebaseApp? = FirebaseApp.app(), connectorConfig: ConnectorConfig, settings: DataConnectSettings = DataConnectSettings()) -> DataConnect {
 
     guard let app = app else {
       fatalError("No Firebase App present")
@@ -42,12 +40,28 @@ public class DataConnect {
     return DataConnect(connectorConfig: connectorConfig, app: app, settings: settings)
   }
 
-  public class func useEmulator(connectorConfig: ConnectorConfig, app: FirebaseApp? = FirebaseApp.app(), host: String = "localhost",
-                                port: Int = 9510) -> DataConnect {
-    let emulatorSettings = DataConnectSettings(host: host, port: port, sslEnabled: false)
-    return DataConnect.dataConnect(connectorConfig: connectorConfig, app: app, settings: emulatorSettings)
-  }
+  // MARK: Emulator
 
+  public func useEmulator(host: String = "localhost",
+                          port: Int = 9510) {
+      settings = DataConnectSettings(host: host, port: port, sslEnabled: false)
+
+      // TODO: - shutdown grpc client
+      // self.grpcClient.close
+      // self.operations.close
+
+      guard let projectID = app.options.projectID else {
+        fatalError("Firebase DataConnect requires the projectID to be set in the app options")
+      }
+
+      grpcClient = GrpcClient(
+        projectId: projectID,
+        settings: settings,
+        connectorConfig: connectorConfig,
+        auth: Auth.auth(app: self.app))
+
+      operationsManager = OperationsManager(grpcClient: grpcClient)
+  }
 
   // MARK: Init
 
