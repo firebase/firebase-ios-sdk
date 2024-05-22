f3cebf981d1e6cde55f60fe26540b2515507ad2d
Nick Cooke <nickcooke@google.com>
2024-05-22 14:45:55 -0400

    [Release] Update FirestoreInternal binary for 10.27.0 / M148

--- Package.swift
+++ Package.swift
@@ -1572,8 +1572,8 @@ func firestoreTargets() -> [Target] {
     } else {
       return .binaryTarget(
         name: "FirebaseFirestoreInternal",
-        url: "https://dl.google.com/firebase/ios/bin/firestore/10.25.0/rc0/FirebaseFirestoreInternal.zip",
-        checksum: "afa9ec6af0712406a1fcd4a69d01d31156308de23d3a8e2622721c72fd72485e"
+        url: "https://dl.google.com/firebase/ios/bin/firestore/10.27.0/rc0/FirebaseFirestoreInternal.zip",
+        checksum: ""
       )
     }
   }()
