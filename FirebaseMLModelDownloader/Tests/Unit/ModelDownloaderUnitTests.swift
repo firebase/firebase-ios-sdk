0f93d9fbe04217457c30b90cb33be9873d07ed25
Paul Beusterien <paulbeusterien@google.com>
2024-07-15 11:36:32 -0700

    [ml-model-downloader] Update unit test deprecated API

--- FirebaseMLModelDownloader/Tests/Unit/ModelDownloaderUnitTests.swift
+++ FirebaseMLModelDownloader/Tests/Unit/ModelDownloaderUnitTests.swift
@@ -277,7 +277,7 @@
 
       guard let binaryData = try? modelOptions.serializedData(),
             let jsonData = try? modelOptions.jsonUTF8Data(),
-            let binaryEvent = try? ModelOptions(serializedData: binaryData),
+            let binaryEvent = try? ModelOptions(serializedBytes: binaryData),
             let jsonEvent = try? ModelOptions(jsonUTF8Data: jsonData) else {
         XCTFail("Encoding error.")
         return
