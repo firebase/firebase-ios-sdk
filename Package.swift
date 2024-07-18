1df34d1c5607c38bb25ab238d0a40b709dfc7854
Paul Beusterien <paulbeusterien@google.com>
2024-07-18 08:18:33 -0700

    review

--- Package.swift
+++ Package.swift
@@ -1313,9 +1313,6 @@ let package = Package(
       path: "FirebaseVertexAI/Tests/Unit",
       resources: [
         .process("vertexai-sdk-test-data/mock-responses"),
-        // Including this README ensures that SPM will always generate a `Bundle.module`, even if
-        // the mock-responses have not been downloaded with the update_vertexai_responses.sh script.
-        .process("README.md"),
       ],
       cSettings: [
         .headerSearchPath("../../../"),
