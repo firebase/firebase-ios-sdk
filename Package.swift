e6b2498e9ea2470684dcccf7606875d48245caf0
Paul Beusterien <paulbeusterien@google.com>
2024-07-18 07:32:21 -0700

    Revert "[Infra] Remove missing resource for Vertex AI (#13300)"
    This reverts commit f3db80a8d9ab8d70d3a1fb105e95582d6785118a.

--- Package.swift
+++ Package.swift
@@ -1312,6 +1312,7 @@ let package = Package(
       dependencies: ["FirebaseVertexAI", "SharedTestUtilities"],
       path: "FirebaseVertexAI/Tests/Unit",
       resources: [
+        .process("vertexai-sdk-test-data/mock-responses"),
         // Including this README ensures that SPM will always generate a `Bundle.module`, even if
         // the mock-responses have not been downloaded with the update_vertexai_responses.sh script.
         .process("README.md"),
