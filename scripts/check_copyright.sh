f18260f229b68ebda4a39a3b4133c0f36de7ce8d
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2024-07-25 12:51:41 -0400

    [Infra] Update check_copyright.sh to ignore root `third_party` directories (#13403)

--- scripts/check_copyright.sh
+++ scripts/check_copyright.sh
@@ -24,6 +24,7 @@ options=(
 list=$(git grep "${options[@]}" -- \
     '*.'{c,cc,cmake,h,js,m,mm,py,rb,sh,swift} \
     CMakeLists.txt '**/CMakeLists.txt' \
+    ':(exclude)third_party/**' \
     ':(exclude)**/third_party/**')
 
 # Allow copyrights before 2020 without LLC.
