c4809b92707ac9697e3b4635a09e4a071d5734af
cherylEnkidu <96084918+cherylEnkidu@users.noreply.github.com>
2024-08-19 23:36:08 +0800

    Move FirebaseLogger into DataConnect (#13516)

--- FirebaseDataConnect/Sources/Internal/FirebaseLogger/DataConnectLogger.swift
+++ FirebaseDataConnect/Sources/Internal/FirebaseLogger/DataConnectLogger.swift
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import FirebaseSharedSwift
-
 extension FirebaseLogger {
   static let dataConnect = FirebaseLogger(category: "data_connect")
 }
