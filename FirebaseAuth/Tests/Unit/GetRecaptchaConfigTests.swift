fdd4a27a57b3125f659e72a948f68fcaa8a9ae22
Pragati <pragatimodi@google.com>
2024-09-03 18:41:39 -0700

    add recaptcha config unit tests

--- FirebaseAuth/Tests/Unit/GetRecaptchaConfigTests.swift
+++ FirebaseAuth/Tests/Unit/GetRecaptchaConfigTests.swift
@@ -39,10 +39,10 @@ class GetRecaptchaConfigTests: RPCBaseTests {
     )
   }
 
-  /** @fn testSuccessfulGetRecaptchaConfigRequest
-      @brief This test simulates a successful @c getRecaptchaConfig Flow.
+  /** @fn testSuccessfulGetRecaptchaConfigRequestRecaptchaEnabled
+      @brief This test simulates a successful @c getRecaptchaConfig Flow when recaptcha is enabled.
    */
-  func testSuccessfulGetRecaptchaConfigRequest() async throws {
+  func testSuccessfulGetRecaptchaConfigRequestRecaptchaEnabled() async throws {
     let kTestRecaptchaKey = "projects/123/keys/456"
     let request = GetRecaptchaConfigRequest(requestConfiguration: makeRequestConfiguration())
 
@@ -51,6 +51,24 @@ class GetRecaptchaConfigTests: RPCBaseTests {
     rpcIssuer.rceMode = enforcementMode
     let response = try await AuthBackend.call(with: request)
     XCTAssertEqual(response.recaptchaKey, kTestRecaptchaKey)
+    XCTAssertEqual(
+      response.enforcementState,
+      [
+        ["provider": "EMAIL_PASSWORD_PROVIDER", "enforcementState": enforcementMode],
+        ["provider": "PHONE_PROVIDER", "enforcementState": enforcementMode],
+      ]
+    )
+  }
+
+  /** @fn testSuccessfulGetRecaptchaConfigRequestRecaptchaDisabled
+   @brief This test simulates a successful @c getRecaptchaConfig Flow when recaptcha is disabled.
+   */
+  func testSuccessfulGetRecaptchaConfigRequestRecaptchaDisabled() async throws {
+    let request = GetRecaptchaConfigRequest(requestConfiguration: makeRequestConfiguration())
+    let enforcementMode = "OFF"
+    rpcIssuer.rceMode = enforcementMode
+    let response = try await AuthBackend.call(with: request)
+    XCTAssertEqual(response.recaptchaKey, nil)
     XCTAssertEqual(
       response.enforcementState,
       [
