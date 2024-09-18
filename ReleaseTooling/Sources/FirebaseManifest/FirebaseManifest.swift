96a3341ea7e26ab422347f20650c1cf1ee9a6446
Andrew Heard <andrewheard@google.com>
2024-09-18 13:58:19 -0400

    Remove `FirebaseVertexAI` from `FirebaseManifest`

--- ReleaseTooling/Sources/FirebaseManifest/FirebaseManifest.swift
+++ ReleaseTooling/Sources/FirebaseManifest/FirebaseManifest.swift
@@ -53,7 +53,6 @@ public let shared = Manifest(
     Pod("FirebasePerformance", platforms: ["ios", "tvos"], zip: true),
     Pod("FirebaseStorage", zip: true),
     Pod("FirebaseMLModelDownloader", isBeta: true, zip: true),
-    Pod("FirebaseVertexAI", isBeta: true, allowWarnings: true, zip: true),
     Pod("Firebase", allowWarnings: true, platforms: ["ios", "tvos", "macos"], zip: true),
   ]
 )
