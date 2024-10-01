2bdaa3d9d6f22b6cce95501e2f000951ba93e119
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-10-01 12:15:56 +0330

    docs: fix a typo in a comment

--- FirebaseInAppMessaging/Tests/Unit/FIRIAMMessageClientCacheTests.m
+++ FirebaseInAppMessaging/Tests/Unit/FIRIAMMessageClientCacheTests.m
@@ -328,7 +328,7 @@
   self.clientCache.analycisEventDislayCheckFlow = mockAnalyticsEventFlow;
 
   // m2 and m4 are messages rendered on 'test_event' Firebase Analytics event
-  // so we espect the analytics event listening flow to be started
+  // so we expect the analytics event listening flow to be started
   OCMExpect([mockAnalyticsEventFlow start]);
   [self.clientCache setMessageData:@[ m1, m2, m3, m4 ]];
   OCMVerifyAll((id)mockAnalyticsEventFlow);
