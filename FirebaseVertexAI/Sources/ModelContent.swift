8a99f992023e717304c5414b9ce13ddd521a41d7
Andrew Heard <andrewheard@google.com>
2024-10-03 13:06:38 -0400

    Remove generateContent autoclosure

--- FirebaseVertexAI/Sources/ModelContent.swift
+++ FirebaseVertexAI/Sources/ModelContent.swift
@@ -31,6 +31,23 @@ public extension ModelContent.Part {
 }
 
 @available(iOS 15.0, macOS 11.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, *)
+extension [ModelContent] {
+  // TODO: Rename and refactor this.
+  func throwIfError() throws {
+    for content in self {
+      for part in content.parts {
+        switch part {
+        case let errorPart as ErrorPart:
+          throw errorPart
+        default:
+          break
+        }
+      }
+    }
+  }
+}
+
+@available(iOS 15.0, macOS 11.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, *)
 public struct TextPart: ModelContent.Part {
   public let textValue: String
 
