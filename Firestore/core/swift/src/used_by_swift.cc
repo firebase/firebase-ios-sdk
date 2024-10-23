163cbddea7192c3eedb94b35f1a694e929000868
cherylEnkidu <cheryllin@google.com>
2024-10-22 22:19:15 -0400

    init

---
+++ Firestore/core/swift/src/used_by_swift.cc
@@ -0,0 +1,10 @@
+//
+// Created by Cheryl Lin on 2024-10-22.
+//
+
+#include "Firestore/core/swift/include/used_by_swift.h"
+#include <iostream>
+
+UsedBySwift::UsedBySwift(std::string content) {
+  std::cout << "ctor runs with value: " << content << std::endl;
+}
