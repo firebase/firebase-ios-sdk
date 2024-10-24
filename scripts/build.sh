7a0c4ad012b9dddab825fb6b6c33506b4ec3d192
Andrew Heard <andrewheard@google.com>
2024-10-24 11:09:21 -0400

    Fix scheme name

--- scripts/build.sh
+++ scripts/build.sh
@@ -506,7 +506,7 @@ case "$product-$platform-$method" in
   VertexIntegration-*-*)
     RunXcodebuild \
       -project 'FirebaseVertexAI/Tests/TestApp/VertexAITestApp.xcodeproj' \
-      -scheme "VertexAISample" \
+      -scheme "VertexAITestApp" \
       "${xcb_flags[@]}" \
       build \
       test
