c50ccdab1b77fd6e622949baca21c1f3c2da47ef
Andrew Heard <andrewheard@google.com>
2024-10-24 13:59:38 -0400

    Add AppCheckDebugProvider

--- FirebaseVertexAI/Tests/TestApp/VertexAITestApp/VertexAITestApp.swift
+++ FirebaseVertexAI/Tests/TestApp/VertexAITestApp/VertexAITestApp.swift
@@ -12,12 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import FirebaseAppCheck
 import FirebaseCore
 import SwiftUI
 
 @main
 struct VertexAITestApp: App {
   init() {
+    AppCheck.setAppCheckProviderFactory(AppCheckDebugProviderFactory())
     FirebaseApp.configure()
   }
 
