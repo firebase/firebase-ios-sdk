df79d27e955b0c218464764f820a55f3c2b657d2
Nick Cooke <nickcooke@google.com>
2024-10-30 17:02:50 -0400

    [Auth] Lock down property visibility for SetAccountInfoRequest

--- FirebaseAuth/Sources/Swift/Backend/RPC/SetAccountInfoRequest.swift
+++ FirebaseAuth/Sources/Swift/Backend/RPC/SetAccountInfoRequest.swift
@@ -89,47 +89,47 @@ class SetAccountInfoRequest: IdentityToolkitRequest, AuthRPCRequest {
   var displayName: String?
 
   /// The local ID of the user.
-  var localID: String?
+  private let localID: String? = nil
 
   /// The email of the user.
-  var email: String?
+  var email: String? = nil
 
   /// The photoURL of the user.
   var photoURL: URL?
 
   /// The new password of the user.
-  var password: String?
+  var password: String? = nil
 
   /// The associated identity providers of the user.
-  var providers: [String]?
+  private let providers: [String]? = nil
 
   /// The out-of-band code of the change email request.
   var oobCode: String?
 
   /// Whether to mark the email as verified or not.
-  var emailVerified: Bool = false
+  private let emailVerified: Bool = false
 
   /// Whether to mark the user to upgrade to federated login.
-  var upgradeToFederatedLogin: Bool = false
+  private let upgradeToFederatedLogin: Bool = false
 
   /// The captcha challenge.
-  var captchaChallenge: String?
+  private let captchaChallenge: String? = nil
 
   /// Response to the captcha.
-  var captchaResponse: String?
+  private let captchaResponse: String? = nil
 
   /// The list of user attributes to delete.
   ///
   /// Every element of the list must be one of the predefined constant starts with
   /// `SetAccountInfoUserAttribute`.
-  var deleteAttributes: [String]?
+  private let deleteAttributes: [String]? = nil
 
   /// The list of identity providers to delete.
   var deleteProviders: [String]?
 
   /// Whether the response should return access token and refresh token directly.
   /// The default value is `true` .
-  var returnSecureToken: Bool = true
+  private let returnSecureToken: Bool = true
 
   init(requestConfiguration: AuthRequestConfiguration) {
     super.init(endpoint: kSetAccountInfoEndpoint, requestConfiguration: requestConfiguration)
