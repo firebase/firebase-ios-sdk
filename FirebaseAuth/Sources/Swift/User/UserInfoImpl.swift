905bc2e1d3c777e6e79691452e4c9bd3fdda8a83
Nick Cooke <nickcooke@google.com>
2024-11-01 19:30:11 -0400

    Attempt to fix int. test build (1)

--- FirebaseAuth/Sources/Swift/User/UserInfoImpl.swift
+++ FirebaseAuth/Sources/Swift/User/UserInfoImpl.swift
@@ -14,6 +14,7 @@
 
 import Foundation
 
+@available(iOS 13, tvOS 13, macOS 10.15, macCatalyst 13, watchOS 7, *)
 extension UserInfoImpl: NSSecureCoding {}
 
 @available(iOS 13, tvOS 13, macOS 10.15, macCatalyst 13, watchOS 7, *)
