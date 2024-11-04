729ee55b18af68b37f491f2df1aed31c8c9ea7c9
Gorbenko Roman <45801227+rofle100lvl@users.noreply.github.com>
2024-11-04 17:23:06 +0100

    Make Package.Swift Explicit

--- Package.swift
+++ Package.swift
@@ -111,7 +111,7 @@ let package = Package(
     ),
     .library(
       name: "FirebasePerformance",
-      targets: ["FirebasePerformanceTarget"]
+      targets: ["FirebasePerformanceTarget", "FirebasePerformance"]
     ),
     .library(
       name: "FirebaseRemoteConfig",
