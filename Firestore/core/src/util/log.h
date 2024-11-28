49eee517b241d5b2b88054cbfbd3f51f83c8f22c
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2024-11-27 20:41:56 -0500

    Update Firestore/core/src/util/log.h

--- Firestore/core/src/util/log.h
+++ Firestore/core/src/util/log.h
@@ -11,7 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. Hello
+ * limitations under the License.
  */
 
 #ifndef FIRESTORE_CORE_SRC_UTIL_LOG_H_
