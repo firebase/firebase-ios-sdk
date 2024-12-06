e86d6c1b705caf7ce967bc2b413802c713744df2
Andrew Heard <andrewheard@google.com>
2024-12-06 16:00:49 -0500

    Make `images` and `raiFilteredReason` public in `ImageGenerationResponse`

--- FirebaseVertexAI/Sources/Types/Internal/Imagen/ImageGenerationResponse.swift
+++ FirebaseVertexAI/Sources/Types/Internal/Imagen/ImageGenerationResponse.swift
@@ -16,8 +16,8 @@ import Foundation
 
 @available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, *)
 public struct ImageGenerationResponse<ImageType: ImagenImageRepresentable> {
-  let images: [ImageType]
-  let raiFilteredReason: String?
+  public let images: [ImageType]
+  public let raiFilteredReason: String?
 }
 
 // MARK: - Codable Conformances
