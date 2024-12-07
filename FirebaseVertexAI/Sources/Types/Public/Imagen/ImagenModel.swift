f6b95def24b9425468c47596b41617f9762aab0b
Andrew Heard <andrewheard@google.com>
2024-12-07 12:47:55 -0500

    [Vertex AI] Add `ImagenModel` with `generateImages` functions (#14226)

---
+++ FirebaseVertexAI/Sources/Types/Public/Imagen/ImagenModel.swift
@@ -0,0 +1,92 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import FirebaseAppCheckInterop
+import FirebaseAuthInterop
+import Foundation
+
+@available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, *)
+public final class ImagenModel {
+  /// The resource name of the model in the backend; has the format "models/model-name".
+  let modelResourceName: String
+
+  /// The backing service responsible for sending and receiving model requests to the backend.
+  let generativeAIService: GenerativeAIService
+
+  /// Configuration parameters for sending requests to the backend.
+  let requestOptions: RequestOptions
+
+  init(name: String,
+       projectID: String,
+       apiKey: String,
+       requestOptions: RequestOptions,
+       appCheck: AppCheckInterop?,
+       auth: AuthInterop?,
+       urlSession: URLSession = .shared) {
+    modelResourceName = name
+    generativeAIService = GenerativeAIService(
+      projectID: projectID,
+      apiKey: apiKey,
+      appCheck: appCheck,
+      auth: auth,
+      urlSession: urlSession
+    )
+    self.requestOptions = requestOptions
+  }
+
+  public func generateImages(prompt: String) async throws
+    -> ImageGenerationResponse<ImagenInlineDataImage> {
+    return try await generateImages(
+      prompt: prompt,
+      parameters: imageGenerationParameters(storageURI: nil)
+    )
+  }
+
+  public func generateImages(prompt: String, storageURI: String) async throws
+    -> ImageGenerationResponse<ImagenFileDataImage> {
+    return try await generateImages(
+      prompt: prompt,
+      parameters: imageGenerationParameters(storageURI: storageURI)
+    )
+  }
+
+  func generateImages<T: Decodable>(prompt: String,
+                                    parameters: ImageGenerationParameters) async throws
+    -> ImageGenerationResponse<T> {
+    let request = ImageGenerationRequest<T>(
+      model: modelResourceName,
+      options: requestOptions,
+      instances: [ImageGenerationInstance(prompt: prompt)],
+      parameters: parameters
+    )
+
+    return try await generativeAIService.loadRequest(request: request)
+  }
+
+  func imageGenerationParameters(storageURI: String?) -> ImageGenerationParameters {
+    // TODO(#14221): Add support for configuring these parameters.
+    return ImageGenerationParameters(
+      sampleCount: 1,
+      storageURI: storageURI,
+      seed: nil,
+      negativePrompt: nil,
+      aspectRatio: nil,
+      safetyFilterLevel: nil,
+      personGeneration: nil,
+      outputOptions: nil,
+      addWatermark: nil,
+      includeResponsibleAIFilterReason: true
+    )
+  }
+}
