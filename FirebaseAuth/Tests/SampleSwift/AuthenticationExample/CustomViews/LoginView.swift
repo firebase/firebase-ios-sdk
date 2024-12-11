ab76008a860b4e5f02e933821e1090de6d5d8967
Nick Cooke <nickcooke@google.com>
2024-12-11 14:34:22 -0500

    Remove MFA bits from LoginView.swift

--- FirebaseAuth/Tests/SampleSwift/AuthenticationExample/CustomViews/LoginView.swift
+++ FirebaseAuth/Tests/SampleSwift/AuthenticationExample/CustomViews/LoginView.swift
@@ -18,12 +18,15 @@ import FirebaseAuth
 
 struct LoginView: View {
   @Environment(\.dismiss) private var dismiss
-  @State private var multiFactorResolver: MultiFactorResolver? = nil
-  @State private var onetimePasscode = ""
-  @State private var showingAlert = false
 
   @State private var email: String = ""
   @State private var password: String = ""
+
+  private weak var delegate: (any LoginDelegate)?
+
+  init(delegate: (any LoginDelegate)? = nil) {
+    self.delegate = delegate
+  }
 
   var body: some View {
     Group {
@@ -74,12 +77,6 @@ struct LoginView: View {
       Spacer()
     }
     .padding()
-    .alert("Enter one time passcode.", isPresented: $showingAlert) {
-      TextField("Verification Code", text: $onetimePasscode)
-        .textInputAutocapitalization(.never)
-      Button("Cancel", role: .cancel) {}
-      Button("Submit", action: submitOnetimePasscode)
-    }
   }
 
   private func login() {
@@ -88,25 +85,19 @@ struct LoginView: View {
         _ = try await AppManager.shared
           .auth()
           .signIn(withEmail: email, password: password)
-        //              } catch let error as AuthErrorCode
-        //                where error.code == .secondFactorRequired {
-        //                // error as? AuthErrorCode == nil because AuthErrorUtils returns generic
-        //                /Errors
-        //                // https://firebase.google.com/docs/auth/ios/totp-mfa#sign_in_users_with_a_second_factor
-        //                // TODO(ncooke3): Fix?
+        // TODO(ncooke3): Investigate possible improvements.
+//      } catch let error as AuthErrorCode
+//        where error.code == .secondFactorRequired {
+//        // error as? AuthErrorCode == nil because AuthErrorUtils returns generic
+//        /Errors
+//        // https://firebase.google.com/docs/auth/ios/totp-mfa#sign_in_users_with_a_second_factor
       } catch let error as NSError
         where error.code == AuthErrorCode.secondFactorRequired.rawValue {
         let mfaKey = AuthErrorUserInfoMultiFactorResolverKey
-        guard
-          let resolver = error.userInfo[mfaKey] as? MultiFactorResolver,
-          let multiFactorInfo = resolver.hints.first
-        else { return }
-        if multiFactorInfo.factorID == TOTPMultiFactorID {
-          // Show the alert to enter the TOTP verification code.
-          multiFactorResolver = resolver
-          showingAlert = true
-        } else {
-          // TODO(ncooke3): Implement handling of other MFA provider (phone).
+        guard let resolver = error.userInfo[mfaKey] as? MultiFactorResolver else { throw error }
+        await MainActor.run {
+          dismiss()
+          delegate?.loginDidOccur(resolver: resolver)
         }
       } catch {
         print(error.localizedDescription)
@@ -122,31 +113,13 @@ struct LoginView: View {
           password: password
         )
         // Sign-in was successful.
-        dismiss()
+        await MainActor.run {
+          dismiss()
+          delegate?.loginDidOccur(resolver: nil)
+        }
       } catch {
         // TODO(ncooke3): Implement error display.
         print(error.localizedDescription)
-      }
-    }
-  }
-
-  private func submitOnetimePasscode() {
-    Task {
-      guard onetimePasscode.count > 0 else { return }
-      let multiFactorInfo = multiFactorResolver!.hints[0]
-      let assertion = TOTPMultiFactorGenerator.assertionForSignIn(
-        withEnrollmentID: multiFactorInfo.uid,
-        // TODO(ncooke3): Probably should avoid network request if empty passcode.
-        oneTimePassword: self.onetimePasscode
-      )
-      do {
-        _ = try await multiFactorResolver!.resolveSignIn(with: assertion)
-        // MFA login was successful.
-        dismiss()
-      } catch {
-        // Wrong or expired OTP. Re-prompt the user.
-        // TODO(ncooke3): Show error to user.
-        print(error)
       }
     }
   }
