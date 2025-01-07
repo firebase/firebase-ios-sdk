7d2362bc014d79f683cfab3142b034668277ee55
cherylEnkidu <cheryllin@google.com>
2025-01-07 14:27:11 -0500

    fixing format

--- Firestore/Swift/Source/SwiftAPI/Pipeline.swift
+++ Firestore/Swift/Source/SwiftAPI/Pipeline.swift
@@ -1,9 +1,20 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to
+// build this package.
+
+// Copyright 2025 Google LLC
 //
-//  Pipeline.swift
-//  Firebase
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Cheryl Lin on 2024-12-18.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #if SWIFT_PACKAGE
   import FirebaseFirestoreCpp
