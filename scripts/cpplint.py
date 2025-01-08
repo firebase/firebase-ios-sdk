aebe23e7e3383403c6a95bd786628f9007b7f1ff
Denver Coneybeare <dconeybe@google.com>
2025-01-08 17:35:00 -0500

    also remove c++14 warnings

--- scripts/cpplint.py
+++ scripts/cpplint.py
@@ -270,7 +270,6 @@ Syntax: cpplint.py [--verbose=#] [--output=emacs|eclipse|vs7|junit|sed|gsed]
 # here!  cpplint_unittest.py should tell you if you forget to do this.
 _ERROR_CATEGORIES = [
     'build/class',
-    'build/c++14',
     'build/c++tr1',
     'build/deprecated',
     'build/endif_comment',
