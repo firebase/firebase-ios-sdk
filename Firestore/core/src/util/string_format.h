68ccc04ae18176dd4567d7be5a8dcb10c7544912
Denver Coneybeare <dconeybe@google.com>
2025-01-09 17:12:30 -0500

    format code

--- Firestore/core/src/util/string_format.h
+++ Firestore/core/src/util/string_format.h
@@ -65,14 +65,15 @@ struct FormatChoice<5> {};
 class FormatArg final : public absl::AlphaNum {
  public:
   template <typename T>
-  FormatArg(T&& value,
-    // TODO(b/388888512) Remove the usage of StringifySink since it is not part
-    //  of absl's public API. Moreover, subclassing AlphaNum is not supported
-    //  either, so find a way to do this without these two caveats.
-    //  See https://github.com/firebase/firebase-ios-sdk/pull/14331 for a
-    //  partial proposal.
-            absl::strings_internal::StringifySink&& sink =
-                {})  // NOLINT(runtime/explicit)
+  FormatArg(
+      T&& value,
+      // TODO(b/388888512) Remove the usage of StringifySink since it is not
+      //  part of absl's public API. Moreover, subclassing AlphaNum is not
+      //  supported either, so find a way to do this without these two caveats.
+      //  See https://github.com/firebase/firebase-ios-sdk/pull/14331 for a
+      //  partial proposal.
+      absl::strings_internal::StringifySink&& sink =
+          {})  // NOLINT(runtime/explicit)
       : FormatArg{std::forward<T>(value), std::move(sink),
                   internal::FormatChoice<0>{}} {
   }
