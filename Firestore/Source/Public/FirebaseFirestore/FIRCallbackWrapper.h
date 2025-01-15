a318a204923f92e04dceac7bcc5d7cc892c3aeee
wu-hui <wu.hui.github@gmail.com>
2025-01-15 15:07:36 -0500

    std::unique_ptr is not supported in swift

--- Firestore/Source/Public/FirebaseFirestore/FIRCallbackWrapper.h
+++ Firestore/Source/Public/FirebaseFirestore/FIRCallbackWrapper.h
@@ -52,7 +52,7 @@ NS_SWIFT_NAME(CallbackWrapper)
 // are invoked on a different thread than the one they were originally defined in. If this callback
 // is expected to be called on a different thread, it should be marked as `Sendable` to ensure
 // thread safety.
-+ (std::unique_ptr<core::EventListener<std::vector<api::PipelineResult>>>)
++ (std::shared_ptr<core::EventListener<std::vector<api::PipelineResult>>>)
     wrapPipelineCallback:(std::shared_ptr<api::Firestore>)firestore
               completion:(void (^NS_SWIFT_SENDABLE)(
                              std::shared_ptr<std::vector<api::PipelineResult>> result,
