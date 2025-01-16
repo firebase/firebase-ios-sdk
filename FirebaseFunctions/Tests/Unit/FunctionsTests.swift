4ee820eb99dc510c637e6b2a758cd94f40e44392
Eblen M <eblenmacari@gmail.com>
2025-01-15 18:12:19 -0600

    Update FunctionsTests.swift
    Remove optionals.

--- FirebaseFunctions/Tests/Unit/FunctionsTests.swift
+++ FirebaseFunctions/Tests/Unit/FunctionsTests.swift
@@ -405,29 +405,27 @@ class FunctionsTests: XCTestCase {
       XCTExpectFailure("Failed to download stream")
   }
 
-  func testGenerateStreamContentCanceled() async {
+  func testGenerateStreamContentCanceled() async{
     var response = [String]()
     let responseQueue = DispatchQueue(label: "responseQueue")
     let options = HTTPSCallableOptions(requireLimitedUseAppCheckTokens: true)
     let input: [String: Any] = ["data": "Why is the sky blue"]
 
     let task = Task.detached { [self] in
-      let stream = try await functions?.stream(
+      let stream = try await functions!.stream(
         at: URL(string: "http://127.0.0.1:5001/demo-project/us-central1/genStream")!,
         withObject: input,
         options: options,
         timeout: 4.0
       )
       // First chunk of the stream comes as NSDictionary
-      if let stream = stream {
         for try await result in stream {
           if let dataChunk = result.data as? NSDictionary {
             for (key, value) in dataChunk {
               responseQueue.sync {
                 response.append("\(key) \(value)")
-              }
+              
             }
-          } else {
             // Last chunk is the concatenated result so we have to parse it as String else will
             // fail.
             if let dataString = result.data as? String {
