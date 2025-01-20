deb3fd0d098a1c6e3ce6989c08c6fe3b2a61caa2
Denver Coneybeare <dconeybe@google.com>
2025-01-20 12:15:44 -0500

    Firestore: Remove obsolete NOLINT comments (#14347)

--- Firestore/core/include/firebase/firestore/timestamp.h
+++ Firestore/core/include/firebase/firestore/timestamp.h
@@ -23,8 +23,8 @@
 #include <string>
 
 #if !defined(_STLPORT_VERSION)
-#include <chrono>  // NOLINT(build/c++11)
-#endif             // !defined(_STLPORT_VERSION)
+#include <chrono>
+#endif  // !defined(_STLPORT_VERSION)
 
 namespace firebase {
 
