5eb33bd1f8a55cb799987764baca341cebdce879
Andrew Heard <andrewheard@google.com>
2025-02-04 18:29:09 -0500

    Add docs for `ImagenSafetySettings` and related types

--- FirebaseVertexAI/Sources/Types/Public/Imagen/ImagenSafetySettings.swift
+++ FirebaseVertexAI/Sources/Types/Public/Imagen/ImagenSafetySettings.swift
@@ -14,11 +14,23 @@
 
 import Foundation
 
+/// Settings for controlling the aggressiveness of filtering out sensitive content.
+///
+/// See the [Responsible AI and usage
+/// guidelines](https://cloud.google.com/vertex-ai/generative-ai/docs/image/responsible-ai-imagen#config-safety-filters)
+/// for more details.
 @available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, *)
 public struct ImagenSafetySettings {
   let safetyFilterLevel: ImagenSafetyFilterLevel?
   let personFilterLevel: ImagenPersonFilterLevel?
 
+  /// Initializes safety settings for the Imagen model.
+  ///
+  /// - Parameters:
+  ///   - safetyFilterLevel: A filter level controlling how aggressively to filter out sensitive
+  ///   content from generated images.
+  ///   - personFilterLevel: A filter level controlling whether generation of images containing
+  ///   people or faces is allowed.
   public init(safetyFilterLevel: ImagenSafetyFilterLevel? = nil,
               personFilterLevel: ImagenPersonFilterLevel? = nil) {
     self.safetyFilterLevel = safetyFilterLevel
