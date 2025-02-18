05700ea581666fcb3fc23c808a4df5c471703831
Eblen M <joseeblen@xwf.google.com>
2025-02-18 13:35:53 -0600

    Update FirebaseFunctions/Sources/Callable+Codable.swift
    Accepted suggestion
    
    Co-authored-by: Nick Cooke <36927374+ncooke3@users.noreply.github.com>

--- FirebaseFunctions/Sources/Callable+Codable.swift
+++ FirebaseFunctions/Sources/Callable+Codable.swift
@@ -15,7 +15,7 @@
 import FirebaseSharedSwift
 import Foundation
 
-@available(macOS 12.0, iOS 15.0, tvOS 8.0,*)
+@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
 public enum StreamResponse<Message: Decodable, Result: Decodable>: Decodable {
   /// The message yielded by the callable function.
   case message(Message)
