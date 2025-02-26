672c5eda0916d6327fa65690b3e521321233d8b3
Nick Cooke <nickcooke@google.com>
2025-02-26 18:35:23 -0500

    Add doc header

--- FirebaseFunctions/Sources/Callable+Codable.swift
+++ FirebaseFunctions/Sources/Callable+Codable.swift
@@ -215,6 +215,12 @@ public enum StreamResponse<Message: Decodable, Result: Decodable>: Decodable,
 
 @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
 public extension Callable {
+  /// Creates a stream that yields responses from the streaming callable function.
+  ///
+  /// - Parameter data: The `Request` data to pass to the callable function.
+  /// - Throws: A ``FunctionsError`` if the parameter `data` cannot be encoded.
+  /// - Returns: A stream wrapping responses yielded by the streaming callable
+  /// function  or a ``FunctionsError`` if an error occurred.
   func stream(_ data: Request? = nil) throws -> AsyncThrowingStream<Response, Error> {
     let encoded: Any
     do {
