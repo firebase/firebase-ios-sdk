041b9d16d7c78ec8bfe57dee50301f7daddf3d03
Andrew Heard <andrewheard@google.com>
2025-03-12 11:44:55 -0400

    Add system instruction integration tests

--- FirebaseVertexAI/Tests/TestApp/Tests/Integration/CountTokensIntegrationTests.swift
+++ FirebaseVertexAI/Tests/TestApp/Tests/Integration/CountTokensIntegrationTests.swift
@@ -40,6 +40,10 @@ struct CountTokensIntegrationTests {
     SafetySetting(harmCategory: .dangerousContent, threshold: .blockLowAndAbove),
     SafetySetting(harmCategory: .civicIntegrity, threshold: .blockLowAndAbove),
   ]
+  let systemInstruction = ModelContent(
+    role: "system",
+    parts: "You are a friendly and helpful assistant."
+  )
 
   @Test(arguments: InstanceConfig.allConfigs)
   func countTokens_text(_ config: InstanceConfig) async throws {
@@ -63,5 +67,56 @@ struct CountTokensIntegrationTests {
     let promptTokensDetails = try #require(response.promptTokensDetails.first)
     #expect(promptTokensDetails.modality == .text)
     #expect(promptTokensDetails.tokenCount == response.totalTokens)
+  }
+
+  @Test(arguments: [
+    InstanceConfig.vertexV1,
+    InstanceConfig.vertexV1Beta,
+    /* System instructions are not supported on the v1 Developer API. */
+    InstanceConfig.developerV1Beta,
+  ])
+  func countTokens_text_systemInstruction(_ config: InstanceConfig) async throws {
+    let model = VertexAI.componentInstance(config).generativeModel(
+      modelName: ModelNames.gemini2Flash,
+      generationConfig: generationConfig,
+      safetySettings: safetySettings,
+      systemInstruction: systemInstruction // Not supported on the v1 Developer API
+    )
+
+    let response = try await model.countTokens("What is your favourite colour?")
+
+    #expect(response.totalTokens == 14)
+    switch config.apiConfig.service {
+    case .vertexAI:
+      #expect(response.totalBillableCharacters == 61)
+    case .developer:
+      #expect(response.totalBillableCharacters == nil)
+    }
+    #expect(response.promptTokensDetails.count == 1)
+    let promptTokensDetails = try #require(response.promptTokensDetails.first)
+    #expect(promptTokensDetails.modality == .text)
+    #expect(promptTokensDetails.tokenCount == response.totalTokens)
+  }
+
+  @Test(arguments: [
+    /* System instructions are not supported on the v1 Developer API. */
+    InstanceConfig.developerV1,
+  ])
+  func countTokens_text_systemInstruction_unsupported(_ config: InstanceConfig) async throws {
+    let model = VertexAI.componentInstance(config).generativeModel(
+      modelName: ModelNames.gemini2Flash,
+      systemInstruction: systemInstruction // Not supported on the v1 Developer API
+    )
+
+    try await #require(
+      throws: BackendError.self,
+      """
+      If this test fails (i.e., `countTokens` succeeds), remove \(config) from this test and add it
+      to `countTokens_text_systemInstruction`.
+      """,
+      performing: {
+        try await model.countTokens("What is your favourite colour?")
+      }
+    )
   }
 }
