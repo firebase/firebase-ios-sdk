d83ee8e5e610942e232a8547f542c89569692b27
Nick Cooke <nickcooke@google.com>
2025-03-12 17:22:08 -0400

    Disable swift 6 for non-sendable type

--- FirebaseCore/Internal/Tests/Unit/HeartbeatStorageTests.swift
+++ FirebaseCore/Internal/Tests/Unit/HeartbeatStorageTests.swift
@@ -16,7 +16,7 @@
 import XCTest
 
 extension HeartbeatsBundle {
-  static let testHeartbeatBundle: Self = {
+  nonisolated(unsafe) static let testHeartbeatBundle: Self = {
     var heartbeatBundle = HeartbeatsBundle(capacity: 1)
     heartbeatBundle.append(Heartbeat(agent: "dummy_agent", date: Date()))
     return heartbeatBundle
