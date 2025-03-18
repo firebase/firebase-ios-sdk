08b406a9857153ec185910ad83e65ef84cad4989
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2025-03-18 12:30:12 -0400

    add foundation import

--- FirebaseCore/Internal/Sources/Utilities/AtomicBox.swift
+++ FirebaseCore/Internal/Sources/Utilities/AtomicBox.swift
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import Foundation
+
 final class AtomicBox<T> {
   private var _value: T
   private let lock = NSLock()
