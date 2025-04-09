0b6091c6ca1bdaa1dfb3e9a99c553eae55582178
Paul Beusterien <paulbeusterien@google.com>
2025-04-09 16:00:15 -0700

    More CI updates for Xcode 16 minimum (#14655)
    Co-authored-by: Nick Cooke <nickcooke@google.com>

--- IntegrationTesting/CocoapodsIntegrationTest/scripts/build_with_environment.sh
+++ IntegrationTesting/CocoapodsIntegrationTest/scripts/build_with_environment.sh
@@ -27,7 +27,7 @@ function runXcodebuild() {
     -workspace 'CocoapodsIntegrationTest.xcworkspace'
     -scheme 'CocoapodsIntegrationTest'
     -sdk 'iphonesimulator'
-    -destination 'platform=iOS Simulator,name=iPhone 15'
+    -destination 'platform=iOS Simulator,name=iPhone 16'
     CODE_SIGNING_REQUIRED=NO
     clean
     build
