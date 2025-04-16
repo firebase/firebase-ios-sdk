e3ee26981739927f9ee548b5f86eeedd67e2bdc9
Nick Cooke <nickcooke@google.com>
2025-04-16 10:25:31 -0400

    [Infra] Temp. fix for RC's iOS integration tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -501,9 +501,13 @@ case "$product-$platform-$method" in
       AppHost-FirebaseRemoteConfig-Unit-Tests \
       ../../../FirebaseRemoteConfig/Tests/Swift/AccessToken.json
 
+    # Integration tests are only run on iOS to minimize flake failures.
+    # TODO(ncooke3): Remove -sdk and -destination flags.
     RunXcodebuild \
       -workspace 'gen/FirebaseRemoteConfig/FirebaseRemoteConfig.xcworkspace' \
       -scheme "FirebaseRemoteConfig-Unit-swift-api-tests" \
+      -sdk 'iphonesimulator' \
+      -destination 'platform=iOS Simulator,name=iPhone 16,OS=18.3.1' \
       "${xcb_flags[@]}" \
       build \
       test
