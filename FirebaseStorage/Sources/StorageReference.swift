80dfe67b6f70f6b4999bea3a85d7b4c856e05cb2
Morgan Chen <morganchen12@gmail.com>
2025-04-24 14:56:47 -0700

    re-add missing foundation import

--- FirebaseStorage/Sources/StorageReference.swift
+++ FirebaseStorage/Sources/StorageReference.swift
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import Foundation
 import FirebaseCoreInternal
 
 /// `StorageReference` represents a reference to a Google Cloud Storage object. Developers can
