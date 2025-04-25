15e6b03e5b56466cae27792a2ccbdc221e05044d
Andrew Heard <andrewheard@google.com>
2025-04-25 11:22:36 -0400

    Fix import in `GenerateContentResponseTests.swift`

--- FirebaseAI/Tests/Unit/Types/GenerateContentResponseTests.swift
+++ FirebaseAI/Tests/Unit/Types/GenerateContentResponseTests.swift
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import FirebaseVertexAI
+import FirebaseAI
 import XCTest
 
 @available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, *)
