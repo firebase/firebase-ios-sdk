78f272f03b82d0f8c7359e3a9cb714312ea24ee0
Nick Cooke <nickcooke@google.com>
2025-04-29 11:48:53 -0400

    Revert SharedSwift change

--- FirebaseSharedSwift/Sources/third_party/FirebaseDataEncoder/FirebaseDataEncoder.swift
+++ FirebaseSharedSwift/Sources/third_party/FirebaseDataEncoder/FirebaseDataEncoder.swift
@@ -286,7 +286,7 @@ public class FirebaseDataEncoder {
   /// - returns: A new `Data` value containing the encoded JSON data.
   /// - throws: `EncodingError.invalidValue` if a non-conforming floating-point value is encountered during encoding, and the encoding strategy is `.throw`.
   /// - throws: An error if any value throws an error during encoding.
-  open func encode<T : Encodable>(_ value: T) throws -> sending Any {
+  open func encode<T : Encodable>(_ value: T) throws -> Any {
     let encoder = __JSONEncoder(options: self.options)
 
     guard let topLevel = try encoder.box_(value) else {
