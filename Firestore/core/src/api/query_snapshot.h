dde6ee92d47a20c9c61dc0ed041a785315d10194
wu-hui <wu.hui.github@gmail.com>
2025-04-30 11:07:57 -0400

    [realppl 9] realppl public api and integration tests

--- Firestore/core/src/api/query_snapshot.h
+++ Firestore/core/src/api/query_snapshot.h
@@ -22,14 +22,107 @@
 #include <utility>
 
 #include "Firestore/core/src/api/api_fwd.h"
+#include "Firestore/core/src/api/document_change.h"
+#include "Firestore/core/src/api/document_snapshot.h"
 #include "Firestore/core/src/api/snapshot_metadata.h"
 #include "Firestore/core/src/core/event_listener.h"
 #include "Firestore/core/src/core/query.h"
 #include "Firestore/core/src/core/view_snapshot.h"
+#include "Firestore/core/src/util/exception.h"
 
 namespace firebase {
 namespace firestore {
 namespace api {
+
+static inline DocumentChange::Type DocumentChangeTypeForChange(
+    const core::DocumentViewChange& change) {
+  switch (change.type()) {
+    case core::DocumentViewChange::Type::Added:
+      return DocumentChange::Type::Added;
+    case core::DocumentViewChange::Type::Modified:
+    case core::DocumentViewChange::Type::Metadata:
+      return DocumentChange::Type::Modified;
+    case core::DocumentViewChange::Type::Removed:
+      return DocumentChange::Type::Removed;
+  }
+
+  HARD_FAIL("Unknown DocumentViewChange::Type: %s", change.type());
+}
+
+/**
+ * Calculates the changes in a ViewSnapshot, and returns the changes (either
+ * DocumentChange or PipelineResultChange).
+ */
+template <typename TChange, typename TDocWrapper>
+std::vector<TChange> GenerateChangesFromSnapshot(
+    const core::ViewSnapshot& snapshot,
+    bool include_metadata_changes,
+    const std::function<TDocWrapper(const model::Document&, SnapshotMetadata)>&
+        doc_factory) {
+  if (include_metadata_changes && snapshot.excludes_metadata_changes()) {
+    util::ThrowInvalidArgument(
+        "To include metadata changes with your document "
+        "changes, you must call "
+        "addSnapshotListener(includeMetadataChanges:true).");
+  }
+
+  std::vector<TChange> changes;
+  constexpr size_t npos = TChange::npos;  // Assumes TChange exposes npos
+
+  if (snapshot.old_documents().empty()) {
+    // Special case the first snapshot because index calculation is simple.
+    model::DocumentComparator doc_comparator =
+        snapshot.query_or_pipeline().Comparator();
+    size_t index = 0;
+    for (const core::DocumentViewChange& change : snapshot.document_changes()) {
+      const model::Document& doc = change.document();
+      SnapshotMetadata metadata(
+          /*pending_writes=*/snapshot.mutated_keys().contains(doc->key()),
+          /*from_cache=*/snapshot.from_cache());
+
+      TDocWrapper document = doc_factory(doc, metadata);
+
+      changes.emplace_back(TChange::Type::Added, std::move(document), npos,
+                           index++);
+    }
+
+  } else {
+    // Handle subsequent snapshots with incremental index tracking.
+    model::DocumentSet index_tracker = snapshot.old_documents();
+    for (const core::DocumentViewChange& change : snapshot.document_changes()) {
+      if (!include_metadata_changes &&
+          change.type() == core::DocumentViewChange::Type::Metadata) {
+        continue;
+      }
+
+      const model::Document& doc = change.document();
+      SnapshotMetadata metadata(
+          /*pending_writes=*/snapshot.mutated_keys().contains(doc->key()),
+          /*from_cache=*/snapshot.from_cache());
+
+      TDocWrapper document = doc_factory(doc, metadata);
+
+      size_t old_index = npos;
+      size_t new_index = npos;
+
+      if (change.type() != core::DocumentViewChange::Type::Added) {
+        old_index = index_tracker.IndexOf(change.document()->key());
+        index_tracker = index_tracker.erase(change.document()->key());
+      }
+      if (change.type() != core::DocumentViewChange::Type::Removed) {
+        index_tracker = index_tracker.insert(change.document());
+        new_index = index_tracker.IndexOf(change.document()->key());
+      }
+
+      auto type = static_cast<typename TChange::Type>(
+          DocumentChangeTypeForChange(change));
+
+      // A TChange object is constructed from the TDocWrapper.
+      changes.emplace_back(type, std::move(document), old_index, new_index);
+    }
+  }
+  return changes;
+}
 
 /**
  * A `QuerySnapshot` contains zero or more `DocumentSnapshot` objects.
