38c0f8387fabbfda844dbca7efa73ccc568aa9dd
Harlan Haskins <harlan@harlanhaskins.com>
2025-05-02 15:35:27 -0400

    Use new Google Utility for detecting whether to support background URL session uploads (#14794)

--- Package.swift
+++ Package.swift
@@ -150,7 +150,7 @@ let package = Package(
     ),
     .package(
       url: "https://github.com/google/GoogleUtilities.git",
-      "8.0.0" ..< "9.0.0"
+      "8.1.0" ..< "9.0.0"
     ),
     .package(
       url: "https://github.com/google/gtm-session-fetcher.git",
