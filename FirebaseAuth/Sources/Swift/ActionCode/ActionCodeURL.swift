bf822b111faa1622482d113bc3b836be3a7a7e1a
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2025-05-08 19:01:57 -0400

    [Auth] Add Swift 6 conformance to `FirebaseAuth/Sources/Swift/ActionCode/` directory (#14833)
    Co-authored-by: Morgan Chen <morganchen12@gmail.com>

--- FirebaseAuth/Sources/Swift/ActionCode/ActionCodeURL.swift
+++ FirebaseAuth/Sources/Swift/ActionCode/ActionCodeURL.swift
@@ -14,8 +14,11 @@
 
 import Foundation
 
+// TODO(Swift 6 Breaking): This type is immutable. Consider removing `open` at
+// breaking change so checked Sendable can be used.
+
 /// This class will allow developers to easily extract information about out of band links.
-@objc(FIRActionCodeURL) open class ActionCodeURL: NSObject {
+@objc(FIRActionCodeURL) open class ActionCodeURL: NSObject, @unchecked Sendable {
   /// Returns the API key from the link. nil, if not provided.
   @objc(APIKey) public let apiKey: String?
 
