34884e8b6734a0ef803e3918a72416fa5f2f44d5
Andrew Heard <andrewheard@google.com>
2025-05-21 18:00:55 -0400

    Support any minimum version, add docs

--- scripts/quickstart_spm_xcodeproj.sh
+++ scripts/quickstart_spm_xcodeproj.sh
@@ -16,16 +16,40 @@
 
 
 # Modify a .xcodeproj to use a specific branch.
-# TODO: Update to transform from a release, as well as from `main`.
 
 set -xeuo pipefail
 
 SAMPLE=$1
 XCODEPROJ=${SAMPLE}/${SAMPLE}Example.xcodeproj/project.pbxproj
 
-REQUIREMENT_REGEX='({\s*isa = XCRemoteSwiftPackageReference;\s*repositoryURL = "https://github\.com/firebase/firebase-ios-sdk\.git";\s*requirement = {\s*)kind = upToNextMajorVersion;\s*minimumVersion = 11.13.0;(\s*};\s*};)'
+# Regex matches SemVer `firebase-ios-sdk` dependency in project.pbxproj:
+# {
+#   isa = XCRemoteSwiftPackageReference;
+#	  repositoryURL = "https://github.com/firebase/firebase-ios-sdk.git";
+#   requirement = {
+#     kind = upToNextMajorVersion;
+#	    minimumVersion = xx.yy.zz;
+#	  };
+# };
+REQUIREMENT_REGEX='({\s*isa = XCRemoteSwiftPackageReference;\
+\s*repositoryURL = "https://github\.com/firebase/firebase-ios-sdk\.git";\
+\s*requirement = {\s*)kind = upToNextMajorVersion;\
+\s*minimumVersion = \d+\.\d+\.\d+;(\s*};\
+(\s*};\s*};)'
+
+# Replaces the minimumVersion requirement with a branch requirement.
 REPLACEMENT_REGEX="\1branch = $BRANCH_NAME;\n\t\t\t\tkind = branch;\2"
-perl -i -0777 -pe "s#$REQUIREMENT_REGEX#$REPLACEMENT_REGEX#g" "$XCODEPROJ"
+
+# Performs the replacement using Perl.
+#
+# -0777 Enables reading all input in one go (slurp), rather than line-by-line.
+# -p Causes Perl to loop through the input line by line.
+# -i Edits the file in place.
+# -e Provides the expression to execute.
+perl -0777 -pie "s#$REQUIREMENT_REGEX#$REPLACEMENT_REGEX#g" "$XCODEPROJ" || {
+  echo "Failed to update quickstart's Xcode project to the branch: $BRANCH_NAME"
+  exit 1
+}
 
 # Point SPM CI to the tip of `main` of
 # https://github.com/google/GoogleAppMeasurement so that the release process
