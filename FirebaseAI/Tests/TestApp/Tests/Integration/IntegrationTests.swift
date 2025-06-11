ef57bc1616de5215173c9949e17e6e97d5f91735
Andrew Heard <andrewheard@google.com>
2025-06-11 19:14:29 -0400

    [Firebase AI] Remove `v1` integration tests

--- FirebaseAI/Tests/TestApp/Tests/Integration/IntegrationTests.swift
+++ FirebaseAI/Tests/TestApp/Tests/Integration/IntegrationTests.swift
@@ -19,7 +19,7 @@ import FirebaseCore
 import FirebaseStorage
 import XCTest
 
-// TODO(#14405): Migrate to Swift Testing and parameterize tests to run on both `v1` and `v1beta`.
+// TODO(#14405): Migrate to Swift Testing and parameterize tests to run on both Vertex AI and Google AI.
 final class IntegrationTests: XCTestCase {
   // Set temperature, topP and topK to lowest allowed values to make responses more deterministic.
   let generationConfig = GenerationConfig(
