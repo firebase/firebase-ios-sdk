604ec4c091a3d02f47ccf5030f228a45c599ab7d
Daymon <17409137+daymxn@users.noreply.github.com>
2025-08-13 16:02:51 -0500

    Update AppCheckOptions.swift
    Co-authored-by: Nick Cooke <36927374+ncooke3@users.noreply.github.com>

--- FirebaseAI/Sources/AppCheckOptions.swift
+++ FirebaseAI/Sources/AppCheckOptions.swift
@@ -27,7 +27,7 @@ public struct AppCheckOptions: Sendable, Hashable, Equatable {
   ///   - requireLimitedUseTokens: When sending tokens to the backend, this option enables
   ///     the usage of App Check's `limitedUseTokens` instead of the standard cached tokens.
   ///
-  ///     A new `limitedUseToken` will be generated for each request; providing a smaller attack
+  ///     A new limited-use tokens will be generated for each request; providing a smaller attack
   ///     surface for malicious parties to hijack tokens. When used alongside [replay protection](https://firebase.google.com/docs/app-check/custom-resource-backend#replay-protection),
   ///     limited-use tokens are also _consumed_ after each request, ensuring they can't be used
   ///     again.
