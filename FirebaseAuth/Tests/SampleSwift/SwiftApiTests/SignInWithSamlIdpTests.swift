ee47804ec2d654ce592407726bab863489bc3447
Srushti Vaidya <srushtisv@google.com>
2025-09-05 03:22:51 +0530

    lint fix

--- FirebaseAuth/Tests/SampleSwift/SwiftApiTests/SignInWithSamlIdpTests.swift
+++ FirebaseAuth/Tests/SampleSwift/SwiftApiTests/SignInWithSamlIdpTests.swift
@@ -1,18 +1,16 @@
-/*
- * Copyright 2025 Google
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #if os(iOS)
 
@@ -20,7 +18,7 @@
   import XCTest
 
   @available(iOS 15.0, macOS 12.0, tvOS 16.0, *)
-  class SamlSignInIntegrationTests: TestsBase {
+  class SignInWithSamlIdpTests: TestsBase {
     func testSignInWithSamlFailureInvalidProvider() async throws {
       try? await deleteCurrentUserAsync()
       let invalidProvider = "saml.invalid"
@@ -55,7 +53,6 @@
       let providerId = "saml.googleidp"
       let spAcsUrl = "https://example.com/saml-acs"
       let invalidSamlResp = "invalid%25"
-
       do {
         _ = try await Auth.auth().signInWithSamlIdp(
           ProviderId: providerId,
