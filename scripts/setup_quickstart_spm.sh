e5bcbc6f457acc45f9c52b7177212af3d6dd0254
Nick Cooke <nickcooke@google.com>
2025-09-05 12:10:04 -0400

    add

--- scripts/setup_quickstart_spm.sh
+++ scripts/setup_quickstart_spm.sh
@@ -1,4 +1,6 @@
-# Copyright 2025 Google
+#!/usr/bin/env bash
+
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
