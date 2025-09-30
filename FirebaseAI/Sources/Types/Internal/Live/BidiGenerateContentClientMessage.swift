29dfd73ec05ff8e0be3f9ab035ae53f3df2a50c9
Daymon <17409137+daymxn@users.noreply.github.com>
2025-09-30 15:16:41 -0500

    Update FirebaseAI/Sources/Types/Internal/Live/BidiGenerateContentClientMessage.swift
    Co-authored-by: Andrew Heard <andrewheard@google.com>

--- FirebaseAI/Sources/Types/Internal/Live/BidiGenerateContentClientMessage.swift
+++ FirebaseAI/Sources/Types/Internal/Live/BidiGenerateContentClientMessage.swift
@@ -31,26 +31,6 @@ enum BidiGenerateContentClientMessage {
   case toolResponse(BidiGenerateContentToolResponse)
 }
 
-@available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, *)
-extension BidiGenerateContentClientMessage: Encodable {
-  enum CodingKeys: CodingKey {
-    case setup
-    case clientContent
-    case realtimeInput
-    case toolResponse
-  }
-
-  func encode(to encoder: any Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    switch self {
-    case let .setup(setup):
-      try container.encode(setup, forKey: .setup)
-    case let .clientContent(clientContent):
-      try container.encode(clientContent, forKey: .clientContent)
-    case let .realtimeInput(realtimeInput):
-      try container.encode(realtimeInput, forKey: .realtimeInput)
-    case let .toolResponse(toolResponse):
-      try container.encode(toolResponse, forKey: .toolResponse)
-    }
-  }
-}
+@available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, *)
+@available(watchOS, unavailable)
+extension BidiGenerateContentClientMessage: Encodable {}
