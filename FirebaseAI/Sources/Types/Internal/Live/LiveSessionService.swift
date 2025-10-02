6f7575d1254a0274994a8ab37118838084266e43
Daymon <daymxn@google.com>
2025-10-02 13:08:43 -0500

    Use configured location in websocket

--- FirebaseAI/Sources/Types/Internal/Live/LiveSessionService.swift
+++ FirebaseAI/Sources/Types/Internal/Live/LiveSessionService.swift
@@ -309,10 +309,9 @@ actor LiveSessionService {
   /// Will apply the required app check and auth headers, as the backend expects them.
   private nonisolated func createWebsocket() async throws -> AsyncWebSocket {
     let host = apiConfig.service.endpoint.rawValue.withoutPrefix("https://")
-    // TODO: (b/448722577) Set a location based on the api config
     let urlString = switch apiConfig.service {
     case .vertexAI:
-      "wss://\(host)/ws/google.firebase.vertexai.v1beta.LlmBidiService/BidiGenerateContent/locations/us-central1"
+      "wss://\(host)/ws/google.firebase.vertexai.v1beta.LlmBidiService/BidiGenerateContent/locations/\(apiConfig.location ?? "us-central1")"
     case .googleAI:
       "wss://\(host)/ws/google.firebase.vertexai.v1beta.GenerativeService/BidiGenerateContent"
     }
