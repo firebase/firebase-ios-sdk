ec5d9dae8e1abffa628c2503638b068c6c91d53b
Daymon <daymxn@google.com>
2025-10-14 17:08:10 -0500

    Fix available markers

--- FirebaseAI/Tests/Unit/Types/Live/VoiceConfigTests.swift
+++ FirebaseAI/Tests/Unit/Types/Live/VoiceConfigTests.swift
@@ -16,7 +16,7 @@ import XCTest
 
 @testable import FirebaseAI
 
-@available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, *)
+@available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, *)
 @available(watchOS, unavailable)
 final class VoiceConfigTests: XCTestCase {
   let encoder = JSONEncoder()
