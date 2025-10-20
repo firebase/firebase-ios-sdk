475a65b00bcca86e120084aa02797f06115a14b3
Andrew Heard <andrewheard@google.com>
2025-10-20 17:49:34 -0400

    Fix import in `GenerationConfigTests`

--- FirebaseAI/Tests/Unit/GenerationConfigTests.swift
+++ FirebaseAI/Tests/Unit/GenerationConfigTests.swift
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import FirebaseAILogic
+@testable import FirebaseAILogic
 import Foundation
 import XCTest
 
