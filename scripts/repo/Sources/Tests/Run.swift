00e28455d8ef0b6fc6d746ff8ea8c1c52b8f0772
Daymon <daymxn@google.com>
2025-10-20 14:23:57 -0500

    Formatting

--- scripts/repo/Sources/Tests/Run.swift
+++ scripts/repo/Sources/Tests/Run.swift
@@ -24,46 +24,46 @@ extension Tests {
     nonisolated(unsafe) static var configuration = CommandConfiguration(
       abstract: "Run the integration tests for a given platform.",
       usage: """
-          tests run [--overwrite] [--secrets <file_path>] [--xcode <version_or_path>] [--platforms <platforms> ...] [--sdk <sdk>]
+        tests run [--overwrite] [--secrets <file_path>] [--xcode <version_or_path>] [--platforms <platforms> ...] [<sdk>]
 
-          tests run --xcode Xcode_16.4.0 --platforms iOS macOS --sdk AI
-          tests run --xcode "/Applications/Xcode_15.0.0.app" --platforms tvOS --sdk Storage
-          tests run --overwrite --secrets ./scripts/secrets/AI.json --sdk AI
-        """,
+        tests run --xcode Xcode_16.4.0 --platforms iOS --platforms macOS AI
+        tests run --xcode "/Applications/Xcode_15.0.0.app" --platforms tvOS Storage
+        tests run --overwrite --secrets ./scripts/secrets/AI.json AI
+      """,
       discussion: """
-            If multiple Xcode versions are installed, you must specify an Xcode version manually via the
-            'xcode' option. If you run the script without doing so, the script will log an error message
-            that contains all the Xcode versions installed, telling you to manually specify the 'xcode' option. 
-            
-            Note that Xcode versions can be specified as either the application name, or a full path. For
-            example, the following are both valid: 
-            "Xcode_16.4.0" and "/Applications/Xcode_16.4.0.app".
+          If multiple Xcode versions are installed, you must specify an Xcode version manually via the
+          'xcode' option. If you run the script without doing so, the script will log an error message
+          that contains all the Xcode versions installed, telling you to manually specify the 'xcode' option.
 
-            If your tests have encrypted secret files, you can pass a json file to the script via the
-            'secrets' option. The script will automatically decrypt them before running the tests, and 
-            delete them after running the tests. You'll also need to provide the password that the secret
-            files were encrypted with via the 'secrets_passphrase' environment variable. The json file
-            should be an array of json elements in the format of:
-            { encrypted: <path-to-encrypted-file>, destination: <where-to-output-decrypted-file> }
+          Note that Xcode versions can be specified as either the application name, or a full path. For
+          example, the following are both valid:
+          "Xcode_16.4.0" and "/Applications/Xcode_16.4.0.app".
 
-            If you pass a secret file, but decrypted files already exist at the destination, the script
-            will NOT overwrite them. The script will also not delete these files either. If you want
-            the script to overwrite and delete secret files, regardless if they existed before the script
-            ran, you can pass the 'overwrite' flag.
-        """,
+          If your tests have encrypted secret files, you can pass a json file to the script via the
+          'secrets' option. The script will automatically decrypt them before running the tests, and
+          delete them after running the tests. You'll also need to provide the password that the secret
+          files were encrypted with via the 'secrets_passphrase' environment variable. The json file
+          should be an array of json elements in the format of:
+          { encrypted: <path-to-encrypted-file>, destination: <where-to-output-decrypted-file> }
+
+          If you pass a secret file, but decrypted files already exist at the destination, the script
+          will NOT overwrite them. The script will also not delete these files either. If you want
+          the script to overwrite and delete secret files, regardless if they existed before the script
+          ran, you can pass the 'overwrite' flag.
+      """,
     )
 
     @Option(
       help:
-        """
-        Xcode version to run tests against. \
-        Can be either the application name, or a full path (eg; "Xcode_16.4.0" or "/Applications/Xcode_16.4.0.app").
-        By default, the script will look for your local Xcode installation.
-        """
+      """
+      Xcode version to run tests against. \
+      Can be either the application name, or a full path (eg; "Xcode_16.4.0" or "/Applications/Xcode_16.4.0.app").
+      By default, the script will look for your local Xcode installation.
+      """
     )
     var xcode: String = ""
 
-    @Option(parsing: .upToNextOption, help: "Platforms to run rests on.")
+    @Option(help: "Platforms to run rests on.")
     var platforms: [Platform] = [.iOS]
 
     @Option(help: "Path to a json file containing an array of secret files to use, if any.")
@@ -72,18 +72,19 @@ extension Tests {
     @Flag(help: "Overwrite existing decrypted secret files.")
     var overwrite: Bool = false
 
-    @Option(
+    @Argument(
       help: """
-        The SDK to run integration tests for.
-        There should be a build target for the SDK that follows the format "Firebase{SDK}Integration"
-        """)
-    var sdk: String = "AI"
+      The SDK to run integration tests for.
+      There should be a build target for the SDK that follows the format "Firebase{SDK}Integration"
+      """
+    )
+    var sdk: String
 
-    static let log: Logger = Logger(label: "Tests::Run")
+    static let log: Logger = .init(label: "Tests::Run")
     private var log: Logger { Decrypt.log }
 
     /// A path to the Xcode to use.
-    /// 
+    ///
     /// Only populated after `validate()` runs.
     private var xcodePath: String = ""
 
@@ -97,48 +98,51 @@ extension Tests {
 
     /// When the `xcode` option isn't provided, try to find an installation on disk.
     mutating func findAndValidateXcodeOnDisk() throws {
-        let xcodes = try findXcodeVersions()
-        guard xcodes.count == 1 else {
-          let formattedXcodes = xcodes.map { $0.path(percentEncoded: false) }
-          log.error(
-            "Multiple Xcode versions found.",
-            metadata: ["versions": "\(formattedXcodes)"])
+      let xcodes = try findXcodeVersions()
+      guard xcodes.count == 1 else {
+        let formattedXcodes = xcodes.map { $0.path(percentEncoded: false) }
+        log.error(
+          "Multiple Xcode versions found.",
+          metadata: ["versions": "\(formattedXcodes)"]
+        )
 
-          throw ValidationError(
-            "Multiple Xcode installations found. Explicitly pass the 'xcode' option to specify which to use."
-          )
-        }
-        xcodePath = xcodes[0].path()
-        log.debug("Found Xcode installation", metadata: ["path": "\(xcodePath)"])
+        throw ValidationError(
+          "Multiple Xcode installations found. Explicitly pass the 'xcode' option to specify which to use."
+        )
+      }
+      xcodePath = xcodes[0].path()
+      log.debug("Found Xcode installation", metadata: ["path": "\(xcodePath)"])
     }
 
     /// When the `xcode` option is provided, ensure it exists.
-    /// 
-    /// The `xcode` argument can be either a full path to the application, or just the application name.
+    ///
+    /// The `xcode` argument can be either a full path to the application, or just the application
+    /// name.
     mutating func validateProvidedXcode() throws {
-        if xcode.hasSuffix(".app") {
-          // it's a full path to the Xcode, just ensure it exists
-          guard FileManager.default.fileExists(atPath: xcode) else {
-            throw ValidationError("Xcode application not found at path: \(xcode)")
-          }
-          xcodePath = URL(filePath: xcode).path()
-        } else {
-          // it's the application name, find an Xcode installation that matches
-          let xcodes = try findXcodeVersions()
-          guard
-            let match = xcodes.first(where: {
-              $0.path(percentEncoded: false).hasSuffix("\(xcode).app")
-            })
-          else {
-            let formattedXcodes = xcodes.map { $0.path(percentEncoded: false) }
-            log.error("Invalid Xcode specified.",
-            metadata: ["versions": "\(formattedXcodes)"])
-            throw ValidationError(
-              "Failed to find an Xcode installation that matches: \(xcode)")
-          }
-          xcodePath = match.path()
-          log.debug("Found matching Xcode", metadata: ["path": "\(xcodePath)"])
+      if xcode.hasSuffix(".app") {
+        // it's a full path to the Xcode, just ensure it exists
+        guard FileManager.default.fileExists(atPath: xcode) else {
+          throw ValidationError("Xcode application not found at path: \(xcode)")
         }
+        xcodePath = URL(filePath: xcode).path()
+      } else {
+        // it's the application name, find an Xcode installation that matches
+        let xcodes = try findXcodeVersions()
+        guard
+          let match = xcodes.first(where: {
+            $0.path(percentEncoded: false).hasSuffix("\(xcode).app")
+          })
+        else {
+          let formattedXcodes = xcodes.map { $0.path(percentEncoded: false) }
+          log.error("Invalid Xcode specified.",
+                    metadata: ["versions": "\(formattedXcodes)"])
+          throw ValidationError(
+            "Failed to find an Xcode installation that matches: \(xcode)"
+          )
+        }
+        xcodePath = match.path()
+        log.debug("Found matching Xcode", metadata: ["path": "\(xcodePath)"])
+      }
     }
 
     mutating func run() throws {
@@ -155,7 +159,8 @@ extension Tests {
               metadata: [
                 "file": "\(file.destination)",
                 "error": "\(error.localizedDescription)",
-              ])
+              ]
+            )
           }
         }
       }
@@ -180,7 +185,8 @@ extension Tests {
       for platform in platforms {
         log.info(
           "Running integration tests",
-          metadata: ["sdk": "\(sdk)", "platform": "\(platform)"])
+          metadata: ["sdk": "\(sdk)", "platform": "\(platform)"]
+        )
 
         let build = Process(
           buildScript.path(percentEncoded: false),
@@ -189,12 +195,13 @@ extension Tests {
         )
 
         let exitCode = try build.runWithSignals([
-          "Firebase\(sdk)Integration", "\(platform)"
+          "Firebase\(sdk)Integration", "\(platform)",
         ])
         guard exitCode == 0 else {
           log.error(
             "Failed to run integration tests.",
-            metadata: ["sdk": "\(sdk)", "platform": "\(platform)"])
+            metadata: ["sdk": "\(sdk)", "platform": "\(platform)"]
+          )
           throw ExitCode(exitCode)
         }
       }
@@ -204,31 +211,43 @@ extension Tests {
       let applicationDirs = FileManager.default.urls(
         for: .applicationDirectory, in: .allDomainsMask
       ).filter { url in
-        // file manager lists application dirs that CAN exist, so we should check if they actually do exist before trying to get their contents
+        // file manager lists application dirs that CAN exist, so we should check if they actually
+        // do exist before trying to get their contents
         let exists = FileManager.default.fileExists(atPath: url.path())
         if !exists {
-            log.debug("Application directory doesn't exists, so we're skipping it.", metadata: ["directory": "\(url.path())"])
+          log.debug(
+            "Application directory doesn't exists, so we're skipping it.",
+            metadata: ["directory": "\(url.path())"]
+          )
         }
         return exists
       }
 
-      log.debug("Searching application directories for Xcode installations.", metadata: ["directories": "\(applicationDirs)"])
+      log.debug(
+        "Searching application directories for Xcode installations.",
+        metadata: ["directories": "\(applicationDirs)"]
+      )
 
       let allApplications = try applicationDirs.flatMap { URL in
-        return try FileManager.default.contentsOfDirectory(
-          at: URL, includingPropertiesForKeys: nil)
+        try FileManager.default.contentsOfDirectory(
+          at: URL, includingPropertiesForKeys: nil
+        )
       }
 
       let xcodes = allApplications.filter { file in
         let isXcode = file.lastPathComponent.contains(/Xcode.*\.app/)
         if !isXcode {
-            log.debug("Application isn't an Xcode installation, so we're skipping it.", metadata: ["application": "\(file.lastPathComponent)"])
+          log.debug(
+            "Application isn't an Xcode installation, so we're skipping it.",
+            metadata: ["application": "\(file.lastPathComponent)"]
+          )
         }
         return isXcode
       }
       guard !xcodes.isEmpty else {
         throw ValidationError(
-          "Failed to find any Xcode versions installed. Please install Xcode.")
+          "Failed to find any Xcode versions installed. Please install Xcode."
+        )
       }
 
       log.debug("Found Xcode installations.", metadata: ["installations": "\(xcodes)"])
