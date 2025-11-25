a1aa0b054c3be294c00facf2bd77dcb95e51bc0a
Andrew Heard <andrewheard@google.com>
2025-11-25 15:17:39 -0500

    Add `SendableMetatype` typealias for older Xcode versions

--- FirebaseAI/Sources/Types/Public/Generable/JSONSchema.swift
+++ FirebaseAI/Sources/Types/Public/Generable/JSONSchema.swift
@@ -20,7 +20,7 @@ import Foundation
 /// the desired format.
 @available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, *)
 public struct JSONSchema: Sendable {
-  enum Kind {
+  enum Kind: Sendable {
     case string
     case integer
     case double
