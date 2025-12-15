ab1c16a0472a551ac1d69e70cace875264e748a7
Nick Cooke <nickcooke@google.com>
2025-12-15 13:58:45 -0500

    add error checks. will revert

--- Crashlytics/Crashlytics/Models/FIRCLSSettings.m
+++ Crashlytics/Crashlytics/Models/FIRCLSSettings.m
@@ -191,6 +191,7 @@ NSString *const AppVersion = @"app_version";
 - (void)deleteCachedSettings {
   __weak FIRCLSSettings *weakSelf = self;
 #if FIREBASE_CRASHYLTICS_TESTING
+#error "Mango: Check one."
   dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
 #endif
     __strong FIRCLSSettings *strongSelf = weakSelf;
@@ -201,6 +202,7 @@ NSString *const AppVersion = @"app_version";
       [strongSelf.fileManager removeItemAtPath:strongSelf.fileManager.settingsCacheKeyPath];
     }
 #if FIREBASE_CRASHYLTICS_TESTING
+#error "Mango: Check two."
   });
 #endif
 
