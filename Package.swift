a6ddc56a89164b1dbd01330cea5428f01bda0b91
Andrew Heard <andrewheard@google.com>
2026-01-06 20:37:59 -0500

    [Firestore] Re-enable binary distribution by default (#15673)
    Co-authored-by: Nick Cooke <36927374+ncooke3@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -20,10 +20,7 @@ import PackageDescription
 
 let firebaseVersion = "12.8.0"
 
-// For private preview, Firestore must be built from source.
-let shouldUseSourceFirestore = true
-// Remove the above and uncomment the line below before merging Firestore to main.
-// let shouldUseSourceFirestore = Context.environment["FIREBASE_SOURCE_FIRESTORE"] != nil
+let shouldUseSourceFirestore = Context.environment["FIREBASE_SOURCE_FIRESTORE"] != nil
 
 let package = Package(
   name: "Firebase",
@@ -1606,8 +1603,8 @@ func firestoreTargets() -> [Target] {
     } else {
       return .binaryTarget(
         name: "FirebaseFirestoreInternal",
-        url: "https://dl.google.com/firebase/ios/bin/firestore/12.4.0/rc0/FirebaseFirestoreInternal.zip",
-        checksum: "58b916624c01a56c5de694cfc9c5cc7aabcafb13b54e7bde8c83bacc51a3460d"
+        url: "https://dl.google.com/firebase/ios/bin/firestore/12.8.0/pre_rc0/FirebaseFirestoreInternal.zip",
+        checksum: "4f8bb4fe4f6c7cb82712d59d0ea305787e0d6104391273903a39b5045a4b53cc"
       )
     }
   }()
