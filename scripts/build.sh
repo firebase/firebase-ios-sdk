16d07b65fe6b6943ca27b7c1cde24d80f550471f
cherylEnkidu <cheryllin@google.com>
2026-01-23 15:34:38 -0500

    improve the workflow for CI running

--- scripts/build.sh
+++ scripts/build.sh
@@ -372,17 +372,44 @@ case "$product-$platform-$method" in
         test
     ;;
 
+  Firestore-*-xcodebuild)
+      "${firestore_emulator}" start
+      trap '"${firestore_emulator}" stop' ERR EXIT
+
+      RunXcodebuild \
+          -workspace 'Firestore/Example/Firestore.xcworkspace' \
+          -scheme "Firestore_IntegrationTests_$platform" \
+          -enableCodeCoverage YES \
+          "${xcb_flags[@]}" \
+          # Memory intensive, so we limit jobs
+          -jobs 4 \
+          build-for-testing
+      ;;
+
+  Firestore-*-xcodetest)
+      RunXcodebuild \
+          -workspace 'Firestore/Example/Firestore.xcworkspace' \
+          -scheme "Firestore_IntegrationTests_$platform" \
+          -enableCodeCoverage YES \
+          "${xcb_flags[@]}" \
+          test-without-building
+      ;;
+
   FirestoreEnterprise-*-xcodebuild)
+      "${firestore_emulator}" start
+      trap '"${firestore_emulator}" stop' ERR EXIT
+
       RunXcodebuild \
           -workspace 'Firestore/Example/Firestore.xcworkspace' \
           -scheme "Firestore_IntegrationTests_Enterprise_$platform" \
           -enableCodeCoverage YES \
           "${xcb_flags[@]}" \
+          # Memory intensive, so we limit jobs
           -jobs 4 \
           build-for-testing
+      ;;
 
-      sleep 10
-
+  FirestoreEnterprise-*-xcodetest)
       RunXcodebuild \
           -workspace 'Firestore/Example/Firestore.xcworkspace' \
           -scheme "Firestore_IntegrationTests_Enterprise_$platform" \
