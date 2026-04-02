76e2e4dae44cce0fee5c2fbb9039c51547ff04c5
cherylEnkidu <96084918+cherylEnkidu@users.noreply.github.com>
2026-04-02 11:47:32 -0400

    Update PipelineTests.swift
    Remove the intentional failure

--- Firestore/Swift/Tests/Integration/PipelineTests.swift
+++ Firestore/Swift/Tests/Integration/PipelineTests.swift
@@ -276,7 +276,6 @@ class PipelineIntegrationTests: FSTIntegrationTestCase {
   }
 
   func testSupportsCollectionGroupAsSource() async throws {
-    XCTAssertTrue(false, "Intentional failure for testing")
     let db = firestore()
 
     let rootCollForTest = collectionRef()
