abb5d71364ea83bac9c6267cb447302fe3f1e37f
Andrew Heard <andrewheard@google.com>
2026-04-20 16:15:46 -0400

    Fix build on Xcode 26.1

--- FirebaseAI/Tests/Unit/Types/GenerationOptionsTests.swift
+++ FirebaseAI/Tests/Unit/Types/GenerationOptionsTests.swift
@@ -12,85 +12,87 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@testable import FirebaseAILogic
-import XCTest
+#if compiler(>=6.2.3)
+  @testable import FirebaseAILogic
+  import XCTest
 
-#if canImport(FoundationModels)
-  import FoundationModels
-#endif
-
-final class GenerationOptionsTests: XCTestCase {
   #if canImport(FoundationModels)
-    @available(iOS 26.0, macOS 26.0, *)
-    @available(tvOS, unavailable)
-    @available(watchOS, unavailable)
-    func testConversionToFoundationModels() throws {
+    import FoundationModels
+  #endif
+
+  final class GenerationOptionsTests: XCTestCase {
+    #if canImport(FoundationModels)
+      @available(iOS 26.0, macOS 26.0, *)
+      @available(tvOS, unavailable)
+      @available(watchOS, unavailable)
+      func testConversionToFoundationModels() throws {
+        let options = FirebaseAI.GenerationOptions(
+          sampling: .greedy,
+          temperature: 0.5,
+          maximumResponseTokens: 100
+        )
+
+        let afmOptions = options.toFoundationModels()
+
+        XCTAssertEqual(afmOptions.temperature, 0.5)
+        XCTAssertEqual(afmOptions.maximumResponseTokens, 100)
+        XCTAssertNotNil(afmOptions.sampling)
+        XCTAssertEqual(afmOptions.sampling, .greedy)
+      }
+    #endif // canImport(FoundationModels)
+
+    func testEquatable_emptyOptions() throws {
+      let options = FirebaseAI.GenerationOptions()
+
+      XCTAssertNil(options.sampling)
+      XCTAssertNil(options.temperature)
+      XCTAssertNil(options.maximumResponseTokens)
+    }
+
+    func testGenerationSchema_greedy() throws {
+      let temperature = 0.9
+      let maximumResponseTokens = 200
       let options = FirebaseAI.GenerationOptions(
         sampling: .greedy,
-        temperature: 0.5,
-        maximumResponseTokens: 100
+        temperature: temperature,
+        maximumResponseTokens: maximumResponseTokens
       )
 
-      let afmOptions = options.toFoundationModels()
-
-      XCTAssertEqual(afmOptions.temperature, 0.5)
-      XCTAssertEqual(afmOptions.maximumResponseTokens, 100)
-      XCTAssertNotNil(afmOptions.sampling)
-      XCTAssertEqual(afmOptions.sampling, .greedy)
+      XCTAssertEqual(options.sampling, .greedy)
+      XCTAssertEqual(options.temperature, temperature)
+      XCTAssertEqual(options.maximumResponseTokens, maximumResponseTokens)
     }
-  #endif // canImport(FoundationModels)
 
-  func testEquatable_emptyOptions() throws {
-    let options = FirebaseAI.GenerationOptions()
+    func testGenerationSchema_probabilityThreshold() throws {
+      let topP = 0.8
+      let seed: UInt64 = 5_000_000_000
+      let temperature = 0.6
+      let maximumResponseTokens = 80
+      let options = FirebaseAI.GenerationOptions(
+        sampling: .random(probabilityThreshold: topP, seed: seed),
+        temperature: temperature,
+        maximumResponseTokens: maximumResponseTokens
+      )
 
-    XCTAssertNil(options.sampling)
-    XCTAssertNil(options.temperature)
-    XCTAssertNil(options.maximumResponseTokens)
+      XCTAssertEqual(options.sampling, .random(probabilityThreshold: topP, seed: seed))
+      XCTAssertEqual(options.temperature, temperature)
+      XCTAssertEqual(options.maximumResponseTokens, maximumResponseTokens)
+    }
+
+    func testGenerationSchema_topK() throws {
+      let topK = 5
+      let seed: UInt64 = 6_000_000_000
+      let temperature = 0.4
+      let maximumResponseTokens = 1000
+      let options = FirebaseAI.GenerationOptions(
+        sampling: .random(top: topK, seed: seed),
+        temperature: temperature,
+        maximumResponseTokens: maximumResponseTokens
+      )
+
+      XCTAssertEqual(options.sampling, .random(top: topK, seed: seed))
+      XCTAssertEqual(options.temperature, temperature)
+      XCTAssertEqual(options.maximumResponseTokens, maximumResponseTokens)
+    }
   }
-
-  func testGenerationSchema_greedy() throws {
-    let temperature = 0.9
-    let maximumResponseTokens = 200
-    let options = FirebaseAI.GenerationOptions(
-      sampling: .greedy,
-      temperature: temperature,
-      maximumResponseTokens: maximumResponseTokens
-    )
-
-    XCTAssertEqual(options.sampling, .greedy)
-    XCTAssertEqual(options.temperature, temperature)
-    XCTAssertEqual(options.maximumResponseTokens, maximumResponseTokens)
-  }
-
-  func testGenerationSchema_probabilityThreshold() throws {
-    let topP = 0.8
-    let seed: UInt64 = 5_000_000_000
-    let temperature = 0.6
-    let maximumResponseTokens = 80
-    let options = FirebaseAI.GenerationOptions(
-      sampling: .random(probabilityThreshold: topP, seed: seed),
-      temperature: temperature,
-      maximumResponseTokens: maximumResponseTokens
-    )
-
-    XCTAssertEqual(options.sampling, .random(probabilityThreshold: topP, seed: seed))
-    XCTAssertEqual(options.temperature, temperature)
-    XCTAssertEqual(options.maximumResponseTokens, maximumResponseTokens)
-  }
-
-  func testGenerationSchema_topK() throws {
-    let topK = 5
-    let seed: UInt64 = 6_000_000_000
-    let temperature = 0.4
-    let maximumResponseTokens = 1000
-    let options = FirebaseAI.GenerationOptions(
-      sampling: .random(top: topK, seed: seed),
-      temperature: temperature,
-      maximumResponseTokens: maximumResponseTokens
-    )
-
-    XCTAssertEqual(options.sampling, .random(top: topK, seed: seed))
-    XCTAssertEqual(options.temperature, temperature)
-    XCTAssertEqual(options.maximumResponseTokens, maximumResponseTokens)
-  }
-}
+#endif // compiler(>=6.2.3)
