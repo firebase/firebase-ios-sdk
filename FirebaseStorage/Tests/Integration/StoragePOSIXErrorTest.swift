8c7581e8845184b69459c3f595df5164c1012d90
Paul Beusterien <paulbeusterien@google.com>
2026-07-10 16:15:31 -0700

    style

--- FirebaseStorage/Tests/Integration/StoragePOSIXErrorTest.swift
+++ FirebaseStorage/Tests/Integration/StoragePOSIXErrorTest.swift
@@ -1,18 +1,22 @@
-import Foundation
 @testable import FirebaseStorage
-import GTMSessionFetcherCore
+import Foundation
+#if COCOAPODS
+  import GTMSessionFetcher
+#else
+  import GTMSessionFetcherCore
+#endif
 import XCTest
 
 @available(iOS 13.0, macOS 10.15, macCatalyst 13.0, tvOS 13.0, watchOS 6.0, *)
 class StoragePOSIXErrorTest: StorageIntegrationCommon {
   func testPutFileWithPOSIXError40() async throws {
     let ref = storage.reference(withPath: "ios/public/testPOSIX40")
-    
+
     let data = try XCTUnwrap("Hello".data(using: .utf8))
     let tmpDirURL = URL(fileURLWithPath: NSTemporaryDirectory())
     let fileURL = tmpDirURL.appendingPathComponent(#function + "hello.txt")
     try data.write(to: fileURL, options: .atomicWrite)
-    
+
     do {
       let metadata = try await ref.putFileAsync(from: fileURL)
       XCTAssertEqual(metadata.size, Int64(data.count))
