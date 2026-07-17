40660ad914bd59aaf1c148594f8fffab01c0c5d1
Paul Beusterien <paulbeusterien@google.com>
2026-07-17 16:26:37 -0700

    fix(RemoteConfig): use modern QoS class for background queue
    Updates the background queue dispatch to use QOS_CLASS_BACKGROUND instead of the deprecated DISPATCH_QUEUE_PRIORITY_BACKGROUND identifier.

--- FirebaseRemoteConfig/Sources/RCNConfigExperiment.m
+++ FirebaseRemoteConfig/Sources/RCNConfigExperiment.m
@@ -158,7 +158,7 @@ static NSString *const kMethodNameLatestStartTime =
                          completionHandler:handler];
   } else {
     if (handler) {
-      dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+      dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), ^{
         handler(nil);
       });
     }
