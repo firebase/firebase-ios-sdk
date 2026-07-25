c3d2c3cb155a10d1373a88042cf5a87b1f6656f5
Paul Beusterien <paulbeusterien@google.com>
2026-07-24 17:55:25 -0700

    style(auth): format parameter list for executeMFARequest

--- FirebaseAuth/Sources/Swift/AuthProvider/PhoneAuthProvider.swift
+++ FirebaseAuth/Sources/Swift/AuthProvider/PhoneAuthProvider.swift
@@ -310,8 +310,9 @@ import Foundation
 
     private func executeMFARequest(startMFARequestInfo: AuthProtoStartMFAPhoneRequestInfo,
                                    multiFactorSession session: MultiFactorSession,
-                                   enrollmentInjector: ((StartMFAEnrollmentRequest) async throws
-                                     -> Void)? = nil,
+                                   enrollmentInjector: (
+                                     (StartMFAEnrollmentRequest) async throws -> Void
+                                   )? = nil,
                                    signInInjector: ((StartMFASignInRequest) async throws -> Void)? =
                                      nil) async throws -> String? {
       guard let idToken = session.idToken else {
