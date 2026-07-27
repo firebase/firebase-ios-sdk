7f799e8c750deb73bc4abdbaf5934c512201beac
Ali Ghaznavi <55113970+mghaznav@users.noreply.github.com>
2026-07-27 11:35:23 -0400

    Removes unused Crashlytics integration with the now deprecated MetricKit API (#16444)

--- Crashlytics/Crashlytics/Models/FIRCLSFileManager.h
+++ Crashlytics/Crashlytics/Models/FIRCLSFileManager.h
@@ -57,8 +57,6 @@
 - (BOOL)removeContentsOfDirectoryAtPath:(NSString *)path;
 - (BOOL)moveItemAtPath:(NSString *)path toDirectory:(NSString *)destDir;
 - (BOOL)didCrashOnPreviousExecution;
-- (BOOL)metricKitDiagnosticFileExists;
-- (void)createEmptyMetricKitFile:(NSString *)reportPath;
 - (void)enumerateFilesInDirectory:(NSString *)directory
                        usingBlock:(void (^)(NSString *filePath, NSString *extension))block;
 - (NSNumber *)fileSizeAtPath:(NSString *)path;
