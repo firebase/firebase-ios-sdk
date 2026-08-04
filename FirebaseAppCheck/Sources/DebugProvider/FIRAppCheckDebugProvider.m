1f3f6e1d3152dbf669d114d64b4898953a0ef262
AustinBenoit <austinbenoit@google.com>
2026-08-04 13:44:48 -0400

    chore(app_check): Update the app check debug token message

--- FirebaseAppCheck/Sources/DebugProvider/FIRAppCheckDebugProvider.m
+++ FirebaseAppCheck/Sources/DebugProvider/FIRAppCheckDebugProvider.m
@@ -86,10 +86,18 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)logDebugTokenExchangeError {
   if (self.projectID.length > 0 && self.googleAppID.length > 0) {
     FIRLogWarning(kFIRLoggerAppCheck, kFIRLoggerAppCheckMessageCodeDebugTokenExchangeFailed,
-                  @"Failed to exchange debug token. If you haven't registered it, "
-                   "you can do so in the Firebase Console: "
-                   "https://console.firebase.google.com/project/%@/appcheck/apps?selectedAppId=%@",
-                  self.projectID, self.googleAppID);
+                  @"\nFailed to exchange debug token. To use this token for app debugging, register it with your project.\n\n"
+                  "Firebase App Check debug token: %@\n\n"
+                  "You can do so in the Firebase Console: \n"
+                  "https://console.firebase.google.com/project/%@/appcheck/apps?selectedAppId=%@ \n\n"
+                  "Or using the Firebase CLI: \n"
+                  "firebase appcheck:debugtokens:create %@ --app %@\n\n"
+                  "Note: To keep your project secure, please revoke and delete this token using the \n"
+                  "Firebase Console or the CLI (`firebase appcheck:debugtokens:delete`) when you finish debugging.\n\n"
+                  "Warning: This debug token is a secret and should not be shared or uploaded to source code.\n\n"
+                  "Debug Token Guide: https://firebase.google.com/docs/app-check/ios/debug-provider\n"
+                  "Firebase CLI install instructions: https://firebase.google.com/docs/cli\n",
+                  debugToken, self.projectID, self.googleAppID, self.localDebugToken, self.googleAppID);
   }
 }
 
