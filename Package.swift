3fdc59c140eef9ef5b71516a7dc5fc082e02009c
Andrew Heard <andrewheard@google.com>
2026-08-10 13:16:44 -0400

    Touch `Package.swift` to trigger `infra.spm_global` workflow

--- Package.swift
+++ Package.swift
@@ -707,6 +707,7 @@ func packageTargets() -> [Target] {
 
       // MARK: - Firebase Functions
 
+      // TODO: Remove this comment before merging. Using it to trigger tests on CI.
       .target(
         name: "FirebaseFunctions",
         dependencies: [
