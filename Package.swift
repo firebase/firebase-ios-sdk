16e9a9a05f12fbd85efc5cf2dcd27b16569f4329
Andrew Heard <andrewheard@google.com>
2026-08-14 13:05:09 -0400

    chore: set up GeminiForFoundationModels package
    Initialize the Swift package structure for GeminiForFoundationModels
    with Swift 6 language mode and platform deployment targets.
    
    - Set swift-tools-version to 6.1 and enable Swift 6 language mode.
    - Rename target and test directories to PascalCase naming.
    - Define supported Apple platforms (iOS 15+, macOS 10.15+, tvOS 15+,
      watchOS 7+).
    - Add Apache 2.0 license headers to package manifest, source, and
      test files.

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 6.1
+
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import PackageDescription
+
+let package = Package(
+  name: "gemini-for-foundation-models",
+  platforms: [.iOS(.v15), .macCatalyst(.v15), .macOS(.v10_15), .tvOS(.v15), .watchOS(.v7)],
+  products: [
+    .library(
+      name: "GeminiForFoundationModels",
+      targets: ["GeminiForFoundationModels"]
+    )
+  ],
+  targets: [
+    .target(
+      name: "GeminiForFoundationModels",
+    ),
+    .testTarget(
+      name: "GeminiForFoundationModelsTests",
+      dependencies: [
+        "GeminiForFoundationModels"
+      ]
+    ),
+  ],
+  swiftLanguageModes: [.v6]
+)
