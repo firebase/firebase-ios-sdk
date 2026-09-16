25a52c4d8a0854f4fe94b6a883da8f152b785d6a
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2026-09-16 14:18:42 -0400

    Apply suggestion from @ncooke3

--- Package.swift
+++ Package.swift
@@ -1720,5 +1720,7 @@ func appCheckDependency() -> Package.Dependency {
     return .package(url: appCheckURL, branch: branch)
   }
 
-  return .package(url: appCheckURL, "12.0.0" ..< "13.0.0")
+  // TODO: Update to point to AppCheck 12.0
+  // return .package(url: appCheckURL, "12.0.0" ..< "13.0.0")
+  return .package(url: appCheckURL, "11.3.0" ..< "12.0.0")
 }
