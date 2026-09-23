d169df99889acde750d724d43a4d5acffe6925cb
Paul Beusterien <paulbeusterien@google.com>
2026-09-22 19:52:15 -0700

    GoogleSignIn is incompatible with Firebase 13 for now

--- ReleaseTooling/Sources/ZipBuilder/ZipBuilder.swift
+++ ReleaseTooling/Sources/ZipBuilder/ZipBuilder.swift
@@ -459,9 +459,9 @@ struct ZipBuilder {
     }
     // We don't release GoogleSignIn, but we include its latest
     // version for convenience in the Zip and Carthage builds.
-    podsToInstall.append(CocoaPodUtils.VersionedPod(name: "GoogleSignIn",
-                                                    version: nil,
-                                                    platforms: ["ios"]))
+    // podsToInstall.append(CocoaPodUtils.VersionedPod(name: "GoogleSignIn",
+    //                                                 version: nil,
+    //                                                 platforms: ["ios"]))
 
     print("Final expected versions for the Zip file: \(podsToInstall)")
     let (installedPods, frameworks, carthageGoogleUtilitiesXcframeworkFirebase) =
