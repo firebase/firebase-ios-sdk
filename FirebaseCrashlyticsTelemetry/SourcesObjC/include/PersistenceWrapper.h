cc68f48cd773dacb291c65ed129e7eb80a2903e0
Ali Ghaznavi <alighaznavi@google.com>
2026-09-25 13:53:12 -0400

    Fix broken import

--- FirebaseCrashlyticsTelemetry/SourcesObjC/include/PersistenceWrapper.h
+++ FirebaseCrashlyticsTelemetry/SourcesObjC/include/PersistenceWrapper.h
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "FirebaseCrashlyticsTelemetry/SourcesObjC/PersistenceBufferWrapper.h"
-#import "FirebaseCrashlyticsTelemetry/SourcesObjC/PersistenceSpan.h"
+#import "../PersistenceBufferWrapper.h"
+#import "../PersistenceSpan.h"
